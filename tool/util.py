4d111a334f72bf1da0db7d583721a89a3850ebfa
Lijuan Qian <lijuanqian@google.com>
2020-06-18 21:04:34 -0400

    add license

--- tool/util.py
+++ tool/util.py
@@ -1,4 +1,19 @@
 # Lint as: python3
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """utility functions used to build the property-value tree structure """
 import stat_config_pb2
 import collections
@@ -44,7 +59,7 @@ def _read_pop_obs_spec():
     data_gni = file_gni.read()
   text_format.Parse(data_gni, pop_obs_spec_list)
   
-  
+
   #result = {'vertical name': {number of cprops: [PobObsSpec]}}
   result = collections.defaultdict(lambda: collections.defaultdict(list))
   for pos in pop_obs_spec_list.spec:
@@ -57,6 +72,8 @@ def _read_pop_obs_spec():
                    cpv, pos.name))
   #print(result)
   return result
+  
+
     
 if __name__ == "__main__":
   _read_pop_obs_spec()
