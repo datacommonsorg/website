da5100e91a7927cca831d8dc622f3edaa5b8607a
beets <beets@users.noreply.github.com>
2020-06-23 15:32:38 -0700

    Merge d3 into master (#21)
    * draw line chart function with d3 library (#5)
    
    * draw line chart function with d3 library
    
    * Remove comments
    
    * Add more styling to line charts (#6)
    
    * added tick formatting
    * add dots for smaller datasets
    * zero-index the y-axis. I find this gives a better sense of scale.
    
    * Use d3 for group bar chart (#7)
    
    * Use d3 for group bar chart
    
    demo link: https://screenshot.googleplex.com/jEek0HQ2Ga4
    
    * Remove comment
    
    * Remove unused functions; Use new legend creation for line chart (#9)
    
    demo: https://screenshot.googleplex.com/UnqD8sJbFpm
    
    * Use d3 for stack bar chart (#12)
    
    * Use d3 for stack bar chart; Update getColorFn take the domain list
    
    demo: https://screenshot.googleplex.com/EOnWvXYhEF3
    
    * Update
    
    * Use d3 for single bar chart (#13)
    
    demo: https://screenshot.googleplex.com/6XcyoEtz9Rp
    
    * Chart styling updates (#14)
    
    - Update axes styles to follow the line chart styling
    - Add padding before the left hand bar charts (paddingOuter)
    - Update chart colors to use [Spectral](https://github.com/d3/d3-scale-chromatic#schemeSpectral)
    
    Remove red borders around charts since they're not click targets
    
    * Wrap long tick labels (#17)
    
    * Wrap long x-axis tick text
    * Removed unused code and fix typescript warnings.
    
    * Comment out broken test for now
      TypeError: tspan.node(...).getComputedTextLength is not a function
    
    Co-authored-by: Bo Xu <shifucun@users.noreply.github.com>

--- static/js/chart/draw.ts
+++ static/js/chart/draw.ts
@@ -15,398 +15,399 @@
  */
 
 import * as _SVG from "@svgdotjs/svg.js";
-import moment from "moment";
+import * as d3 from "d3";
 
 import {
   DataGroup,
   DataPoint,
   BarLayout,
-  Layout,
   Range,
   computeCoordinate,
 } from "./base";
-import { drawBarXTicks, getLineXTick, drawLineXTicks } from "./xtick";
-import {
-  Y_TICK_MARGIN,
-  computeYTickWidth,
-  drawYTicks,
-  getYTick,
-} from "./ytick";
 
-const TOP_MARGIN = 10;
-const SIDE_MARGIN = 15;
-const BAR_GAP_RATIO = 0.25;
-const GROUP_BAR_GAP = 1;
+const NUM_X_TICKS = 5;
+const NUM_Y_TICKS = 5;
+const MARGIN = { top: 20, right: 10, bottom: 30, left: 35, yAxis: 3 };
 
-// Colors - 500 level colors from the Google Material palette
-const COLORS = [
-  "#4285F4",
-  "#EA4335",
-  "#FBBC04",
-  "#34A853",
-  "#FA7B17",
-  "#F439A0",
-  "#A142F4",
-  "#24C1E0",
-];
-
-enum BarType {
-  Stack = 1,
-  Group = 2,
+function getColorFn(labels: string[]) {
+  let k = labels.length;
+  k = k < 3 || k > 11 ? 10 : k;  // Spectral colors exist for k = 3 -> 11
+  return d3
+    .scaleOrdinal<string, string>()
+    .domain(labels)
+    .range(d3.quantize(d3.interpolateSpectral, k));
 }
 
-function createLegend(textList: string[]): Element {
-  let legendElem = document.createElement("div");
-  legendElem.classList.add("legend-box");
-  for (let i = 0; i < textList.length; i++) {
-    let textElem = document.createElement("div");
-    textElem.textContent = textList[i];
-    textElem.classList.add("legend-text");
-    textElem.style.color = COLORS[i];
-    legendElem.appendChild(textElem);
-  }
-  return legendElem;
+function appendLegendElem(
+  elem: string, color: d3.ScaleOrdinal<string, string>, keys: string[]) {
+  d3.select("#" + elem)
+    .append("div")
+    .attr("class", "legend")
+    .selectAll("div")
+    .data(keys)
+    .join("div")
+    .attr("style", (d) => `background: ${color(d)}`)
+    .append("span")
+    .text((d) => d);
 }
 
 /**
- * Function to draw bars based on the data values.
+ * From https://bl.ocks.org/mbostock/7555321
+ * Wraps axis text by fitting as many words per line as would fit a given width.
  */
-function drawBars(
-  canvas: any,
-  layout: BarLayout,
-  dataPoints: DataPoint[],
-  valueRange: Range
+function wrap(text: d3.Selection<SVGElement, any, any, any>, width: number) {
+  text.each(function () {
+    var text = d3.select(this),
+      words = text.text().split(/\s+/).reverse(),
+      word,
+      line: Array<string> = [],
+      lineNumber = 0,
+      lineHeight = 1.1, // ems
+      y = text.attr("y"),
+      dy = parseFloat(text.attr("dy")),
+      tspan = text
+        .text(null)
+        .append("tspan")
+        .attr("x", 0)
+        .attr("y", y)
+        .attr("dy", dy + "em");
+    while ((word = words.pop())) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text
+          .append("tspan")
+          .attr("x", 0)
+          .attr("y", y)
+          .attr("dy", ++lineNumber * lineHeight + dy + "em")
+          .text(word);
+      }
+    }
+  });
+}
+
+function addXAxis(
+  svg: d3.Selection<SVGElement, any, any, any>,
+  height: number,
+  xScale: d3.AxisScale<any>
 ) {
-  for (let i = 0; i < dataPoints.length; i++) {
-    let barX =
-      layout.xRange.low + layout.barGap + i * (layout.barGap + layout.barWidth);
-    // Pick the Y coordinate by factoring in the data value relative to limit.
-    let barY = computeCoordinate(
-      dataPoints[i].value,
-      valueRange,
-      layout.yRange
+  var axis = svg
+    .append("g")
+      .attr("class", "x axis")
+      .attr("transform", `translate(0, ${height - MARGIN.bottom})`)
+      .call(d3.axisBottom(xScale).ticks(NUM_X_TICKS).tickSizeOuter(0))
+      .call((g) => g.select(".domain").remove());
+
+  if (typeof xScale.bandwidth === "function") {
+    axis.selectAll(".tick text")
+      .call(wrap, xScale.bandwidth());
+  }
+}
+
+function addYAxis(
+  svg: d3.Selection<SVGElement, any, any, any>,
+  width: number,
+  yScale: d3.AxisScale<any>
+) {
+  svg
+    .append("g")
+    .attr("class", "y axis")
+    .attr("transform", `translate(${width - MARGIN.right},0)`)
+    .call(
+      d3
+        .axisLeft(yScale)
+        .ticks(NUM_Y_TICKS, "1s")
+        .tickSize(width - 10 - MARGIN.right)
+    )
+    .call((g) => g.select(".domain").remove())
+    .call((g) =>
+      g.selectAll(".tick:not(:first-of-type) line").attr("class", "grid-line")
+    )
+    .call((g) =>
+      g
+        .selectAll(".tick text")
+        .attr("x", -width + MARGIN.left + MARGIN.yAxis)
+        .attr("dy", -4)
     );
-    let barHeight = layout.yRange.high - barY;
-    let barElem = canvas.rect(layout.barWidth, barHeight).attr({
-      x: barX,
-      y: barY,
-      fill: COLORS[i],
-    });
-    barElem.addClass("highlight-target color-" + COLORS[i]);
-  }
-}
-
-function drawStackBars(
-  canvas: any,
-  layout: BarLayout,
-  dataGroups: DataGroup[],
-  valueRange: Range
-) {
-  for (let i = 0; i < dataGroups.length; i++) {
-    let y1 = layout.yRange.high;
-    let barX =
-      layout.xRange.low + layout.barGap + i * (layout.barGap + layout.barWidth);
-    let sumValue = 0;
-    for (let j = 0; j < dataGroups[i].value.length; j++) {
-      sumValue += dataGroups[i].value[j].value;
-      let h = computeCoordinate(sumValue, valueRange, layout.yRange);
-      canvas.rect(layout.barWidth, y1 - h).attr({
-        x: barX,
-        y: h,
-        fill: COLORS[j],
-      });
-      y1 = h;
-    }
-  }
-}
-
-function drawGroupBars(
-  canvas: any,
-  layout: BarLayout,
-  dataGroups: DataGroup[],
-  valueRange: Range
-) {
-  for (let i = 0; i < dataGroups.length; i++) {
-    let barX =
-      layout.xRange.low + layout.barGap + i * (layout.barGap + layout.barWidth);
-    let n = dataGroups[i].value.length;
-    let innerBarWidth = (layout.barWidth - (n-1) * GROUP_BAR_GAP) / n;
-    for (let j = 0; j < n; j++) {
-      let y = computeCoordinate(dataGroups[i].value[j].value, valueRange, layout.yRange);
-      canvas.rect(innerBarWidth, layout.yRange.high - y).attr({
-        x: barX,
-        y: y,
-        fill: COLORS[j],
-      });
-      barX += (innerBarWidth + GROUP_BAR_GAP);
-    }
-  }
-}
-
-/**
- * Function to draw lines based on the data values.
- */
-function drawLines(
-  canvas: any,
-  layout: Layout,
-  dataGroups: DataGroup[],
-  xValueRange: Range,
-  yValueRange: Range
-) {
-  for (let i = 0; i < dataGroups.length; i++) {
-    for (let j = 0; j < dataGroups[i].value.length - 1; j++) {
-      let p1 = dataGroups[i].value[j];
-      let p2 = dataGroups[i].value[j + 1];
-      let x1 = computeCoordinate(
-        moment.utc(p1.label).unix(),
-        xValueRange,
-        layout.xRange
-      );
-      let y1 = computeCoordinate(p1.value, yValueRange, layout.yRange);
-      let x2 = computeCoordinate(
-        moment.utc(p2.label).unix(),
-        xValueRange,
-        layout.xRange
-      );
-      let y2 = computeCoordinate(p2.value, yValueRange, layout.yRange);
-      canvas.line(x1, y1, x2, y2).stroke({ width: 2, color: COLORS[i] });
-    }
-  }
 }
 
 /**
  * Draw single bar chart.
- * @param parentId
- * @param parentWidth
- * @param parentHeight
+ * @param id
+ * @param width
+ * @param height
  * @param dataPoints
  * @param unit
  */
 function drawSingleBarChart(
-  parentId: string,
-  parentWidth: number,
-  parentHeight: number,
+  id: string,
+  width: number,
+  height: number,
   dataPoints: DataPoint[],
   unit?: string
 ) {
   let textList = dataPoints.map((dataPoint) => dataPoint.label);
   let values = dataPoints.map((dataPoint) => dataPoint.value);
-  let yTick = getYTick(new Range(0, Math.max(...values)));
-  yTick.unit = unit;
-  let yTickWidth = computeYTickWidth(yTick);
-  // Create canvas.
-  const canvas = _SVG
-    .SVG()
-    .addTo("#" + parentId)
-    .size(parentWidth, parentHeight);
-  // Create initial layout.
-  let xRange = new Range(yTickWidth + Y_TICK_MARGIN, parentWidth);
-  let yRange = new Range(TOP_MARGIN, parentHeight);
-  let numBars = dataPoints.length;
-  let width = xRange.high - xRange.low;
-  // Use 20% as bar gap.
-  let barGap = Math.round((width / numBars) * BAR_GAP_RATIO);
-  // Account for gaps between bars, before the first bar and after last bar.
-  let barWidth = (width - (numBars + 1) * barGap) / numBars;
-  let layout = new BarLayout(xRange, yRange, barGap, barWidth);
-  // Draw X ticks.
-  let xTicksHeight = drawBarXTicks(canvas, layout, textList);
-  // Update layout.
-  layout.yRange.high -= xTicksHeight;
-  // Draw Y ticks.
-  drawYTicks(canvas, layout, yTick);
-  // Draw bars.
-  drawBars(canvas, layout, dataPoints, yTick.valueRange);
+
+  let x = d3
+    .scaleBand()
+    .domain(textList)
+    .rangeRound([MARGIN.left, width - MARGIN.right])
+    .paddingInner(0.1)
+    .paddingOuter(0.1);
+
+  let y = d3
+    .scaleLinear()
+    .domain([0, d3.max(values)])
+    .nice()
+    .rangeRound([height - MARGIN.bottom, MARGIN.top]);
+
+  let color = getColorFn(textList);
+
+  let svg = d3
+      .select("#" + id)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+  addXAxis(svg, height, x);
+  addYAxis(svg, width, y);
+
+    svg
+      .append("g")
+      .selectAll("rect")
+      .data(dataPoints)
+      .join("rect")
+      .attr("x", d => x(d.label))
+      .attr("y", d => y(d.value))
+      .attr("width", x.bandwidth())
+      .attr("height", d => y(0) - y(d.value))
+      .attr("fill", d => color(d.label))
 }
 
 /**
  * Draw stack bar chart.
  *
- * @param parentId
- * @param parentWidth
- * @param parentHeight
+ * @param id
+ * @param width
+ * @param height
  * @param dataGroups
  * @param unit
  */
 function drawStackBarChart(
-  parentId: string,
-  parentWidth: number,
-  parentHeight: number,
+  id: string,
+  width: number,
+  height: number,
   dataGroups: DataGroup[],
   unit?: string
 ) {
-  drawComplexBarChart(
-    parentId,
-    parentWidth,
-    parentHeight,
-    dataGroups,
-    BarType.Stack,
-    unit
-  );
+  const keys = dataGroups[0].value.map((dp) => dp.label);
+
+  let data = [];
+  for (let dataGroup of dataGroups) {
+    let curr: { [property: string]: any } = { "label": dataGroup.label };
+    for (let dataPoint of dataGroup.value) {
+      curr[dataPoint.label] = dataPoint.value;
+    }
+    data.push(curr);
+  }
+
+  let series = d3.stack().keys(keys)(data);
+
+  let x = d3.scaleBand()
+    .domain(dataGroups.map(dg => dg.label))
+    .rangeRound([MARGIN.left, width - MARGIN.right])
+    .paddingInner(0.1)
+    .paddingOuter(0.1);
+
+  let y = d3
+    .scaleLinear()
+    .domain([0, d3.max(series, d => d3.max(d, d => d[1]))]).nice()
+    .nice()
+    .rangeRound([height - MARGIN.bottom, MARGIN.top]);
+
+  let color = getColorFn(keys);
+
+  const svg = d3
+    .select("#" + id)
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  addXAxis(svg, height, x);
+  addYAxis(svg, width, y);
+
+  svg.append("g")
+    .selectAll("g")
+    .data(series)
+    .enter().append("g")
+      .attr("fill", d => color(d.key))
+    .selectAll("rect")
+    .data(d => d)
+    .join("rect")
+      .attr("x", (d) =>x(String(d.data.label)))
+      .attr("y", d => y(d[1]))
+      .attr("width", x.bandwidth())
+      .attr("height", d => y(d[0]) - y(d[1]));
+
+  appendLegendElem(id, color, keys);
 }
 
 /**
  * Draw group bar chart.
  *
- * @param parentId
- * @param parentWidth
- * @param parentHeight
+ * @param id
+ * @param width
+ * @param height
  * @param dataGroups
  * @param unit
  */
 function drawGroupBarChart(
-  parentId: string,
-  parentWidth: number,
-  parentHeight: number,
+  id: string,
+  width: number,
+  height: number,
   dataGroups: DataGroup[],
   unit?: string
 ) {
-  drawComplexBarChart(
-    parentId,
-    parentWidth,
-    parentHeight,
-    dataGroups,
-    BarType.Group,
-    unit
-  );
-}
+  let keys = dataGroups[0].value.map((dp) => dp.label);
+  let x0 = d3
+    .scaleBand()
+    .domain(dataGroups.map((dg) => dg.label))
+    .rangeRound([MARGIN.left, width - MARGIN.right])
+    .paddingInner(0.1)
+    .paddingOuter(0.1);
 
-/**
- * Draw complex (stack or group) bar chart.
- *
- * @param parentId
- * @param parentWidth
- * @param parentHeight
- * @param dataGroups
- * @param unit
- */
-function drawComplexBarChart(
-  parentId: string,
-  parentWidth: number,
-  parentHeight: number,
-  dataGroups: DataGroup[],
-  barType: BarType,
-  unit?: string
-) {
-  let parentElement = document.getElementById(parentId);
-  // Pick one group and use the data point label as text list.
-  let textList = dataGroups[0].value.map((dataPoint) => dataPoint.label);
-  let values;
-  if (barType == BarType.Stack) {
-    values = dataGroups.map((dataGroup) => dataGroup.sum());
-  } else if (barType == BarType.Group) {
-    values = dataGroups.map((dataGroup) => dataGroup.max());
-  }
-  let maxValue = Math.max(...values);
-  let yTick = getYTick(new Range(0, maxValue));
-  yTick.unit = unit;
+  let x1 = d3
+    .scaleBand()
+    .domain(keys)
+    .rangeRound([0, x0.bandwidth()])
+    .padding(0.05);
 
-  let yTickWidth = computeYTickWidth(yTick);
-  // Draw legend and compute the height.
-  // Add legend to the container box in the end.
-  let legendElem = createLegend(textList);
-  parentElement.appendChild(legendElem);
-  let bbox = legendElem.getBoundingClientRect();
-  let legendHeight = bbox.height;
-  parentElement.removeChild(legendElem);
-  // Create canvas.
-  const canvas = _SVG
-    .SVG()
-    .addTo("#" + parentId)
-    .size(parentWidth, parentHeight - legendHeight);
-  // Create initial layout.
-  let xRange = new Range(yTickWidth + Y_TICK_MARGIN, parentWidth);
-  let yRange = new Range(TOP_MARGIN, parentHeight - legendHeight);
-  let numBars = dataGroups.length;
-  let width = xRange.high - xRange.low;
-  let barGap = Math.round((width / numBars) * BAR_GAP_RATIO);
-  // Account for gaps between bars, before the first bar and after last bar.
-  let barWidth = (width - (numBars + 1) * barGap) / numBars;
-  let layout = new BarLayout(xRange, yRange, barGap, barWidth);
-  // Draw X ticks.
-  let xTickText = dataGroups.map((dataGroup) => dataGroup.label);
-  let xTicksHeight = drawBarXTicks(canvas, layout, xTickText);
-  // Update layout.
-  layout.yRange.high -= xTicksHeight;
-  // Draw Y ticks.
-  drawYTicks(canvas, layout, yTick);
+  let maxV = Math.max(...dataGroups.map((dataGroup) => dataGroup.max()));
+  let y = d3
+    .scaleLinear()
+    .domain([0, maxV])
+    .nice()
+    .rangeRound([height - MARGIN.bottom, MARGIN.top]);
 
-  // Draw bars.
-  if (barType == BarType.Stack) {
-    drawStackBars(canvas, layout, dataGroups, yTick.valueRange);
-  } else if (barType == BarType.Group) {
-    drawGroupBars(canvas, layout, dataGroups, yTick.valueRange);
-  }
-  parentElement.appendChild(legendElem);
+
+  let color = getColorFn(keys);
+
+  let svg = d3
+    .select("#" + id)
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  addXAxis(svg, height, x0);
+  addYAxis(svg, width, y);
+  svg
+    .append("g")
+    .selectAll("g")
+    .data(dataGroups)
+    .join("g")
+    .attr("transform", (dg) => `translate(${x0(dg.label)},0)`)
+    .selectAll("rect")
+    .data((dg) => dg.value.map((dp) => ({ key: dp.label, value: dp.value })))
+    .join("rect")
+    .attr("x", (d) => x1(d.key))
+    .attr("y", (d) => y(d.value))
+    .attr("width", x1.bandwidth())
+    .attr("height", (d) => y(0) - y(d.value))
+    .attr("fill", (d) => color(d.key));
+
+
+  appendLegendElem(id, color, keys);
 }
 
 /**
  * Draw line chart.
- * @param parentId
- * @param parentWidth
- * @param parentHeight
+ * @param id
+ * @param width
+ * @param height
  * @param dataGroups
  * @param unit
  */
 function drawLineChart(
-  parentId: string,
-  parentWidth: number,
-  parentHeight: number,
+  id: string,
+  width: number,
+  height: number,
   dataGroups: DataGroup[],
   unit?: string
 ) {
-  let parentElement = document.getElementById(parentId);
-  // Pick one group and use the data point label as text list.
-  let legendText = dataGroups.map((dataGroup) => dataGroup.label);
-  let tickText = dataGroups[0].value.map((dataPoint) => dataPoint.label);
-  let minV = Math.min(...dataGroups.map((dataGroup) => dataGroup.min()));
+  let minV = 0;
   let maxV = Math.max(...dataGroups.map((dataGroup) => dataGroup.max()));
-  let rangeMin = Math.max(0, minV * 2 - maxV);
-  let yTick = getYTick(new Range(rangeMin, maxV));
-  yTick.unit = unit;
-  let yTickWidth = computeYTickWidth(yTick);
-  // Draw legend and compute the height.
-  // Add legend to the container box in the end.
-  let hasLegend = dataGroups.length > 1;
-  let legendHeight = 0;
-  let legendElem;
-  if (hasLegend) {
-    legendElem = createLegend(legendText);
-    parentElement.appendChild(legendElem);
-    let bbox = legendElem.getBoundingClientRect();
-    legendHeight = bbox.height;
-    parentElement.removeChild(legendElem);
-  }
-  // Create canvas.
-  const canvas = _SVG
-    .SVG()
-    .addTo("#" + parentId)
-    .size(parentWidth, parentHeight - legendHeight);
-  // Create initial layout.
-  let xRange = new Range(
-    yTickWidth + Y_TICK_MARGIN + SIDE_MARGIN,
-    parentWidth - SIDE_MARGIN
-  );
-  let yRange = new Range(TOP_MARGIN, parentHeight - legendHeight);
-  let layout = new Layout(xRange, yRange);
 
-  let xTick = getLineXTick(tickText);
-  // Draw X ticks.
-  let xTicksHeight = drawLineXTicks(canvas, layout, xTick);
-  // Update layout.
-  layout.yRange.high -= xTicksHeight;
-  // Draw Y ticks.
-  drawYTicks(canvas, layout, yTick);
-  // Draw lines.
-  drawLines(canvas, layout, dataGroups, xTick.valueRange, yTick.valueRange);
-  if (hasLegend) {
-    parentElement.appendChild(legendElem);
+  let svg = d3
+    .select("#" + id)
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  let xScale = d3
+    .scaleTime()
+    .domain(d3.extent(dataGroups[0].value, (d) => new Date(d.label).getTime()))
+    .range([MARGIN.left, width - MARGIN.right]);
+
+  let yScale = d3
+    .scaleLinear()
+    .domain([minV, maxV])
+    .range([height - MARGIN.bottom, MARGIN.top])
+    .nice(NUM_Y_TICKS);
+
+  addXAxis(svg, height, xScale);
+  addYAxis(svg, width, yScale);
+
+  let legendText = dataGroups.map((dataGroup) => dataGroup.label ? dataGroup.label: 'a');
+  let colorFn = getColorFn(legendText);
+
+  for (let i = 0; i < dataGroups.length; i++) {
+    let dataGroup = dataGroups[i];
+    let dataset = dataGroup.value.map(function (dp) {
+      return [new Date(dp.label).getTime(), dp.value];
+    });
+    let shouldAddDots = dataset.length < 12;
+    let color = colorFn(dataGroup.label);
+
+    let line = d3
+      .line()
+      .x((d) => xScale(d[0]))
+      .y((d) => yScale(d[1]));
+
+    if (shouldAddDots) {
+      line = line.curve(d3.curveMonotoneX);
+    }
+
+    svg
+      .append("path")
+      .datum(dataset)
+      .attr("class", "line")
+      .style("stroke", color)
+      .attr("d", line);
+
+    if (shouldAddDots) {
+      svg
+        .selectAll(".dot")
+        .data(dataset)
+        .enter()
+        .append("circle")
+        .attr("class", "dot")
+        .attr("cx", (d, i) => xScale(d[0]))
+        .attr("cy", (d) => yScale(d[1]))
+        .attr("fill", color)
+        .attr("r", 3);
+    }
+  }
+
+  if (dataGroups.length > 1) {
+    appendLegendElem(id, colorFn, legendText);
   }
 }
 
 export {
-  createLegend,
-  drawBars,
   drawLineChart,
   drawSingleBarChart,
   drawStackBarChart,
