ed1367f2547518d31ddbbfd9588b21fd2ebf66c7
Lijuan Qian <lijuanqian@google.com>
2020-06-25 18:14:10 -0400

    add required package

--- tools/pv_tree_generator/build_tree_test.py
+++ tools/pv_tree_generator/build_tree_test.py
@@ -24,56 +24,57 @@ from collections import defaultdict
 class BuildTreeTest(unittest.TestCase):
     @staticmethod
     def get_sv():
-      #pick a subset of stat_var dcids
-      level0 = ['MarriedPopulation','DivorcedPopulation','MalePopulation']
-      level1 = ['dc/0k7719speyv21','dc/2pvw6jqmkp41b','dc/06f6zh0wslnx',\
+        #pick a subset of stat_var dcids
+        level0 = ['MarriedPopulation','DivorcedPopulation','MalePopulation']
+        level1 = ['dc/0k7719speyv21','dc/2pvw6jqmkp41b','dc/06f6zh0wslnx',\
                'dc/2rjldly6tsmf','dc/026gmdj2xk1kb','dc/f7g49v7tzy3rd']
-      level2 = ['dc/61fzldryrnte1','dc/6yb4mgxtc1288','dc/esr27kls5vfy6',\
+        level2 = ['dc/61fzldryrnte1','dc/6yb4mgxtc1288','dc/esr27kls5vfy6',\
                 'dc/jhpflf91nvlt6','dc/cmn38d85glq72','dc/pj50xtdgxeh4g',\
                 'dc/pvsbze841l2tc','dc/tpeg0jxdts2t3']
-      sv_dcid = level0 + level1 + level2
-      return sv_dcid
+        sv_dcid = level0 + level1 + level2
+        return sv_dcid
 
     @staticmethod
     def get_triples_(dcids):
-      url = DevelopmentConfig.API_ROOT + '/node/triples'
-      payload = dc_request.send_request(url, req_json={'dcids': dcids})
-      results = defaultdict(list)
-      #skip if the predicate is not in the list
-      predicates = ["measuredProperty", "populationType", "statType", "income", "gender", "age", "incomeStatus"]
-      predicates_all = set()
-      for dcid in dcids:
-          results[dcid]
-          for t in payload[dcid]:
-              if 'objectId' in t:
-                value = t['objectId']
-              elif 'objectValue' in t:
-                value = t['objectValue']
+        url = DevelopmentConfig.API_ROOT + '/node/triples'
+        payload = dc_request.send_request(url, req_json={'dcids': dcids})
+        results = defaultdict(list)
+        #skip if the predicate is not in the list
+        predicates = ["measuredProperty", "populationType", "statType", "income", \
+            "gender", "age", "incomeStatus"]
+        predicates_all = set()
+        for dcid in dcids:
+            results[dcid]
+            for t in payload[dcid]:
+                if 'objectId' in t:
+                    value = t['objectId']
+                elif 'objectValue' in t:
+                    value = t['objectValue']
 
-              if t['predicate'] == 'constraintProperties':
-                if value not in predicates:
-                  continue
-              elif t['predicate'] not in predicates:
-                 continue
-              predicates_all.add(t['predicate'])
-              results[dcid].append((t['subjectId'], t['predicate'], value))
-      return dict(results)
+                if t['predicate'] == 'constraintProperties':
+                    if value not in predicates:
+                        continue
+                elif t['predicate'] not in predicates:
+                    continue
+                predicates_all.add(t['predicate'])
+                results[dcid].append((t['subjectId'], t['predicate'], value))
+        return dict(results)
       
     @patch('build_tree.MAX_LEVEL', 3)
     @patch('dc_request.get_triples')
     @patch('dc_request.get_sv_dcids')
     def test_build_tree(self,mock_get_sv, mock_get_triples):
-      mock_get_sv.side_effect = self.get_sv
-      mock_get_triples.side_effect = self.get_triples_
-      pop_obs_spec = util._read_pop_obs_spec()
-      stat_vars = util._read_stat_var()
-      data = {}
-      vertical = "Demographics"
-      root = build_tree(vertical, pop_obs_spec[vertical], stat_vars, False)
-      data[vertical] = root
-      expected = json.load(open("./hierarchy_test.json","r"))
-      self.assertEqual(data, expected)
-      return
+        mock_get_sv.side_effect = self.get_sv
+        mock_get_triples.side_effect = self.get_triples_
+        pop_obs_spec = util._read_pop_obs_spec()
+        stat_vars = util._read_stat_var()
+        data = {}
+        vertical = "Demographics"
+        root = build_tree(vertical, pop_obs_spec[vertical], stat_vars, False)
+        data[vertical] = root
+        expected = json.load(open("./hierarchy_test.json","r"))
+        self.assertEqual(data, expected)
+        return
       
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
