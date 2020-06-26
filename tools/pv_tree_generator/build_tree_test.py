81516468cfb41d4c4d60cf3ebef642d46aeba9f9
Lijuan Qian <lijuanqian@google.com>
2020-06-25 20:38:38 -0400

    update test

--- tools/pv_tree_generator/build_tree_test.py
+++ tools/pv_tree_generator/build_tree_test.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import util
+from util import _read_pop_obs_spec()
+from util import _read_stat_var()
 import unittest
 from unittest.mock import patch
 import build_tree
@@ -23,12 +24,12 @@ class BuildTreeTest(unittest.TestCase):
     @staticmethod
     def get_sv():
         #pick a subset of stat_var dcids
-        level0 = ['MarriedPopulation','DivorcedPopulation','MalePopulation']
-        level1 = ['dc/0k7719speyv21','dc/2pvw6jqmkp41b','dc/06f6zh0wslnx',\
-               'dc/2rjldly6tsmf','dc/026gmdj2xk1kb','dc/f7g49v7tzy3rd']
-        level2 = ['dc/61fzldryrnte1','dc/6yb4mgxtc1288','dc/esr27kls5vfy6',\
-                'dc/jhpflf91nvlt6','dc/cmn38d85glq72','dc/pj50xtdgxeh4g',\
-                'dc/pvsbze841l2tc','dc/tpeg0jxdts2t3']
+        level0 = ['MarriedPopulation', 'DivorcedPopulation', 'MalePopulation']
+        level1 = ['dc/0k7719speyv21', 'dc/2pvw6jqmkp41b', 'dc/06f6zh0wslnx',\
+               'dc/2rjldly6tsmf', 'dc/026gmdj2xk1kb', 'dc/f7g49v7tzy3rd']
+        level2 = ['dc/61fzldryrnte1', 'dc/6yb4mgxtc1288', 'dc/esr27kls5vfy6',\
+                'dc/jhpflf91nvlt6', 'dc/cmn38d85glq72', 'dc/pj50xtdgxeh4g',\
+                'dc/pvsbze841l2tc', 'dc/tpeg0jxdts2t3']
         sv_dcid = level0 + level1 + level2
         return sv_dcid
 
@@ -58,8 +59,8 @@ class BuildTreeTest(unittest.TestCase):
     def test_build_tree(self,mock_get_sv, mock_get_triples):
         mock_get_sv.side_effect = self.get_sv
         mock_get_triples.side_effect = self.get_triples_
-        pop_obs_spec = util._read_pop_obs_spec()
-        stat_vars = util._read_stat_var()
+        pop_obs_spec = _read_pop_obs_spec()
+        stat_vars = _read_stat_var()
         data = {}
         vertical = "Demographics"
         root = build_tree.build_tree(vertical, pop_obs_spec[vertical], stat_vars, False)
