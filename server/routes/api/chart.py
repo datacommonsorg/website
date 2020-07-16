c4373be085686b352666fc6bdde33ae6cab825d7
Bo Xu <shifucun@users.noreply.github.com>
2020-07-15 18:39:14 -0700

    Add a temp hack for recover missing statsvars in place stats var cache (#78)

--- server/routes/api/chart.py
+++ server/routes/api/chart.py
@@ -37,6 +37,30 @@ bp = Blueprint(
 )
 
 
+# Temporary hack before we clean up place stats var cache.
+MAPPING = {
+    "Count_Person": "TotalPopulation",
+    "Count_Person_Male": "MalePopulation",
+    "Count_Person_Female": "FemalePopulation",
+    "Count_Person_MarriedAndNotSeparated": "MarriedPopulation",
+    "Count_Person_Divorced": "DivorcedPopulation",
+    "Count_Person_NeverMarried": "NeverMarriedPopulation",
+    "Count_Person_Separated": "SeparatedPopulation",
+    "Count_Person_Widowed": "WidowedPopulation",
+    "Median_Age_Person": "MedianAge",
+    "Median_Income_Person": "MedianIncome",
+    "Count_Person_BelowPovertyLevelInThePast12Months": "BelowPovertyLine",
+    "Count_HousingUnit": "HousingUnits",
+    "Count_Household": "Households",
+    "Count_CriminalActivities_CombinedCrime": "TotalCrimes",
+    "UnemploymentRate_Person": "UnemploymentRate",
+    "CumulativeCount_MedicalConditionIncident_COVID_19_ConfirmedOrProbableCase": "NYTCovid19CumulativeCases",
+    "CumulativeCount_MedicalConditionIncident_COVID_19_PatientDeceased": "NYTCovid19CumulativeDeaths",
+    "IncrementalCount_MedicalConditionIncident_COVID_19_ConfirmedOrProbableCase": "NYTCovid19IncrementalCases",
+    "IncrementalCount_MedicalConditionIncident_COVID_19_PatientDeceased": "NYTCovid19IncrementalDeaths"
+}
+
+
 def filter_charts(charts, all_stats_vars):
     """Filter charts from template specs based on statsitical variable.
 
@@ -55,7 +79,8 @@ def filter_charts(charts, all_stats_vars):
     for chart in charts:
         chart_copy = copy.copy(chart)
         chart_copy['statsVars'] = [
-            x for x in chart['statsVars'] if x in all_stats_vars]
+            x for x in chart['statsVars']
+            if x in all_stats_vars or MAPPING.get(x, '') in all_stats_vars]
         if chart_copy['statsVars']:
             result.append(chart_copy)
     return result
