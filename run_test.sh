93769c7ea671aface9213df0e4e9b03d7b64d2d3
beets <beets@users.noreply.github.com>
2020-07-28 09:22:26 -0700

    exit tests upon any failure, and propagate errors from test commands (#179)

--- run_test.sh
+++ run_test.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 # Run test for client side code.
 function run_npm_test {
   cd static
