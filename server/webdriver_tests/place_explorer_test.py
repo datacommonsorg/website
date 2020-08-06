274d05de979d2851d994cc6186da8caadbb41c2c
wh1210 <wh1210@uw.edu>
2020-08-06 14:49:36 -0600

    try use the real web to test the error

--- server/webdriver_tests/place_explorer_test.py
+++ server/webdriver_tests/place_explorer_test.py
@@ -1,76 +1,76 @@
-# Copyright 2020 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# # Copyright 2020 Google LLC
+# #
+# # Licensed under the Apache License, Version 2.0 (the "License");
+# # you may not use this file except in compliance with the License.
+# # You may obtain a copy of the License at
+# #
+# #      http://www.apache.org/licenses/LICENSE-2.0
+# #
+# # Unless required by applicable law or agreed to in writing, software
+# # distributed under the License is distributed on an "AS IS" BASIS,
+# # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# # See the License for the specific language governing permissions and
+# # limitations under the License.
 
-import unittest
-import urllib
+# import unittest
+# import urllib
 
-from flask import abort, url_for
-from flask_testing import LiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-import logging
-import time
+# from flask import abort, url_for
+# from flask_testing import LiveServerTestCase
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.action_chains import ActionChains
+# import logging
+# import time
 
-from main import app
-
-
-USA_URL = '/place?dcid=country/USA'
-USA_DEMOGRAPHICS_URL = '/place?dcid=country/USA&topic=Demographics'
-
-class TestBase(LiveServerTestCase):
-
-    def create_app(self):
-        return app
-
-    def setUp(self):
-        """
-        Will be called before every test
-        """
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-
-        self.driver = webdriver.Chrome(chrome_options=chrome_options)
-        self.driver.get(self.get_server_url() + USA_URL)
-
-    def tearDown(self):
-        self.driver.quit()
-
-    def test_page_server(self):
-        response = urllib.request.urlopen(self.get_server_url())
-        self.assertEqual(response.getcode(), 200)
-        self.assertEqual("United States | Place Explorer | Data Commons", self.driver.title)
+# from main import app
 
 
-class TestPlaceExplorer(TestBase):
+# USA_URL = '/place?dcid=country/USA'
+# USA_DEMOGRAPHICS_URL = '/place?dcid=country/USA&topic=Demographics'
 
-    def test_explorer_original(self):
-        time.sleep(60)
-        self.assertEqual("United States | Place Explorer | Data Commons", self.driver.title)
-        responses = []
-        responses = self.driver.find_elements_by_class_name("chart-container")
-        self.assertEqual(len(responses), 23)
+# class TestBase(LiveServerTestCase):
 
-    def test_more_charts_click(self):
-        time.sleep(60)
-        elem = self.driver.find_element_by_xpath("/html/body/div[1]/main/div/div/div[2]/div[3]/section[1]/h3/a")
-        elem.click()
-        time.sleep(60)
-        self.assertEqual(self.driver.current_url, self.get_server_url() + USA_DEMOGRAPHICS_URL)
+#     def create_app(self):
+#         return app
+
+#     def setUp(self):
+#         """
+#         Will be called before every test
+#         """
+#         chrome_options = webdriver.ChromeOptions()
+#         chrome_options.add_argument('--headless')
+#         chrome_options.add_argument('--no-sandbox')
+#         chrome_options.add_argument('--disable-dev-shm-usage')
+
+#         self.driver = webdriver.Chrome(options=chrome_options)
+#         self.driver.get(self.get_server_url() + USA_URL)
+
+#     def tearDown(self):
+#         self.driver.quit()
+
+
+# class TestPlaceExplorer(TestBase):
+
+#     def test_page_server(self):
+#         response = urllib.request.urlopen(self.get_server_url())
+#         self.assertEqual(response.getcode(), 200)
+#         self.assertEqual("United States | Place Explorer | Data Commons", self.driver.title)
+
+#     def test_explorer_original(self):
+#         time.sleep(5)
+#         self.assertEqual("United States | Place Explorer | Data Commons", self.driver.title)
+#         responses = []
+#         responses = self.driver.find_elements_by_class_name("chart-container")
+#         self.assertEqual(len(responses), 23)
+
+#     def test_more_charts_click(self):
+#         time.sleep(60)
+#         elem = self.driver.find_element_by_xpath("/html/body/div[1]/main/div/div/div[2]/div[3]/section[1]/h3/a")
+#         elem.click()
+#         time.sleep(5)
+#         self.assertEqual(self.driver.current_url, self.get_server_url() + USA_DEMOGRAPHICS_URL)
         
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
--- server/webdriver_tests/timeline_test.py
+++ server/webdriver_tests/timeline_test.py
@@ -15,7 +15,6 @@
 import unittest
 import urllib
 
-from flask import abort, url_for
 from flask_testing import LiveServerTestCase
 from selenium import webdriver
 import logging
@@ -33,27 +32,38 @@ class TestBase(LiveServerTestCase):
         """
         Will be called before every test
         """
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        self.driver = webdriver.Chrome(chrome_options=chrome_options)
-        self.driver.get(self.get_server_url() + '/tools/timeline')
+        # chrome_options = webdriver.ChromeOptions()
+        # chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--no-sandbox')
+        # chrome_options.add_argument('--disable-dev-shm-usage')
+        # self.driver = webdriver.Chrome(options=chrome_options)
+
+        self.driver = webdriver.Chrome()
+
+        self.driver.get('http://datacommons.org/tools/timeline')
+        # self.driver.get(self.get_server_url() + '/tools/timeline')
 
     def tearDown(self):
         self.driver.quit()
 
-    def test_server_and_page(self):
-        response = urllib.request.urlopen(self.get_server_url())
-        self.assertEqual(response.getcode(), 200)
-        self.assertEqual("Timelines Explorer | Data Commons", self.driver.title)
-
 
 class TestCharts(TestBase):
 
-    def test_charts_original(self):
+    def test_server_and_page(self):
+        """
+        Test the server can run successfully.
+        """
+
         response = urllib.request.urlopen(self.driver.current_url)
-        time.sleep(60)
+        self.assertEqual(response.getcode(), 200)
+        self.assertEqual("Timelines Explorer | Data Commons", self.driver.title)
+
+    def test_charts_original(self):
+        """
+        Test the original timeline page. No charts in this page.
+        """
+        response = urllib.request.urlopen(self.driver.current_url)
+        time.sleep(5)
         self.assertEqual(response.getcode(), 200)
         self.assertEqual("Timelines Explorer | Data Commons", self.driver.title)
         responses = []
@@ -62,7 +72,8 @@ class TestCharts(TestBase):
 
     def test_chart_numbers_1(self):
         self.driver.get(self.driver.current_url + '#&statsVar=Median_Age_Person,0,1__Median_Income_Person,0,2&place=geoId/06')
-        time.sleep(60)
+
+        time.sleep(5)
         # TODO: Leave screenshot in future PR.
         # self.driver.save_screenshot("test_screenshots/timeline_ca_2_charts.png")
         response = urllib.request.urlopen(self.driver.current_url)
@@ -74,7 +85,7 @@ class TestCharts(TestBase):
 
     def test_chart_numbers_2(self):
         self.driver.get(self.driver.current_url + '#&statsVar=Median_Age_Person,0,1__Median_Income_Person,0,2__Count_Person_Upto5Years,0,3,0__Count_Person_5To17Years,0,3,1&place=geoId/06,geoId/08')
-        time.sleep(60)
+        time.sleep(5)
         # TODO: Leave screenshot in future PR.
         # self.driver.save_screenshot("test_screenshots/timeline_ca_co_3_charts.png")
         response = urllib.request.urlopen(self.driver.current_url)
