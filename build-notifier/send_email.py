c2a913429b9bdb9c8fcdaa1ef011315f973b7e31
Bo Xu <boxu@google.com>
2020-08-12 23:35:15 -0700

    Update email title so different emails won't merge

--- build-notifier/send_email.py
+++ build-notifier/send_email.py
@@ -8,20 +8,28 @@ arg_parser.add_argument(
   '--email', help='Receiver email', type=str, required=True)
 arg_parser.add_argument(
   '--short_sha', help='Commit short sha', type=str, required=True)
+arg_parser.add_argument(
+  '--pull_request', help='Id of pull request', type=str, required=True)
 args = vars(arg_parser.parse_args())
 
 
 port = 587  # For SSL
 sender_email = 'datcom.ci@gmail.com'
 gmail_smtp = 'smtp.gmail.com'
-url_prefix = 'https://staging.datacommons.org/dev/screenshot/'
-
+website_prefix = 'https://staging.datacommons.org/dev/screenshot'
+repo_prefix = 'https://github.com/datacommonsorg/website/pull'
 
 message = """\
-Subject: Please check the screenshot from your PR
+Subject: Screenshots from {}/{}/commits/{}
 
 The screenshots can be found in this url: {}/{}
-""".format(url_prefix, args['short_sha'])
+""".format(
+  repo_prefix,
+  args['pull_request'],
+  args['short_sha'],
+  website_prefix,
+  args['short_sha']
+)
 
 secret_client = secretmanager.SecretManagerServiceClient()
 secret_name = secret_client.secret_version_path('datcom-ci', 'gmailpw', '1')
