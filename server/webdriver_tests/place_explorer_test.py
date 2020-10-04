7dfdb72750473d5581d8f490bac8195357dccd3a
TJann <18621425+tjann@users.noreply.github.com>
2020-10-04 16:24:19 -0700

    Add chart config for life expectancy. (#461)
    * Add chart config for life expectancy.
    
    * Add chart config for life expectancy.
    
    * Remove the Life exp chart from Demographics. Update webdriver test.
    
    * Update branch
    
    * fix merge conflict

--- server/webdriver_tests/place_explorer_test.py
+++ server/webdriver_tests/place_explorer_test.py
@@ -90,9 +90,8 @@ class TestPlaceExplorer(WebdriverBaseTest):
         age_across_places_chart = age_charts[1]
         chart_title = age_across_places_chart.find_element_by_tag_name(
             "h4").text
-        self.assertEqual(
-            "Population by Gender Per Capita: states near California(2018)",
-            chart_title)
+        self.assertEqual("Median Age by Gender: states near California(2018)",
+                         chart_title, chart_title)
 
 
 if __name__ == '__main__':
