2396536634a16e3d0114444aea99f96d5d63e70c
beets <beets@users.noreply.github.com>
2020-10-06 11:02:19 -0700

    use chosen child place type returned by mixer (#496)

--- static/js/place/types.ts
+++ static/js/place/types.ts
@@ -78,6 +78,7 @@ export interface PageData {
   allChildPlaces: {
     string: string[];
   };
+  childPlacesType: string;
   childPlaces: string[];
   parentPlaces: string[];
   similarPlaces: string[];
