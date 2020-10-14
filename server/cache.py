17e5caec6b771309c4b1d103ea00f92b5c1134b6
Bo Xu <boxu@google.com>
2020-10-14 12:27:23 -0700

    Add redis memcache to production

--- server/cache.py
+++ server/cache.py
@@ -24,5 +24,13 @@ if os.environ.get('FLASK_ENV') == 'staging':
             'CACHE_REDIS_PORT': '6379',
             'CACHE_REDIS_URL': 'redis://10.67.227.235:6379'
         })
+elif os.environ.get('FLASK_ENV') == 'production':
+    cache = Cache(
+        config={
+            'CACHE_TYPE': 'redis',
+            'CACHE_REDIS_HOST': '10.68.41.20',
+            'CACHE_REDIS_PORT': '6379',
+            'CACHE_REDIS_URL': 'redis://10.68.41.20:6379'
+        })
 else:
     cache = Cache(config={'CACHE_TYPE': 'simple'})
