d3f6b02c94d989fb6b58c10d9e93ac1baf80b91f
intrepiditee <31870027+intrepiditee@users.noreply.github.com>
2020-12-07 11:39:30 -0600

    add test for get_stats_collection

--- server/tests/api_stats_test.py
+++ server/tests/api_stats_test.py
@@ -32,3 +32,60 @@ class TestApiGetStatsValue(unittest.TestCase):
             '/api/stats/value?place=geoId/06&stat_var=Count_Person_Male')
         assert response.status_code == 200
         assert json.loads(response.data) == {"value": 19640794}
+
+
+class TestApiGetStatsCollection(unittest.TestCase):
+
+    @mock.patch('services.datacommons.send_request')
+    def test_api_get_stats_collection(self, send_request):
+
+        result = {
+            "data": {
+                "Count_Person_Male": {
+                    "val": {
+                        "geoId/10001": 84271,
+                        "geoId/10003": 268870,
+                        "geoId/10005": 106429
+                    },
+                    "measurementMethod": "CensusACS5yrSurvey",
+                    "importName": "CensusACS5YearSurvey",
+                    "provenanceDomain": "census.gov",
+                    "provenanceUrl": "https://www.census.gov/"
+                },
+                "Count_Person": {
+                    "val": {
+                        "geoId/10001": 178540,
+                        "geoId/10003": 557550,
+                        "geoId/10005": 229389
+                    },
+                    "measurementMethod":
+                        "CensusPEPSurvey",
+                    "importName":
+                        "CensusPEP",
+                    "provenanceDomain":
+                        "census.gov",
+                    "provenanceUrl":
+                        "https://www.census.gov/programs-surveys/popest.html"
+                }
+            }
+        }
+
+        def side_effect(req_url,
+                        req_json={},
+                        compress=False,
+                        post=True,
+                        has_payload=True):
+            if req_url == dc.API_ROOT + "/stat/collection" and req_json == {
+                    'parent_place': 'geoId/10',
+                    'child_type': 'County',
+                    'date': '2018',
+                    'stat_vars': ['Count_Person', 'Count_Person_Male']
+            } and not post and not has_payload:
+                return result
+
+        send_request.side_effect = side_effect
+        response = app.test_client().get(
+            '/api/stats/collection?parent_place=geoId/10&child_type=County'
+            '&date=2018&stat_vars=Count_Person&stat_vars=Count_Person_Male')
+        assert response.status_code == 200
+        assert json.loads(response.data) == result['data']
