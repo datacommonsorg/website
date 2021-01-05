a522e29e408367d426520e4525213d9c6ad53d47
hanlu09205 <59888187+hanlu09205@users.noreply.github.com>
2021-01-05 09:48:36 -0800

    update landing page to respect hl param (#733)
    hl set to en:
    http://127.0.0.1:8080/place/geoId/06?hl=en
    https://screenshot.googleplex.com/BaCcD3zqFuqmS9S
    hl set to zh:
    http://127.0.0.1:8080/place/geoId/06?hl=zh
    https://screenshot.googleplex.com/3RjhrafQpyepL6y
    with topic set:
    http://127.0.0.1:8080/place/geoId/06?hl=zh&topic=Demographics
    https://screenshot.googleplex.com/3XBiTqrihZw3X7J

--- server/webdriver_tests/place_explorer_test.py
+++ server/webdriver_tests/place_explorer_test.py
@@ -31,7 +31,7 @@ class TestPlaceExplorer(WebdriverBaseTest):
 
     def test_page_serve_usa(self):
         """Test the place explorer page for USA can be loaded successfullly."""
-        TITLE_TEXT = "United States - Place Explorer - Data Commons"
+        TITLE_TEXT = "United States of America - Place Explorer - Data Commons"
         PLACE_TYPE_TEXT = "A Country in North America"
 
         # Load USA page.
@@ -59,7 +59,7 @@ class TestPlaceExplorer(WebdriverBaseTest):
 
         # Assert place title is correct.
         title = self.driver.find_element_by_id("place-name")
-        self.assertEqual("United States", title.text)
+        self.assertEqual("United States of America", title.text)
 
         # Assert place type is correct.
         subtitle = self.driver.find_element_by_id("place-type")
