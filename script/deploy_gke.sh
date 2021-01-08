d0bcc57d3229b6f2440f66ce67ac151f4e75a75d
Bo Xu <boxu@google.com>
2021-01-07 21:39:03 -0800

    deploy script

---
+++ script/deploy_gke.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Script to deploy website to a GKE cluster.
+#
+# Usage:
+#
+# ./deploy_key.sh <"prod"|"staging">
+#
+# First argument is either "prod" or "staging".
+#
+# !!! WARNING: Run this script in a clean Git checkout at the desired commit.
+#
+
+set -e
+
+ENV=$1
+
+if [[ $ENV != "staging" && $ENV != "prod" ]]; then
+  echo "First argument should be 'staging' or 'prod' "
+  exit
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT="$(dirname "$DIR")"
+
+WEBSITE_TAG=$(git rev-parse --short HEAD)
+
+cd $ROOT/mixer
+MIXER_TAG=$(git rev-parse --short HEAD)
+cd $DIR
+
+PROJECT_ID=$(yq read deploy/gke/$ENV.yaml project)
+# Only deploy to the primary region for now.
+# Once Bigtable replication is done, deploy for all regions
+REGION=$(yq read deploy/gke/$ENV.yaml region.primary)
+CLUSTER_NAME=website-$REGION
+
+cd $ROOT/deploy/overlays/$ENV
+
+# Deploy to GKE
+kustomize edit set image gcr.io/datcom-ci/datacommons-website=gcr.io/datcom-ci/datacommons-website:$WEBSITE_TAG
+kustomize edit set image gcr.io/datcom-ci/datacommons-mixer=gcr.io/datcom-ci/datacommons-mixer:$MIXER_TAG
+kustomize build > $ENV.yaml
+gcloud config set project $PROJECT_ID
+gcloud container clusters get-credentials $CLUSTER_NAME --region $REGION
+kubectl apply -f $ENV.yaml
+
+# Deploy Cloud Endpoints
+SERVICE_NAME="website-esp.endpoints.$PROJECT_ID.cloud.goog"
+API_TITLE=$SERVICE_NAME
+yq w --style=double $ROOT/gke/endpoints.yaml.tpl name $SERVICE_NAME > endpoints.yaml
+yq w -i endpoints.yaml title "$API_TITLE"
+
+# Deploy ESP configuration
+gsutil cp gs://artifacts.datcom-ci.appspot.com/mixer-grpc/mixer-grpc.$TAG.pb .
+gcloud endpoints services deploy mixer-grpc.$TAG.pb endpoints.yaml --project $PROJECT_ID
