73d43e422d7106959c0a4c61273e4ea4530ff049
Bo Xu <shifucun@users.noreply.github.com>
2021-01-19 15:55:33 -0800

    Update mixer grpc descriptor download path (#776)

--- script/deploy_gke.sh
+++ script/deploy_gke.sh
@@ -66,5 +66,5 @@ yq w --style=double $ROOT/gke/endpoints.yaml.tpl name $SERVICE_NAME > endpoints.
 yq w -i endpoints.yaml title "$API_TITLE"
 
 # Deploy ESP configuration
-gsutil cp gs://artifacts.datcom-ci.appspot.com/mixer-grpc/mixer-grpc.$MIXER_TAG.pb .
+gsutil cp gs://datcom-mixer-grpc/mixer-grpc/mixer-grpc.$MIXER_TAG.pb .
 gcloud endpoints services deploy mixer-grpc.$MIXER_TAG.pb endpoints.yaml --project $PROJECT_ID
