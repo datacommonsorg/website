04ccea1cf0850dd8e9e4ef80559a8732cbab7445
Bo Xu <boxu@google.com>
2021-07-19 19:54:27 -0700

    lint

--- server/webdriver_tests/place_landing_test.py
+++ server/webdriver_tests/place_landing_test.py
@@ -17,8 +17,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-import time
-
 
 class TestPlaceLanding(WebdriverBaseTest):
     """Tests for Place Landing page."""
