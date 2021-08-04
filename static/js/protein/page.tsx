d09bde49be6f2512437afd734a2072d06e6d4cd5
Bo Xu <boxu@google.com>
2021-08-03 19:04:06 -0700

    Pipe in real data for protein page

--- static/js/protein/page.tsx
+++ static/js/protein/page.tsx
@@ -19,22 +19,76 @@
  */
 
 import React from "react";
+import axios from "axios";
+
+import { GraphNode } from "../shared/types";
 
 interface PagePropType {
   dcid: string;
   nodeName: string;
 }
 
-export class Page extends React.Component<PagePropType> {
+interface PageStateType {
+  data: GraphNode;
+}
+
+export class Page extends React.Component<PagePropType, PageStateType> {
   constructor(props: PagePropType) {
     super(props);
+    this.state = { data: null };
   }
 
   componentDidMount(): void {
-    // TOOD: add data fetching here.
+    this.fetchData();
   }
 
   render(): JSX.Element {
-    return <div> Protein Data</div>;
+    const tissueScore = this.getTissueScore();
+    // TODO: use d3 to draw bar chart here.
+    return (
+      <>
+        <h2>{this.props.nodeName}</h2>
+        {Object.keys(tissueScore).map((tissue) => {
+          return (
+            <div key={tissue}>
+              <span>{tissue}</span>: <span>{tissueScore[tissue]}</span>
+            </div>
+          );
+        })}
+      </>
+    );
+  }
+
+  private fetchData(): void {
+    axios.get("/api/protein/" + this.props.dcid).then((resp) => {
+      this.setState({
+        data: resp.data,
+      });
+    });
+  }
+
+  private getTissueScore(): Record<string, string> {
+    // Tissue to score mapping.
+    if (!this.state.data) {
+      return {};
+    }
+    const result = {};
+    for (const neighbour of this.state.data.neighbours) {
+      if (neighbour.property == "detectedProtein") {
+        for (const node of neighbour.nodes) {
+          let tissue = null;
+          let score = null;
+          for (const n of node.neighbours) {
+            if (n.property == "humanTissue") {
+              tissue = n.nodes[0].value;
+            } else if (n.property == "proteinExpressionScore") {
+              score = n.nodes[0].value;
+            }
+          }
+          result[tissue] = score;
+        }
+        return result;
+      }
+    }
   }
 }
