9192e1ca684d2d0d86e33d7be2efae099c5d6526
chejennifer <69875368+chejennifer@users.noreply.github.com>
2021-09-16 13:00:05 -0700

    [map] add links for greenhouse gas emissions plots (#1134)
    * add links for greenhouse gas emissions on landing page
    
    * fix codacy

--- static/js/chart/draw_choropleth.ts
+++ static/js/chart/draw_choropleth.ts
@@ -154,11 +154,14 @@ function addMapPoints(
   projection: d3.GeoProjection,
   getTooltipHtml: (place: NamedPlace) => string
 ): void {
+  const filteredMapPoints = mapPoints.filter(
+    (point) => !_.isNull(projection([point.longitude, point.latitude]))
+  );
   d3.select(`#${MAP_ITEMS_GROUP_ID}`)
     .append("g")
     .attr("class", "map-points-layer")
     .selectAll("circle")
-    .data(mapPoints)
+    .data(filteredMapPoints)
     .enter()
     .append("circle")
     .attr("class", "dot")
