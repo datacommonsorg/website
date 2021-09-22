77d754acbbfe53f4ded8fc8ae026e3d9b7cf6179
Bo Xu <boxu@google.com>
2021-09-22 10:55:00 -0700

    Update translator schema mapping to use stat var observation

--- static/js/translator/constants.ts
+++ static/js/translator/constants.ts
@@ -75,86 +75,85 @@ WHERE {
   },
   all: {
     mapping: `
-Node: E:StatisticalPopulation->E1
-typeOf: StatisticalPopulation
-dcid: C:StatisticalPopulation->id
-provenance: E:StatisticalPopulation->E3
-isPublic: C:StatisticalPopulation->is_public
-populationType: C:StatisticalPopulation->population_type
-populationGroup: C:StatisticalPopulation->population_group
-location: E:StatisticalPopulation->E2
-p1: C:StatisticalPopulation->p1
-v1: C:StatisticalPopulation->v1
-p2: C:StatisticalPopulation->p2
-v2: C:StatisticalPopulation->v2
-p3: C:StatisticalPopulation->p3
-v3: C:StatisticalPopulation->v3
-p4: C:StatisticalPopulation->p4
-v4: C:StatisticalPopulation->v4
-p5: C:StatisticalPopulation->p5
-v5: C:StatisticalPopulation->v5
-p6: C:StatisticalPopulation->p6
-v6: C:StatisticalPopulation->v6
-p7: C:StatisticalPopulation->p7
-v7: C:StatisticalPopulation->v7
-p8: C:StatisticalPopulation->p8
-v8: C:StatisticalPopulation->v8
-p9: C:StatisticalPopulation->p9
-v9: C:StatisticalPopulation->v9
-p10: C:StatisticalPopulation->p10
-v10: C:StatisticalPopulation->v10
-numConstraints: C:StatisticalPopulation->num_constraints
+Node: E:StatisticalVariable->E1
+typeOf: StatisticalVariable
+dcid: C:StatisticalVariable->id
+provenance: E:StatisticalVariable->E2
+populationType: C:StatisticalVariable->population_type
+measuredProperty: C:StatisticalVariable->measured_prop
+statType: C:StatisticalVariable->stat_type
+measurementQualifier: C:StatisticalVariable->measurement_qualifier
+measurementDenominator: E:StatisticalVariable->E3
+p1: C:StatisticalVariable->p1
+v1: C:StatisticalVariable->v1
+p2: C:StatisticalVariable->p2
+v2: C:StatisticalVariable->v2
+p3: C:StatisticalVariable->p3
+v3: C:StatisticalVariable->v3
+p4: C:StatisticalVariable->p4
+v4: C:StatisticalVariable->v4
+p5: C:StatisticalVariable->p5
+v5: C:StatisticalVariable->v5
+p6: C:StatisticalVariable->p6
+v6: C:StatisticalVariable->v6
+p7: C:StatisticalVariable->p7
+v7: C:StatisticalVariable->v7
+p8: C:StatisticalVariable->p8
+v8: C:StatisticalVariable->v8
+p9: C:StatisticalVariable->p9
+v9: C:StatisticalVariable->v9
+p10: C:StatisticalVariable->p10
+v10: C:StatisticalVariable->v10
+numConstraints: C:StatisticalVariable->num_constraints
 functionalDeps: dcid
 
-Node: E:StatisticalPopulation->E2
-typeOf: Place
-dcid: C:StatisticalPopulation->place_key
-functionalDeps: dcid
-
-Node: E:StatisticalPopulation->E3
+Node: E:StatisticalVariable->E2
 typeOf: Provenance
-dcid: C:StatisticalPopulation->prov_id
+dcid: C:StatisticalVariable->prov_id
+functionalDeps: dcid
+
+Node: E:StatisticalVariable->E3
+typeOf: StatisticalVariable
+typeOf: Property
+dcid: C:StatisticalVariable->measurement_denominator
 functionalDeps: dcid
 
 
-Node: E:Observation->E1
-typeOf: Observation
-dcid: C:Observation->id
-provenance: E:Observation->E3
-isPublic: C:Observation->is_public
-measuredProperty: C:Observation->measured_prop
-observationDate: C:Observation->observation_date
-observationPeriod: C:Observation->observation_period
-meanValue: C:Observation->mean_value
-medianValue: C:Observation->median_value
-sumValue: C:Observation->sum_value
-minValue: C:Observation->min_value
-maxValue: C:Observation->max_value
-measuredValue: C:Observation->measured_value
-percentile10: C:Observation->p10
-percentile25: C:Observation->p25
-percentile75: C:Observation->p75
-percentile90: C:Observation->p90
-unit: C:Observation->unit
-marginOfError: C:Observation->margin_of_error
-measurementMethod: C:Observation->measurement_method
-measurementResult: C:Observation->measurement_result
-stdError: C:Observation->std_error
-meanStdError: C:Observation->mean_std_error
-sampleSize: C:Observation->sample_size
-growthRate: C:Observation->growth_rate
-observedNode: E:Observation->E2
+Node: E:StatVarObservation->E1
+typeOf: StatVarObservation
+dcid: C:StatVarObservation->id
+observationAbout: E:StatVarObservation->E2
+variableMeasured: E:StatVarObservation->E3
+value: C:StatVarObservation->value
+observationDate: C:StatVarObservation->observation_date
+observationPeriod: C:StatVarObservation->observation_period
+measurementMethod: C:StatVarObservation->measurement_method
+unit: C:StatVarObservation->unit
+scalingFactor: C:StatVarObservation->scaling_factor
+samplePopulation: C:StatVarObservation->sample_population
+location: E:StatVarObservation->E4
+provenance: E:StatVarObservation->E5
 functionalDeps: dcid
 
-Node: E:Observation->E2
-typeOf: StatisticalPopulation
+Node: E:StatVarObservation->E2
 typeOf: Place
-dcid: C:Observation->observed_node_key
+dcid: C:StatVarObservation->observation_about
 functionalDeps: dcid
 
-Node: E:Observation->E3
+Node: E:StatVarObservation->E3
+typeOf: StatisticalVariable
+typeOf: Property
+dcid: C:StatVarObservation->variable_measured
+functionalDeps: dcid
+
+Node: E:StatVarObservation->E4
+typeOf: GeoCoordinates
+dcid: C:StatVarObservation->location
+functionalDeps: dcid
+
+Node: E:StatVarObservation->E5
 typeOf: Provenance
-dcid: C:Observation->prov_id
+dcid: C:StatVarObservation->prov_id
 functionalDeps: dcid
 
 
@@ -162,7 +161,6 @@ Node: E:Place->E1
 typeOf: Place
 subType: C:Place->type
 dcid: C:Place->id
-isPublic: C:Place->is_public
 name: C:Place->name
 alternateName: C:Place->alternate_name
 timezone: C:Place->timezone
@@ -198,8 +196,11 @@ Node: E:PlaceExt->E1
 typeOf: Place
 subType: C:PlaceExt->type
 dcid: C:PlaceExt->id
-isPublic: C:PlaceExt->is_public
 kmlCoordinates: C:PlaceExt->kml_coordinates
+geoJsonCoordinates: C:PlaceExt->geo_json_coordinates
+geoJsonCoordinatesDP1: C:PlaceExt->geo_json_coordinates_dp1
+geoJsonCoordinatesDP2: C:PlaceExt->geo_json_coordinates_dp2
+geoJsonCoordinatesDP3: C:PlaceExt->geo_json_coordinates_dp3
 provenance: E:PlaceExt->E2
 functionalDeps: dcid
 
@@ -212,7 +213,6 @@ functionalDeps: dcid
 Node: E:Instance->E1
 typeOf: Thing
 dcid: C:Instance->id
-isPublic: C:Instance->is_public
 name: C:Instance->name
 subType: C:Instance->type
 provenance: E:Instance->E2
@@ -227,7 +227,7 @@ functionalDeps: dcid
 Node: E:Quantity->E1
 typeOf: Quantity
 subType: C:Quantity->type
-dcid: C:Quantity->row_info.id
+dcid: C:Quantity->id
 unitOfMeasure: C:Quantity->unit_of_measure
 value: C:Quantity->value
 startValue: C:Quantity->start_value
@@ -239,16 +239,15 @@ functionalDeps: dcid
 Node: E:Provenance->E1
 typeOf: Provenance
 dcid: C:Provenance->id
-isPublic: C:Provenance->is_public
 name: C:Provenance->name
 curator: E:Provenance->E2
-aclGroup: E:Provenance->E3
-source: E:Provenance->E4
+source: E:Provenance->E3
 url: C:Provenance->provenance_url
+description: C:Provenance->provenance_description
 importUrl: C:Provenance->mcf_url
 importTime: C:Provenance->timestamp_secs
 importDuration: C:Provenance->duration_secs
-provenance: E:Provenance->E5
+provenance: E:Provenance->E4
 functionalDeps: dcid
 
 Node: E:Provenance->E2
@@ -257,16 +256,11 @@ dcid: C:Provenance->curator
 functionalDeps: dcid
 
 Node: E:Provenance->E3
-typeOf: ACLGroup
-dcid: C:Provenance->acl_group
-functionalDeps: dcid
-
-Node: E:Provenance->E4
 typeOf: Source
 dcid: C:Provenance->source
 functionalDeps: dcid
 
-Node: E:Provenance->E5
+Node: E:Provenance->E4
 typeOf: Provenance
 dcid: C:Provenance->prov_id
 functionalDeps: dcid
@@ -276,7 +270,6 @@ Node: E:Curator->E1
 typeOf: Curator
 dcid: C:Curator->id
 provenance: E:Curator->E2
-isPublic: C:Curator->is_public
 email: C:Curator->email
 name: C:Curator->name
 functionalDeps: dcid
@@ -287,25 +280,10 @@ dcid: C:Curator->prov_id
 functionalDeps: dcid
 
 
-Node: E:ACLGroup->E1
-typeOf: ACLGroup
-dcid: C:ACLGroup->id
-provenance: E:ACLGroup->E2
-isPublic: C:ACLGroup->is_public
-email: C:ACLGroup->email
-functionalDeps: dcid
-
-Node: E:ACLGroup->E2
-typeOf: Provenance
-dcid: C:ACLGroup->prov_id
-functionalDeps: dcid
-
-
 Node: E:Source->E1
 typeOf: Source
 dcid: C:Source->id
 provenance: E:Source->E2
-isPublic: C:Source->is_public
 domain: C:Source->domain
 functionalDeps: dcid
 
@@ -327,32 +305,21 @@ dcid: C:Triple->prov_id
 functionalDeps: dcid
 `,
     sparql: `
-SELECT ?node ?freelunch_percent ?unemploy_rate
+SELECT ?node ?life_expectancy ?median_age
 WHERE {
   ?node typeOf State .
 
-  ?pop typeOf StatisticalPopulation .
-  ?pop location ?node .
-  ?pop p1 "hasSocioEconomicHealthFactor" .
-  ?pop v1 "FreeLunch" .
-  ?pop numConstraints 1 .
+  ?obs1 typeOf StatVarObservation .
+  ?obs1 observationAbout ?node .
+  ?obs1 variableMeasured LifeExpectancy_Person .
+  ?obs1 observationDate "2018" .
+  ?obs1 value ?life_expectancy .
 
-  ?pop2 typeOf StatisticalPopulation .
-  ?pop2 location ?node .
-  ?pop2 numConstraints 0 .
-  ?pop2 populationType "Person" .
-
-  ?obs typeOf Observation .
-  ?obs observedNode ?pop .
-  ?obs measuredProperty percent .
-  ?obs observationDate "2018" .
-  ?obs measuredValue ?freelunch_percent .
-
-  ?obs2 typeOf Observation .
-  ?obs2 observedNode ?pop2 .
-  ?obs2 measuredProperty unemploymentRate .
+  ?obs2 typeOf StatVarObservation .
+  ?obs2 observationAbout ?node .
+  ?obs2 variableMeasured Median_Age_Person .
   ?obs2 observationDate "2018" .
-  ?obs2 measuredValue ?unemploy_rate .
+  ?obs2 value ?median_age .
 }
 `,
   },
