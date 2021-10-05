cfe5b6a5f1004b25f8a300ef9bf3445c5b03acb9
Bo Xu <shifucun@users.noreply.github.com>
2021-10-05 11:19:32 -0700

    [Place Page Chart] Air toxics cancer risk (#1156)
    * [Place Page Chart] Air toxics cancer risk
    
    * fix unit

--- static/js/i18n/i18n.tsx
+++ static/js/i18n/i18n.tsx
@@ -207,6 +207,14 @@ function formatNumber(value: number, unit?: string): string {
       shouldAddUnit = true;
       unitKey = "micro-gram-per-cubic-meter";
       break;
+    case "MTCO2e":
+      shouldAddUnit = true;
+      unitKey = "metric-tons-of-co2";
+      break;
+    case "per-million":
+      shouldAddUnit = true;
+      unitKey = "per-million";
+      break;
   }
   let returnText = Intl.NumberFormat(intl.locale, formatOptions).format(value);
   if (shouldAddUnit) {
