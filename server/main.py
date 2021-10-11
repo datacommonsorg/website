6141eac3a8286ec285b4bc9db1dfcdd40e8cff45
Carolyn Au <beets@users.noreply.github.com>
2021-10-11 10:46:12 -0700

    [sustainability] add ability to run_server to start multiple instances (#1172)
    Added the following flags to run_server.sh:
    -e for the environment
    -p for the localhost port

--- server/main.py
+++ server/main.py
@@ -19,6 +19,7 @@ This module contains the request handler codes and the main app.
 import json
 import logging
 import os
+import sys
 
 import flask
 from flask import request
@@ -36,6 +37,8 @@ app.jinja_env.globals['GA_ACCOUNT'] = app.config['GA_ACCOUNT']
 app.jinja_env.globals['PRIVATE'] = app.config['PRIVATE']
 app.jinja_env.globals['SUSTAINABILITY'] = app.config['SUSTAINABILITY']
 app.jinja_env.globals['NAME'] = app.config['NAME']
+app.jinja_env.globals['BASE_HTML'] = 'sustainability/base.html' if app.config[
+    'SUSTAINABILITY'] else 'base.html'
 
 GCS_BUCKET = app.config['GCS_BUCKET']
 _MAX_SEARCH_RESULTS = 1000
@@ -183,4 +186,5 @@ def version():
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to GKE,
     # a webserver process such as Gunicorn will serve the app.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    port = sys.argv[1] if len(sys.argv) >= 2 else 8080
+    app.run(host='127.0.0.1', port=port, debug=True)
