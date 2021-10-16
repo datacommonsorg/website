de44d09935246de25140e65ff225cb073a6b6be3
Carolyn Au <beets@users.noreply.github.com>
2021-10-15 17:34:31 -0700

    [timeline] only show all model data for projections (#1189)
    * don't show models for historical
    
    * cleanup code past mixer #641
    
    * thicken timeline line somewhat to get closer to place pages (2.5px)

--- static/js/tools/timeline/chart.tsx
+++ static/js/tools/timeline/chart.tsx
@@ -195,7 +195,6 @@ class Chart extends Component<ChartPropsType> {
       data: {},
       sources: new Set<string>(),
     };
-    this.statData.dates = [];
     for (const place in ipccData.placeData) {
       const placeData = ipccData.placeData[place];
       modelData.places.push(place);
@@ -223,10 +222,6 @@ class Chart extends Component<ChartPropsType> {
             means[date] = _.mean(means[date]);
           }
           this.statData.data[place].data[sv].val = means;
-          this.statData.dates = _.union(
-            this.statData.dates,
-            Object.keys(means)
-          );
         }
       }
     }
