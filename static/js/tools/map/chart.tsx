22a2dc49ab872da29b9d9b5a4f84da36da2377b9
Bo Xu <shifucun@users.noreply.github.com>
2021-10-26 07:20:43 -0700

    Revert "Improve scatter plot loading by parallel fetch population data for specified years (#1216)" (#1219)

--- static/js/tools/map/chart.tsx
+++ static/js/tools/map/chart.tsx
@@ -36,6 +36,7 @@ import {
 import { formatNumber } from "../../i18n/i18n";
 import {
   EARTH_NAMED_TYPED_PLACE,
+  EUROPE_PLACE_DCID,
   INDIA_PLACE_DCID,
   USA_PLACE_DCID,
 } from "../../shared/constants";
