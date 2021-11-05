e6a32491a367e7537e33ad0efef774adcd4ad1dc
Bo Xu <shifucun@users.noreply.github.com>
2021-11-05 12:58:26 -0700

    Run python/webdriver tests in parallel (#1291)
    * Run python/webdriver tests in parallel
    
    * less

--- run_test.sh
+++ run_test.sh
@@ -79,9 +79,9 @@ function run_py_test {
   setup_python
   cd server
   export FLASK_ENV=test
-  python3 -m pytest tests/**.py --ignore=sustainability
+  python3 -m pytest -n 20 tests/**.py --ignore=sustainability
   export FLASK_ENV=test-sustainability
-  python3 -m pytest tests/sustainability/**.py
+  python3 -m pytest -n 20 tests/sustainability/**.py
   cd ..
   echo -e "#### Checking Python style"
   if ! yapf --recursive --diff --style=google -p server/ tools/; then
@@ -102,7 +102,7 @@ function run_webdriver_test {
   fi
   export FLASK_ENV=webdriver
   export GOOGLE_CLOUD_PROJECT=datcom-website-dev
-  python3 -m pytest webdriver_tests/*.py
+  python3 -m pytest -n 10 webdriver_tests/**.py
   cd ..
 }
 
