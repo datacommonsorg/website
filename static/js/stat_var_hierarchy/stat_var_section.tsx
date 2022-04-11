b1690fdcb9b099768e4d1e7cc42215404b53c8bf
Jennifer Chen <chejennifer@google.com>
2022-04-11 13:31:16 -0700

    add test

--- static/js/stat_var_hierarchy/stat_var_section.tsx
+++ static/js/stat_var_hierarchy/stat_var_section.tsx
@@ -78,7 +78,7 @@ export class StatVarSection extends React.Component<
 
   render(): JSX.Element {
     const context = this.context;
-    const prefix = this.getPrefix();
+    const prefix = this.getPrefix(this.props.data);
     const showPrefix =
       context.statVarHierarchyType !== StatVarHierarchyType.BROWSER && prefix;
     return (
@@ -137,8 +137,8 @@ export class StatVarSection extends React.Component<
       });
   }
 
-  private getPrefix(): string {
-    const svNamesList = this.props.data.map((sv) => sv.displayName);
+  private getPrefix(svList: StatVarInfo[]): string {
+    const svNamesList = svList.map((sv) => sv.displayName);
     // Only get prefix if there is more than 1 stat var.
     if (svNamesList.length < 2) {
       return "";
