196e74dc8ae88aa678cf01ef749f498b272ca38b
Julian Eisenschlos <eisenjulian@users.noreply.github.com>
2022-04-28 21:40:53 +0200

    Adds plots for rich search interface (#1584)
    * Add rich search plots
    
    * [BQ] Updated queries (#1575)
    
    * update contribute link
    
    * fix broken provenance link
    
    * don't include breadcrumb source in chart sources list
    
    * fix karnataka no place types on scatter tool
    
    * updated queries
    
    * address pr comments
    
    * fix some naming
    
    * revert chart_region
    
    * reverting changes
    
    * bring back chart region fix
    
    * Integrate match (#1565)
    
    * Use statvar match API for ai search.
    
    * Add rich search plots
    
    * Split TextSearchBar into new file
    
    * Remove place search box and unify with the text search
    
    * Undo changes to hide empty plot in tools/timeline/chart.tsx
    
    Co-authored-by: chejennifer <69875368+chejennifer@users.noreply.github.com>
    Co-authored-by: Francesco Piccinno <stack.box@gmail.com>
    Co-authored-by: Carolyn Au <beets@users.noreply.github.com>

--- static/js/tools/timeline/chart_region.tsx
+++ static/js/tools/timeline/chart_region.tsx
@@ -159,12 +159,13 @@ class ChartRegion extends Component<ChartRegionPropsType> {
 
   private onDataUpdate(groupId: string, data: StatData) {
     this.allStatData[groupId] = data;
-    if (this.downloadLink && Object.keys(this.allStatData).length > 0) {
-      this.downloadLink.style.display = "inline-block";
-      this.bulkDownloadLink.style.display = "inline-block";
-    } else {
-      this.downloadLink.style.display = "none";
-      this.bulkDownloadLink.style.display = "none";
+    const displayStyle =
+      Object.keys(this.allStatData).length > 0 ? "inline-block" : "none";
+    if (this.downloadLink) {
+      this.downloadLink.style.display = displayStyle;
+    }
+    if (this.bulkDownloadLink) {
+      this.bulkDownloadLink.style.display = displayStyle;
     }
   }
 
