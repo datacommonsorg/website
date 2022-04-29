e77dfc63d6019260f551916753fcdc347393368f
Senthamizhan <senthamizhan1998@gmail.com>
2022-04-29 23:26:12 +0530

    Added FAQ and Feedback pages for IITM env

--- server/routes/static.py
+++ server/routes/static.py
@@ -47,8 +47,13 @@ def about():
 
 @bp.route('/faq')
 def faq():
-    current_date = date.today().strftime('%-d %b %Y')
+    current_date = date.today().strftime('%d %b %Y')
     current_year = date.today().strftime('%Y')
+    env_name = current_app.config.get('ENV_NAME', None)
+    if env_name == 'IITM':
+        return render_template('private_dc/iitm/faq.html',
+                                current_date=current_date,
+                                current_year=current_year)
     return render_template('static/faq.html',
                            current_date=current_date,
                            current_year=current_year)
@@ -61,4 +66,7 @@ def disclaimers():
 
 @bp.route('/feedback')
 def feedback():
+    env_name = current_app.config.get('ENV_NAME', None)
+    if env_name == 'IITM':
+        return render_template('private_dc/iitm/feedback.html')
     return render_template('static/feedback.html')
