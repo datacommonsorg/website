1fe2c4cd18d79ce602f457dd843948698ab6abf6
bsram88 <bsram88@gmail.com>
2022-04-29 18:17:21 +0530

    home white menu + body overflow + particles width

--- static/private_dc/iitm/creative.js
+++ static/private_dc/iitm/creative.js
@@ -40,21 +40,4 @@
   $(window).scroll(navbarCollapse);
 
 
-
-  // Magnific popup calls
-  $('.popup-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
-  });
-
 })(jQuery); // End of use strict
