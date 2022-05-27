f7eefbb683fba04f8b46b3cc811f199d422f4de1
Suhana Bedi <57412795+suhana13@users.noreply.github.com>
2022-05-27 17:56:50 -0500

    Add the remaining charts for the protein browser page (#1610)
    * add the remaining charts and unit tests
    
    * add PR suggestions and fixes
    
    * PR nits
    
    * implement PR nits
    
    * implement PR nits
    
    * implement PR nits
    
    * implement PR nits
    
    * nit: reduce nested loops
    
    * implement nits
    
    * implement nit
    
    Co-authored-by: Suhana Bedi <suhanabedi@google.com>
    Co-authored-by: chejennifer <69875368+chejennifer@users.noreply.github.com>

---
+++ static/js/protein/data_processing_utils.ts
@@ -0,0 +1,476 @@
+import _ from "lodash";
+
+import { GraphNodes } from "../shared/types";
+import { ProteinStrData } from "./chart";
+import { ProteinNumData } from "./chart";
+import { ProteinVarType } from "./page";
+import { InteractingProteinType } from "./page";
+
+const VARIANT_CATEGORY = [
+  "GeneticVariantFunctionalCategoryUTR3",
+  "GeneticVariantFunctionalCategoryUTR5",
+  "GeneticVariantFunctionalCategoryCodingSynon",
+  "GeneticVariantFunctionalCategoryFrameshift",
+  "GeneticVariantFunctionalCategoryIntron",
+  "GeneticVariantFunctionalCategoryMissense",
+  "GeneticVariantFunctionalCategoryNearGene3",
+  "GeneticVariantFunctionalCategoryNearGene5",
+  "GeneticVariantFunctionalCategoryNonsense",
+  "GeneticVariantFunctionalCategorySplice3",
+  "GeneticVariantFunctionalCategorySplice5",
+  "GeneticVariantFunctionalCategoryStopLoss",
+  "GeneticVariantFunctionalCategoryUnknown",
+  "GeneticVariantFunctionalCDSIndel",
+  "GeneticVariantFunctionalCategoryCDSReference",
+  "GeneticVariantFunctionalCategoryncRNA",
+];
+
+const VARIANT_AFFECTS = [
+  "ClinSigAffects",
+  "ClinSigAssociation",
+  "ClinSigAssociationNotFound",
+  "ClinSigBenign",
+  "ClinSigBenignLikelyBenign",
+  "ClinSigConflictingPathogenicity",
+  "ClinSigDrugResponse",
+  "ClinSigHistocompatability",
+  "ClinSigLikelyBenign",
+  "ClinSigLikelyPathogenic",
+  "ClinSigNotProvided",
+  "ClinSigOther",
+  "ClinSigPathogenic",
+  "ClinSigPathogenicLikelyPathogenic",
+  "ClinSigProtective",
+  "ClinSigRiskFactor",
+  "ClinSigUncertain",
+  "ClinSigUnknown",
+  "ClinSigUntested",
+];
+
+const CHEM_RELATIONS = [
+  "RelationshipAssociationTypeAssociated",
+  "RelationshipAssociationTypeNotAssociated",
+  "RelationshipAssociationTypeAmbiguous",
+];
+
+export function getTissueScore(data: GraphNodes): ProteinStrData[] {
+  // Tissue to score mapping.
+  if (!data) {
+    return [];
+  }
+  const returnData: ProteinStrData[] = [];
+  // check for null values
+  if (_.isEmpty(data.nodes) || _.isEmpty(data.nodes[0].neighbors)) {
+    return [];
+  }
+  for (const neighbour of data.nodes[0].neighbors) {
+    if (neighbour.property !== "detectedProtein") {
+      continue;
+    }
+    for (const node of neighbour.nodes) {
+      let tissue = null;
+      let score = null;
+      // check for null or non-existent property values
+      if (_.isEmpty(node.neighbors)) {
+        continue;
+      }
+      for (const n of node.neighbors) {
+        if (n.property === "humanTissue") {
+          // check for empty list and null tissue values
+          if (_.isEmpty(n.nodes) || _.isEmpty(n.nodes[0].value)) {
+            continue;
+          }
+          tissue = n.nodes[0].value;
+        } else if (n.property === "proteinExpressionScore") {
+          // check if the list is empty or not
+          if (_.isEmpty(n.nodes)) {
+            continue;
+          }
+          score = n.nodes[0].value;
+        }
+      }
+      returnData.push({ name: tissue, value: score });
+    }
+    return returnData;
+  }
+  return [];
+}
+export function getProteinInteraction(
+  data: GraphNodes,
+  nodeName: string
+): InteractingProteinType[] {
+  // Protein Interaction to confidence score mapping.
+  if (!data) {
+    return [];
+  }
+  const returnData = [] as InteractingProteinType[];
+  const seen = new Set();
+  // check for null values
+  if (_.isEmpty(data.nodes) || _.isEmpty(data.nodes[0].neighbors)) {
+    return [];
+  }
+
+  for (const neighbour of data.nodes[0].neighbors) {
+    if (neighbour.property !== "interactingProtein") {
+      continue;
+    }
+    for (const node of neighbour.nodes) {
+      let protein_name = null;
+      let confidence_score = null;
+      let parent_protein = null;
+      // check for null or non-existent property values
+      if (_.isEmpty(node.neighbors)) {
+        continue;
+      }
+      for (const n of node.neighbors) {
+        if (n.property === "name") {
+          // check for empty list and null tissue values
+          if (_.isEmpty(n.nodes) || _.isEmpty(n.nodes[0].value)) {
+            continue;
+          }
+          protein_name = n.nodes[0].value;
+          parent_protein = nodeName;
+        } else if (n.property === "confidenceScore") {
+          // not checking for empty values because if name exists, confidence score must exist
+          for (const n1 of n.nodes) {
+            for (const n2 of n1.neighbors) {
+              if (n2.property === "value") {
+                if (_.isEmpty(n.nodes)) {
+                  continue;
+                }
+                const num = Number(n2.nodes[0].value);
+                if (num <= 1) {
+                  confidence_score = num;
+                }
+              }
+            }
+          }
+        }
+      }
+      // checking for duplicates
+      if (!seen.has(protein_name)) {
+        returnData.push({
+          name: protein_name,
+          value: confidence_score,
+          parent: parent_protein,
+        });
+        seen.add(protein_name);
+      }
+    }
+    return returnData;
+  }
+  return [];
+}
+
+export function getDiseaseGeneAssoc(data: GraphNodes): ProteinNumData[] {
+  // Disease Gene Associations
+  if (!data) {
+    return [];
+  }
+  const returnData: ProteinNumData[] = [];
+  const seen = new Set();
+  // check for null values
+  if (_.isEmpty(data.nodes) || _.isEmpty(data.nodes[0].neighbors)) {
+    return [];
+  }
+
+  for (const neighbour of data.nodes[0].neighbors) {
+    if (neighbour.property !== "geneID") {
+      continue;
+    }
+    for (const node of neighbour.nodes) {
+      let score = null;
+      let disease = null;
+      // check for null or non-existent property values
+      if (_.isEmpty(node.neighbors)) {
+        continue;
+      }
+      for (const n of node.neighbors) {
+        if (n.property !== "geneID") {
+          continue;
+        }
+        for (const n1 of n.nodes) {
+          for (const n2 of n1.neighbors) {
+            if (n2.property === "diseaseOntologyID") {
+              for (const n3 of n2.nodes) {
+                if (n3.neighbors === undefined) {
+                  continue;
+                }
+                for (const n4 of n3.neighbors) {
+                  if (n4.property !== "commonName") {
+                    continue;
+                  }
+                  // check if the list is empty or not
+                  if (_.isEmpty(n4.nodes) || _.isEmpty(n4.nodes[0].value)) {
+                    continue;
+                  }
+                  disease = n4.nodes[0].value;
+                }
+              }
+            } else if (n2.property === "associationConfidenceInterval") {
+              if (_.isEmpty(n2.nodes)) {
+                continue;
+              }
+              score = Number(n2.nodes[0].value);
+            }
+          }
+          if (!seen.has(disease) && !!score) {
+            returnData.push({ name: disease, value: score });
+          }
+          seen.add(disease);
+        }
+      }
+    }
+    return returnData;
+  }
+  return [];
+}
+
+export function getVarGeneAssoc(data: GraphNodes): ProteinVarType[] {
+  // Variant Gene Associations
+  if (!data) {
+    return [];
+  }
+  const returnData = [] as ProteinVarType[];
+  const seen = new Set();
+  // check for null values
+  if (_.isEmpty(data.nodes) || _.isEmpty(data.nodes[0].neighbors)) {
+    return [];
+  }
+
+  for (const neighbour of data.nodes[0].neighbors) {
+    if (neighbour.property !== "geneID") {
+      continue;
+    }
+    for (const node of neighbour.nodes) {
+      let score = null;
+      let variant = null;
+      let tissue = null;
+      let interval = null;
+      for (const n of node.neighbors) {
+        if (n.property !== "geneSymbol") {
+          continue;
+        }
+
+        for (const n1 of n.nodes) {
+          for (const n2 of n1.neighbors) {
+            if (n2.property === "referenceSNPClusterID") {
+              // check if the list is empty or not
+              if (_.isEmpty(n2.nodes)) {
+                continue;
+              }
+              if (n2.nodes[0].value !== null) {
+                variant = n2.nodes[0].value;
+              }
+            } else if (n2.property === "log2AllelicFoldChange") {
+              // check if the list is empty or not
+              if (_.isEmpty(n2.nodes)) {
+                continue;
+              }
+              if (n2.nodes[0].value !== null) {
+                score = n2.nodes[0].value;
+              }
+            } else if (
+              n2.property === "log2AllelicFoldChangeConfidenceInterval"
+            ) {
+              // check if the list is empty or not
+              if (_.isEmpty(n2.nodes)) {
+                continue;
+              }
+              if (n2.nodes[0].value !== null) {
+                interval = n2.nodes[0].value;
+              }
+            } else if (n2.property === "associatedTissue") {
+              // check if the list is empty or not
+              if (_.isEmpty(n2.nodes)) {
+                continue;
+              }
+              if (n2.nodes[0].value) {
+                tissue = n2.nodes[0].value;
+              }
+            }
+          }
+          if (!seen.has(variant) && !!score) {
+            returnData.push({
+              id: variant,
+              name: tissue,
+              value: score,
+              interval: interval,
+            });
+          }
+          seen.add(variant);
+        }
+      }
+    }
+    return returnData;
+  }
+  return [];
+}
+
+export function getVarTypeAssoc(data: GraphNodes): ProteinNumData[] {
+  // Variant Gene Associations
+  if (!data) {
+    return [];
+  }
+  const returnData: ProteinNumData[] = [];
+  // check for null values
+  if (_.isEmpty(data.nodes) || _.isEmpty(data.nodes[0].neighbors)) {
+    return [];
+  }
+
+  const varResult = {};
+  for (const neighbour of data.nodes[0].neighbors) {
+    if (neighbour.property !== "geneID") {
+      continue;
+    }
+    for (const node of neighbour.nodes) {
+      let variant = null;
+      for (const n of node.neighbors) {
+        if (n.property !== "geneID") {
+          continue;
+        }
+        for (const n1 of n.nodes) {
+          for (const n2 of n1.neighbors) {
+            if (n2.property === "geneticVariantFunctionalCategory") {
+              // check if the list is empty or not
+              if (_.isEmpty(n2.nodes) || _.isEmpty(n2.nodes[0].value)) {
+                continue;
+              }
+              variant = n2.nodes[0].value;
+              if (variant in varResult) {
+                varResult[variant] = varResult[variant] + 1;
+              } else {
+                varResult[variant] = 1;
+              }
+            }
+          }
+        }
+      }
+    }
+    for (const k of VARIANT_CATEGORY) {
+      if (k in varResult) {
+        continue;
+      }
+      varResult[k] = 0;
+    }
+
+    for (const m in varResult) {
+      returnData.push({ name: m, value: varResult[m] });
+    }
+    return returnData;
+  }
+  return [];
+}
+
+export function getVarSigAssoc(data: GraphNodes): ProteinNumData[] {
+  // Variant Gene Associations
+  if (!data) {
+    return [];
+  }
+  const result: ProteinNumData[] = [];
+  // check for null values
+  if (_.isEmpty(data.nodes) || _.isEmpty(data.nodes[0].neighbors)) {
+    return [];
+  }
+
+  const varResult = {};
+  for (const neighbour of data.nodes[0].neighbors) {
+    if (neighbour.property !== "geneID") {
+      continue;
+    }
+    for (const node of neighbour.nodes) {
+      let variant = null;
+      for (const n of node.neighbors) {
+        if (n.property !== "geneID") {
+          continue;
+        }
+        for (const n1 of n.nodes) {
+          for (const n2 of n1.neighbors) {
+            if (n2.property === "clinicalSignificance") {
+              // check if the list is empty or not
+              if (_.isEmpty(n2.nodes) || _.isEmpty(n2.nodes[0].value)) {
+                continue;
+              }
+              variant = n2.nodes[0].value;
+              if (variant in varResult) {
+                varResult[variant] = varResult[variant] + 1;
+              } else {
+                varResult[variant] = 1;
+              }
+            }
+          }
+        }
+      }
+    }
+    for (const k of VARIANT_AFFECTS) {
+      if (k in varResult) {
+        continue;
+      }
+      varResult[k] = 0;
+    }
+
+    for (const m in varResult) {
+      result.push({ name: m, value: varResult[m] });
+    }
+    return result;
+  }
+  return [];
+}
+
+export function getChemicalGeneAssoc(data: GraphNodes): ProteinNumData[] {
+  // Chem Gene Associations
+  if (!data) {
+    return [];
+  }
+  const result: ProteinNumData[] = [];
+  const chemAssoc = {};
+  // check for null values
+  if (_.isEmpty(data.nodes) || _.isEmpty(data.nodes[0].neighbors)) {
+    return [];
+  }
+
+  for (const neighbour of data.nodes[0].neighbors) {
+    if (neighbour.property !== "geneID") {
+      continue;
+    }
+    if (_.isEmpty(neighbour.nodes)) {
+      continue;
+    }
+    for (const node of neighbour.nodes) {
+      let association = null;
+      for (const n of node.neighbors) {
+        if (n.property !== "geneID") {
+          continue;
+        }
+        for (const n1 of n.nodes) {
+          for (const n2 of n1.neighbors) {
+            if (n2.property === "relationshipAssociationType") {
+              // check if the list is empty or not
+              if (_.isEmpty(n2.nodes) || _.isEmpty(n2.nodes[0].value)) {
+                continue;
+              }
+              association = n2.nodes[0].value;
+              if (association in chemAssoc) {
+                chemAssoc[association] = chemAssoc[association] + 1;
+              } else {
+                chemAssoc[association] = 1;
+              }
+            }
+          }
+        }
+      }
+    }
+    for (const k of CHEM_RELATIONS) {
+      if (k in chemAssoc) {
+        continue;
+      }
+      chemAssoc[k] = 0;
+    }
+    for (const m in chemAssoc) {
+      result.push({ name: m, value: chemAssoc[m] });
+    }
+
+    return result;
+  }
+  return [];
+}
