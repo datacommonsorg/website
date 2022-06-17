99fb254a6090359a3ba7b8c4d79888f8e03a07a8
raebo1017 <98435133+raebo1017@users.noreply.github.com>
2022-06-17 14:20:05 -0700

    Ranking chart API: Restrict parent place type and add population filter  (#1639)
    * fix the parent place bug
    
    * fix the parent place bug and add population filter
    
    * fix lint
    
    * to refresh the check, nothing change
    
    * refactor the code

--- server/tests/api_place_test.py
+++ server/tests/api_place_test.py
@@ -638,10 +638,10 @@ class TestApiRankingChartHelper(unittest.TestCase):
 
 class TestApiRankingChart(unittest.TestCase):
 
-    @patch('routes.api.place.get_name')
+    @patch('routes.api.place.get_i18n_name')
     @patch('routes.api.place.get_ranking_chart_configs')
     @patch('routes.api.place.get_place_type')
-    @patch('routes.api.place.get_parent_place')
+    @patch('routes.api.place.parent_places')
     @patch('routes.api.place.dc.get_stat_set_within_place')
     def test_api_ranking_chart_not_earth(
         self,
@@ -652,23 +652,32 @@ class TestApiRankingChart(unittest.TestCase):
         mock_name,
     ):
         test_dcid = 'test_dcid'
-        parent_dcid = "parent_dcid"
+        parent_dcid1 = "parent_dcid1"
+        parent_dcid2 = "parent_dcid2"
         place_type = "place_type"
         geo1 = test_dcid
         geo2 = "dcid2"
+        geo3 = "dcid3"
         place_name1 = "place_name1"
         place_name2 = "place_name2"
+        place_name3 = "place_name3"
+        parent_place_type1 = "County"
+        parent_place_type2 = "Continent"
         sv1 = "sv1"
         sv2 = "sv2"
         sv3 = "sv3"
         sv1_date1 = "2020"
         sv1_date2 = "2022"
-        sv2_date1 = "2022"
+        sv2_date1 = "2020"
         sv2_date2 = "2022"
-        sv1_value1 = 99
-        sv1_value2 = 50
-        sv2_value1 = 10
+        sv1_value1 = 98
+        sv1_value2 = 100
+        sv1_value3 = 12
+        sv2_value1 = 100
         sv2_value2 = 20
+        sv2_value3 = 230
+        count_person_value1 = 1001
+        count_person_value2 = 500
         source1 = "source1"
         source2 = "source2"
         config1 = {
@@ -693,7 +702,15 @@ class TestApiRankingChart(unittest.TestCase):
 
         def parent_place_side_effect(*args):
             if args[0] == test_dcid:
-                return {test_dcid: [{"dcid": parent_dcid}]}
+                return {
+                    test_dcid: [{
+                        "types": [parent_place_type1],
+                        "dcid": parent_dcid1
+                    }, {
+                        "types": [parent_place_type2],
+                        "dcid": parent_dcid2
+                    }]
+                }
             else:
                 return {}
 
@@ -708,8 +725,12 @@ class TestApiRankingChart(unittest.TestCase):
         mock_place_type.side_effect = place_type_side_effect
 
         def name_side_effect(*args):
-            if sorted(args[0]) == sorted([geo1, geo2]):
-                return {geo1: place_name1, geo2: place_name2}
+            if sorted(args[0]) == sorted([geo1, geo2, geo3]):
+                return {
+                    geo1: place_name1,
+                    geo2: place_name2,
+                    geo3: place_name3,
+                }
             else:
                 return {}
 
@@ -728,6 +749,11 @@ class TestApiRankingChart(unittest.TestCase):
                             'date': sv1_date2,
                             'value': sv1_value2,
                             'metaHash': 1
+                        },
+                        geo3: {
+                            'date': sv1_date2,
+                            'value': sv1_value3,
+                            'metaHash': 1
                         }
                     },
                 },
@@ -741,6 +767,30 @@ class TestApiRankingChart(unittest.TestCase):
                         geo2: {
                             'date': sv2_date2,
                             'value': sv2_value2,
+                            'metaHash': 2
+                        },
+                        geo3: {
+                            'date': sv2_date2,
+                            'value': sv2_value3,
+                            'metaHash': 2
+                        }
+                    }
+                },
+                "Count_Person": {
+                    'stat': {
+                        geo1: {
+                            'date': sv2_date1,
+                            'value': count_person_value1,
+                            'metaHash': 2
+                        },
+                        geo2: {
+                            'date': sv2_date2,
+                            'value': count_person_value2,
+                            'metaHash': 2
+                        },
+                        geo3: {
+                            'date': sv2_date1,
+                            'value': count_person_value1,
                             'metaHash': 2
                         }
                     }
@@ -759,8 +809,8 @@ class TestApiRankingChart(unittest.TestCase):
         }
 
         def stats_within_place_side_effect(*args):
-            if args[0] == parent_dcid and args[1] == place_type and sorted(
-                    args[2]) == sorted([sv1, sv2]):
+            if args[0] == parent_dcid1 and args[1] == place_type and sorted(
+                    args[2]) == sorted([sv1, sv2, "Count_Person"]):
                 return stats_within_place
             else:
                 return {}
@@ -773,7 +823,8 @@ class TestApiRankingChart(unittest.TestCase):
         response_data = json.loads(response.data)
         expected_data = {
             sv1: {
-                'date': f'{sv1_date1} – {sv1_date2}',
+                'date':
+                    f'{sv1_date1} – {sv1_date2}',
                 'data': [{
                     "rank": 1,
                     "value": sv1_value1,
@@ -781,22 +832,24 @@ class TestApiRankingChart(unittest.TestCase):
                     "placeName": place_name1
                 }, {
                     "rank": 2,
-                    "value": sv1_value2,
-                    "placeDcid": geo2,
-                    "placeName": place_name2
+                    "value": sv1_value3,
+                    "placeDcid": geo3,
+                    "placeName": place_name3
                 }],
-                'numDataPoints': 2,
-                'exploreUrl': "/ranking/sv1/place_type/parent_dcid?h=test_dcid",
+                'numDataPoints':
+                    2,
+                'exploreUrl':
+                    "/ranking/sv1/place_type/parent_dcid1?h=test_dcid",
                 'sources': [source1]
             },
             sv2: {
                 'date':
-                    sv2_date1,
+                    f'{sv1_date1} – {sv1_date2}',
                 'data': [{
                     "rank": 1,
-                    "value": sv2_value2,
-                    "placeDcid": geo2,
-                    "placeName": place_name2
+                    "value": sv2_value3,
+                    "placeDcid": geo3,
+                    "placeName": place_name3
                 }, {
                     "rank": 2,
                     "value": sv2_value1,
@@ -806,13 +859,13 @@ class TestApiRankingChart(unittest.TestCase):
                 'numDataPoints':
                     2,
                 'exploreUrl':
-                    "/ranking/sv2/place_type/parent_dcid?h=test_dcid&scaling=100&unit=%",
+                    "/ranking/sv2/place_type/parent_dcid1?h=test_dcid&scaling=100&unit=%",
                 'sources': [source2]
             }
         }
         assert response_data == expected_data
 
-    @patch('routes.api.place.get_name')
+    @patch('routes.api.place.get_i18n_name')
     @patch('routes.api.place.get_ranking_chart_configs')
     @patch('routes.api.place.dc.get_stat_set_within_place')
     def test_api_ranking_chart_earth(
@@ -826,8 +879,10 @@ class TestApiRankingChart(unittest.TestCase):
         place_type = "Country"
         geo1 = "dcid1"
         geo2 = "dcid2"
+        geo3 = "dcid3"
         place_name1 = "place_name1"
         place_name2 = "place_name2"
+        place_name3 = "place_name3"
         sv1 = "sv1"
         sv2 = "sv2"
         sv3 = "sv3"
@@ -837,8 +892,12 @@ class TestApiRankingChart(unittest.TestCase):
         sv2_date2 = "2022"
         sv1_value1 = 99
         sv1_value2 = 50
+        sv1_value3 = 30
         sv2_value1 = 10
         sv2_value2 = 20
+        sv2_value3 = 15
+        count_person_value1 = 1001
+        count_person_value2 = 500
         source1 = "source1"
         source2 = "source2"
         config1 = {
@@ -862,8 +921,8 @@ class TestApiRankingChart(unittest.TestCase):
         mock_configs.return_value = [config1, config2]
 
         def name_side_effect(*args):
-            if sorted(args[0]) == sorted([geo1, geo2]):
-                return {geo1: place_name1, geo2: place_name2}
+            if sorted(args[0]) == sorted([geo1, geo2, geo3]):
+                return {geo1: place_name1, geo2: place_name2, geo3: place_name3}
             else:
                 return {}
 
@@ -882,8 +941,13 @@ class TestApiRankingChart(unittest.TestCase):
                             'date': sv1_date2,
                             'value': sv1_value2,
                             'metaHash': 1
-                        }
-                    },
+                        },
+                        geo3: {
+                            'date': sv1_date1,
+                            'value': sv1_value3,
+                            'metaHash': 1
+                        },
+                    }
                 },
                 sv2: {
                     'stat': {
@@ -895,6 +959,30 @@ class TestApiRankingChart(unittest.TestCase):
                         geo2: {
                             'date': sv2_date2,
                             'value': sv2_value2,
+                            'metaHash': 2
+                        },
+                        geo3: {
+                            'date': sv2_date1,
+                            'value': sv2_value3,
+                            'metaHash': 2
+                        },
+                    }
+                },
+                "Count_Person": {
+                    'stat': {
+                        geo1: {
+                            'date': sv2_date1,
+                            'value': count_person_value1,
+                            'metaHash': 2
+                        },
+                        geo2: {
+                            'date': sv2_date2,
+                            'value': count_person_value1,
+                            'metaHash': 2
+                        },
+                        geo3: {
+                            'date': sv2_date1,
+                            'value': count_person_value2,
                             'metaHash': 2
                         }
                     }
@@ -914,7 +1002,7 @@ class TestApiRankingChart(unittest.TestCase):
 
         def stats_within_place_side_effect(*args):
             if args[0] == parent_dcid and args[1] == place_type and sorted(
-                    args[2]) == sorted([sv1, sv2]):
+                    args[2]) == sorted([sv1, sv2, "Count_Person"]):
                 return stats_within_place
             else:
                 return {}
