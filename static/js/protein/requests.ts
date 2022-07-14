47de66a64a7dc63c9a56411a1f1ae170a7301edf
Roger Jin <rogershijin@google.com>
2022-07-14 09:45:26 -0400

    promise chaining

--- static/js/protein/requests.ts
+++ static/js/protein/requests.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from "axios";
-import { V1BaseDatum, V1Response } from "./data_processing_utils";
+import { deduplicateInteractionDCIDs, V1BaseDatum, V1Response, valuesFromResponse } from "./data_processing_utils";
 
 const V1_ENDPOINT_ROOT = "https://autopush.api.datacommons.org/v1"
 // endpoints for protein-protein interaction graph
@@ -20,10 +20,33 @@ function postV1Bulk(endpoint: string, dcids: string[]): Promise<V1Response<V1Bas
     })
 }
 
+/**
+ * Given list of protein DCIDs, fetch their interactors
+ */
 export function fetchInteractionData(proteinDCIDs: string[]): ReturnType<typeof postV1Bulk> {
     return postV1Bulk(PPI_ENDPOINTS.INTERACTORS, proteinDCIDs)
 }
 
+/**
+ * Given list of interaction DCIDs, fetch their confidence scores 
+ */
 export function fetchScoreData(interactionDCIDs: string[]): ReturnType<typeof postV1Bulk> {
     return postV1Bulk(PPI_ENDPOINTS.CONFIDENCE_SCORE, interactionDCIDs)
+}
+
+/**
+ * Given list of protein DCIDs, fetch their interactors and then the confidence scores of the interactions.
+ * Return both the list of interactors and the score request. 
+ */
+export function fetchInteractionsThenScores(proteinDCIDs: string[]): Promise<[string[][], V1Response<V1BaseDatum>]> {
+    return fetchInteractionData(proteinDCIDs).then( (resp) => {
+        const interactionData = valuesFromResponse(resp).map(
+          (interactions) => {
+            return interactions.map(({ dcid }) => dcid);
+          }
+        );
+        return Promise.all([
+            interactionData, fetchScoreData(interactionData.map(deduplicateInteractionDCIDs).flat(1))
+        ])
+    })
 }
