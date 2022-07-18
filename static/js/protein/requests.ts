1031185a956be91f21c2aa914c1ac0917c3ecfc4
Roger Jin <rogershijin@google.com>
2022-07-18 17:34:22 -0400

    update requests

--- static/js/protein/requests.ts
+++ static/js/protein/requests.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import axios from "axios";
 
 import {
@@ -21,9 +37,6 @@ export function fetchInteractionData(
 ): Promise<V1Response<V1BaseDatum>> {
   return axios.post(PPI_ENDPOINTS.INTERACTORS, {
     entities: proteinDCIDs,
-    headers: {
-      "Content-Type": "application/json",
-    },
   });
 }
 
@@ -35,9 +48,6 @@ export function fetchScoreData(
 ): Promise<V1Response<V1BioDatum>> {
   return axios.post(PPI_ENDPOINTS.CONFIDENCE_SCORE, {
     entities: interactionDCIDs,
-    headers: {
-      "Content-Type": "application/json",
-    },
   });
 }
 
