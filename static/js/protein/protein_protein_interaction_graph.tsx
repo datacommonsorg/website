29aa1d18a619af651e0b90769d21dd2b66695688
Roger Jin <rogershijin@google.com>
2022-07-18 17:08:53 -0400

    delete BFS-iter for now

--- static/js/protein/protein_protein_interaction_graph.tsx
+++ static/js/protein/protein_protein_interaction_graph.tsx
@@ -23,21 +23,11 @@ import React from "react";
 
 import { drawProteinInteractionGraph } from "./chart";
 import {
-  getInteractionTarget,
-  getLink,
   getProteinInteractionGraphData,
-  nodeFromID,
-  ppiDCIDFromID,
-  ppiIDFromDCID,
-  scoreDataFromResponse,
 } from "./data_processing_utils";
 import { InteractingProteinType } from "./page";
-import { fetchInteractionsThenScores } from "./requests";
 import {
-  bioDCID,
-  InteractionLink,
   MultiLevelInteractionGraphData,
-  ProteinNode,
 } from "./types";
 
 type InteractionGraphProps = {
@@ -66,7 +56,7 @@ export class ProteinProteinInteractionGraph extends React.Component<
   constructor(props: InteractionGraphProps) {
     super(props);
     this.state = {
-      graphData: getProteinInteractionGraphData(props.interactionDataDepth1),
+      graphData: null,
       depth: DEFAULTS.DEPTH,
       scoreThreshold: DEFAULTS.SCORE_THRESHOLD,
       maxInteractions: DEFAULTS.MAX_INTERACTIONS,
@@ -77,27 +67,14 @@ export class ProteinProteinInteractionGraph extends React.Component<
    * Perform BFS and (re)draw graph
    */
   componentDidUpdate(prevProps: InteractionGraphProps | null): void {
-    // takes two calls to this method to draw the graph.
-    //  1) first call performs BFS and updates the graph
-    //  2) second call draws the graph
-
-    // this branch executes on first call to this method
     if (prevProps !== this.props) {
       const graphData = getProteinInteractionGraphData(
         this.props.interactionDataDepth1
       );
-      // chain two BFS iterations
-      const expansions = this.bfsIter(graphData).then(() =>
-        this.bfsIter(graphData)
-      );
-      // updating state will then trigger the second call to this method
-      expansions.then(() => {
-        this.setState({
-          graphData,
-        });
-      });
+      this.setState({
+        graphData,
+      })
     }
-    // this branch executes on second call to this method
     if (!_.isEmpty(this.state.graphData)) {
       drawProteinInteractionGraph("protein-interaction-graph", {
         nodeData: this.state.graphData.nodeDataNested
@@ -116,165 +93,5 @@ export class ProteinProteinInteractionGraph extends React.Component<
         <div id="protein-interaction-graph"></div>
       </>
     );
-  }
-
-  /**
-   * Given graph data, return promise to perform one iteration of BFS to add one layer to graph data.
-   *
-   * Note this is a bit peculiar in that new links discovered by BFS are split into two types:
-   *  1) expansion links connect a last-layer node and a node not yet in the graph
-   *  2) cross links connect a last-layer node and a node already in the graph
-   *
-   * This method has four stages:
-   *  1) compute expansion links
-   *  2) compute cross links
-   *  3) compute new nodes from expansion links
-   *  4) update graphData:
-   *      - add cross links to the last layer in {graphData.linkDataNested}
-   *      - add expansion links as a new layer of {graphData.linkDataNested}
-   *      - add new nodes as a new layer of {graphData.nodeDataNested}
-   *
-   * Mutates: graphData
-   * Notes:
-   * - We choose not to setState here to avoid unnecessary rerendering when we
-   * chain multiple calls to this method in BFS.
-   */
-  private bfsIter(graphData: MultiLevelInteractionGraphData): Promise<void> {
-    const nodesLastLayer: ProteinNode[] = _.last(graphData.nodeDataNested);
-    const nodeDCIDsLastLayer: bioDCID[] = nodesLastLayer.map((nodeDatum) =>
-      ppiDCIDFromID(nodeDatum.id)
-    );
-
-    const proteinDCIDSet: Set<bioDCID> = new Set(
-      graphData.nodeDataNested
-        .flat(1)
-        .map((node: ProteinNode) => ppiDCIDFromID(node.id))
-    );
-
-    // set of links
-    const linkSet = new Set<[string, string]>();
-    graphData.linkDataNested.flat(1).forEach((linkDatum) => {
-      linkSet.add([linkDatum.sourceID, linkDatum.targetID]);
-      linkSet.add([linkDatum.targetID, linkDatum.sourceID]);
-    });
-
-    // expand graphData by 1 layer.
-    const expandPromise = fetchInteractionsThenScores(nodeDCIDsLastLayer).then(
-      ([interactionData, scoreResp]) => {
-        // Each interaction A_B will induce two keys A_B and B_A, both mapped to the confidence score of A_B.
-        // This object serves two purposes:
-        // 1) O(1) interaction score retrieval
-        // 2) O(1) check for whether a given protein interacts with a depth 1 protein
-        const scoresNewLayer = scoreDataFromResponse(scoreResp);
-
-        // To store links retrieved from response between last layer and new layer
-        const expansionLinks: InteractionLink[] = [];
-        // To store new links retrieved from response between last layer and itself and all previous layers
-        const crossLinks: InteractionLink[] = [];
-
-        // Stage 1: compute expansion links
-        console.assert(interactionData.length == nodesLastLayer.length);
-        for (let i = 0; i < interactionData.length; i++) {
-          const dcidArray = interactionData[i];
-          const parent = nodesLastLayer[i];
-          const filteredSorted = dcidArray
-            // we filter for two conditions:
-            // 1) child protein can't already be in the graph
-            //    (we want to add ${this.state.maxInteractors} *new* children per parent, if they exist)
-            // 2) parent-child interaction confidence score must be above ${this.state.scoreThreshold}
-            .filter((interactionDCID) => {
-              const interactionID = ppiIDFromDCID(interactionDCID);
-              const childDCID = getInteractionTarget(
-                interactionDCID,
-                ppiDCIDFromID(parent.id),
-                true
-              ) as bioDCID;
-              return (
-                !proteinDCIDSet.has(childDCID) &&
-                _.get(
-                  scoresNewLayer,
-                  interactionID,
-                  DEFAULTS.MISSING_SCORE_FILLER
-                ) >= this.state.scoreThreshold
-              );
-            })
-            // Sort in descending order of interaction confidence score.
-
-            // There's a tradeoff here between initial load time and responsiveness to user-input.
-            // Currently we filter before sorting, which makes the initial load cheaper,
-            // But we would have to re-sort on new user-input.
-            // Another option is to pay the one-time up-front cost of sorting the unfiltered interaction list and cache.
-            .sort((interactionDCID1, interactionDCID2) => {
-              const [score1, score2] = [
-                interactionDCID1,
-                interactionDCID2,
-              ].map((dcid) =>
-                _.get(
-                  scoresNewLayer,
-                  ppiIDFromDCID(dcid),
-                  DEFAULTS.MISSING_SCORE_FILLER
-                )
-              );
-              return score1 - score2;
-            })
-            .slice(0, this.state.maxInteractions);
-
-          // add an InteractionLink for each interaction
-          filteredSorted.forEach((interactionDCID) => {
-            const interactionID = ppiIDFromDCID(interactionDCID);
-            const targetID = getInteractionTarget(
-              interactionDCID,
-              ppiDCIDFromID(parent.id)
-            );
-            expansionLinks.push(
-              getLink(parent.id, targetID, scoresNewLayer[interactionID])
-            );
-          });
-        }
-
-        // Stage 2: compute cross links
-        // check if any of the proteins in the layer we just expanded interact with each other or previous proteins
-        nodeDCIDsLastLayer.forEach((nodeDCID1) => {
-          proteinDCIDSet.forEach((nodeDCID2) => {
-            const [nodeID1, nodeID2] = [nodeDCID1, nodeDCID2].map(
-              ppiIDFromDCID
-            );
-
-            if (linkSet.has([nodeID1, nodeID2])) {
-              return;
-            }
-
-            const interactionScore = _.get(
-              scoresNewLayer,
-              `${nodeID1}_${nodeID2}`,
-              DEFAULTS.MISSING_SCORE_FILLER
-            );
-            if (interactionScore > this.state.scoreThreshold) {
-              crossLinks.push(getLink(nodeID1, nodeID2, interactionScore));
-            }
-          });
-        });
-
-        // Stage 3: compute new nodes
-        // deduplicate target IDs to get set of new node IDs
-        const newNodeIDs = new Set(
-          expansionLinks.map(({ targetID }) => targetID)
-        );
-
-        // TODO: nodes are currently colored by the group they're added with, but should be colored by min(parent.depth)
-        // (if node is discovered after 2 iterations of BFS but the center protein is added as a parent during the cross link stage,
-        //  node should be assigned depth 1)
-        const newNodes = Array.from(newNodeIDs).map((id) =>
-          nodeFromID(id, graphData.nodeDataNested.length)
-        );
-
-        // Stage 4: update graphData
-        _.last(graphData.linkDataNested).push(...crossLinks);
-        graphData.nodeDataNested.push(newNodes);
-        graphData.linkDataNested.push(expansionLinks);
-      }
-    );
-
-    return expandPromise;
   }
 }
