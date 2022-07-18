8f82dbf08fe78d2d04044256c7b812ebab818b5a
Roger Jin <rogershijin@google.com>
2022-07-18 11:19:01 -0400

    use https url

--- static/js/protein/requests.ts
+++ static/js/protein/requests.ts
@@ -1,33 +1,13 @@
 import axios from "axios";
+import { deduplicateInteractionDCIDs, getFromResponse } from "./data_processing_utils";
+import { V1Response, V1BaseDatum, bioDCID, V1BioDatum } from "./types";
 
-import {
-  deduplicateInteractionDCIDs,
-  V1BaseDatum,
-  V1Response,
-  valuesFromResponse,
-} from "./data_processing_utils";
-
-const V1_ENDPOINT_ROOT = "/api";
+const V1_ENDPOINT_ROOT = "https://autopush.api.datacommons.org/v1";
 // endpoints for protein-protein interaction graph
 const PPI_ENDPOINTS = {
   INTERACTORS: `${V1_ENDPOINT_ROOT}/bulk/property/in/interactingProtein/values`,
   CONFIDENCE_SCORE: `${V1_ENDPOINT_ROOT}/bulk/property/out/confidenceScore/values`,
 };
-
-/**
- * Given a V1 bulk endpoint and list of dcids, make a post request to the endpoint
- */
-function postV1Bulk(
-  endpoint: string,
-  dcids: string[]
-): Promise<V1Response<V1BaseDatum>> {
-  return axios.post(endpoint, {
-    entities: dcids,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-}
 
 /**
  * Given list of protein DCIDs, fetch their interactors
@@ -37,6 +17,9 @@ export function fetchInteractionData(
 ): Promise<V1Response<V1BaseDatum>> {
   return axios.post(PPI_ENDPOINTS.INTERACTORS, {
     entities: proteinDCIDs,
+    headers:{
+      'Content-Type': "application/json"
+    }
   });
 }
 
@@ -44,10 +27,13 @@ export function fetchInteractionData(
  * Given list of interaction DCIDs, fetch their confidence scores
  */
 export function fetchScoreData(
-  interactionDCIDs: string[]
-): Promise<V1Response<V1BaseDatum>> {
+  interactionDCIDs: bioDCID[]
+): Promise<V1Response<V1BioDatum>> {
   return axios.post(PPI_ENDPOINTS.CONFIDENCE_SCORE, {
     entities: interactionDCIDs,
+    headers:{
+      'Content-Type': "application/json"
+    }
   });
 }
 
@@ -56,14 +42,14 @@ export function fetchScoreData(
  * Return promise for both the list of lists of interactors and the score response.
  */
 export function fetchInteractionsThenScores(
-  proteinDCIDs: string[]
-): Promise<[string[][], V1Response<V1BaseDatum>]> {
+  proteinDCIDs: bioDCID[]
+): Promise<[bioDCID[][], V1Response<V1BioDatum>]> {
   return fetchInteractionData(proteinDCIDs).then((resp) => {
     // list of lists of interactors where the ith list contains the interactors of {proteinDCIDs[i]}
     // each list of interactors is deduplicated such that
     //  1) each element is unique
     //  2) if A_B appears in the list, then B_A does not appear
-    const interactionData: string[][] = valuesFromResponse(resp).map((interactions) => {
+    const interactionData: bioDCID[][] = getFromResponse(resp, "values").map((interactions) => {
       return interactions.map(({ dcid }) => dcid);
     }).map(deduplicateInteractionDCIDs);
 
