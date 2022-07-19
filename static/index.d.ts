b40b0863ce0da8f9be99e4c328e38fca8a06e9a2
raebo1017 <raexu1017@gmail.com>
2022-07-19 15:54:59 -0700

    change file for global variable declaration

--- static/index.d.ts
+++ static/index.d.ts
@@ -1,1 +1,14 @@
 declare module "googlemaps";
+
+// Set new property gtag on window.
+declare module "google-analytics" {
+    global {
+        interface Window {
+            gtag: (
+                event: string,
+                eventName: string,
+                parameter: Record<string, string | string[]>
+            ) => void;
+        }
+    }
+}
