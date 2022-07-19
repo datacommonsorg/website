8d5811a319dda339b146c24affe8c77a48579461
raebo1017 <raexu1017@gmail.com>
2022-07-19 15:56:08 -0700

    fix lint

--- static/index.d.ts
+++ static/index.d.ts
@@ -2,13 +2,13 @@ declare module "googlemaps";
 
 // Set new property gtag on window.
 declare module "google-analytics" {
-    global {
-        interface Window {
-            gtag: (
-                event: string,
-                eventName: string,
-                parameter: Record<string, string | string[]>
-            ) => void;
-        }
+  global {
+    interface Window {
+      gtag: (
+        event: string,
+        eventName: string,
+        parameter: Record<string, string | string[]>
+      ) => void;
     }
+  }
 }
