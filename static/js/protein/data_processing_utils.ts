02ac3757e94f3716e1d622e88bbf5a395833bac7
Roger Jin <rogershijin@google.com>
2022-07-20 11:48:08 -0400

    fix undefined loop bug

--- static/js/protein/data_processing_utils.ts
+++ static/js/protein/data_processing_utils.ts
@@ -794,7 +794,7 @@ export function scoreDataFromResponse(
   const scoreList = scoreValues
     // map scoreValues to IntactMiScore if available, otherwise map to default interaction score
     .map((bioData: V1BioDatum[], i) => {
-      for (const bioDatum of bioData) {
+      for (const bioDatum of bioData || []) {
         if (_.get(bioDatum, "dcid", "").includes(INTERACTION_QUANTITY_DCID)) {
           const score = quantityFromDcid(
             bioDatum.dcid,
