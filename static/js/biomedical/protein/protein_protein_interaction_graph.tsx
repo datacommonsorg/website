cfb3e39fd49faba52a46b980fa10c68df761e63d
rogershijin <rogershijin@gmail.com>
2022-08-04 14:50:26 -0400

    [Protein Browser] Add user input for number of interactors and confidence threshold, update button (#1698)
    * fix escape typing bug
    
    * run linter
    
    * add update button
    
    * add css
    
    * fix button alignment
    
    * button css
    
    * add update logic
    
    * formatting
    
    * remove newline
    
    * add mins/maxes for inputs, at least until table view is added
    
    * sort loading alpha
    
    * fix codacy errors
    
    * fetchdata on update, as suggested by @chejennifer
    
    * run linter
    
    * newline
    
    * delete loading
    
    * add fetchData to componentDidUpdate to handle case discovered by @chejennifer
    
    * linter
    
    * another simplification of componentDidUpdate as discovered by @chejennifer
    
    Co-authored-by: Roger Jin <rogershijin@google.com>

--- static/js/biomedical/protein/protein_protein_interaction_graph.tsx
+++ static/js/biomedical/protein/protein_protein_interaction_graph.tsx
@@ -21,7 +21,7 @@
 import axios from "axios";
 import _ from "lodash";
 import React from "react";
-import { FormGroup, Input, Label } from "reactstrap";
+import { Button, Col, FormGroup, Input, Label, Row } from "reactstrap";
 
 import { drawProteinInteractionGraph } from "./chart";
 import { BioDcid, MultiLevelInteractionGraphData } from "./types";
@@ -43,9 +43,17 @@ interface State {
 
 const CHART_ID = "protein-interaction-graph";
 const DEPTH_INPUT_ID = "ppi-input-depth";
+const NUM_INTERACTORS_INPUT_ID = "ppi-input-num-interactors";
+const SCORE_THRESHOLD_INPUT_ID = "ppi-input-score-threshold";
 
 const MIN_DEPTH = 1;
 const MAX_DEPTH = 3;
+
+const MIN_NUM_INTERACTORS = 0;
+const MAX_NUM_INTERACTORS = 20;
+
+const MIN_SCORE_THRESHOLD = 0;
+const MAX_SCORE_THRESHOLD = 1;
 
 const DEFAULTS = {
   DEPTH: 2,
@@ -73,11 +81,6 @@ export class ProteinProteinInteractionGraph extends React.Component<
   }
 
   componentDidUpdate(prevProps: Props, prevState: State): void {
-    // do nothing on parent rerender or if we've loaded the same graph twice
-    if (_.isEqual(prevProps, this.props) && _.isEqual(prevState, this.state)) {
-      return;
-    }
-    // if graph has updated to something nonempty, redraw it
     if (
       !_.isEmpty(this.state.graphData) &&
       (!_.isEqual(prevState.graphData, this.state.graphData) ||
@@ -93,8 +96,10 @@ export class ProteinProteinInteractionGraph extends React.Component<
       });
       return;
     }
-    // if graph is the same but user input has changed, fetch new data
-    this.fetchData();
+    // refetch if component is updated with new center protein dcid
+    if (!_.isEqual(prevProps, this.props)) {
+      this.fetchData();
+    }
   }
 
   render(): JSX.Element {
@@ -104,19 +109,69 @@ export class ProteinProteinInteractionGraph extends React.Component<
     return (
       <>
         <div id={CHART_ID}></div>
-        <FormGroup>
-          <Label for={DEPTH_INPUT_ID}>Depth</Label>
-          <Input
-            id={DEPTH_INPUT_ID}
-            type="number"
-            min={MIN_DEPTH}
-            max={MAX_DEPTH}
-            onChange={(e) => {
-              this.setState({ depth: Number(e.target.value) });
-            }}
-            value={this.state.depth}
-          />
-        </FormGroup>
+        <Row>
+          <Col md={2}>
+            <FormGroup>
+              <Label for={DEPTH_INPUT_ID}>Depth</Label>
+              <Input
+                id={DEPTH_INPUT_ID}
+                className="ppi-input"
+                type="number"
+                min={MIN_DEPTH}
+                max={MAX_DEPTH}
+                onChange={(e) => {
+                  this.setState({ depth: Number(e.target.value) });
+                }}
+                value={this.state.depth}
+              />
+            </FormGroup>
+          </Col>
+          <Col md={2}>
+            <FormGroup>
+              <Label for={NUM_INTERACTORS_INPUT_ID}>Interactors</Label>
+              <Input
+                id={NUM_INTERACTORS_INPUT_ID}
+                className="ppi-input"
+                type="number"
+                min={MIN_NUM_INTERACTORS}
+                max={MAX_NUM_INTERACTORS}
+                onChange={(e) => {
+                  this.setState({ numInteractions: Number(e.target.value) });
+                }}
+                value={this.state.numInteractions}
+              />
+            </FormGroup>
+          </Col>
+          <Col md={3}>
+            <FormGroup>
+              <Label for={SCORE_THRESHOLD_INPUT_ID}>Confidence Threshold</Label>
+              <Input
+                id={SCORE_THRESHOLD_INPUT_ID}
+                className="ppi-input"
+                type="number"
+                min={MIN_SCORE_THRESHOLD}
+                max={MAX_SCORE_THRESHOLD}
+                step={0.1}
+                onChange={(e) => {
+                  this.setState({ scoreThreshold: Number(e.target.value) });
+                }}
+                value={this.state.scoreThreshold}
+              />
+            </FormGroup>
+          </Col>
+          {/* align button vertically to text fields
+          reference: https://stackoverflow.com/a/48017075 */}
+          <Col className="form-group align-self-end" md={2}>
+            <Button
+              className="ppi-update-button"
+              onClick={() => {
+                this.fetchData();
+              }}
+            >
+              Update
+            </Button>
+          </Col>
+        </Row>
       </>
     );
   }
