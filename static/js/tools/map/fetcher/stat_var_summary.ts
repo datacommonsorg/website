3b4cdc72bbfb5b86e7f32b973bd0d5f6781ab843
Bo Xu <shifucun@users.noreply.github.com>
2022-10-12 18:12:49 -0700

    Add data fetcher for stat var summary and all dates, used by time slider

---
+++ static/js/tools/map/fetcher/stat_var_summary.ts
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Fetch stat var summary data for time slider.
+ */
+
+import axios from "axios";
+import _ from "lodash";
+import { Dispatch, useContext, useEffect } from "react";
+
+import { StatVarSummary } from "../../../shared/types";
+import { ChartDataType, ChartStoreAction } from "../chart_store";
+import { Context } from "../context";
+
+export function useFetchStatVarSummary(
+  dispatch: Dispatch<ChartStoreAction>
+): void {
+  const { statVar, display } = useContext(Context);
+  useEffect(() => {
+    const contextOk = display.value.showTimeSlider && statVar.value.dcid;
+    if (!contextOk) {
+      return;
+    }
+    const action: ChartStoreAction = {
+      type: ChartDataType.STAT_VAR_SUMMARY,
+      error: null,
+      context: {
+        statVar: {
+          dcid: statVar.value.dcid,
+        },
+      },
+    };
+    axios
+      .get("/api/stats/stat-var-summary", {
+        params: {
+          statVars: [statVar.value.dcid],
+        },
+      })
+      .then((resp) => {
+        if (_.isEmpty(resp.data)) {
+          action.error = "error fetching stat var summary data";
+        } else {
+          action.payload = resp.data as StatVarSummary;
+        }
+        dispatch(action);
+        console.log("stat var summary data dispatched");
+      })
+      .catch(() => {
+        action.error = "error fetching stat var summary data";
+        dispatch(action);
+      });
+  }, [statVar.value.dcid, display.value.showTimeSlider, dispatch]);
+}
