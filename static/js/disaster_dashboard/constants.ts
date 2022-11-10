693f30e8e85266262a06c3b6ba3654fe0d949686
Jennifer Chen <chejennifer@google.com>
2022-11-10 19:10:22 +0000

    add data fetcher functions

---
+++ static/js/disaster_dashboard/constants.ts
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Constants used across components in Disaster Dashboard
+ */
+
+import { DisasterType } from "./types";
+
+// id of the spinner screen for the map component
+export const CONTENT_SPINNER_ID = "content-spinner-screen";
+
+// Map of Disaster type to event types encompassed by the disaster type
+export const DISASTER_EVENT_TYPES = {
+  [DisasterType.EARTHQUAKE]: ["EarthquakeEvent"],
+  [DisasterType.FIRE]: ["WildlandFireEvent", "WildfireEvent"],
+  [DisasterType.STORM]: [
+    "CycloneEvent",
+    "HurricaneTyphoonEvent",
+    "HurricaneEvent",
+    "TornadoEvent",
+  ],
+  [DisasterType.FLOOD]: ["FloodEvent"],
+  [DisasterType.DROUGHT]: ["DroughtEvent"],
+};
+
+export const DISASTER_EVENT_INTENSITIES = {
+  [DisasterType.EARTHQUAKE]: ["magnitude"],
+  [DisasterType.DROUGHT]: ["directDeaths", "directInjuries"],
+};
