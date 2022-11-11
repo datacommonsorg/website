00cfe27de4c388a2462cd926c263e84db4571f31
Carolyn Au <cyin@google.com>
2022-11-11 15:11:52 -0800

    modularize examples for download tool

--- server/routes/tools.py
+++ server/routes/tools.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import flask
-import os
+import json
 
 from flask import current_app, request
 
@@ -23,6 +23,19 @@ bp = flask.Blueprint("tools", __name__, url_prefix='/tools')
 # ../../static/js/tools/map/util.ts
 ALLOW_LEAFLET_FLAG = "leaflet"
 
+# List of DCIDs displayed in the info page for download tool
+# NOTE: EXACTLY 2 EXAMPLES REQUIRED.
+_DOWNLOAD_INFO_DCIDS = [
+    { 'name': 'Alabama', 'dcid': 'geoId/01' },
+    { 'name': 'Alaska', 'dcid': 'geoId/02' },
+]
+
+# List of DCIDs displayed in the IITM DC version of info page for download tool
+# NOTE: EXACTLY 2 EXAMPLES REQUIRED.
+_DOWNLOAD_INFO_DCIDS_IITM = [
+    { 'name': 'Tamil Nadu', 'dcid': 'wikidataId/Q1445' },
+    { 'name': 'Delhi', 'dcid': 'wikidataId/Q1353' },
+]
 
 @bp.route('/timeline')
 def timeline():
@@ -63,5 +76,11 @@ def stat_var():
 
 @bp.route('/download')
 def download():
+    env_name = current_app.config.get('ENV_NAME', None)
+    info_places = _DOWNLOAD_INFO_DCIDS
+    if env_name == 'IITM':
+        info_places = _DOWNLOAD_INFO_DCIDS_IITM
     return flask.render_template(
-        'tools/download.html', maps_api_key=current_app.config['MAPS_API_KEY'])
+        'tools/download.html',
+        info_places=json.dumps(info_places),
+        maps_api_key=current_app.config['MAPS_API_KEY'])
