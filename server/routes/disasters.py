7c3825bad7509c968df14257ab82dbde3416d68c
Julia Wu <wujulia.jw@gmail.com>
2023-01-04 15:30:58 -0800

    Set default dcid to ''

--- server/routes/disasters.py
+++ server/routes/disasters.py
@@ -93,7 +93,7 @@ def get_properties(dcid):
 
 @bp.route('/event')
 @bp.route('/event/<path:dcid>')
-def event_node(dcid=''):
+def event_node(dcid=DEFAULT_EVENT_DCID):
   if not os.environ.get('FLASK_ENV') in [
       'autopush', 'local', 'dev', 'stanford', 'local-stanford',
       'stanford-staging'
