b3b28f9b03819e019e18f54b8c63bf60e90d398f
Carolyn Au <cyin@google.com>
2023-01-04 16:25:20 -0800

    always reload config locally

--- server/routes/disasters.py
+++ server/routes/disasters.py
@@ -20,6 +20,8 @@ import os
 import flask
 import routes.api.place as place_api
 from google.protobuf.json_format import MessageToJson
+import os
+import lib.util
 
 DEFAULT_PLACE_DCID = "Earth"
 DEFAULT_PLACE_TYPE = "Planet"
@@ -30,41 +32,47 @@ bp = Blueprint("disasters", __name__, url_prefix='/disasters')
 
 @bp.route('/v0')
 def disaster_dashboard_v0():
-  european_countries = json.dumps(
-      dc.get_places_in(["europe"], "Country").get("europe", []))
-  return flask.render_template('custom_dc/stanford/disaster_dashboard_v0.html',
-                               european_countries=european_countries)
+    european_countries = json.dumps(
+        dc.get_places_in(["europe"], "Country").get("europe", []))
+    return flask.render_template(
+        'custom_dc/stanford/disaster_dashboard_v0.html',
+        european_countries=european_countries)
 
 
 @bp.route('/')
 @bp.route('/<path:place_dcid>', strict_slashes=False)
 def disaster_dashboard(place_dcid=DEFAULT_PLACE_DCID):
-  all_configs = current_app.config['DISASTER_DASHBOARD_CONFIGS']
-  if len(all_configs) < 1:
-    return "Error: no config installed"
+    all_configs = current_app.config['DISASTER_DASHBOARD_CONFIGS']
+    if current_app.config['LOCAL']:
+      # Reload configs for faster local iteration.
+      # TODO: Delete this when we are close to launch
+      all_configs = lib.util.get_disaster_dashboard_configs()
 
-  # Find the config for the topic & place.
-  dashboard_config = None
-  default_config = None
-  for config in all_configs:
-    if place_dcid in config.metadata.place_dcid:
-      dashboard_config = config
-      break
-    if DEFAULT_PLACE_DCID in config.metadata.place_dcid:
-      # TODO: Add a better way to find the default config.
-      default_config = config
-  if not dashboard_config:
-    # Use the default config instead
-    dashboard_config = default_config
+    if len(all_configs) < 1:
+        return "Error: no config installed"
 
-  place_type = DEFAULT_PLACE_TYPE
-  if place_dcid != DEFAULT_PLACE_DCID:
-    place_type = place_api.get_place_type(place_dcid)
-  place_name = place_api.get_i18n_name([place_dcid
-                                       ]).get(place_dcid, escape(place_dcid))
+    # Find the config for the topic & place.
+    dashboard_config = None
+    default_config = None
+    for config in all_configs:
+        if place_dcid in config.metadata.place_dcid:
+            dashboard_config = config
+            break
+        if DEFAULT_PLACE_DCID in config.metadata.place_dcid:
+            # TODO: Add a better way to find the default config.
+            default_config = config
+    if not dashboard_config:
+        # Use the default config instead
+        dashboard_config = default_config
 
-  return flask.render_template('custom_dc/stanford/disaster_dashboard.html',
-                               place_type=place_type,
-                               place_name=place_name,
-                               place_dcid=place_dcid,
-                               config=MessageToJson(dashboard_config))
+    place_type = DEFAULT_PLACE_TYPE
+    if place_dcid != DEFAULT_PLACE_DCID:
+        place_type = place_api.get_place_type(place_dcid)
+    place_name = place_api.get_i18n_name([place_dcid
+                                         ]).get(place_dcid, escape(place_dcid))
+
+    return flask.render_template('custom_dc/stanford/disaster_dashboard.html',
+                                 place_type=place_type,
+                                 place_name=place_name,
+                                 place_dcid=place_dcid,
+                                 config=MessageToJson(dashboard_config))
