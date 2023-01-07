6d34aeee5f25a292217b92e0d085bc0ce133dc8c
Jehangir Amjad <jehangiramjad@gmail.com>
2023-01-07 14:07:08 -0800

    lint fixes

--- server/lib/nl_detection.py
+++ server/lib/nl_detection.py
@@ -6,62 +6,60 @@ from typing import Any, Dict, List
 
 @dataclass
 class Place:
-    """Place attributes."""
-    dcid: str
-    name: str
+  """Place attributes."""
+  dcid: str
+  name: str
 
 
 @dataclass
 class SV:
-    """Statistical Variable attributes."""
-    dcid: str
-    name: str
-    place_type: str
+  """Statistical Variable attributes."""
+  dcid: str
+  name: str
+  place_type: str
 
 
 @dataclass
 class PlaceDetection:
-    """Various attributes of place detection."""
-    query: str
-    query_without_place: str
-    places_found: List[str]
-    main_place: Place
-    
+  """Various attributes of place detection."""
+  query: str
+  query_without_place: str
+  places_found: List[str]
+  main_place: Place
+
 
 @dataclass
 class SVDetection:
-    """Various attributes of SV detection."""
-    query: str
-    sv2scores: Dict[str, float]
-    
-    highlight_svs: List[str]
-    relevant_svs: List[str]
+  """Various attributes of SV detection."""
+  query: str
+  sv2scores: Dict[str, float]
+
+  highlight_svs: List[str]
+  relevant_svs: List[str]
 
 
 class ClassificationAttributes(ABC):
-    """Abctract class to hold classification attributes."""
-    pass
+  """Abctract class to hold classification attributes."""
+  pass
 
 
 @dataclass
 class SimpleClassificationAttributes(ClassificationAttributes):
-    """Simple classification attributes."""
-    sv_detected: SV
+  """Simple classification attributes."""
+  sv_detected: SV
 
 
 @dataclass
 class Classifier:
-    """Classifier."""
-    type: str
-    attributes: List[ClassificationAttributes]
+  """Classifier."""
+  type: str
+  attributes: List[ClassificationAttributes]
 
 
 @dataclass
 class Detection:
-    """Detection attributes."""
-    original_query: str
-    places_detected: PlaceDetection
-    svs_detected: SVDetection
-    classifications: List[Classifier]
-
-    
+  """Detection attributes."""
+  original_query: str
+  places_detected: PlaceDetection
+  svs_detected: SVDetection
+  classifications: List[Classifier]
