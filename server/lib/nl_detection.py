b6405e4fe54b404507f981fdf7924f60e491f8cb
Jehangir Amjad <jehangiramjad@gmail.com>
2023-01-07 14:33:12 -0800

    edits based on comments

--- server/lib/nl_detection.py
+++ server/lib/nl_detection.py
@@ -1,3 +1,17 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # The following classes are used for the NL Detection.
 from abc import ABC
 from dataclasses import dataclass
@@ -9,6 +23,7 @@ class Place:
   """Place attributes."""
   dcid: str
   name: str
+  place_type: str
 
 
 @dataclass
@@ -16,14 +31,14 @@ class SV:
   """Statistical Variable attributes."""
   dcid: str
   name: str
-  place_type: str
 
 
 @dataclass
 class PlaceDetection:
   """Various attributes of place detection."""
-  query: str
-  query_without_place: str
+  query_original: str
+  query_place_substr: str
+  query_without_place_substr: str
   places_found: List[str]
   main_place: Place
 
@@ -34,9 +49,6 @@ class SVDetection:
   query: str
   sv2scores: Dict[str, float]
 
-  highlight_svs: List[str]
-  relevant_svs: List[str]
-
 
 class ClassificationAttributes(ABC):
   """Abctract class to hold classification attributes."""
@@ -46,6 +58,7 @@ class ClassificationAttributes(ABC):
 @dataclass
 class SimpleClassificationAttributes(ClassificationAttributes):
   """Simple classification attributes."""
+  place_detected: Place
   sv_detected: SV
 
 
