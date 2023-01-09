84f4a96267efed082d27a1ab5f6b333336d347d6
Alex Chen <alexyfchen@google.com>
2023-01-08 18:14:58 -0800

    Add place type to debug string

--- server/routes/nl_interface.py
+++ server/routes/nl_interface.py
@@ -412,7 +412,8 @@ def _result_with_debug_info(data_dict, status, embeddings_build,
     elif classification.type == ClassificationType.TEMPORAL:
       temporal_classification = str(classification.type)
     elif classification.type == ClassificationType.CONTAINED_IN:
-      contained_in_classification = str(classification.type)
+      contained_in_classification = \
+          str(classification.attributes.contained_in_place_type)
 
   debug_info = {
       "debug": {
