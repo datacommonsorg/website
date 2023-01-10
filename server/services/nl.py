693c845893093eb5f80b8a030f80c1940e34d2f1
Jehangir Amjad <1021616+jehangiramjad@users.noreply.github.com>
2023-01-09 21:53:33 -0800

    [NL Interface] Query interpretation fine tuning (disable clustering for multi SV detection; defaulting to Earth place under some circumstances) (#1988)

--- server/services/nl.py
+++ server/services/nl.py
@@ -295,6 +295,10 @@ class Model:
         contained_in_place_type = place_enum
         break
 
+    # If place_type is just PLACE, that means no actual type was detected.
+    if contained_in_place_type == ContainedInPlaceType.PLACE:
+      return None
+
     # TODO: need to detect the type of place for this contained in.
     attributes = ContainedInClassificationAttributes(
         contained_in_place_type=contained_in_place_type)
