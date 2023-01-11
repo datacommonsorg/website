3495e155e575ad80850e3a37d98ce2c3a03b8c64
Bo Xu <shifucun@users.noreply.github.com>
2023-01-10 17:37:37 -0800

    Pass along context in data_spec

--- server/lib/nl_page_config.py
+++ server/lib/nl_page_config.py
@@ -154,8 +154,7 @@ def _multiple_place_bar_block(places: List[Place], svs: List[str], sv2name):
   return block, stat_var_spec_map
 
 
-def build_page_config(detection: Detection, data_spec: DataSpec,
-                      context_history):
+def build_page_config(detection: Detection, data_spec: DataSpec):
 
   main_place_spec = data_spec.main_place_spec
   contained_place_spec = data_spec.contained_place_spec
@@ -173,22 +172,10 @@ def build_page_config(detection: Detection, data_spec: DataSpec,
   classificationType = classifier.type
 
   # No stat vars found
-  primary_sv = data_spec.primary_sv
-  primary_sv_siblings = data_spec.primary_sv_siblings
-  use_context_sv = False
-  context_place = None
-  if not primary_sv:
-    for context in reversed(context_history):
-      if context and context['debug'] and context['debug']['primary_sv']:
-        primary_sv = context['debug']['primary_sv']
-        primary_sv_siblings = context['debug']['primary_sv_siblings']
-        use_context_sv = True
-        context_place = Place(dcid=context['place_dcid'],
-                              name=context['place_name'],
-                              place_type=context['place_type'])
-        break
 
-  if not primary_sv:
+  context_place = data_spec.context_place
+
+  if not data_spec.primary_sv:
     if main_place_spec.place:
       block = category.blocks.add()
       block.title = main_place_spec.name
@@ -197,15 +184,18 @@ def build_page_config(detection: Detection, data_spec: DataSpec,
       tile.type = subject_page_pb2.Tile.TileType.PLACE_OVERVIEW
     return page_config
 
-  all_svs = [primary_sv] + primary_sv_siblings
+  all_svs = [data_spec.primary_sv] + data_spec.primary_sv_siblings
   if contained_place_spec.svs:
     all_svs += contained_place_spec.svs
   sv2name = get_sv_name(all_svs)
 
+  primary_sv = data_spec.primary_sv
+  primary_sv_siblings = data_spec.primary_sv_siblings
+
   if classificationType in [
       ClassificationType.SIMPLE, ClassificationType.OTHER
   ]:
-    if use_context_sv:
+    if data_spec.use_context_sv:
       # Only place is asked, draw comparison between two places
       block, stat_var_spec_map = _multiple_place_bar_block(
           [context_place, detection.places_detected.main_place], [primary_sv],
