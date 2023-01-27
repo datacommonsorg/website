4ab14006f70ac6ec3b321876d6923e9a8ace1604
Jehangir Amjad <1021616+jehangiramjad@users.noreply.github.com>
2023-01-27 11:46:49 -0800

    [NL Model Server] Embeddings + SV Search (#2108)
    In this PR, we have the following:
    
    1. Port over the embeddings loading
    2. SV search over the embeddings
    
    A major TODO is to add unit tests. For now, this is just a port from the
    previous server/services/nl.py

--- nl_server/__init__.py
+++ nl_server/__init__.py
@@ -14,10 +14,43 @@
 
 from flask import Flask
 
+import logging
+import os
 import routes
+
+nl_embeddings_cache_key = 'nl_embeddings'
+nl_embeddings_cache_path = '~/.datacommons/'
+nl_embeddings_cache_expire = 3600 * 24  # Cache for 1 day
 
 
 def create_app():
   app = Flask(__name__)
   app.register_blueprint(routes.bp)
+
+  def load_model():
+    # In local dev, cache the model in disk so each hot reload won't download
+    # the model again.
+    if os.environ.get('FLASK_ENV') == 'local':
+      from diskcache import Cache
+      cache = Cache(nl_embeddings_cache_path)
+      cache.expire()
+      nl_embeddings = cache.get(nl_embeddings_cache_key)
+      app.config['NL_EMBEDDINGS'] = nl_embeddings
+      if nl_embeddings:
+        logging.info("Use cached model in: " + cache.directory)
+        return
+
+    # Some specific imports for the NL Interface.
+    from lib.nl_embeddings import Embeddings
+    nl_embeddings = Embeddings()
+    app.config['NL_EMBEDDINGS'] = nl_embeddings
+    if os.environ.get('FLASK_ENV') == 'local':
+      with Cache(cache.directory) as reference:
+        reference.set(nl_embeddings_cache_key,
+                      nl_embeddings,
+                      expire=nl_embeddings_cache_expire)
+
+  # Initialize the NL module.
+  load_model()
+
   return app
