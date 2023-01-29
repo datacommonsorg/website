2b94b77a52a06b5799b8b20c3f5cef933f357eee
Bo Xu <shifucun@users.noreply.github.com>
2023-01-29 11:22:52 -0800

    Add version config for embeddings file (#2117)
    Instead of hard coding the embedding as a constant, it is set in a
    config file now.
    
    Also performed some code clean up, including logging, autopush handling

--- nl_server/main.py
+++ nl_server/main.py
@@ -14,13 +14,14 @@
 """Main entry module for NL app."""
 
 import logging
-import threading
 
 from __init__ import create_app
-import pubsub
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(lineno)d : %(message)s')
+logging.basicConfig(
+    level=logging.INFO,
+    format="\u3010%(asctime)s\u3011\u3010%(levelname)s\u3011\u3010 %(filename)s:%(lineno)s \u3011 %(message)s ",
+    datefmt="%H:%M:%S",
+    )
 
 app = create_app()
 
@@ -29,9 +30,6 @@ app = create_app()
 def healthz():
   return ""
 
-
-thread = threading.Thread(target=pubsub.subscribe)
-thread.start()
 
 if __name__ == '__main__':
   # This is used when running locally only. When deploying to GKE,
