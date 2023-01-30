ef4dc128b056ee6eaf0d45c33fb3e99d1d7f4830
Carolyn Au <cyin@google.com>
2023-01-30 10:23:23 -0800

    update param handlign for json

--- server/routes/api/disaster_api.py
+++ server/routes/api/disaster_api.py
@@ -130,8 +130,8 @@ def json_event_data():
     return "error: must provide a place field", 400
   filter_prop = request.args.get('filterProp', '')
   filter_unit = request.args.get('filterUnit', '')
-  filter_upper_limit = float(request.args.get('filterUpperLimit', '0'))
-  filter_lower_limit = float(request.args.get('filterLowerLimit', '0'))
+  filter_upper_limit = float(request.args.get('filterUpperLimit', float("inf")))
+  filter_lower_limit = float(request.args.get('filterLowerLimit', -float("inf")))
   event_points = []
   disaster_data = current_app.config['DISASTER_DASHBOARD_DATA']
   for event in disaster_data.get(event_type, {}).get(date, []):
