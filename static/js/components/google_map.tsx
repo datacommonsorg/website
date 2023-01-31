0956d481450e3392f730a9231c19a5be76116f5b
Julia Wu <wujulia.jw@gmail.com>
2023-01-31 15:22:35 -0800

    respond to reviewer comments

--- static/js/components/google_map.tsx
+++ static/js/components/google_map.tsx
@@ -24,36 +24,10 @@ import React from "react";
 
 const DEFAULT_MAP_ZOOM = 4;
 
-// number of digits after decimal point in LatLong/<lat>_<long> dcids
-const LAT_LONG_PRECISION = 5;
-
-interface GoogleMapPropType {
-  /**
-   * DCID of the place/event to show a map for.
-   */
-  dcid: string;
-}
-
-interface GoogleMapStateType {
-  /**
-   * Stores responses from api calls.
-   */
-  markerLocation: GoogleMapCoordinates;
-  mapInfo: MapInfoResponse;
-}
-
-interface GoogleMapCoordinates {
-  /**
-   * Lat/Long coordinates, stored in a form the Google maps API expects.
-   */
-  lat: number;
-  lng: number;
-}
-
+/**
+ * Response format of /api/place/mapinfo/.
+ */
 interface MapInfoResponse {
-  /**
-   * Response format of /api/place/mapinfo/.
-   */
   up: number;
   down: number;
   left: number;
@@ -62,25 +36,25 @@ interface MapInfoResponse {
 }
 
 /**
- * Converts a DCID of the form latLong/<latitude>_<longitude> to
- * lat/long coordinates used by the google maps API.
- * @param dcid latLong DCID to convert
- * @returns a {lat: latitude, lng: longitude} object for the google maps API
+ * Lat/Long coordinates, stored in a form the Google maps API expects.
  */
-function convertLatLongDcid(dcid: string): GoogleMapCoordinates {
-  if (dcid.startsWith("latLong/") && dcid.includes("_")) {
-    const coordinateStrings = dcid.slice("latLong/".length).split("_");
-    const coords = coordinateStrings.map((coordString) =>
-      // Add decimal point to parsed latitude and longitude numbers
-      parseFloat(
-        coordString.slice(0, -LAT_LONG_PRECISION) +
-          "." +
-          coordString.slice(-LAT_LONG_PRECISION)
-      )
-    );
-    return { lat: coords[0], lng: coords[1] };
-  }
-  return null;
+interface GoogleMapCoordinates {
+  lat: number;
+  lng: number;
+}
+
+interface GoogleMapPropType {
+  // DCID of the place/event to show a map for.
+  dcid: string;
+}
+
+interface GoogleMapStateType {
+  // parsed response of propvals api
+  markerLocation: GoogleMapCoordinates;
+  // parsed response of mapinfo api
+  mapInfo: MapInfoResponse;
+  // Whether there is data for map to render
+  shouldShowMap: boolean;
 }
 
 /**
@@ -88,9 +62,6 @@ function convertLatLongDcid(dcid: string): GoogleMapCoordinates {
  * @param container the element to render the map in
  */
 function initMap(container: HTMLDivElement): google.maps.Map {
-  // set height for map to be visible
-  container.style.height = "100%";
-  container.style.minHeight = "160px";
   const mapOptions = {
     mapTypeControl: false,
     draggable: true,
@@ -106,15 +77,13 @@ function initMap(container: HTMLDivElement): google.maps.Map {
 
 /**
  * Draw polygons on map given KML coordinates for each polygon.
- * @param state current component state with KML Coordinates stored in it
+ * @param mapInfo response from mapinfo api with KML Coordinates stored in it
  * @param map map to draw polygon in
  */
 function drawKmlCoordinates(
-  state: GoogleMapStateType,
+  mapInfo: MapInfoResponse,
   map: google.maps.Map
 ): void {
-  const mapInfo = state.mapInfo;
-
   // Adjust map bounds
   const sw = new google.maps.LatLng(mapInfo.down, mapInfo.left);
   const ne = new google.maps.LatLng(mapInfo.up, mapInfo.right);
@@ -172,11 +141,18 @@ export class GoogleMap extends React.Component<
         right: null,
         coordinateSequenceSet: [],
       },
+      shouldShowMap: false,
     };
   }
 
   render(): JSX.Element {
-    return <div className="map-container" ref={this.div}></div>;
+    return (
+      <>
+        {this.state.shouldShowMap && (
+          <div className="map-container" ref={this.div}></div>
+        )}
+      </>
+    );
   }
 
   componentDidMount(): void {
@@ -184,74 +160,67 @@ export class GoogleMap extends React.Component<
   }
 
   componentDidUpdate(): void {
-    // Initialize map
-    const map = initMap(this.div.current);
+    if (this.state.shouldShowMap) {
+      // initialize Map
+      const map = initMap(this.div.current);
 
-    // plot KML Coordinates
-    if (Object.values(this.state.mapInfo).every((val) => val !== null)) {
-      drawKmlCoordinates(this.state, map);
-    }
-    // plot marker
-    else if (this.state.markerLocation.lat && this.state.markerLocation.lng) {
-      drawMarker(this.state.markerLocation, map);
+      // plot KML Coordinates
+      if (Object.values(this.state.mapInfo).every((val) => val !== null)) {
+        drawKmlCoordinates(this.state.mapInfo, map);
+      }
+      // plot marker
+      else if (this.state.markerLocation.lat && this.state.markerLocation.lng) {
+        drawMarker(this.state.markerLocation, map);
+      }
     }
   }
 
   private fetchData(): void {
-    // Skip API calls if we can get lat/long from DCID itself
-    if (
-      this.props.dcid.startsWith("latLong/") &&
-      this.props.dcid.includes("_")
-    ) {
-      this.setState({
-        markerLocation: convertLatLongDcid(this.props.dcid),
-      });
-    } else {
-      // Get KML Coordinates for polygons
-      const polygonPromise = axios
-        .get(`/api/place/mapinfo/${this.props.dcid}`)
-        .then((resp) => resp.data);
+    // Get KML Coordinates for polygons
+    const polygonPromise = axios
+      .get(`/api/place/mapinfo/${this.props.dcid}`)
+      .then((resp) => resp.data)
+      .catch((error) => console.log(error));
 
-      // Get lat/long from properties
-      const latitudePromise = axios
-        .get(`/api/browser/propvals/latitude/${this.props.dcid}`)
-        .then((resp) => resp.data);
-      const longitudePromise = axios
-        .get(`/api/browser/propvals/longitude/${this.props.dcid}`)
-        .then((resp) => resp.data);
+    // Get lat/long from properties
+    const latitudePromise = axios
+      .get(`/api/browser/propvals/latitude/${this.props.dcid}`)
+      .then((resp) => resp.data)
+      .catch((error) => console.log(error));
+    const longitudePromise = axios
+      .get(`/api/browser/propvals/longitude/${this.props.dcid}`)
+      .then((resp) => resp.data)
+      .catch((error) => console.log(error));
 
-      Promise.all([polygonPromise, latitudePromise, longitudePromise]).then(
-        ([mapInfo, latitudes, longitudes]) => {
-          // Update state with mapInfo
-          if (mapInfo && Object.keys(mapInfo).length > 0) {
-            this.setState({
-              mapInfo: mapInfo,
-            });
-          }
-
-          // Update state with lat/long if both are present
-          if (
-            latitudes.values &&
-            longitudes.values &&
-            !_.isEmpty(latitudes.values.out) &&
-            !_.isEmpty(longitudes.values.out)
-          ) {
-            const latValues = latitudes.values.out.map(
-              (latValue) => latValue.value
-            );
-            const longValues = longitudes.values.out.map(
-              (longValue) => longValue.value
-            );
-            //TODO (juliawu): Update logic to use highest precision lat/long if
-            //                multiple values are provided
-            const coordinates = {
-              lat: parseFloat(latValues[0]),
-              lng: parseFloat(longValues[0]),
-            };
-            this.setState({ markerLocation: coordinates });
-          }
+    Promise.all([polygonPromise, latitudePromise, longitudePromise])
+      .then(([mapInfo, latitudes, longitudes]) => {
+        // Update state with mapInfo
+        if (!_.isEmpty(mapInfo)) {
+          this.setState({
+            mapInfo: mapInfo,
+            shouldShowMap: true,
+          });
         }
-      );
-    }
+
+        // Update state with lat/long if both are present
+        if (
+          latitudes.values &&
+          longitudes.values &&
+          !_.isEmpty(latitudes.values.out) &&
+          !_.isEmpty(longitudes.values.out)
+        ) {
+          //TODO (juliawu): Update logic to use highest precision lat/long if
+          //                multiple values are provided
+          const coordinates = {
+            lat: parseFloat(latitudes.values.out[0].value),
+            lng: parseFloat(longitudes.values.out[0].value),
+          };
+          this.setState({
+            markerLocation: coordinates,
+            shouldShowMap: true,
+          });
+        }
+      })
+      .catch((error) => console.log(error));
   }
 }
