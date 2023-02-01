078b20802f0f93d9c9bfc856ae9cd312c3dc890f
Bo Xu <shifucun@users.noreply.github.com>
2023-01-31 17:54:28 -0800

    try different version

--- run_test.sh
+++ run_test.sh
@@ -64,7 +64,7 @@ function run_lint_fix {
     pip3 install isort -q
   fi
   yapf -r -i -p --style='{based_on_style: google, indent_width: 2}' server/ nl_server/ tools/ -e=*pb2.py
-  isort server/ nl_server/ tools/  -sg *pb2.py  --profile google
+  isort server/ nl_server/ tools/  --skip-glob *pb2.py  --profile google
   deactivate
 }
 
@@ -105,7 +105,7 @@ function run_py_test {
     exit 1
   fi
 
-  if ! isort server/ nl_server/ tools/ -c -sg *pb2.py --profile google; then
+  if ! isort server/ nl_server/ tools/ -c --skip-glob *pb2.py --profile google; then
     echo "Fix Python import sort orders by running ./run_test.sh -f"
     exit 1
   fi
