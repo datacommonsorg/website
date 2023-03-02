b5a47e04988b35a1a2a2b6b021da37cd88ae29a6
Alex Chen <alexyfchen@google.com>
2023-03-01 19:53:01 -0800

    PR comment

--- deploy/terraform-datacommons-website/modules/gke/configure_cluster.sh
+++ deploy/terraform-datacommons-website/modules/gke/configure_cluster.sh
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Region is like "us-central1", zone is like "us-central1-a"
 if [[ $LOCATION =~ ^[a-z]+-[a-z0-9]+$ ]]; then
   REGION=$LOCATION
 else
