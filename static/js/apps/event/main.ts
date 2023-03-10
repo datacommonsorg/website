e49389f8af4ab0fd73e8dceef0e30d342e1fb9a4
Carolyn Au <cyin@google.com>
2023-03-09 16:15:58 -0800

    header updates

--- static/js/apps/event/main.ts
+++ static/js/apps/event/main.ts
@@ -40,7 +40,9 @@ function renderPage(): void {
   const dcid = document.getElementById("node").dataset.dcid;
   const nodeName = document.getElementById("node").dataset.nn;
   const properties = JSON.parse(document.getElementById("node").dataset.pv);
-  console.log(properties);
+  const provenance = JSON.parse(
+    document.getElementById("node").dataset.provenance
+  );
 
   // Event place
   const placeDcid = document.getElementById("place").dataset.dcid;
@@ -60,6 +62,7 @@ function renderPage(): void {
       dcid: dcid,
       name: nodeName,
       properties: properties,
+      provenance: provenance,
       place: place,
       subjectConfig,
       parentPlaces: getFilteredParentPlaces(parentPlaces, place),
