642cb747248ba3d36cebb7ac6037754a5c932fdc
Suhana Bedi <suhanabedi@google.com>
2023-03-13 14:23:07 -0500

    style: run linter

--- server/routes/api/disease.py
+++ server/routes/api/disease.py
@@ -60,13 +60,11 @@ def get_disease_parents(dcid):
   # dcid of the biggest parent node where iteration stops
   while (curr_dcid != FINAL_PARENT_DISEASE_DCID):
     node_dcids = dc.property_values([curr_dcid],
-                                            "specializationOf").get(
-                                                curr_dcid, [])
+                                    "specializationOf").get(curr_dcid, [])
     if not node_dcids:
       break
     node_dcid = node_dcids[0]
-    node_names = dc.property_values([node_dcid],
-                                            "name").get(node_dcid, [])
+    node_names = dc.property_values([node_dcid], "name").get(node_dcid, [])
     node_name = node_dcid
     if node_names:
       node_name = node_names[0]
@@ -76,4 +74,3 @@ def get_disease_parents(dcid):
   return Response(json.dumps(list_parent, cls=DiseaseParentEncoder),
                   200,
                   mimetype='application/json')
-
