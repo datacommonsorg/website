datacommonsorg/website

commit 79e7d6f06c3451dbaeed51d09f05c47c633bbebf

NL: Improve counter tracking (#2409)
1. Add page config proto to Debug Info (instead of utterance)
2. Track timings of large sections
3. Distinguish between info / error / timing counters
4. Refactor counters into its own module

TESTED: all integration tests pass; validated locally

server/lib/nl/counters.py
@@ -1,0 +1,70 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+from typing import Dict
+
+
+#
+# A helper class to track info / error / timing counters.
+#
+class Counters:
+
+  def __init__(self):
+    self._warn: Dict = {}
+    self._info: Dict = {}
+    self._timing: Dict = {}
+
+  #
+  # Adds/updates an info counter.
+  #
+  # For a given counter, caller should always pass the same type
+  # for value.  If value is numeric, then its a single updated
+  # counter, otherwise, counter is a list of values.
+  #
+  def info(self, counter: str, value: any):
+    self._update(self._info, counter, value)
+
+  #
+  # Adds/updates a warning counter.
+  #
+  # Same behavior as info()
+  #
+  def warn(self, counter: str, value: any):
+    self._update(self._warn, counter, value)
+
+  #
+  # Given start-time, computes the elapsed time and
+  # accounts it to the given counter.
+  #
+  def timeit(self, counter: str, start: float):
+    duration = round(time.time() - start, 2)
+    self._timing[counter] = self._timing.get(counter, 0) + duration
+
+  #
+  # Returns a dict with all counters.
+  #
+  def get(self) -> Dict:
+    return {'ERROR': self._warn, 'INFO': self._info, 'TIMING': self._timing}
+
+  def _update(self, counters: Dict, counter: str, value: any):
+    should_add = counter not in counters
+    if isinstance(value, int) or isinstance(value, float):
+      if should_add:
+        counters[counter] = 0
+      counters[counter] += value
+    else:
+      if should_add:
+        counters[counter] = []
+      counters[counter].append(value)

commit 1ea8f35263fbb38e3655ad5bd357710ea0fda102

NL: minor counter fixes and fix debug info regression (#2413)
Fix regression introduced in
https://github.com/datacommonsorg/website/pull/2409, where if
page-config is empty, we don't show up debug info.

Also, Jehangir's review comments from
https://github.com/datacommonsorg/website/pull/2409.

server/lib/nl/counters.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
+import logging
 import time
 from typing import Dict
 
@@ -22,7 +24,7 @@ from typing import Dict
 class Counters:
 
   def __init__(self):
-    self._warn: Dict = {}
+    self._err: Dict = {}
     self._info: Dict = {}
     self._timing: Dict = {}
 
@@ -33,6 +35,8 @@ class Counters:
   # for value.  If value is numeric, then its a single updated
   # counter, otherwise, counter is a list of values.
   #
+  # REQUIRES: `value` should be a JSON serializable type
+  #
   def info(self, counter: str, value: any):
     self._update(self._info, counter, value)
 
@@ -41,8 +45,10 @@ class Counters:
   #
   # Same behavior as info()
   #
-  def warn(self, counter: str, value: any):
-    self._update(self._warn, counter, value)
+  # REQUIRES: `value` should be a JSON serializable type
+  #
+  def err(self, counter: str, value: any):
+    self._update(self._err, counter, value)
 
   #
   # Given start-time, computes the elapsed time and
@@ -53,10 +59,16 @@ class Counters:
     self._timing[counter] = self._timing.get(counter, 0) + duration
 
   #
-  # Returns a dict with all counters.
+  # Returns a JSON serializable dict with all counters.
   #
   def get(self) -> Dict:
-    return {'ERROR': self._warn, 'INFO': self._info, 'TIMING': self._timing}
+    result = {'ERROR': self._err, 'INFO': self._info, 'TIMING': self._timing}
+    try:
+      json.dumps(result)
+      return result
+    except TypeError as e:
+      logging.exception(e)
+      return {'ERROR': 'FATAL: Found unserializable counter value!'}
 
   def _update(self, counters: Dict, counter: str, value: any):
     should_add = counter not in counters
@@ -65,6 +77,8 @@ class Counters:
         counters[counter] = 0
       counters[counter] += value
     else:
+      if isinstance(value, set):
+        value = list(value)
       if should_add:
         counters[counter] = []
       counters[counter].append(value)

server/lib/nl/fulfillment/base.py
@@ -125,7 +125,7 @@ def populate_charts(state: PopulateState) -> bool:
     if (populate_charts_for_places(state, state.uttr.places)):
       return True
     else:
-      state.uttr.counters.warn('failed_populate_main_places', state.uttr.places)
+      state.uttr.counters.err('failed_populate_main_places', state.uttr.places)
   else:
     # If user has not provided a place, seek a place from the context.
     # Otherwise the result seems unexpected to them.
@@ -133,7 +133,7 @@ def populate_charts(state: PopulateState) -> bool:
       if (populate_charts_for_places(state, [pl])):
         return True
       else:
-        state.uttr.counters.warn('failed_populate_context_place', pl.dcid)
+        state.uttr.counters.err('failed_populate_context_place', pl.dcid)
 
   # If this query did not have a place, but had a contained-in attribute, we
   # might try specific default places.
@@ -153,7 +153,7 @@ def populate_charts_for_places(state: PopulateState,
     if _add_charts_with_place_fallback(state, places, state.uttr.svs):
       return True
     else:
-      state.uttr.counters.warn('failed_populate_main_svs', state.uttr.svs)
+      state.uttr.counters.err('failed_populate_main_svs', state.uttr.svs)
   else:
     # If we have not found an SV, only then seek an SV from the context.
     # Otherwise the result seems unexpected to them.
@@ -161,10 +161,10 @@ def populate_charts_for_places(state: PopulateState,
       if _add_charts_with_place_fallback(state, places, svs):
         return True
       else:
-        state.uttr.counters.warn('failed_populate_context_svs', svs)
+        state.uttr.counters.err('failed_populate_context_svs', svs)
   logging.info('Doing fallback for %s - %s',
                ', '.join(_get_place_names(places)), ', '.join(state.uttr.svs))
-  state.uttr.counters.warn('num_populate_fallbacks', 1)
+  state.uttr.counters.err('num_populate_fallbacks', 1)
   return False
 
 
@@ -213,21 +213,21 @@ def _add_charts_with_place_fallback(state: PopulateState, places: List[Place],
   while parent_type:
     if state.place_type:
       # Pick next parent type.
-      state.uttr.counters.warn('parent_place_type_fallback', parent_type)
+      state.uttr.counters.err('parent_place_type_fallback', parent_type)
       state.place_type = parent_type
     else:
       # Pick parent place.
       parents = utils.get_immediate_parent_places(place.dcid, parent_type,
                                                   state.uttr.counters)
       if not parents:
-        state.uttr.counters.warn('failed_get_parent_places', {
+        state.uttr.counters.err('failed_get_parent_places', {
             'dcid': place.dcid,
             'type': parent_type
         })
         return False
 
       # There's typically a single parent, pick the first.
-      state.uttr.counters.warn('parent_place_fallback', {
+      state.uttr.counters.err('parent_place_fallback', {
           'child': place.dcid,
           'parent': parents[0].dcid
       })
@@ -290,7 +290,7 @@ class ExistenceCheckStateTracker:
           exist_cv.exist_event = utils.event_existence_for_place(
               places[0], chart_vars.event, self.state.uttr.counters)
           if not exist_cv.exist_event:
-            state.uttr.counters.warn('failed_event_existence_check', {
+            state.uttr.counters.err('failed_event_existence_check', {
                 'places': places,
                 'event': chart_vars.event
             })
@@ -317,7 +317,7 @@ class ExistenceCheckStateTracker:
     else:
       logging.info('Existence check failed for %s - %s', ', '.join(self.places),
                    ', '.join(self.all_svs))
-      self.state.uttr.counters.warn('failed_existence_check', {
+      self.state.uttr.counters.err('failed_existence_check', {
           'places': self.places,
           'svs': list(self.all_svs),
       })
@@ -331,7 +331,7 @@ class ExistenceCheckStateTracker:
           if sv in exist_svs:
             ecv.exist_svs.append(sv)
         if len(ecv.exist_svs) < len(ecv.chart_vars.svs):
-          self.state.uttr.counters.warn(
+          self.state.uttr.counters.err(
               'failed_partial_existence_check', {
                   'places': self.places,
                   'svs': list(set(ecv.chart_vars.svs) - set(exist_svs)),
@@ -342,7 +342,7 @@ class ExistenceCheckStateTracker:
           es.extended_exist_svs.append(esv)
 
       if len(es.extended_exist_svs) < len(es.extended_svs):
-        self.state.uttr.counters.warn(
+        self.state.uttr.counters.err(
             'failed_existence_check_extended_svs', {
                 'places': self.places,
                 'svs': list(set(es.extended_svs) - set(es.extended_exist_svs))
@@ -402,8 +402,7 @@ def _add_charts(state: PopulateState, places: List[Place],
                            ChartOriginType.PRIMARY_CHART):
             found = True
           else:
-            state.uttr.counters.warn('failed_populate_callback_primary_event',
-                                     1)
+            state.uttr.counters.err('failed_populate_callback_primary_event', 1)
       else:
         exist_svs = exist_cv.exist_svs
         if exist_svs:
@@ -413,7 +412,7 @@ def _add_charts(state: PopulateState, places: List[Place],
                            ChartOriginType.PRIMARY_CHART):
             found = True
           else:
-            state.uttr.counters.warn('failed_populate_callback_primary', 1)
+            state.uttr.counters.err('failed_populate_callback_primary', 1)
 
     # Infer comparison charts with extended SVs.
     extended_svs = sv2extensions.get(exist_state.sv, [])
@@ -435,7 +434,7 @@ def _add_charts(state: PopulateState, places: List[Place],
                        ChartOriginType.SECONDARY_CHART):
         found = True
       else:
-        state.uttr.counters.warn('failed_populate_callback_secondary', 1)
+        state.uttr.counters.err('failed_populate_callback_secondary', 1)
 
   logging.info("Add chart %s %s returning %r" %
                (', '.join(_get_place_names(places)), svs, found))

server/lib/nl/fulfillment/comparison.py
@@ -38,9 +38,9 @@ def populate(uttr: Utterance) -> bool:
     if populate_charts_for_places(state, places_to_compare):
       return True
     else:
-      uttr.counters.warn('comparison_failed_populate_places', dcids)
+      uttr.counters.err('comparison_failed_populate_places', dcids)
   if not place_comparison_candidates:
-    uttr.counters.warn('comparison_failed_to_find_multiple_places', 1)
+    uttr.counters.err('comparison_failed_to_find_multiple_places', 1)
   return False
 
 
@@ -48,10 +48,10 @@ def _populate_cb(state: PopulateState, chart_vars: ChartVars,
                  places: List[Place], chart_origin: ChartOriginType) -> bool:
   logging.info('populate_cb for comparison')
   if len(places) < 2:
-    state.uttr.counters.warn('comparison_failed_cb_toofewplaces', 1)
+    state.uttr.counters.err('comparison_failed_cb_toofewplaces', 1)
     return False
   if chart_vars.event:
-    state.uttr.counters.warn('comparison_failed_cb_events', 1)
+    state.uttr.counters.err('comparison_failed_cb_events', 1)
     return False
   chart_vars.response_type = "comparison chart"
   chart_vars.include_percapita = True

server/lib/nl/fulfillment/containedin.py
@@ -43,8 +43,8 @@ def populate(uttr: Utterance) -> bool:
         PopulateState(uttr=uttr, main_cb=_populate_cb, place_type=place_type)):
       return True
     else:
-      uttr.counters.warn('containedin_failed_populate_placetype',
-                         place_type.value)
+      uttr.counters.err('containedin_failed_populate_placetype',
+                        place_type.value)
   return False
 
 
@@ -54,24 +54,24 @@ def _populate_cb(state: PopulateState, chart_vars: ChartVars,
   logging.info('populate_cb for contained-in')
 
   if chart_vars.event:
-    state.uttr.counters.warn('containedin_failed_cb_events', 1)
+    state.uttr.counters.err('containedin_failed_cb_events', 1)
     return False
   if not state.place_type:
-    state.uttr.counters.warn('containedin_failed_cb_missing_type', 1)
+    state.uttr.counters.err('containedin_failed_cb_missing_type', 1)
     return False
   if not utils.has_map(state.place_type):
-    state.uttr.counters.warn('containedin_failed_cb_nonmap_type',
-                             state.place_type)
+    state.uttr.counters.err('containedin_failed_cb_nonmap_type',
+                            state.place_type)
     return False
   if not chart_vars:
-    state.uttr.counters.warn('containedin_failed_cb_missing_chat_vars', 1)
+    state.uttr.counters.err('containedin_failed_cb_missing_chat_vars', 1)
     return False
   if not chart_vars.svs:
-    state.uttr.counters.warn('containedin_failed_cb_missing_svs', 1)
+    state.uttr.counters.err('containedin_failed_cb_missing_svs', 1)
     return False
   if len(contained_places) > 1:
-    state.uttr.counters.warn('containedin_failed_cb_toomanyplaces',
-                             contained_places)
+    state.uttr.counters.err('containedin_failed_cb_toomanyplaces',
+                            contained_places)
     return False
 
   chart_vars.response_type = "comparison map"

server/lib/nl/fulfillment/correlation.py
@@ -55,8 +55,8 @@ def populate(uttr: Utterance) -> bool:
         PopulateState(uttr=uttr, main_cb=None, place_type=place_type)):
       return True
     else:
-      uttr.counters.warn('correlation_failed_populate_placestype',
-                         place_type.value)
+      uttr.counters.err('correlation_failed_populate_placestype',
+                        place_type.value)
   return False
 
 
@@ -65,14 +65,13 @@ def _populate_correlation_for_place_type(state: PopulateState) -> bool:
     if (_populate_correlation_for_place(state, pl)):
       return True
     else:
-      state.uttr.counters.warn('correlation_failed_populate_main_place',
-                               pl.dcid)
+      state.uttr.counters.err('correlation_failed_populate_main_place', pl.dcid)
   for pl in places_from_context(state.uttr):
     if (_populate_correlation_for_place(state, pl)):
       return True
     else:
-      state.uttr.counters.warn('correlation_failed_populate_context_place',
-                               pl.dcid)
+      state.uttr.counters.err('correlation_failed_populate_context_place',
+                              pl.dcid)
 
   default_place = get_default_contained_in_place(state)
   if default_place:
@@ -98,9 +97,9 @@ def _populate_correlation_for_place(state: PopulateState, place: Place) -> bool:
   main_svs = utils.sv_existence_for_places(places_to_check, main_svs,
                                            state.uttr.counters)
   if not main_svs:
-    state.uttr.counters.warn('correlation_failed_existence_check_main_sv',
-                             main_svs)
-    state.uttr.counters.warn('correlation_failed_missing_main_sv', 1)
+    state.uttr.counters.err('correlation_failed_existence_check_main_sv',
+                            main_svs)
+    state.uttr.counters.err('correlation_failed_missing_main_sv', 1)
     logging.info('Correlation found no Main SV')
     return False
 
@@ -113,10 +112,10 @@ def _populate_correlation_for_place(state: PopulateState, place: Place) -> bool:
     if context_svs:
       break
     else:
-      state.uttr.counters.warn('correlation_failed_existence_check_context_sv',
-                               opened_svs)
+      state.uttr.counters.err('correlation_failed_existence_check_context_sv',
+                              opened_svs)
   if not context_svs:
-    state.uttr.counters.warn('correlation_failed_missing_context_sv', 1)
+    state.uttr.counters.err('correlation_failed_missing_context_sv', 1)
     logging.info('Correlation found no Context SV')
     return False
 

server/lib/nl/fulfillment/event.py
@@ -34,7 +34,7 @@ def populate(uttr: nl_uttr.Utterance) -> bool:
       not isinstance(event_classification[0].attributes,
                      detection.EventClassificationAttributes) or
       not event_classification[0].attributes.event_types):
-    uttr.counters.warn('event_failed_no_event_types', 1)
+    uttr.counters.err('event_failed_no_event_types', 1)
     return False
   event_types = event_classification[0].attributes.event_types
 
@@ -58,13 +58,13 @@ def _populate_event(state: base.PopulateState,
     if (_populate_event_for_place(state, event_types, pl)):
       return True
     else:
-      state.uttr.counters.warn('event_failed_populate_main_place', pl.dcid)
+      state.uttr.counters.err('event_failed_populate_main_place', pl.dcid)
   if not state.uttr.places:
     for pl in ctx.places_from_context(state.uttr):
       if (_populate_event_for_place(state, event_types, pl)):
         return True
       else:
-        state.uttr.counters.warn('event_failed_populate_context_place', pl.dcid)
+        state.uttr.counters.err('event_failed_populate_context_place', pl.dcid)
 
   # Use a default of USA.
   return _populate_event_for_place(state, event_types, _DEFAULT_EVENT_PLACE)
@@ -76,7 +76,7 @@ def _populate_event_for_place(state: base.PopulateState,
   event_type = event_types[0]
   if not utils.event_existence_for_place(place.dcid, event_type,
                                          state.uttr.counters):
-    state.uttr.counters.warn('event_failed_existence_check', {
+    state.uttr.counters.err('event_failed_existence_check', {
         'place': place.dcid,
         'event': event_type
     })

server/lib/nl/fulfillment/overview.py
@@ -29,7 +29,7 @@ def populate(uttr: Utterance) -> bool:
   if uttr.places:
     _add_place_overview(uttr.places[0], uttr)
     return True
-  uttr.counters.warn('overview_failed_noplaces', 1)
+  uttr.counters.err('overview_failed_noplaces', 1)
   return False
 
 

server/lib/nl/fulfillment/ranking_across_places.py
@@ -44,8 +44,8 @@ def populate(uttr: Utterance):
                       ranking_types=ranking_types)):
       return True
     else:
-      uttr.counters.warn('ranking-across-places_failed_populate_placetype',
-                         place_type.value)
+      uttr.counters.err('ranking-across-places_failed_populate_placetype',
+                        place_type.value)
 
   return False
 
@@ -54,18 +54,17 @@ def _populate_cb(state: PopulateState, chart_vars: ChartVars,
                  places: List[Place], chart_origin: ChartOriginType) -> bool:
   logging.info('populate_cb for ranking_across_places')
   if not state.ranking_types:
-    state.uttr.counters.warn('ranking-across-places_failed_cb_norankingtypes',
-                             1)
+    state.uttr.counters.err('ranking-across-places_failed_cb_norankingtypes', 1)
     return False
   if len(places) > 1:
-    state.uttr.counters.warn('ranking-across-places_failed_cb_toomanyplaces',
-                             [p.dcid for p in places])
+    state.uttr.counters.err('ranking-across-places_failed_cb_toomanyplaces',
+                            [p.dcid for p in places])
     return False
   if not state.place_type:
-    state.uttr.counters.warn('ranking-across-places_failed_cb_noplacetype', 1)
+    state.uttr.counters.err('ranking-across-places_failed_cb_noplacetype', 1)
     return False
   if not chart_vars.svs and not chart_vars.event:
-    state.uttr.counters.warn('ranking-across-places_failed_cb_emptyvars', {
+    state.uttr.counters.err('ranking-across-places_failed_cb_emptyvars', {
         'svs': chart_vars.svs,
         'event': chart_vars.event,
     })

server/lib/nl/fulfillment/ranking_across_vars.py
@@ -49,7 +49,7 @@ def populate(uttr: Utterance):
                       ranking_types=ranking_types)):
       return True
     else:
-      uttr.counters.warn('ranking-across-vars_failed_populate', 1)
+      uttr.counters.err('ranking-across-vars_failed_populate', 1)
 
   return False
 
@@ -58,26 +58,26 @@ def _populate_cb(state: PopulateState, chart_vars: ChartVars,
                  places: List[Place], chart_origin: ChartOriginType) -> bool:
   logging.info('populate_cb for ranking_across_vars')
   if chart_vars.event:
-    state.uttr.counters.warn('ranking-across-vars_failed_cb_events', 1)
+    state.uttr.counters.err('ranking-across-vars_failed_cb_events', 1)
     return False
   if not state.ranking_types:
-    state.uttr.counters.warn('ranking-across-vars_failed_cb_norankingtypes', 1)
+    state.uttr.counters.err('ranking-across-vars_failed_cb_norankingtypes', 1)
     return False
   if len(places) > 1:
-    state.uttr.counters.warn('ranking-across-vars_failed_cb_toomanyplaces',
-                             [p.dcid for p in places])
+    state.uttr.counters.err('ranking-across-vars_failed_cb_toomanyplaces',
+                            [p.dcid for p in places])
     return False
   if state.place_type:
-    state.uttr.counters.warn('ranking-across-vars_failed_cb_hasplacetype',
-                             state.place_type.value)
+    state.uttr.counters.err('ranking-across-vars_failed_cb_hasplacetype',
+                            state.place_type.value)
     return False
   if len(chart_vars.svs) < 2:
-    state.uttr.counters.warn('ranking-across-vars_failed_cb_toofewvars',
-                             chart_vars.svs)
+    state.uttr.counters.err('ranking-across-vars_failed_cb_toofewvars',
+                            chart_vars.svs)
     return False
   if not chart_vars.is_topic_peer_group:
-    state.uttr.counters.warn('ranking-across-vars_failed_cb_notpeergroup',
-                             chart_vars.svs)
+    state.uttr.counters.err('ranking-across-vars_failed_cb_notpeergroup',
+                            chart_vars.svs)
     return False
 
   # Ranking among peer group of SVs.

server/lib/nl/fulfillment/simple.py
@@ -34,7 +34,7 @@ def populate(uttr: Utterance) -> bool:
     # If both the SVs and places are empty, then do not attempt to fulfill.
     # This avoids using incorrect context for unrelated queries like
     # [meaning of life]
-    uttr.counters.warn('simple_failed_noplaceandsv', 1)
+    uttr.counters.err('simple_failed_noplaceandsv', 1)
     return False
   return populate_charts(PopulateState(uttr=uttr, main_cb=_populate_cb))
 

server/lib/nl/fulfillment/size_across_entities.py
@@ -67,12 +67,12 @@ def populate(uttr: Utterance):
   # Only if there are no SVs detected and contained-in is detected, do we consider
   # the SIZE_TYPE classification.
   if uttr.svs:
-    uttr.counters.warn('size-across-entities_failed_foundvars', uttr.svs)
+    uttr.counters.err('size-across-entities_failed_foundvars', uttr.svs)
     return False
   place_type = utils.get_contained_in_type(uttr)
   size_types = utils.get_size_types(uttr)
   if not place_type or not size_types:
-    uttr.counters.warn('size-across-entities_failed_noplaceorsizetype', '-')
+    uttr.counters.err('size-across-entities_failed_noplaceorsizetype', '-')
     return False
 
   uttr.svs = _get_vars(place_type)
@@ -95,18 +95,18 @@ def _populate_cb(state: PopulateState, chart_vars: ChartVars,
                  places: List[Place], chart_origin: ChartOriginType) -> bool:
   logging.info('populate_cb for size_across_entities')
   if not state.ranking_types:
-    state.uttr.counters.warn('size-across-entities_failed_cb_norankingtypes', 1)
+    state.uttr.counters.err('size-across-entities_failed_cb_norankingtypes', 1)
     return False
   if len(places) > 1:
-    state.uttr.counters.warn('size-across-entities_failed_cb_toomanyplaces',
-                             [p.dcid for p in places])
+    state.uttr.counters.err('size-across-entities_failed_cb_toomanyplaces',
+                            [p.dcid for p in places])
     return False
   if not state.place_type:
-    state.uttr.counters.warn('size-across-entities_failed_cb_noplacetype', 1)
+    state.uttr.counters.err('size-across-entities_failed_cb_noplacetype', 1)
     return False
   if not chart_vars.svs:
-    state.uttr.counters.warn('size-across-entities_failed_cb_emptyvars',
-                             chart_vars.svs)
+    state.uttr.counters.err('size-across-entities_failed_cb_emptyvars',
+                            chart_vars.svs)
     return False
 
   chart_vars.response_type = "ranking table"

server/lib/nl/fulfillment/time_delta_across_places.py
@@ -51,22 +51,22 @@ def _populate_cb(state: PopulateState, chart_vars: ChartVars,
                  places: List[Place], chart_origin: ChartOriginType) -> bool:
   logging.info('populate_cb for time_delta_across_places')
   if chart_vars.event:
-    state.uttr.counters.warn('time-delta-across-places_failed_cb_events', 1)
+    state.uttr.counters.err('time-delta-across-places_failed_cb_events', 1)
     return False
   if not state.time_delta_types:
-    state.uttr.counters.warn(
+    state.uttr.counters.err(
         'time-delta-across-places_failed_cb_notimedeltatypes', 1)
     return False
   if len(places) > 1:
-    state.uttr.counters.warn('time-delta-across-places_failed_cb_toomanyplaces',
-                             [p.dcid for p in places])
+    state.uttr.counters.err('time-delta-across-places_failed_cb_toomanyplaces',
+                            [p.dcid for p in places])
     return False
   if len(chart_vars.svs) > 1:
-    state.uttr.counters.warn('time-delta-across-places_failed_cb_toomanysvs',
-                             chart_vars.svs)
+    state.uttr.counters.err('time-delta-across-places_failed_cb_toomanysvs',
+                            chart_vars.svs)
     return False
   if not state.place_type:
-    state.uttr.counters.warn(
+    state.uttr.counters.err(
         'time-delta-across-places_failed_cb_missingchildtype', chart_vars.svs)
     return False
 

server/lib/nl/fulfillment/time_delta_across_vars.py
@@ -45,23 +45,23 @@ def _populate_cb(state: PopulateState, chart_vars: ChartVars,
                  places: List[Place], chart_origin: ChartOriginType) -> bool:
   logging.info('populate_cb for time_delta_across_vars')
   if chart_vars.event:
-    state.uttr.counters.warn('time-delta-across-vars_failed_cb_events', 1)
+    state.uttr.counters.err('time-delta-across-vars_failed_cb_events', 1)
     return False
   if not state.time_delta_types:
-    state.uttr.counters.warn(
-        'time-delta-across-vars_failed_cb_notimedeltatypes', 1)
+    state.uttr.counters.err('time-delta-across-vars_failed_cb_notimedeltatypes',
+                            1)
     return False
   if len(places) > 1:
-    state.uttr.counters.warn('time-delta-across-vars_failed_cb_toomanyplaces',
-                             [p.dcid for p in places])
+    state.uttr.counters.err('time-delta-across-vars_failed_cb_toomanyplaces',
+                            [p.dcid for p in places])
     return False
   if len(chart_vars.svs) < 2:
-    state.uttr.counters.warn('time-delta-across-vars_failed_cb_toofewsvs',
-                             chart_vars.svs)
+    state.uttr.counters.err('time-delta-across-vars_failed_cb_toofewsvs',
+                            chart_vars.svs)
     return False
   if not chart_vars.is_topic_peer_group:
-    state.uttr.counters.warn('time-delta-across-vars_failed_cb_nopeergroup',
-                             chart_vars.svs)
+    state.uttr.counters.err('time-delta-across-vars_failed_cb_nopeergroup',
+                            chart_vars.svs)
     return False
 
   found = False

static/js/apps/nl_interface/query_result.tsx
@@ -130,10 +130,10 @@ export const QueryResult = memo(function QueryResult(
           </a>
         </Container>
         <Container>
-          {debugData && chartsData && (
+          {debugData && (
             <DebugInfo
               debugData={debugData}
-              pageConfig={chartsData.config}
+              pageConfig={chartsData ? chartsData.config : null}
             ></DebugInfo>
           )}
           {chartsData && chartsData.config && (