d844a296a1c2f4a1a223d66f73685d6881bc0de3
Carolyn Au <cyin@google.com>
2023-03-14 11:11:16 -0700

    tests pass!

--- server/lib/subject_page_config.py
+++ server/lib/subject_page_config.py
@@ -60,11 +60,12 @@ def is_tile_match(tile, stat_var_key, chart_type):
   """
   if tile.type != chart_type:
     return False
-  # When there are multiple stat vars for this tile, this will not be a match
-  # TODO: re-visit this condition based on the chart logic.
-  if len(tile.stat_var_key) != 1:
-    return False
-  return tile.stat_var_key[0] == stat_var_key
+  return stat_var_key in tile.stat_var_key
+  # # When there are multiple stat vars for this tile, this will not be a match
+  # # TODO: re-visit this condition based on the chart logic.
+  # if len(tile.stat_var_key) != 1:
+  #   return False
+  # return tile.stat_var_key[0] == stat_var_key
 
 
 def trim_config(page_config, variable, chart_type):
@@ -94,25 +95,154 @@ def trim_config(page_config, variable, chart_type):
   return page_config
 
 
-def remove_empty_charts(page_config, place_dcid):
-  """
-  Returns the page config stripped of charts with no data.
-  TODO: Add checks for child places, given the tile type.
-  """
-  all_stat_vars = get_all_variables(page_config)
-  if all_stat_vars:
-    stat_vars_existence = dc.observation_existence(all_stat_vars, [place_dcid])
+# def remove_empty_charts(page_config, place_dcid):
+#   """
+#   Returns the page config stripped of charts with no data.
+#   TODO: Add checks for child places, given the tile type.
+#   """
+#   all_stat_vars = get_all_variables(page_config)
+#   if all_stat_vars:
+#     stat_vars_existence = dc.observation_existence(all_stat_vars, [place_dcid])
 
-    for stat_var in stat_vars_existence['variable']:
-      if not stat_vars_existence['variable'][stat_var]['entity'][place_dcid]:
-        # This is for the main place, only remove the tile type for single place.
-        for tile_type in [
+#     for stat_var in stat_vars_existence['variable']:
+#       if not stat_vars_existence['variable'][stat_var]['entity'][place_dcid]:
+#         # This is for the main place, only remove the tile type for single place.
+#         for tile_type in [
+#             subject_page_pb2.Tile.TileType.HISTOGRAM,
+#             subject_page_pb2.Tile.TileType.LINE,
+#             subject_page_pb2.Tile.TileType.BAR,
+#         ]:
+#           page_config = trim_config(page_config, stat_var, tile_type)
+#   return page_config
+
+FILTER_TILE_TYPES = [
+            subject_page_pb2.Tile.TileType.HIGHLIGHT,
+            subject_page_pb2.Tile.TileType.RANKING,
             subject_page_pb2.Tile.TileType.HISTOGRAM,
+            subject_page_pb2.Tile.TileType.MAP,
+            subject_page_pb2.Tile.TileType.SCATTER,
+            subject_page_pb2.Tile.TileType.BIVARIATE,
             subject_page_pb2.Tile.TileType.LINE,
             subject_page_pb2.Tile.TileType.BAR,
-        ]:
-          page_config = trim_config(page_config, stat_var, tile_type)
+]
+
+def exist_keys_category(place_dcid, category, stat_vars_existence):
+  print(">>>>>>>>>>>>>>>>>>>>>>>>>>>")
+  print(stat_vars_existence)
+  exist_keys = {}
+  for stat_var_key, spec in category.stat_var_spec.items():
+    stat_var = spec.stat_var
+    print(stat_var)
+    sv_exist = stat_vars_existence['variable'][stat_var]['entity'][place_dcid]
+    if spec.denom:
+      print(stat_var)
+      denom_exist = stat_vars_existence['variable'][spec.denom]['entity'][place_dcid]
+      exist_keys[stat_var_key] = sv_exist and denom_exist
+    else:
+      exist_keys[stat_var_key] = sv_exist
+  print(exist_keys)
+  return exist_keys
+
+
+def remove_empty_charts(page_config, place_dcid):
+  all_stat_vars = get_all_variables(page_config)
+  if not all_stat_vars:
+    return page_config
+
+  stat_vars_existence = dc.observation_existence(all_stat_vars, [place_dcid])
+  print(">>>>>>>>>>>>>>>>>>>>>>>>>>>")
+  print(stat_vars_existence)
+
+  """
+  - each cateogry
+    - each block, column
+      - each tile
+        - clear stat var if no data
+        - if no more stat vars, remove tile
+    - filter out stat var keys
+  """
+  for category in page_config.categories:
+    exist_keys = exist_keys_category(place_dcid, category, stat_vars_existence)
+    print("exist keys")
+    print(exist_keys)
+
+    for block in category.blocks:
+      for column in block.columns:
+        new_tiles = []
+        for t  in column.tiles:
+          if not t.type in FILTER_TILE_TYPES:
+            new_tiles.append(t)
+            continue
+          filtered_keys = [k for k in t.stat_var_key if exist_keys[k]]
+          print(filtered_keys)
+          if len(filtered_keys):
+            del t.stat_var_key[:]
+            t.stat_var_key.extend(filtered_keys)
+            new_tiles.append(t)
+        # if len(new_tiles):
+        print("tiles:")
+        print(new_tiles)
+        del column.tiles[:]
+        column.tiles.extend(new_tiles)
+      columns = [x for x in block.columns if len(x.tiles) > 0]
+      print("columns:")
+      print(columns)
+      del block.columns[:]
+      block.columns.extend(columns)
+    blocks = [x for x in category.blocks if len(x.columns) > 0]
+    print("blocks:")
+    print(blocks)
+    del category.blocks[:]
+    category.blocks.extend(blocks)
+    # category.stat_var_spec.clear()
+    for key, exists in exist_keys.items():
+      if not exists:
+        del category.stat_var_spec[key]
+      # print(spec)
+      # category.stat_var_spec[key].CopyFrom(spec)
+    # category.stat_var_spec["kept_0"] = { "stat_var": "Area_fireEvent" }
+    # category.stat_var_spec.update(exist_keys)
+  categories = [x for x in page_config.categories if len(x.blocks) > 0]
+  del page_config.categories[:]
+  page_config.categories.extend(categories)
   return page_config
+
+  # for category in page_config.categories:
+  #   for stat_var_key, spec in category.stat_var_spec.items():
+  #     stat_var = spec.stat_var
+  #     if not stat_vars_existence['variable'][stat_var]['entity'][place_dcid]:
+  #       page_config = trim_config(page_config, stat_var, FILTER_TILE_TYPES)
+  # #     # No data for stat var - remove tiles
+  #     for block in category.blocks:
+  #       for column in block.columns:
+  #         # Remove matched tile
+  #         tiles = []
+  #         for t in column.tiles:
+  #           if not t.type in FILTER_TILE_TYPES:
+  #             tiles.append(t)
+  #             continue
+  #           if stat_var_key in t.stat_var_key:
+  #             new_keys = [k for k in t.stat_var_key if not t.stat_var_key == stat_var_key]
+  #             del t.stat_var_key[:]
+  #             t.stat_var_key.extend(new_keys)
+  #             if len(t.stat_var_key) > 0:
+  #               tiles.append(t)
+  #         # tiles = [
+  #         #     x for x in column.tiles
+  #         #     if not is_tile_match(x, stat_var_key, chart_type)
+  #         # ]
+  #         del column.tiles[:]
+  #         column.tiles.extend(tiles)
+  #       columns = [x for x in block.columns if len(x.tiles) > 0]
+  #       del block.columns[:]
+  #       block.columns.extend(columns)
+  #     blocks = [x for x in category.blocks if len(x.columns) > 0]
+  #     del category.blocks[:]
+  #     category.blocks.extend(blocks)
+  # categories = [x for x in page_config.categories if len(x.blocks) > 0]
+  # del page_config.categories[:]
+  # page_config.categories.extend(categories)
+  # return page_config
 
 
 def place_metadata(place_dcid) -> PlaceMetadata:
--- server/tests/lib/subject_page_config_test.py
+++ server/tests/lib/subject_page_config_test.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import unittest
+from unittest import mock
 
 from google.protobuf import text_format
 
@@ -42,58 +43,186 @@ class TestGetAllVariables(unittest.TestCase):
     assert sorted(lib_subject_page_config.get_all_variables(config)) == expected
 
 
-class TestTrimConfig(unittest.TestCase):
+# class TestTrimConfig(unittest.TestCase):
 
-  def test_trim_tiles(self):
-    config = lib_util.get_subject_page_config(
-        "server/tests/test_data/disaster.textproto")
-    config = lib_subject_page_config.trim_config(
-        config, "Count_fireEvent", subject_page_pb2.Tile.TileType.HISTOGRAM)
-    config = lib_subject_page_config.trim_config(
-        config, "Count_fireEvent", subject_page_pb2.Tile.TileType.MAP)
-    result = text_format.MessageToString(config.categories[-1])
+#   def test_trim_tiles(self):
+#     config = lib_util.get_subject_page_config(
+#         "server/tests/test_data/disaster.textproto")
+#     config = lib_subject_page_config.trim_config(
+#         config, "Count_fireEvent", subject_page_pb2.Tile.TileType.HISTOGRAM)
+#     config = lib_subject_page_config.trim_config(
+#         config, "Count_fireEvent", subject_page_pb2.Tile.TileType.MAP)
+#     result = text_format.MessageToString(config.categories[-1])
 
-    category = subject_page_pb2.Category()
-    text_format.Parse(
-        """
-        title: "Fires"
-        blocks {
-            columns {
-                tiles {
-                    type: HISTOGRAM
-                    title: "Total area affected by fires over time"
-                    stat_var_key: "fire_area"
+#     category = subject_page_pb2.Category()
+#     text_format.Parse(
+#         """
+#         title: "Fires"
+#         blocks {
+#             columns {
+#                 tiles {
+#                     type: HISTOGRAM
+#                     title: "Total area affected by fires over time"
+#                     stat_var_key: "fire_area"
+#                 }
+#             }
+#             columns {
+#                 tiles {
+#                     type: MAP
+#                     title: "Total area affected by fires"
+#                     stat_var_key: "fire_area"
+#                 }
+#             }
+#         }
+#         stat_var_spec {
+#             key: "fire_count"
+#             value {
+#             stat_var: "Count_fireEvent"
+#             name: "Count of fires"
+#             }
+#         }
+#         stat_var_spec {
+#             key: "fire_area"
+#             value {
+#             stat_var: "Area_fireEvent"
+#             name: "Area affected by fires"
+#             }
+#         }
+#         """, category)
+#     expect = text_format.MessageToString(category)
+#     assert result == expect
+
+#     # Remove two more tiles, then the entire Fire category should be removed.
+#     config = lib_subject_page_config.trim_config(
+#         config, "Area_fireEvent", subject_page_pb2.Tile.TileType.HISTOGRAM)
+#     config = lib_subject_page_config.trim_config(
+#         config, "Area_fireEvent", subject_page_pb2.Tile.TileType.MAP)
+#     assert config.categories[-1].title == "Floods"
+
+
+class TestRemoveEmptyCharts(unittest.TestCase):
+
+    def test_exist_keys_category(self):
+        category = subject_page_pb2.Category()
+        text_format.Parse(
+            """
+            stat_var_spec {
+                key: "filtered_0"
+                value {
+                    stat_var: "Count_fireEvent"
                 }
             }
-            columns {
-                tiles {
-                    type: MAP
-                    title: "Total area affected by fires"
-                    stat_var_key: "fire_area"
+            stat_var_spec {
+                key: "kept_0"
+                value {
+                    stat_var: "Area_fireEvent"
                 }
             }
-        }
-        stat_var_spec {
-            key: "fire_count"
-            value {
-            stat_var: "Count_fireEvent"
-            name: "Count of fires"
+            stat_var_spec {
+                key: "kept_1"
+                value {
+                    stat_var: "Area_fireEvent"
+                    denom: "TotalArea"
+                }
+            }
+            stat_var_spec {
+                key: "filtered_1"
+                value {
+                    stat_var: "Area_fireEvent"
+                    denom: "Nonexistent"
+                }
+            }
+            """, category)
+        stat_vars_existence = {
+            'variable': {
+                'Count_fireEvent': {'entity': {'place_id': False}},
+                'Area_fireEvent': {'entity': {'place_id': True}},
+                'TotalArea': {'entity': {'place_id': True}},
+                'Nonexistent': {'entity': {'place_id': False}},
             }
         }
-        stat_var_spec {
-            key: "fire_area"
-            value {
-            stat_var: "Area_fireEvent"
-            name: "Area affected by fires"
-            }
+        result = lib_subject_page_config.exist_keys_category('place_id', category, stat_vars_existence)
+        expect = {
+            "kept_0": True,
+            "kept_1": True,
+            "filtered_0": False,
+            "filtered_1": False,
         }
-        """, category)
-    expect = text_format.MessageToString(category)
-    assert result == expect
+        assert result == expect
 
-    # Remove two more tiles, then the entire Fire category should be removed.
-    config = lib_subject_page_config.trim_config(
-        config, "Area_fireEvent", subject_page_pb2.Tile.TileType.HISTOGRAM)
-    config = lib_subject_page_config.trim_config(
-        config, "Area_fireEvent", subject_page_pb2.Tile.TileType.MAP)
-    assert config.categories[-1].title == "Floods"
+    @mock.patch('server.services.datacommons.observation_existence')
+    def test_remove_empty_charts(self, mock_observation_existence):
+        def obs_side_effect(all_svs, place_dcids):
+           return {
+            'variable': {
+                "sv_exists_1": {'entity': {'place_id': True}},
+                "sv_exists_3": {'entity': {'place_id': True}},
+                "sv_filtered_1": {'entity': {'place_id': False}},
+                "sv_filtered_2": {'entity': {'place_id': False}},
+                "sv_filtered_3": {'entity': {'place_id': False}},
+            }
+           }
+        mock_observation_existence.side_effect = obs_side_effect
+        config = lib_util.get_subject_page_config(
+            "server/tests/test_data/existence.textproto")
+        result = lib_subject_page_config.remove_empty_charts(config, 'place_id')
+        print("result:")
+        print(result)
+        expect = lib_util.get_subject_page_config(
+            "server/tests/test_data/existence_expect.textproto")
+        assert result == expect
+
+
+#   def test_trim_tiles(self):
+#     config = lib_util.get_subject_page_config(
+#         "server/tests/test_data/existence.textproto")
+#     config = lib_subject_page_config.trim_config(
+#         config, "Count_fireEvent", subject_page_pb2.Tile.TileType.HISTOGRAM)
+#     config = lib_subject_page_config.trim_config(
+#         config, "Count_fireEvent", subject_page_pb2.Tile.TileType.MAP)
+#     result = text_format.MessageToString(config.categories[-1])
+
+#     category = subject_page_pb2.Category()
+#     text_format.Parse(
+#         """
+#         title: "Fires"
+#         blocks {
+#             columns {
+#                 tiles {
+#                     type: HISTOGRAM
+#                     title: "Total area affected by fires over time"
+#                     stat_var_key: "fire_area"
+#                 }
+#             }
+#             columns {
+#                 tiles {
+#                     type: MAP
+#                     title: "Total area affected by fires"
+#                     stat_var_key: "fire_area"
+#                 }
+#             }
+#         }
+#         stat_var_spec {
+#             key: "fire_count"
+#             value {
+#             stat_var: "Count_fireEvent"
+#             name: "Count of fires"
+#             }
+#         }
+#         stat_var_spec {
+#             key: "fire_area"
+#             value {
+#             stat_var: "Area_fireEvent"
+#             name: "Area affected by fires"
+#             }
+#         }
+#         """, category)
+#     expect = text_format.MessageToString(category)
+#     assert result == expect
+
+#     # Remove two more tiles, then the entire Fire category should be removed.
+#     config = lib_subject_page_config.trim_config(
+#         config, "Area_fireEvent", subject_page_pb2.Tile.TileType.HISTOGRAM)
+#     config = lib_subject_page_config.trim_config(
+#         config, "Area_fireEvent", subject_page_pb2.Tile.TileType.MAP)
+#     assert config.categories[-1].title == "Floods"
