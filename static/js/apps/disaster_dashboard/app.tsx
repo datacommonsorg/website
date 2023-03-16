datacommonsorg/website

commit c171cf016bee79d90dda5ca8cea990537814e779

[Sustainability] add framework for new pages (#2421)
- Created a subject_page jinja macro for basic subject page
setup.
- Will update textproto with more charts in an upcoming PR
- If pages converge more in future, we can further extract a single css/js bundle for subject pages (including topic)

static/js/apps/sustainability/app.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright 2022 Google LLC
+ * Copyright 2023 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  */
 
 /**
- * Main component for the disaster dashboard.
+ * Main component for sustainability explorer.
  */
 
 import React from "react";
@@ -28,7 +28,7 @@ import { ChildPlaces } from "../../shared/child_places";
 import { ParentBreadcrumbs } from "../../shared/parent_breadcrumbs";
 import { SubjectPageMetadata } from "../../types/subject_page_types";
 
-const PAGE_ID = "disaster";
+const PAGE_ID = "sustainability";
 
 interface AppPropType {
   /**

static/js/shared/child_places.tsx
@@ -21,8 +21,8 @@
 import _ from "lodash";
 import React from "react";
 
-import { displayNameForPlaceType } from "../../place/util";
-import { ChildPlacesByType, NamedTypedPlace } from "../../shared/types";
+import { displayNameForPlaceType } from "../place/util";
+import { ChildPlacesByType, NamedTypedPlace } from "../shared/types";
 
 interface ChildPlacesPropType {
   parentPlace: NamedTypedPlace;

static/js/shared/parent_breadcrumbs.tsx
@@ -20,7 +20,7 @@
 
 import React from "react";
 
-import { NamedTypedPlace } from "../../shared/types";
+import { NamedTypedPlace } from "./types";
 
 interface ParentBreadcrumbsPropType {
   /**

server/__init__.py
@@ -89,10 +89,12 @@ def register_routes_disasters(app):
   # Install blueprints specific to Stanford DC
   from server.routes import disasters
   from server.routes import event
+  from server.routes import sustainability
   from server.routes.api import disaster_api
   app.register_blueprint(disasters.bp)
   app.register_blueprint(disaster_api.bp)
   app.register_blueprint(event.bp)
+  app.register_blueprint(sustainability.bp)
 
   if app.config['TEST']:
     return
@@ -101,6 +103,9 @@ def register_routes_disasters(app):
   app.config[
       'DISASTER_DASHBOARD_CONFIG'] = libutil.get_disaster_dashboard_config()
   app.config['DISASTER_EVENT_CONFIG'] = libutil.get_disaster_event_config()
+  app.config[
+      'DISASTER_SUSTAINABILITY_CONFIG'] = libutil.get_disaster_sustainability_config(
+      )
 
   if app.config['INTEGRATION']:
     return

server/config/subject_page/sustainability.textproto
@@ -1,0 +1,62 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## This file specifies the config for sustainability explorer.
+
+metadata {
+  topic_id: "sustainability_explorer"
+  contained_place_types {
+    key: "Planet"
+    value: "Country"
+  }
+  contained_place_types {
+    key: "Continent"
+    value: "Country"
+  }
+  contained_place_types {
+    key: "Country"
+    value: "AdministrativeArea1"
+  }
+  contained_place_types {
+    key: "AdministrativeArea1"
+    value: "AdministrativeArea2"
+  }
+}
+
+categories {
+  title: "Air quality"
+  blocks {
+    columns {
+      tiles {}
+    }
+  }
+}
+
+categories {
+  title: "Temperature"
+  blocks {
+    columns {
+      tiles {}
+    }
+  }
+}
+
+categories {
+  title: "Energy"
+  blocks {
+    columns {
+      tiles {}
+    }
+  }
+}

server/lib/subject_page_config.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 from dataclasses import dataclass
-from typing import Dict, List
+from typing import Dict, List, Union
 
 from flask import escape
 
@@ -46,11 +46,14 @@ FILTER_TILE_TYPES = [
 @dataclass
 class PlaceMetadata:
   """Place metadata for subject pages."""
+  place_dcid: str
   place_name: str
   place_types: str
   parent_places: List[str]
   # If set, use this to override the contained_place_types map in config metadata.
   contained_place_types_override: Dict[str, str]
+  # Corresponds to typescript type ChildPlacesByType
+  child_places: Dict[str, List[Dict[str, Union[str, int]]]]
 
 
 def get_all_variables(page_config):
@@ -148,5 +151,16 @@ def place_metadata(place_dcid) -> PlaceMetadata:
   if EUROPE_DCID in parent_dcids:
     contained_place_types_override = EUROPE_CONTAINED_PLACE_TYPES
 
-  return PlaceMetadata(place_name, place_types, parent_places,
-                       contained_place_types_override)
+  child_places = place_api.child_fetch(place_dcid)
+  for place_type in child_places:
+    child_places[place_type].sort(key=lambda x: x['pop'], reverse=True)
+    child_places[place_type] = child_places[place_type][:place_api.
+                                                        CHILD_PLACE_LIMIT]
+
+  return PlaceMetadata(
+      place_dcid=escape(place_dcid),
+      place_name=place_name,
+      place_types=place_types,
+      parent_places=parent_places,
+      child_places=child_places,
+      contained_place_types_override=contained_place_types_override)

commit 03255113dc46eaf63562f81437661829dc6729cd

[Sustainability] fix bugs introduced in #2421 (#2432)
- Fix page titles of disaster dashboard pages
- Fix links for child places

server/templates/custom_dc/stanford/disaster_dashboard.html
@@ -19,7 +19,7 @@
 
 {% set main_id = 'disaster_dashboard' %}
 {% set page_id = 'page-disaster-dashboard' %}
-{% set title = place_metadata.place_name + 'Disasters Dashboard' %}
+{% set title = place_metadata.place_name + ' - Disasters Dashboard' %}
 {% set subpage_title = 'Severe Weather and Natural Disasters Dashboard' %}
 
 {% block head %}

static/js/apps/disaster_dashboard/app.tsx
@@ -49,6 +49,7 @@ export function App(props: AppPropType): JSX.Element {
           <ChildPlaces
             childPlaces={props.metadata.childPlaces}
             parentPlace={props.metadata.place}
+            urlFormatString="/disasters/${placeDcid}"
           ></ChildPlaces>
         </div>
         <div className="col-md-9x col-lg-10">
@@ -56,6 +57,7 @@ export function App(props: AppPropType): JSX.Element {
           <ParentBreadcrumbs
             place={props.metadata.place}
             parentPlaces={props.metadata.parentPlaces}
+            urlFormatString="/disasters/${placeDcid}"
           ></ParentBreadcrumbs>
           <SubjectPageMainPane
             id={PAGE_ID}

static/js/apps/sustainability/app.tsx
@@ -49,6 +49,7 @@ export function App(props: AppPropType): JSX.Element {
           <ChildPlaces
             childPlaces={props.metadata.childPlaces}
             parentPlace={props.metadata.place}
+            urlFormatString="/sustainability/${placeDcid}"
           ></ChildPlaces>
         </div>
         <div className="col-md-9x col-lg-10">
@@ -56,6 +57,7 @@ export function App(props: AppPropType): JSX.Element {
           <ParentBreadcrumbs
             place={props.metadata.place}
             parentPlaces={props.metadata.parentPlaces}
+            urlFormatString="/sustainability/${placeDcid}"
           ></ParentBreadcrumbs>
           <SubjectPageMainPane
             id={PAGE_ID}

static/js/shared/child_places.tsx
@@ -23,10 +23,13 @@ import React from "react";
 
 import { displayNameForPlaceType } from "../place/util";
 import { ChildPlacesByType, NamedTypedPlace } from "../shared/types";
+import { formatString, ReplacementStrings } from "../utils/tile_utils";
 
 interface ChildPlacesPropType {
   parentPlace: NamedTypedPlace;
   childPlaces: ChildPlacesByType;
+  // Format string must include ${placeDcid} for child place URLs.
+  urlFormatString: string;
 }
 
 export function ChildPlaces(props: ChildPlacesPropType): JSX.Element {
@@ -42,16 +45,23 @@ export function ChildPlaces(props: ChildPlacesPropType): JSX.Element {
           <div className="child-place-type">
             {displayNameForPlaceType(placeType, true /* isPlural */)}
           </div>
-          {props.childPlaces[placeType].map((place, i) => (
-            <a
-              href={"/disasters/" + place.dcid}
-              className="child-place-link"
-              key={`child-place-${i}`}
-            >
-              {place.name || place.dcid}
-              {i < props.childPlaces[placeType].length - 1 ? "," : ""}
-            </a>
-          ))}
+          {props.childPlaces[placeType].map((place, i) => {
+            const rs: ReplacementStrings = {
+              place: "",
+              date: "",
+              placeDcid: place.dcid,
+            };
+            return (
+              <a
+                href={formatString(props.urlFormatString, rs)}
+                className="child-place-link"
+                key={`child-place-${i}`}
+              >
+                {place.name || place.dcid}
+                {i < props.childPlaces[placeType].length - 1 ? "," : ""}
+              </a>
+            );
+          })}
         </div>
       ))}
     </div>

static/js/shared/parent_breadcrumbs.tsx
@@ -20,6 +20,7 @@
 
 import React from "react";
 
+import { formatString, ReplacementStrings } from "../utils/tile_utils";
 import { NamedTypedPlace } from "./types";
 
 interface ParentBreadcrumbsPropType {
@@ -31,6 +32,10 @@ interface ParentBreadcrumbsPropType {
    * List of parent places.
    */
   parentPlaces: NamedTypedPlace[];
+  /**
+   * Format string must include ${placeDcid} for child place URLs.
+   */
+  urlFormatString: string;
 }
 
 export function ParentBreadcrumbs(
@@ -49,9 +54,17 @@ export function ParentBreadcrumbs(
   const num = props.parentPlaces.length;
   const breadcrumbs = props.parentPlaces.map((place, index) => {
     const name = place.name.split(",")[0];
+    const rs: ReplacementStrings = {
+      place: "",
+      date: "",
+      placeDcid: place.dcid,
+    };
     return (
       <React.Fragment key={place.dcid}>
-        <a className="place-links" href={`/disasters/${place.dcid}`}>
+        <a
+          className="place-links"
+          href={formatString(props.urlFormatString, rs)}
+        >
           {name}
         </a>
         {index < num - 1 && <span>, </span>}

static/js/utils/tile_utils.tsx
@@ -22,11 +22,14 @@ import { getStatsVarLabel } from "../shared/stats_var_labels";
 import { StatVarSpec } from "../shared/types";
 
 export interface ReplacementStrings {
+  // TODO: Rename place to placeName. Make it optional.
   place: string;
+  // TODO: Make it optional.
   date: string;
   statVar?: string;
   xDate?: string;
   yDate?: string;
+  placeDcid?: string;
 }
 
 /**