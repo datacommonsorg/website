e821b910465d70827e6d15dbdbbd11ad847bc7bd
Bo Xu <shifucun@users.noreply.github.com>
2023-03-27 17:18:33 -0700

    resolve comments

--- server/routes/topic_page.py
+++ server/routes/topic_page.py
@@ -31,10 +31,33 @@ _DEBUG_TOPICS = ['dev', _SDG_TOPIC, _NL_DISASTER_TOPIC]
 _SDG_COMPARISON_PLACES = [
     'country/USA', 'country/CHN', 'country/JPN', 'country/IND'
 ]
-# Max number of places; Choose 6 to match the bar_tile bar limits.s
+# Max number of places; Choose 6 to match the bar_tile bar limits.
 _MAX_NUM_PLACES = 6
 
 bp = flask.Blueprint('topic_page', __name__, url_prefix='/topic')
+
+
+def get_sdg_config(place_dcid, topic_config):
+  topic_place_config = topic_config
+  topic_place_config.metadata.place_dcid.append(place_dcid)
+  # Populuate comparison places (fixed places) for all tiles
+  comparison_places = [place_dcid]
+  more_places = request.args.getlist('places')
+  if more_places:
+    comparison_places.extend(more_places)
+  for p in _SDG_COMPARISON_PLACES:
+    if len(comparison_places) == _MAX_NUM_PLACES:
+      break
+    if p not in comparison_places:
+      comparison_places.append(p)
+  for category in topic_place_config.categories:
+    for block in category.blocks:
+      for column in block.columns:
+        for tile in column.tiles:
+          tile.comparison_places.extend(comparison_places)
+  topic_place_config = lib_subject_page_config.remove_empty_charts(
+      topic_place_config, place_dcid, '')
+  return topic_place_config
 
 
 @bp.route('', strict_slashes=False)
@@ -75,32 +98,14 @@ def topic_page(topic_id=None, place_dcid=None):
   # Find the config for the topic & place.
   topic_place_config = None
   if topic_id == _SDG_TOPIC:
-    topic_place_config = topic_configs[0]
-    topic_place_config.metadata.place_dcid.append(place_dcid)
-    # Populuate comparison places (fixed places) for all tiles
-    comparison_places = [place_dcid]
-    more_places = request.args.getlist('places')
-    if more_places:
-      comparison_places.extend(more_places)
-    for p in _SDG_COMPARISON_PLACES:
-      if p not in comparison_places:
-        comparison_places.append(p)
-        if len(comparison_places) == _MAX_NUM_PLACES:
-          break
-    for category in topic_place_config.categories:
-      for block in category.blocks:
-        for column in block.columns:
-          for tile in column.tiles:
-            tile.comparison_places.extend(comparison_places)
-    topic_place_config = lib_subject_page_config.remove_empty_charts(
-        topic_place_config, place_dcid, '')
+    topic_place_config = get_sdg_config(place_dcid, topic_configs[0])
   else:
     for config in topic_configs:
       if place_dcid in config.metadata.place_dcid:
         topic_place_config = config
         break
-    if not topic_place_config:
-      return "Error: no config found"
+  if not topic_place_config:
+    return "Error: no config found"
 
   # TODO: should use place metadata API to fetch these data in one call.
   place_type = place_api.get_place_type(place_dcid)
