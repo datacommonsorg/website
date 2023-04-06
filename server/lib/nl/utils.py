6f1d7983d6d623fed9442f02eba7e3671a5d3e71
Prashanth R <shanth@google.com>
2023-04-06 13:49:55 -0700

    NL: Use a score different threshold for multi-sv (#2551)
    This is to handle cases where multi-sv is behind very slightly from the
    single-sv score. Also don't truncate multi-sv scores.
    
    Before:
    
    
    ![image](https://user-images.githubusercontent.com/4375037/230481830-ffbaf80c-065c-4d57-8974-fe3abbbcd65f.png)
    
    After:
    
    
    ![image](https://user-images.githubusercontent.com/4375037/230481865-2fbd436c-dfea-4640-9476-222c8ee28956.png)

--- server/lib/nl/utils.py
+++ server/lib/nl/utils.py
@@ -753,6 +753,13 @@ def is_multi_sv(uttr: nl_uttr.Utterance) -> bool:
   if (not uttr.detection.svs_detected.sv_scores or not uttr.multi_svs or
       not uttr.multi_svs.candidates):
     return False
+  # Get the top single-sv and multi-sv scores.
   top_sv_score = uttr.detection.svs_detected.sv_scores[0]
   top_multi_sv_score = uttr.multi_svs.candidates[0].aggregate_score
-  return top_multi_sv_score > top_sv_score
+
+  # Prefer multi-sv when the scores are higher or up to a score differential.
+  if (top_multi_sv_score > top_sv_score or
+      (top_sv_score - top_multi_sv_score <=
+       shared_constants.MULTI_SV_SCORE_DIFFERENTIAL)):
+    return True
+  return False
