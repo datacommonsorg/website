701f4268802f929cd7bdc0805f3c7e43346d498b
natalie <77713883+n-h-diaz@users.noreply.github.com>
2023-04-26 20:48:15 -0700

    update sdg topic page (#2615)

--- tools/sdg/main.py
+++ tools/sdg/main.py
@@ -22,7 +22,7 @@ from subject_page_pb2 import Tile
 
 logging.getLogger().setLevel(logging.INFO)
 
-_SDG_ROOT = "sdg/g/SDG"
+_SDG_ROOT = "dc/g/SDG"
 API_ROOT = "https://autopush.api.datacommons.org"
 API_PATH_SVG_INFO = API_ROOT + '/v1/bulk/info/variable-group'
 _MAX_BLOCKS = 20
@@ -38,6 +38,7 @@ def write_page_config(page_config):
 
 def post(url, req):
   headers = {'Content-Type': 'application/json'}
+  # Set MIXER_API_KEY to the autopush API key
   mixer_api_key = os.environ.get('MIXER_API_KEY', '')
   if mixer_api_key:
     headers['x-api-key'] = mixer_api_key
