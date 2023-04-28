09fbc8c58109434162646931749c482bf87a62bc
Jehangir Amjad <jehangiramjad@gmail.com>
2023-04-28 09:11:20 -0700

    Adding validation tests for embeddings + updating the various embeddings files based on validation error corrections

--- tools/nl/embeddings/build_embeddings_v2_test.py
+++ tools/nl/embeddings/build_embeddings_v2_test.py
@@ -51,6 +51,27 @@ def _compare_files(t, output_path, expected_path):
       t.assertEqual(got, want)
 
 
+def _validate_sentence_to_dcid_map(t, df, dcid_col, sentence_columns):
+  sentence_dcid_map = {}
+  for _, row in df.iterrows():
+    dcid = row[dcid_col]
+
+    for col in sentence_columns:
+      for s in row[col].split(";"):
+        if not s:
+          continue
+        if s in sentence_dcid_map:
+          # Sentence already exists. It could either be a duplicate for the
+          # same SV dcid (which is Ok) OR the same sentence maps to a different
+          # DCID (which is not OK).
+          t.assertEqual(
+              sentence_dcid_map[s], dcid,
+              f"Using the same sentence for more than one StatVar. Sentence: {s}. SV_1: {sentence_dcid_map[s]}, SV_2: {dcid}"
+          )
+        else:
+          sentence_dcid_map[s] = dcid
+
+
 class TestEndToEnd(unittest.TestCase):
 
   def testFailure(self):
@@ -137,3 +158,126 @@ class TestEndToEnd(unittest.TestCase):
                      expected_local_merged_filepath)
       _compare_files(self, tmp_dcid_sentence_csv,
                      expected_dcid_sentence_csv_filepath)
+
+
+class TestEndToEndActualDataFiles(unittest.TestCase):
+
+  def testInputFilesValidations(self):
+    # Verify that the required files exist.
+    sheets_filepath = os.path.join("sheets", "dc_nl_svs_curated.csv")
+    input_other_alternatives_filepath = os.path.join("csv",
+                                                     "other_alternatives.csv")
+    input_palm_alternatives_filepath = os.path.join("csv",
+                                                    "palm_alternatives.csv")
+    output_dcid_sentences_filepath = os.path.join(
+        "csv", "embeddings_input_sv_descriptions.csv")
+
+    # Check that all the files exist.
+    self.assertTrue(os.path.exists(sheets_filepath))
+    self.assertTrue(os.path.exists(input_other_alternatives_filepath))
+    self.assertTrue(os.path.exists(input_palm_alternatives_filepath))
+    self.assertTrue(os.path.exists(output_dcid_sentences_filepath))
+
+    # Perform input file validations.
+
+    # The input data files should not have the same sentence/description map to
+    # different SV dcids.
+    sheets_df = pd.read_csv(sheets_filepath).fillna("")
+    expected_cols = [
+        "dcid", "Name", "Description", "Override_Alternatives",
+        "Curated_Alternatives"
+    ]
+    sheets_sentence_cols = expected_cols[1:]
+
+    self.assertCountEqual(expected_cols, sheets_df.columns.values)
+    _validate_sentence_to_dcid_map(self, sheets_df, "dcid",
+                                   sheets_sentence_cols)
+
+    palm_alts_df = pd.read_csv(input_palm_alternatives_filepath).fillna("")
+    other_alts_df = pd.read_csv(input_other_alternatives_filepath).fillna("")
+    expected_cols = ["dcid", "Alternatives"]
+    alt_sentence_cols = expected_cols[1:]
+
+    self.assertCountEqual(expected_cols, palm_alts_df.columns.values)
+    self.assertCountEqual(expected_cols, other_alts_df.columns.values)
+    _validate_sentence_to_dcid_map(self, palm_alts_df, "dcid",
+                                   alt_sentence_cols)
+    _validate_sentence_to_dcid_map(self, other_alts_df, "dcid",
+                                   alt_sentence_cols)
+
+  def testOutputFileValidations(self):
+    output_dcid_sentences_filepath = os.path.join(
+        "csv", "embeddings_input_sv_descriptions.csv")
+
+    dcid_sentence_df = pd.read_csv(output_dcid_sentences_filepath).fillna("")
+
+    # Check that the dcids do not have duplicate rows.
+    self.assertEqual(len(dcid_sentence_df),
+                     len(dcid_sentence_df["dcid"].unique()))
+    self.assertEqual(len(dcid_sentence_df),
+                     len(dcid_sentence_df["sentence"].unique()))
+
+    # Check that there are no duplicate sentences.
+    sentences = {}
+    for row_index, row in dcid_sentence_df.iterrows():
+      sv_dcid = row["dcid"]
+      for s in row["sentence"].split(";"):
+        if not s:
+          continue
+
+        existing_sv_index = sentences.get(s, ())
+        err_msg = f"\nSentence already exists: {s}.\nCurrent (SV, index): {sv_dcid, row_index}.\nExisting (SV, index): {existing_sv_index}."
+        self.assertFalse(s in sentences, err_msg)
+        sentences[s] = (sv_dcid, row_index)
+
+  def testEmbeddingsValidations(self):
+    # TODO: consider moving this test to a separate file which is
+    # tiggered on demand. This test can take a while to run (esp when the
+    # number of SVs and sentences becomes larger in the future.
+    sheets_filepath = os.path.join("sheets", "dc_nl_svs_curated.csv")
+    input_other_alternatives_filepath = os.path.join("csv",
+                                                     "other_alternatives.csv")
+    input_palm_alternatives_filepath = os.path.join("csv",
+                                                    "palm_alternatives.csv")
+    output_dcid_sentences_filepath = os.path.join(
+        "csv", "embeddings_input_sv_descriptions.csv")
+
+    # Mock the get_sheets_data function to return data from sheets_filepath.
+    be.get_sheets_data = mock.Mock(
+        return_value=pd.read_csv(sheets_filepath).fillna(""))
+
+    # Given that the get_sheets_data() function is mocked, the Context
+    # object does not need a valid `gs` and `bucket` field.
+    ctx = be.Context(gs=None,
+                     model=SentenceTransformer("all-MiniLM-L6-v2"),
+                     bucket="",
+                     tmp="/tmp")
+
+    # Smilarly, sheets_url and worksheet_name can be empty strings.
+    sheets_url = ""
+    worksheet_name = ""
+
+    # Intermediate outputs can be written to a temporary directory.
+    with tempfile.TemporaryDirectory() as tmp_dir:
+      tmp_local_sheets_csv_filepath = os.path.join(tmp_dir, "sheets_data.csv")
+      tmp_local_merged_filepath = os.path.join(tmp_dir, "merged_data.csv")
+
+      embeddings_df = be.build(
+          ctx, sheets_url, worksheet_name, tmp_local_sheets_csv_filepath,
+          tmp_local_merged_filepath,
+          [input_other_alternatives_filepath, input_palm_alternatives_filepath])
+
+      # Verify that embeddings were created for all DCIDs and Sentences.
+      dcid_sentence_df = pd.read_csv(output_dcid_sentences_filepath).fillna("")
+      sentences = set()
+      for alts in dcid_sentence_df["sentence"].values:
+        for s in alts.split(";"):
+          if not s:
+            continue
+          sentences.add(s)
+
+      self.assertEqual(len(sentences), len(embeddings_df))
+
+      # Verify that the number of columns = length of the embeddings vector + one each for the
+      # dcid and sentence columns.
+      self.assertEqual(len(embeddings_df.columns), 384 + 2)
