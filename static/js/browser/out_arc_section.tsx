e2cb969b741f9a3c17d258c9d69a3d0a9f358f5b
Bo Xu <shifucun@users.noreply.github.com>
2023-05-01 22:11:56 +0000

    [V2 API] Migrate more node callers to use V2 API (#2625)
    The migration in general makes the callers simpler and easy to use.
    
    One big simplification is to fetch triples in the browser page, instead
    of fetch labels then fetch property values.

--- static/js/browser/out_arc_section.tsx
+++ static/js/browser/out_arc_section.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Google LLC
+ * Copyright 2023 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ import axios from "axios";
 import _ from "lodash";
 import React from "react";
 
+import { PropertyValues } from "../shared/api_response_types";
 import { loadSpinner, removeSpinner } from "../shared/util";
 import { ArcTableRow } from "./arc_table_row";
 import { ArcValue } from "./types";
@@ -50,7 +51,6 @@ const IGNORED_OUT_ARC_PROPERTIES = new Set([
 
 interface OutArcSectionPropType {
   dcid: string;
-  labels: string[];
   provDomain: { [key: string]: URL };
   nodeTypes: string[];
   showAllProperties: boolean;
@@ -150,102 +150,29 @@ export class OutArcSection extends React.Component<
   private notANodeMessage = `${this.props.dcid} is not a node.`;
 
   private fetchData(): void {
-    // If a node doesn't have out arc property labels (ie. Observation Nodes),
-    // try getting the out arcs from triples data.
-    if (_.isEmpty(this.props.labels)) {
-      this.fetchDataFromTriples();
-      return;
-    }
-    const propValuesPromises = this.props.labels.map((label) => {
-      if (
-        this.props.showAllProperties ||
-        !IGNORED_OUT_ARC_PROPERTIES.has(label)
-      ) {
-        return axios
-          .get(`/api/browser/propvals/${label}/${this.props.dcid}`)
-          .then((resp) => resp.data);
-      }
-    });
-    loadSpinner(LOADING_CONTAINER_ID);
-    Promise.all(propValuesPromises)
-      .then((propValuesData) => {
-        const outArcsByPredProv: OutArcData = {};
-        propValuesData.forEach((valuesData) => {
-          if (!valuesData || _.isEmpty(valuesData.values)) {
-            return;
-          }
-          const predicate = valuesData.property;
-          const values = valuesData.values.out;
-          for (const value of values) {
-            if (!(predicate in outArcsByPredProv)) {
-              outArcsByPredProv[predicate] = {};
-            }
-            const outArcsOfPredicate = outArcsByPredProv[predicate];
-            const provId = value.provenanceId;
-            if (!(provId in outArcsOfPredicate)) {
-              outArcsOfPredicate[provId] = [];
-            }
-            let valueText = "";
-            if (value.dcid) {
-              valueText = value.name ? value.name : value.dcid;
-            } else {
-              valueText = value.value;
-            }
-            outArcsOfPredicate[provId].push({
-              dcid: value.dcid,
-              text: valueText,
-            });
-          }
-        });
-        removeSpinner(LOADING_CONTAINER_ID);
-        this.setState({
-          data: outArcsByPredProv,
-          isDataFetched: true,
-        });
-      })
-      .catch(() => {
-        removeSpinner(LOADING_CONTAINER_ID);
-        this.setState({
-          errorMessage: "Error retrieving property values.",
-          isDataFetched: true,
-        });
-      });
-  }
-
-  private predicateComparator = (a: string, b: string): number => {
-    if (a === "typeOf") {
-      return -1;
-    }
-    if (b === "typeOf") {
-      return 1;
-    }
-    return a > b ? 1 : -1;
-  };
-
-  private fetchDataFromTriples(): void {
     loadSpinner(LOADING_CONTAINER_ID);
     axios
-      .get("/api/node/triples/out/" + this.props.dcid)
+      .get(`/api/node/triples/out/${this.props.dcid}`)
       .then((resp) => {
-        const triplesData = resp.data;
+        const triplesData: PropertyValues = resp.data;
         const outArcsByPredProv: OutArcData = {};
         for (const pred in triplesData) {
           if (IGNORED_OUT_ARC_PROPERTIES.has(pred)) {
             continue;
           }
           const predData = {};
-          for (const node of triplesData[pred]["nodes"]) {
-            const provId = node["provenanceId"];
+          for (const node of triplesData[pred]) {
+            const provId = node.provenanceId;
             if (!(provId in predData)) {
               predData[provId] = [];
             }
             let valueText = "";
             let valueDcid: string;
-            if (node["dcid"]) {
-              valueText = node["name"] ? node["name"] : node["dcid"];
-              valueDcid = node["dcid"];
+            if (node.dcid) {
+              valueText = node.name ? node.name : node.dcid;
+              valueDcid = node.dcid;
             } else {
-              valueText = node["value"];
+              valueText = node.value;
             }
             predData[provId].push({
               dcid: valueDcid,
@@ -268,4 +195,14 @@ export class OutArcSection extends React.Component<
         });
       });
   }
+
+  private predicateComparator = (a: string, b: string): number => {
+    if (a === "typeOf") {
+      return -1;
+    }
+    if (b === "typeOf") {
+      return 1;
+    }
+    return a > b ? 1 : -1;
+  };
 }
