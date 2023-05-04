32df815da04a93513505ef111761c220efa72c16
Bo Xu <shifucun@users.noreply.github.com>
2023-05-04 14:40:03 -0700

    [Node Server] Render line tile on server side and return svg

--- static/src/server.ts
+++ static/src/server.ts
@@ -14,26 +14,56 @@
  * limitations under the License.
  */
 
-// import { LineTile } from "../../static/js/components/tiles/line_tile";
-
 import axios from "axios";
 import express, { Request, Response } from "express";
-import React from "react";
-import ReactDOMServer from "react-dom/server";
+import { JSDOM } from "jsdom";
+
+import { draw, fetchData } from "../../static/js/components/tiles/line_tile";
 
 const app = express();
 const port = process.env.PORT || 3030;
 
+const apiRoot = "http://127.0.0.1:8080";
+
+const dom = new JSDOM(
+  `<html><body><div id="dom-id" style="width:500px"></div></body></html>`,
+  {
+    pretendToBeVisual: true,
+  }
+);
+const window = dom.window;
+global.document = dom.window.document;
+
+(window.Text as any).prototype.getComputedTextLength = function () {
+  return this.textContent.length;
+};
+
+(window.SVGElement as any).prototype.getComputedTextLength = function () {
+  return this.textContent.length;
+};
+
+// JSDom does not define SVGTSpanElements, and use SVGElement instead. Defines
+// a shim for getBBox (only returns width) where each character is 1 px wide.
+(window.Element as any).prototype.getBBox = function () {
+  let maxWidth = 0;
+  const children = this.childNodes;
+  for (let i = 0; i < children.length; i++) {
+    maxWidth = Math.max(children[i].getComputedTextLength(), maxWidth);
+  }
+  return { width: maxWidth };
+};
+
 app.get("/", (req: Request, res: Response) => {
   const query = req.query.q;
   axios
-    .post(`http://127.0.0.1:8080/nl/data?q=${query}`)
+    .post(`${apiRoot}/nl/data?q=${query}`)
     .then((resp) => {
       const tiles = [];
       const svSpec = [];
       const category = resp.data["config"]["categories"][0];
       for (const sv in category["statVarSpec"]) {
         svSpec.push(category["statVarSpec"][sv]);
+        break;
       }
       for (const block of category["blocks"]) {
         for (const column of block["columns"]) {
@@ -45,19 +75,23 @@ app.get("/", (req: Request, res: Response) => {
         }
       }
       // const tile = tiles[0];
-      // const id = "dom-id";
-      const element = ReactDOMServer.renderToString(
-        // React.createElement(LineTile, {
-        //   id: id,
-        //   title: id,
-        //   place: { name: "California", dcid: "geoId/06", types: ["State"] },
-        //   statVarSpec: svSpec,
-        //   svgChartHeight: 100,
-        // })
-        React.createElement(React.Fragment)
-      );
-      console.log(element);
-      res.send(element);
+      const id = "dom-id";
+      const lineTileProp = {
+        id: id,
+        title: id,
+        place: { name: "California", dcid: "geoId/06", types: ["State"] },
+        statVarSpec: svSpec,
+        svgChartHeight: 500,
+        svgChartWidth: 500,
+        apiRoot: apiRoot,
+      };
+
+      fetchData(lineTileProp).then((chartData) => {
+        draw(lineTileProp, chartData, null);
+        const svg = document.querySelector("svg");
+        res.setHeader("Content-Type", "text/html");
+        res.send(svg.outerHTML);
+      });
     })
     .catch((error) => {
       console.error("Error making request:\n", error.message);
