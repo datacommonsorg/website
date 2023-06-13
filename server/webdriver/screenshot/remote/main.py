812c947748acb5fe039c44df2c5e6991ac01dad2
Bo Xu <shifucun@users.noreply.github.com>
2023-06-13 14:24:34 -0700

    fix

--- server/webdriver/screenshot/remote/main.py
+++ server/webdriver/screenshot/remote/main.py
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
-os.environ["FLASK_ENV"] = "webdriver"
-
 import argparse
 import logging
 
