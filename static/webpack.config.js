e796209ed7a6b2c511f29e3f17701c8e1d9e6f4a
Dan Noble <dwnoble@google.com>
2023-07-12 14:30:53 -0700

    Added datacommons.css

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -107,10 +107,14 @@ const config = {
       __dirname + "/js/apps/sustainability/main.ts",
       __dirname + "/css/sustainability.scss",
     ],
-    datacommons: [__dirname + "/library/index.ts"],
+    datacommons: [
+      __dirname + "/library/index.ts",
+      __dirname + "/css/datacommons.scss",
+    ],
     homepage: [
       __dirname + "/js/homepage/main.ts",
-      __dirname + "/css/homepage.scss"]
+      __dirname + "/css/homepage.scss",
+    ],
   },
   output: {
     path: path.resolve(__dirname, "../") + "/server/dist",
