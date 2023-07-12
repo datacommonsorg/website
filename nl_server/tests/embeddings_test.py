1d053762fab792b755ca1118bcc48bc5bf067ba3
Jehangir Amjad <jehangiramjad@gmail.com>
2023-07-12 14:41:59 -0700

    fixing the local cache bug

--- nl_server/tests/embeddings_test.py
+++ nl_server/tests/embeddings_test.py
@@ -33,6 +33,7 @@ _root_dir = os.path.dirname(
 _test_data = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                           'test_data')
 
+_tuned_model_key = "tuned_model"
 
 # TODO(pradh): Expand tests to other index sizes.
 def _get_embeddings_file_path() -> str:
@@ -42,6 +43,13 @@ def _get_embeddings_file_path() -> str:
     embeddings_file = embeddings[loader.DEFAULT_INDEX_TYPE]
     return gcs.download_embeddings(embeddings_file)
 
+def _get_tuned_model_path() -> str:
+  models_config_path = os.path.join(_root_dir, 'deploy/nl/models.yaml')
+  with open(models_config_path) as f:
+    models_map = yaml.full_load(f)
+    tuned_model_dict = {_tuned_model_key: models_map[_tuned_model_key]}
+    models_downloaded_paths = loader.download_models(tuned_model_dict)
+    return models_downloaded_paths[models_map[_tuned_model_key]]
 
 class TestEmbeddings(unittest.TestCase):
 
@@ -56,8 +64,11 @@ class TestEmbeddings(unittest.TestCase):
       print(
           "Could not load the embeddings from the cache for these tests. Loading a new embeddings object."
       )
-      # Using the default NER model.
-      cls.nl_embeddings = Embeddings(_get_embeddings_file_path())
+      # Building a new Embeddings object. It might require downloading the embeddings file
+      # and a finetuned model.
+      # This uses the default embeddings pointed to in embeddings.yaml file and the fine tuned 
+      # model pointed to in models.yaml.
+      cls.nl_embeddings = Embeddings(_get_embeddings_file_path(), _get_tuned_model_path())
 
   @parameterized.expand([
       # All these queries should detect one of the SVs as the top choice.
