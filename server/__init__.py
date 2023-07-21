11fb3b5d847aad0ec62053385a7c70a46a6e4d0b
Dan Noble <dwnoble@google.com>
2023-07-21 15:43:26 -0700

    Added insights skeleton page

--- server/__init__.py
+++ server/__init__.py
@@ -159,7 +159,10 @@ def register_routes_common(app):
 
   from server.routes.factcheck import html as factcheck_html
   app.register_blueprint(factcheck_html.bp)
-
+  
+  from server.routes.insights import html as insights_html
+  app.register_blueprint(insights_html.bp)
+  
   from server.routes.nl import html as nl_html
   app.register_blueprint(nl_html.bp)
 
