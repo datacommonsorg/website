360dbc5dd95be45900575f981215280f2e537f93
chejennifer <69875368+chejennifer@users.noreply.github.com>
2023-07-26 09:33:02 -0700

    [tools revamp] add per capita and log options (#3031)
    - add per capita and log options
    
    note: 
    - the render chart function is getting a little out of hand. To make it
    easier to see the actual logic/code changes, I will do the clean up in a
    separate PR
    
    ![screen-recording-2023-07-26T090816
    941](https://github.com/datacommonsorg/website/assets/69875368/16e5889a-2d70-42aa-a65b-d98a022f6945)

--- static/js/apps/visualization/chart.tsx
+++ static/js/apps/visualization/chart.tsx
@@ -17,12 +17,20 @@
 /**
  * Component that draws a chart based off the state of the visualization tool.
  */
-
+import _ from "lodash";
 import React, { useContext } from "react";
+import { FormGroup, Input, Label } from "reactstrap";
 
 import { LineTile } from "../../components/tiles/line_tile";
 import { MapTile } from "../../components/tiles/map_tile";
 import { ScatterTile } from "../../components/tiles/scatter_tile";
+import {
+  GA_EVENT_TOOL_CHART_OPTION_CLICK,
+  GA_PARAM_TOOL_CHART_OPTION,
+  GA_VALUE_TOOL_CHART_OPTION_LOG_SCALE,
+  GA_VALUE_TOOL_CHART_OPTION_PER_CAPITA,
+  triggerGAEvent,
+} from "../../shared/ga_events";
 import { StatVarInfo } from "../../shared/stat_var";
 import { getStatVarGroups } from "../../utils/app/timeline_utils";
 import { isSelectionComplete } from "../../utils/app/visualization_utils";
@@ -30,16 +38,18 @@ import { AppContext } from "./app_context";
 import { StatVarSelector } from "./stat_var_selector";
 import { VisType } from "./vis_type_configs";
 
+const DEFAULT_DENOM = "Count_Person";
+
 export function Chart(): JSX.Element {
-  const { visType, places, statVars, enclosedPlaceType } =
+  const { visType, places, statVars, enclosedPlaceType, setStatVars } =
     useContext(AppContext);
 
-  function renderChart(): JSX.Element {
+  function renderCharts(): JSX.Element {
     const chartHeight = window.innerHeight * 0.45;
     const statVarSpecs = statVars.map((sv) => {
       return {
-        denom: "",
-        log: false,
+        denom: sv.isPerCapita ? "Count_Person" : "",
+        log: visType === VisType.SCATTER && sv.isLog,
         name: sv.info.title || sv.dcid,
         scaling: 1,
         statVar: sv.dcid,
@@ -52,42 +62,84 @@ export function Chart(): JSX.Element {
       groups: { [groupId: string]: string[] };
       chartOrder: string[];
     };
+    let perCapitaInputs = [];
+    let logInputs = [];
     switch (visType) {
       case VisType.MAP:
+        perCapitaInputs = [
+          {
+            isChecked: statVars[0].isPerCapita,
+            onUpdated: (isChecked: boolean) => {
+              const newStatVars = _.cloneDeep(statVars);
+              newStatVars[0].isPerCapita = isChecked;
+              setStatVars(newStatVars);
+            },
+            label: "Per Capita",
+          },
+        ];
         return (
-          <MapTile
-            id="vis-tool-map"
-            place={places[0]}
-            statVarSpec={statVarSpecs[0]}
-            enclosedPlaceType={enclosedPlaceType}
-            svgChartHeight={chartHeight}
-            title={statVarLabels[0] + " (${date})"}
-            showLoadingSpinner={true}
-          />
+          <div>
+            <MapTile
+              id="vis-tool-map"
+              place={places[0]}
+              statVarSpec={statVarSpecs[0]}
+              enclosedPlaceType={enclosedPlaceType}
+              svgChartHeight={chartHeight}
+              title={statVarLabels[0] + " (${date})"}
+              showLoadingSpinner={true}
+            />
+            {renderFooterOptions(perCapitaInputs, logInputs)}
+          </div>
         );
       case VisType.SCATTER:
+        perCapitaInputs = statVars.slice(0, 2).map((sv, idx) => {
+          return {
+            isChecked: sv.isPerCapita,
+            onUpdated: (isChecked) => {
+              const newStatVars = _.cloneDeep(statVars);
+              newStatVars[idx].isPerCapita = isChecked;
+              setStatVars(newStatVars);
+            },
+            label: idx === 0 ? "y Per Capita" : "x Per Capita",
+          };
+        });
+        logInputs = statVars.slice(0, 2).map((sv, idx) => {
+          return {
+            isChecked: sv.isLog,
+            onUpdated: (isChecked) => {
+              const newStatVars = _.cloneDeep(statVars);
+              newStatVars[idx].isLog = isChecked;
+              setStatVars(newStatVars);
+            },
+            label: idx === 0 ? "y Log Scale" : "x Log Scale",
+          };
+        });
         return (
-          <ScatterTile
-            id="vis-tool-scatter"
-            title={
-              statVarLabels[0] +
-              " (${yDate}) vs " +
-              statVarLabels[1] +
-              " (${xDate})"
-            }
-            place={places[0]}
-            enclosedPlaceType={enclosedPlaceType}
-            statVarSpec={statVarSpecs}
-            svgChartHeight={chartHeight}
-            scatterTileSpec={{}}
-            showLoadingSpinner={true}
-          />
+          <div>
+            <ScatterTile
+              id="vis-tool-scatter"
+              title={
+                statVarLabels[0] +
+                " (${yDate}) vs " +
+                statVarLabels[1] +
+                " (${xDate})"
+              }
+              place={places[0]}
+              enclosedPlaceType={enclosedPlaceType}
+              statVarSpec={statVarSpecs}
+              svgChartHeight={chartHeight}
+              scatterTileSpec={{}}
+              showLoadingSpinner={true}
+            />
+            {renderFooterOptions(perCapitaInputs, logInputs)}
+          </div>
         );
       case VisType.TIMELINE:
         statVars.forEach((sv) => (statVarInfo[sv.dcid] = sv.info));
         lineChartGrouping = getStatVarGroups(
           statVars.map((sv) => sv.dcid),
-          statVarInfo
+          statVarInfo,
+          new Set(statVars.filter((sv) => sv.isPerCapita).map((sv) => sv.dcid))
         );
         return (
           <>
@@ -96,16 +148,36 @@ export function Chart(): JSX.Element {
               const chartSvSpecs = statVarSpecs.filter((sv) =>
                 chartSvs.has(sv.statVar)
               );
+              const chartSVInfo = statVars.filter((sv) =>
+                chartSvs.has(sv.dcid)
+              );
+              const chartPCInputs = [
+                {
+                  isChecked: chartSVInfo[0].isPerCapita,
+                  onUpdated: (isChecked: boolean) => {
+                    const newStatVars = _.cloneDeep(statVars);
+                    statVars.forEach((sv, idx) => {
+                      if (chartSvs.has(sv.dcid)) {
+                        newStatVars[idx].isPerCapita = isChecked;
+                      }
+                    });
+                    setStatVars(newStatVars);
+                  },
+                  label: "Per Capita",
+                },
+              ];
               return (
-                <LineTile
-                  key={chartId}
-                  comparisonPlaces={places.map((place) => place.dcid)}
-                  id={`vis-tool-timeline-${chartId}`}
-                  title=""
-                  statVarSpec={chartSvSpecs}
-                  svgChartHeight={chartHeight}
-                  place={places[0]}
-                />
+                <div key={chartId}>
+                  <LineTile
+                    comparisonPlaces={places.map((place) => place.dcid)}
+                    id={`vis-tool-timeline-${chartId}`}
+                    title=""
+                    statVarSpec={chartSvSpecs}
+                    svgChartHeight={chartHeight}
+                    place={places[0]}
+                  />
+                  {renderFooterOptions(chartPCInputs, logInputs)}
+                </div>
               );
             })}
           </>
@@ -125,7 +197,75 @@ export function Chart(): JSX.Element {
         <div className="title">Variables</div>
         <StatVarSelector />
       </div>
-      <div className="chart-area">{renderChart()}</div>
+      <div className="chart-area">{renderCharts()}</div>
+    </div>
+  );
+}
+
+interface InputInfo {
+  isChecked: boolean;
+  onUpdated: (isChecked: boolean) => void;
+  label: string;
+}
+
+function renderFooterOptions(
+  perCapitaInputs: InputInfo[],
+  logInputs: InputInfo[]
+): JSX.Element {
+  return (
+    <div className="chart-footer-options">
+      <div className="option-section">
+        {perCapitaInputs.map((pcInput, idx) => {
+          return (
+            <span className="chart-option" key={`pc-${idx}`}>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    type="checkbox"
+                    checked={pcInput.isChecked}
+                    onChange={() => {
+                      pcInput.onUpdated(!pcInput.isChecked);
+                      if (!pcInput.isChecked) {
+                        triggerGAEvent(GA_EVENT_TOOL_CHART_OPTION_CLICK, {
+                          [GA_PARAM_TOOL_CHART_OPTION]:
+                            GA_VALUE_TOOL_CHART_OPTION_PER_CAPITA,
+                        });
+                      }
+                    }}
+                  />
+                  {pcInput.label}
+                </Label>
+              </FormGroup>
+            </span>
+          );
+        })}
+      </div>
+      <div className="option-section">
+        {logInputs.map((logInput, idx) => {
+          return (
+            <span className="chart-option" key={`log-${idx}`}>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    type="checkbox"
+                    checked={logInput.isChecked}
+                    onChange={() => {
+                      logInput.onUpdated(!logInput.isChecked);
+                      if (!logInput.isChecked) {
+                        triggerGAEvent(GA_EVENT_TOOL_CHART_OPTION_CLICK, {
+                          [GA_PARAM_TOOL_CHART_OPTION]:
+                            GA_VALUE_TOOL_CHART_OPTION_LOG_SCALE,
+                        });
+                      }
+                    }}
+                  />
+                  {logInput.label}
+                </Label>
+              </FormGroup>
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 }
