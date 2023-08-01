1c2a5c4739e84e2fca6371f4c3e1059bc8b8018a
Jennifer Chen <chejennifer@google.com>
2023-08-01 12:41:04 -0700

    add flag to hide website revamp changes from prod and staging

--- server/app_env/_base.py
+++ server/app_env/_base.py
@@ -65,3 +65,5 @@ class Config:
   # overrides in the default base template. Will not be compiled. Must be the
   # full serving path from /static folder.
   OVERRIDE_CSS_PATH = ''
+  # Hide changes made for the website revamp.
+  HIDE_REVAMP_CHANGES = False
