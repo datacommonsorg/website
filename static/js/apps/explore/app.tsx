3cdbb5665145b67c06747643c578dd368b827e1b
Bo Xu <shifucun@users.noreply.github.com>
2023-08-07 23:51:49 +0000

    Add a url param for sdg mode; Fix some react lint issues; (#3152)

--- static/js/apps/explore/app.tsx
+++ static/js/apps/explore/app.tsx
@@ -21,7 +21,7 @@
 import axios from "axios";
 import _ from "lodash";
 import queryString, { ParsedQuery } from "query-string";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Container } from "reactstrap";
 
 import { Spinner } from "../../components/spinner";
@@ -74,9 +74,9 @@ export function App(): JSX.Element {
   const [hashParams, setHashParams] = useState<ParsedQuery<string>>(
     queryString.parse(window.location.hash)
   );
-  const [savedContext, setSavedContext] = useState<any>([]);
   const [query, setQuery] = useState<string>("");
   const [debugData, setDebugData] = useState<any>({});
+  const savedContext = useRef([]);
 
   useEffect(() => {
     const handleHashChange = () => {
@@ -105,6 +105,7 @@ export function App(): JSX.Element {
       let placeType = getSingleParam(hashParams["pt"]);
       let query = getSingleParam(hashParams["q"]);
       const origQuery = getSingleParam(hashParams["oq"]);
+      const dc = getSingleParam(hashParams["dc"]);
 
       // Do detection only if `q` is set (from search box) or
       // if `oq` is set without accompanying place and topic.
@@ -114,12 +115,16 @@ export function App(): JSX.Element {
           query = origQuery;
         }
         setQuery(query);
-        const detectResp = await fetchDetectData(query, savedContext);
+        const detectResp = await fetchDetectData(
+          query,
+          savedContext.current,
+          dc
+        );
         if (!detectResp) {
           setLoadingStatus("fail");
           return;
         }
-        setSavedContext(detectResp["context"] || []);
+        savedContext.current = detectResp["context"] || [];
         if (_.isEmpty(detectResp["entities"])) {
           setLoadingStatus("fail");
           return;
@@ -168,7 +173,8 @@ export function App(): JSX.Element {
         topics,
         placeType,
         cmpPlaces,
-        cmpTopics
+        cmpTopics,
+        dc
       );
       if (!resp || !resp["place"] || !resp["place"]["dcid"]) {
         setLoadingStatus("fail");
@@ -201,7 +207,7 @@ export function App(): JSX.Element {
           }
         }
       }
-      setSavedContext(resp["context"] || {});
+      savedContext.current = resp["context"] || [];
       setLoadingStatus("loaded");
       setChartData(chartData);
       setUserMessage(resp["userMessage"]);
@@ -213,6 +219,7 @@ export function App(): JSX.Element {
   const cmpPlace = getSingleParam(hashParams["pcmp"]);
   const topic = getSingleParam(hashParams["t"]);
   const placeType = getSingleParam(hashParams["pt"]);
+  const dc = getSingleParam(hashParams["dc"]);
 
   const searchSection = (
     <div className="search-section">
@@ -283,18 +290,20 @@ export function App(): JSX.Element {
                     })}
                   </div>
                 )}
-              <ChildPlaces
-                childPlaces={chartData.childPlaces}
-                parentPlace={chartData.place}
-                urlFormatString={urlString}
-              ></ChildPlaces>
+              {dc !== "sdg" && (
+                <ChildPlaces
+                  childPlaces={chartData.childPlaces}
+                  parentPlace={chartData.place}
+                  urlFormatString={urlString}
+                ></ChildPlaces>
+              )}
             </>
           )}
         </div>
         <div className="col-md-10x col-lg-10">
           {chartData && chartData.pageConfig && (
             <>
-              {searchSection}
+              {dc !== "sdg" && searchSection}
               <div id="place-callout">{chartData.place.name}</div>
               {chartData.parentPlaces.length > 0 && (
                 <ParentPlace
@@ -337,7 +346,9 @@ export function App(): JSX.Element {
     FEEDBACK_LINK,
     query || "",
     debugData,
-    _.isEmpty(savedContext) ? null : savedContext[0]["insightCtx"]
+    _.isEmpty(savedContext.current)
+      ? null
+      : savedContext.current[0]["insightCtx"]
   );
 
   return (
@@ -357,10 +368,12 @@ const fetchFulfillData = async (
   topics: string[],
   placeType: string,
   cmpPlaces: string[],
-  cmpTopics: string[]
+  cmpTopics: string[],
+  dc: string
 ) => {
   try {
     const resp = await axios.post(`/api/explore/fulfill`, {
+      dc,
       entities: places,
       variables: topics,
       childEntityType: placeType,
@@ -374,10 +387,15 @@ const fetchFulfillData = async (
   }
 };
 
-const fetchDetectData = async (query: string, savedContext: any) => {
+const fetchDetectData = async (
+  query: string,
+  savedContext: any,
+  dc: string
+) => {
   try {
     const resp = await axios.post(`/api/explore/detect?q=${query}`, {
       contextHistory: savedContext,
+      dc,
     });
     return resp.data;
   } catch (error) {
