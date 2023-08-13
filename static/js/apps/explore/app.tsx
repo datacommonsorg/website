f2bd79997a0d6d612c254d93b300a6c874ecd30b
Prashanth Radhakrishnan <shanth@google.com>
2023-08-13 13:20:44 -0700

    Move explore-more to LHS

--- static/js/apps/explore/app.tsx
+++ static/js/apps/explore/app.tsx
@@ -321,6 +321,27 @@ export function App(): JSX.Element {
                   onClick={() => setQuery("")}
                 ></ChildPlaces>
               )}
+              {chartData.exploreMore && (
+                <div id="explore-more-section">
+                  <div className="explore-more-head">Explore More</div>
+                  {Object.keys(chartData.exploreMore).map((sv) => {
+                    return Object.keys(chartData.exploreMore[sv]).map(
+                      (prop) => {
+                        const urlSv =
+                          chartData.exploreMore[sv][prop].join(DELIM);
+                        const url = `/explore/#t=${urlSv}&p=${place}&pcmp=${cmpPlace}&pt=${placeType}&dc=${dc}&em=1`;
+                        return (
+                          <>
+                            <a className="explore-more-link" href={url}>
+                              [{sv}] Explore more by {prop}
+                            </a>
+                          </>
+                        );
+                      }
+                    );
+                  })}
+                </div>
+              )}
             </>
           )}
         </div>
@@ -359,27 +380,6 @@ export function App(): JSX.Element {
                   />
                 </NlSessionContext.Provider>
               </RankingUnitUrlFuncContext.Provider>
-              {chartData.exploreMore && (
-                <div id="explore-more-section">
-                  <div className="explore-more-head">Explore More</div>
-                  {Object.keys(chartData.exploreMore).map((sv) => {
-                    return Object.keys(chartData.exploreMore[sv]).map(
-                      (prop) => {
-                        const urlSv =
-                          chartData.exploreMore[sv][prop].join(DELIM);
-                        const url = `/explore/#t=${urlSv}&p=${place}&pcmp=${cmpPlace}&pt=${placeType}&dc=${dc}&em=1`;
-                        return (
-                          <>
-                            <a className="explore-more-link" href={url}>
-                              [{sv}] Explore more by {prop}
-                            </a>
-                          </>
-                        );
-                      }
-                    );
-                  })}
-                </div>
-              )}
             </>
           )}
         </div>
