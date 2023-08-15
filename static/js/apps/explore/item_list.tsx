e6a414c04e166d42bd18e1e66e83c4fa15a76e7a
Bo Xu <shifucun@users.noreply.github.com>
2023-08-15 22:23:53 +0000

    Explore page layout update: move LHS content to the top and bottom of the page (#3213)

---
+++ static/js/apps/explore/item_list.tsx
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Component for rendering the side bar of a subject page.
+ */
+
+import React from "react";
+
+export interface Item {
+  text: string;
+  url: string;
+}
+
+interface ItemListPropType {
+  items: Item[];
+}
+
+export function ItemList(props: ItemListPropType): JSX.Element {
+  return (
+    <div className="explore-item-list">
+      {props.items.map((item, idx) => {
+        return (
+          <div key={idx} className="explore-item-box">
+            <span
+              className="item-list-text"
+              onClick={() => {
+                window.open(item.url, "");
+              }}
+            >
+              {item.text}
+            </span>
+            {idx !== props.items.length - 1 && (
+              <span className="item-list-dot">•</span>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
