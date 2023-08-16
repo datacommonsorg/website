datacommonsorg/website

commit a41c1ac74fac83fb1537b663a82356f5ead2740a

UI Changes: Updated search bar for explore pages (#3218)
Updates the styling of our search bars on the explore pages. Also
removes the `experimental` tag in favor of "Early Preview" text.


Screenshots:
![Screenshot 2023-08-15 at 5 06 43
PM](https://github.com/datacommonsorg/website/assets/4034366/377a35b7-3896-45e1-aba5-fc3efa3d5442)

![Screenshot 2023-08-15 at 5 14 42
PM](https://github.com/datacommonsorg/website/assets/4034366/82add0c5-2f8c-4ba9-9099-d5189594c94a)

static/js/apps/nl_interface/query_search.tsx
@@ -47,7 +47,7 @@ export function QuerySearch(props: QuerySearchProps): JSX.Element {
     <div id="search-container">
       <Container tabIndex={-1}>
         <div className="search-section">
-          <div className="experiment-tag">Experiment</div>
+          <div className="experiment-tag">Early Preview</div>
           <div className="search-box-section">
             <TextSearchBar
               allowEmptySearch={

static/css/explore.scss
@@ -66,8 +66,11 @@
 .search-section {
   align-items: center;
   display: flex;
-  flex-direction: row;
-  margin: 20px 0;
+  flex-direction: column;
+  flex-shrink: 0;
+  height: 79px;
+  margin: 24px 0 60px 0;
+  width: 100%;
 
   .btn {
     height: 2.5rem;
@@ -75,17 +78,19 @@
     border-bottom-left-radius: 0;
   }
 
-  .experiment-tag {
-    border-radius: 10px;
-    border-style: solid;
-    border-width: 3px;
-    color: var(--blue);
-    display: flex;
-    float: left;
-    font: normal 0.85em monospace;
-    padding: 4px 6px;
-    height: 80%;
-    margin-right: 20px;
+  .early-preview-tag {
+    color: var(--gm-3-ref-secondary-secondary-30, #004a77);
+    font-family: Google Sans;
+    font-size: 11px;
+    font-style: normal;
+    font-weight: 500;
+    letter-spacing: 0.1px;
+    line-height: 16px;
+    margin-bottom: 6px;
+    margin-left: auto;
+    margin-right: 37.93px;
+    text-align: right;
+    text-transform: uppercase;
   }
 
   .history-button {
@@ -94,6 +99,60 @@
 
   .search-box-section {
     width: 100%;
+  }
+
+  .search-bar {
+    align-items: center;
+    background: var(--gm-3-ref-secondary-secondary-99, #f7fcff);
+    border-radius: 28px;
+    border: 1px solid var(--gm-3-ref-neutral-neutral-90, #e3e3e3);
+    display: flex;
+    flex-shrink: 0;
+    gap: 4px;
+    height: 56px;
+    margin: 1px;
+    padding: 0px;
+    width: 100%;
+
+    .search-bar-content {
+      align-items: center;
+      align-self: stretch;
+      display: flex;
+      flex-shrink: 0;
+      gap: 4px;
+      height: 100%;
+      padding: 4px 8px 4px 24px;
+      width: 100%;
+
+      #rich-search-button {
+        flex-shrink: 0;
+        height: 42px;
+        width: 42px;
+      }
+
+      .search-input-text {
+        align-items: center;
+        align-self: stretch;
+        background: transparent;
+        border: none;
+        color: var(--gm-3-ref-neutral-neutral-40, #5e5e5e);
+        display: flex;
+        flex: 1 0 0;
+        font-family: Google Sans;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        gap: 10px;
+        height: 48px;
+        line-height: 24px;
+        padding: 0px;
+      }
+
+      .search-input-text:focus {
+        // Overwrite default focus
+        box-shadow: none !important;
+      }
+    }
   }
 }
 
@@ -170,7 +229,7 @@
   font-size: 28px;
   line-height: 36px;
   color: #303030;
-  padding: 12px 0;
+  padding-bottom: 12px;
 }
 
 .topic-item {

static/css/explore_landing.scss
@@ -40,8 +40,11 @@
   .explore-search {
     align-items: center;
     display: flex;
-    flex-direction: row;
-    margin: 2rem 0;
+    flex-direction: column;
+    flex-shrink: 0;
+    height: 79px;
+    margin: 24px 0 60px 0;
+    width: 100%;
 
     .btn {
       height: 2.5rem;
@@ -49,21 +52,73 @@
       border-bottom-left-radius: 0;
     }
 
-    .experiment-tag {
-      border-radius: 10px;
-      border-style: solid;
-      border-width: 3px;
-      color: var(--blue);
-      display: flex;
-      float: left;
-      font: normal 1em monospace;
-      padding: 4px 6px;
-      height: 80%;
-      margin-right: 20px;
+    .early-preview-tag {
+      color: var(--gm-3-ref-secondary-secondary-30, #004a77);
+      font-family: Google Sans;
+      font-size: 11px;
+      font-style: normal;
+      font-weight: 500;
+      letter-spacing: 0.1px;
+      line-height: 16px;
+      margin-bottom: 6px;
+      margin-left: auto;
+      margin-right: 37.93px;
+      text-align: right;
+      text-transform: uppercase;
     }
 
-    .input-query {
+    .search-bar {
+      align-items: center;
+      background: var(--gm-3-ref-secondary-secondary-99, #f7fcff);
+      border-radius: 28px;
+      border: 1px solid var(--gm-3-ref-neutral-neutral-90, #e3e3e3);
+      display: flex;
+      flex-shrink: 0;
+      gap: 4px;
+      height: 56px;
+      margin: 1px;
+      padding: 0px;
       width: 100%;
+
+      .search-bar-content {
+        align-items: center;
+        align-self: stretch;
+        display: flex;
+        flex-shrink: 0;
+        gap: 4px;
+        height: 100%;
+        padding: 4px 8px 4px 24px;
+        width: 100%;
+
+        #rich-search-button {
+          flex-shrink: 0;
+          height: 42px;
+          width: 42px;
+        }
+
+        .search-input-text {
+          align-items: center;
+          align-self: stretch;
+          background: transparent;
+          border: none;
+          color: var(--gm-3-ref-neutral-neutral-40, #5e5e5e);
+          display: flex;
+          flex: 1 0 0;
+          font-family: Google Sans;
+          font-size: 16px;
+          font-style: normal;
+          font-weight: 400;
+          gap: 10px;
+          height: 48px;
+          line-height: 24px;
+          padding: 0px;
+        }
+
+        .search-input-text:focus {
+          // Overwrite default focus
+          box-shadow: none !important;
+        }
+      }
     }
   }
 

static/js/apps/explore/app.tsx
@@ -274,7 +274,7 @@ export function App(): JSX.Element {
 
   const searchSection = (
     <div className="search-section">
-      <div className="experiment-tag">Experiment</div>
+      <div className="early-preview-tag">Early preview</div>
       <div className="search-box-section">
         <TextSearchBar
           inputId="query-search-input"

static/js/apps/explore_landing/app.tsx
@@ -68,7 +68,7 @@ export function App(): JSX.Element {
     <div className="explore-container">
       <Container>
         <div className="explore-search">
-          <div className="experiment-tag">Experiment</div>
+          <div className="early-preview-tag">Early Preview</div>
           <TextSearchBar
             inputId="query-search-input"
             onSearch={(q) => {
@@ -77,7 +77,7 @@ export function App(): JSX.Element {
                   ? placeholderHref
                   : `/explore#q=${encodeURIComponent(q)}&dc=${dc}`;
             }}
-            placeholder={`For example, "${placeholderQuery.title}"`}
+            placeholder={"Enter a question or topic to explore"}
             initialValue={""}
             shouldAutoFocus={true}
             clearValueOnSearch={true}

static/js/components/text_search_bar.tsx
@@ -39,8 +39,8 @@ export function TextSearchBar(props: TextSearchBarPropType): JSX.Element {
     setInvalid(false);
   }, [props.initialValue]);
   return (
-    <div className="input-query">
-      <InputGroup>
+    <div className="search-bar">
+      <InputGroup className="search-bar-content">
         <Input
           id={props.inputId}
           invalid={invalid}
@@ -51,12 +51,25 @@ export function TextSearchBar(props: TextSearchBarPropType): JSX.Element {
             setInvalid(false);
           }}
           onKeyDown={(e) => e.key === "Enter" && handleSearch()}
-          className="pac-target-input"
+          className="pac-target-input search-input-text"
           autoFocus={props.shouldAutoFocus}
-        />
-        <Button onClick={handleSearch} id="rich-search-button">
-          <span className="material-icons search rich-search-icon">search</span>
-        </Button>
+          autoComplete="off"
+        ></Input>
+        <div onClick={handleSearch} id="rich-search-button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="42"
+            height="42"
+            viewBox="0 0 42 42"
+            fill="none"
+            className="search-button-svg"
+          >
+            <path
+              d="M38.4992 21C38.4992 23.4208 38.0399 25.6958 37.1211 27.825C36.2024 29.9542 34.9555 31.8063 33.3805 33.3813C31.8055 34.9563 29.9534 36.2031 27.8242 37.1219C25.6951 38.0406 23.4201 38.5 20.9992 38.5C16.9159 38.5 13.2919 37.2385 10.1273 34.7156C6.96276 32.1927 4.89922 28.9333 3.93672 24.9375L6.73672 24.9375C7.61172 28.175 9.36901 30.8073 12.0086 32.8344C14.6482 34.8615 17.6451 35.875 20.9992 35.875C25.1409 35.875 28.6555 34.4313 31.543 31.5438C34.4305 28.6563 35.8742 25.1417 35.8742 21C35.8742 16.8583 34.4305 13.3438 31.543 10.4563C28.6555 7.56876 25.1409 6.12501 20.9992 6.12501C17.6451 6.12501 14.6482 7.13855 12.0086 9.16564C9.36901 11.1927 7.61172 13.825 6.73672 17.0625L3.93672 17.0625C4.89922 13.0667 6.96276 9.8073 10.1273 7.28439C13.2919 4.76147 16.9159 3.50001 20.9992 3.50001C23.4201 3.50001 25.6951 3.95939 27.8242 4.87813C29.9534 5.79689 31.8055 7.04376 33.3805 8.61876C34.9555 10.1938 36.2024 12.0458 37.1211 14.175C38.0399 16.3042 38.4992 18.5792 38.4992 21ZM27.3867 21L18.6367 29.75L16.7992 27.9125L22.3992 22.3125L3.54297 22.3125L3.54297 19.6875L22.3992 19.6875L16.7992 14.0875L18.6367 12.25L27.3867 21Z"
+              fill="#5E5E5E"
+            />
+          </svg>
+        </div>
       </InputGroup>
     </div>
   );

commit 70e90a158f82f73e8f4e20bb2c3c4393e0b6d026

[Bug fix] Update NL search bar to match explore (#3226)
NL interface search bar was broken by
https://github.com/datacommonsorg/website/pull/3218. This PR fixes this
by updating the NL interface search bar to match the search bar used on
the explore pages.

Before:
![Screenshot 2023-08-15 at 7 18 18
PM](https://github.com/datacommonsorg/website/assets/4034366/37b53eb3-ab59-4d45-b96b-08be55ae6b62)

After:
![Screenshot 2023-08-15 at 7 18 28
PM](https://github.com/datacommonsorg/website/assets/4034366/9081c0b4-2ccd-40bb-8b2e-0de42ef65649)

static/css/nl_interface.scss
@@ -191,30 +191,99 @@ $feedback-link-font-color: #70757a;
   .search-section {
     align-items: center;
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
+    flex-shrink: 0;
+    height: 79px;
+    width: 100%;
 
     .btn {
       height: 2.5rem;
       border-top-left-radius: 0;
       border-bottom-left-radius: 0;
     }
-    .experiment-tag {
-      border-radius: 10px;
-      border-style: solid;
-      border-width: 3px;
-      color: var(--blue);
+    .search-bar-tags { 
+      color: var(--gm-3-ref-secondary-secondary-30, #004a77);
       display: flex;
-      float: left;
-      font: normal 1em monospace;
-      padding: 4px 6px;
-      height: 80%;
-      margin-right: 20px;
+      flex-direction: row;
+      font-family: $font-family-sans-serif;
+      font-size: 11px;
+      font-style: normal;
+      font-weight: 500;
+      letter-spacing: 0.1px;
+      line-height: 16px;
+      margin-bottom: 6px;
+      width: 100%;
+
+      .early-preview-tag {
+        margin-left: auto;
+        margin-right: 37.93px;
+        text-align: right;
+        text-transform: uppercase;
+      }
+
     }
+
+
+
     .history-button {
       margin-left: 0.5rem;
     }
     .search-box-section {
-      flex-grow: 1;
+      width: 100%;
+    }
+
+    .search-bar {
+      align-items: center;
+      background: var(--gm-3-ref-secondary-secondary-99, #f7fcff);
+      border-radius: 28px;
+      border: 1px solid var(--gm-3-ref-neutral-neutral-90, #e3e3e3);
+      display: flex;
+      flex-shrink: 0;
+      gap: 4px;
+      height: 56px;
+      margin: 1px;
+      padding: 0px;
+      width: 100%;
+  
+      .search-bar-content {
+        align-items: center;
+        align-self: stretch;
+        display: flex;
+        flex-shrink: 0;
+        gap: 4px;
+        height: 100%;
+        padding: 4px 8px 4px 24px;
+        width: 100%;
+  
+        #rich-search-button {
+          flex-shrink: 0;
+          height: 42px;
+          width: 42px;
+        }
+  
+        .search-input-text {
+          align-items: center;
+          align-self: stretch;
+          background: transparent;
+          border: none;
+          color: var(--gm-3-ref-neutral-neutral-40, #5e5e5e);
+          display: flex;
+          flex: 1 0 0;
+          font-family: Google Sans;
+          font-size: 16px;
+          font-style: normal;
+          font-weight: 400;
+          gap: 10px;
+          height: 48px;
+          line-height: 24px;
+          padding: 0px;
+        }
+  
+        .search-input-text:focus {
+          // Overwrite default focus
+          box-shadow: none !important;
+        }
+      }
     }
   }
 

static/js/apps/nl_interface/query_search.tsx
@@ -47,7 +47,9 @@ export function QuerySearch(props: QuerySearchProps): JSX.Element {
     <div id="search-container">
       <Container tabIndex={-1}>
         <div className="search-section">
-          <div className="experiment-tag">Early Preview</div>
+          <div className="search-bar-tags">
+            <div className="early-preview-tag">Early preview</div>
+          </div>
           <div className="search-box-section">
             <TextSearchBar
               allowEmptySearch={