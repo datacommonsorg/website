56c0be8ee2cf332cecfa1e629685362340ea2849
Dan Noble <dwnoble@google.com>
2023-08-29 14:13:32 -0700

    Updated timeline slider component to automatically fetch date range for a given variable

--- static/library/slider_component.ts
+++ static/library/slider_component.ts
@@ -15,9 +15,34 @@
  */
 
 import { css, CSSResult, html, LitElement, TemplateResult } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { customElement, property, state } from "lit/decorators.js";
 
 import { ChartEventDetail } from "../js/chart/types";
+import { DEFAULT_API_ENDPOINT } from "./constants";
+import { convertArrayAttribute } from "./utils";
+
+const MOST_RECENT_TEXT = "Most recent";
+const MOST_RECENT_TEXT_LONG = "Most recently available";
+interface ObservationDatesByVariable {
+  observationDates: {
+    date: string;
+    entityCount: {
+      count: number;
+      facet: string;
+    }[];
+  }[];
+  variable: string;
+}
+interface ObservationDatesResponse {
+  datesByVariable: ObservationDatesByVariable[];
+  facets: {
+    [facet: string]: {
+      importName: string;
+      measurementMethod: string;
+      provenanceUrl: string;
+    };
+  };
+}
 
 /**
  * Slider web component
@@ -45,19 +70,27 @@ import { ChartEventDetail } from "../js/chart/types";
 @customElement("datacommons-slider")
 export class DatacommonsSliderComponent extends LitElement {
   static styles: CSSResult = css`
+    .component {
+      border: var(--border-primary);
+      border-radius: var(--border-radius-primary);
+      margin: 1rem 0;
+      padding: 24px;
+    }
     .container {
       display: flex;
       flex-direction: column;
       width: 100%;
-      margin: 16px 0;
     }
     .row {
       display: flex;
+      margin-top: 8px;
       width: 100%;
       flex-direction: row;
     }
     .label {
       flex-shrink: 0;
+      font-weight: 400;
+      font-size: 0.8rem;
     }
     input {
       flex-grow: 1;
@@ -65,22 +98,72 @@ export class DatacommonsSliderComponent extends LitElement {
     }
     .value {
       display: flex;
-      justify-content: center;
-      font-weight: bold;
+      justify-content: flex-start;
+      font-weight: 500;
+      font-size: 11px;
+      margin-bottom: 1rem;
+    }
+    .value-text {
+      margin-left: 2px;
+    }
+    .error {
+      color: red;
+      font-weight: 400;
+      font-family: monospace;
+    }
+    h4 {
+      padding: 0;
+      margin: 0 0 2px 0;
+      font-weight: 500;
+      font-size: 1rem;
     }
   `;
 
   /**
+   * @deprecated
    * Maximum slider value
+   * Deprecated. Use dates property instead.
    */
-  @property({ type: Number })
-  max: number;
+  @property()
+  max: string;
 
   /**
-   * Minimum slider value
+   * @deprecated
+   * Minimum slider value.
+   * Deprecated. Use dates property instead
    */
-  @property({ type: Number })
-  min: number;
+  @property()
+  min: string;
+  /**
+   * Optional: API root to use to fetch data
+   * Defaults to https://datacommons.org
+   */
+  @property()
+  apiRoot: string;
+
+  /**
+   * Type of child place to rank (ex: State, County)
+   */
+  @property()
+  childPlaceType: string;
+
+  /**
+   * Optional: List of date values
+   */
+  @property({ type: Array<string>, converter: convertArrayAttribute })
+  dates?: string[];
+
+  /**
+   * Optional: Header text
+   */
+  @property()
+  header?: string;
+
+  /**
+   * DCID of the parent place
+   */
+  @property()
+  parentPlace: string;
 
   /**
    * Event name to publish on slider change
@@ -91,42 +174,183 @@ export class DatacommonsSliderComponent extends LitElement {
   /**
    * Initial slider value
    */
-  @property({ type: Number })
-  value: number;
+  @property()
+  value: string;
+
+  /**
+   * Statistical variable
+   */
+  @property()
+  variable: string;
+
+  /**
+   * Slider date range
+   */
+  @state()
+  private _dates: string[];
+
+  /**
+   * Currently selected slider value
+   */
+  @state()
+  private _value: number;
+
+  /**
+   * Loading indicator
+   */
+  @state()
+  private _isLoading: boolean = false;
+
+  /**
+   * Currently selected value
+   */
+  @state()
+  private _errorMessage: string = "";
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    if (this.min && this.max) {
+      this._dates = [this.min, this.max];
+    }
+    if (this.dates) {
+      this._dates = [...this.dates];
+    }
+    if (this.value) {
+      this._value = Number(this.value);
+      if (
+        isNaN(this._value) ||
+        this._value < 0 ||
+        this._value > this._dates.length
+      ) {
+        this._value = this._dates.length;
+      }
+    } else {
+      this._value = 0;
+    }
+    this.fetchObservationDates();
+  }
 
   render(): TemplateResult {
-    return html`
-      <div class="container">
-        <div class="row">
-          <div class="label">${this.min}</div>
-          <input
-            class="slider"
-            max="${this.max}"
-            min="${this.min}"
-            title="${this.value}"
-            type="range"
-            value="${this.value}"
-            @change=${this.onSliderChange}
-          />
-          <div class="label">${this.max}</div>
+    if (this._isLoading) {
+      return html`<div class="component">Loading...</div>`;
+    }
+    if (this._errorMessage) {
+      return html`
+        <div class="component error">
+          <h4>datacommons-slider</h4>
+          <div>${this._errorMessage}</div>
         </div>
-        <div class="value">${this.value}</div>
+      `;
+    }
+    if (!this._dates || this._dates.length === 0) {
+      return html`
+        <div class="component error">
+          <h4>datacommons-slider</h4>
+          <div>
+            Please specify either the "dates" attribute or all of the
+            attributes: "variable", "parentPlace", and "childPlaceType".
+          </div>
+        </div>
+      `;
+    }
+
+    return html`
+      <div class="component">
+        <div class="container">
+          ${this.header
+            ? html`<h4 part="header">${this.header}</h4>`
+            : this.defaultHeader()}
+          <div class="row">
+            <div class="label">${this._dates[0]}</div>
+            <input
+              class="slider"
+              max="${this._dates.length}"
+              min="0"
+              title="${this.getValueText()}"
+              type="range"
+              value="${this._value}"
+              @change=${this.onSliderChange}
+              @input=${this.onSliderInput}
+            />
+            <div class="label">${MOST_RECENT_TEXT}</div>
+          </div>
+        </div>
       </div>
     `;
   }
 
+  private defaultHeader() {
+    if (this._value === this._dates.length) {
+      return html`<h4 part="header">Showing most recently available data</h4>`;
+    } else {
+      return html`<h4 part="header">
+        Showing data from ${this.getValueText()}
+      </h4>`;
+    }
+  }
+
+  private getValueText() {
+    if (this._value < 0 || this._value > this._dates.length) {
+      return "Unknown";
+    } else if (this._value === this._dates.length) {
+      return MOST_RECENT_TEXT;
+    } else {
+      return this._dates[this._value];
+    }
+  }
+
   private onSliderChange(e: Event): void {
     const target = e.currentTarget as HTMLInputElement;
-    const value = target.value;
-    this.value = Number(value);
+    const newValue = Number(target.value);
+    this._value = newValue;
+    const dateValue =
+      this._value < this._dates.length ? this._dates[this._value] : undefined;
     this.dispatchEvent(
       new CustomEvent<ChartEventDetail>(this.publish, {
         bubbles: true,
         detail: {
           property: "date",
-          value,
+          value: dateValue,
         },
       })
     );
+  }
+
+  private onSliderInput(e: Event): void {
+    const target = e.currentTarget as HTMLInputElement;
+    const newValue = Number(target.value);
+    this._value = newValue;
+  }
+
+  private async fetchObservationDates() {
+    const apiRoot = `${this.apiRoot || DEFAULT_API_ENDPOINT}`;
+    const apiPath = "api/observation-dates";
+    if (!this.parentPlace || !this.childPlaceType || !this.variable) {
+      console.log("No place found in the slider");
+      return;
+    }
+    this._isLoading = true;
+    const params = new URLSearchParams();
+    params.set("parentEntity", this.parentPlace);
+    params.set("childType", this.childPlaceType);
+    params.set("variable", this.variable);
+
+    const url = `${apiRoot}/${apiPath}?${params.toString()}`;
+    const result = await fetch(url);
+    const resultObj = (await result.json()) as ObservationDatesResponse;
+    console.log("!! RESULT!", resultObj);
+    this._isLoading = false;
+
+    if (
+      resultObj.datesByVariable.length > 0 &&
+      resultObj.datesByVariable[0].observationDates
+    ) {
+      this._dates = resultObj.datesByVariable[0].observationDates.map(
+        (od) => od.date
+      );
+    } else {
+      this._errorMessage = `No date range found for (variable: ${this.variable},  parentPlace: ${this.parentPlace}, childPlaceType: ${this.childPlaceType})`;
+    }
+    this._value = this._dates.length;
   }
 }
