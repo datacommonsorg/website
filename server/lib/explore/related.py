e36f30480cfe55e32eddca863824e28f9041f3d8
Bo Xu <shifucun@users.noreply.github.com>
2023-08-28 20:00:37 -0700

    resolve comments

--- server/lib/explore/related.py
+++ server/lib/explore/related.py
@@ -89,23 +89,8 @@ def compute_related_things(state: ftypes.PopulateState,
       # We found a topic, so break!
       break
 
-  # Check the data existence for related topics
-  all_topics = ([x['dcid'] for x in related_things['parentTopics']] +
-                [x['dcid'] for x in related_things['peerTopics']] +
-                [x['dcid'] for x in related_things['childTopics']])
-
-  valid_topics, _ = utils.sv_existence_for_places(
-      [x.dcid for x in state.uttr.places], all_topics, state.uttr.counters)
-  valid_topics_set = {t: "" for t in valid_topics}
-  related_things['parentTopics'] = [
-      t for t in related_things['parentTopics'] if t['dcid'] in valid_topics_set
-  ]
-  related_things['peerTopics'] = [
-      t for t in related_things['peerTopics'] if t['dcid'] in valid_topics_set
-  ]
-  related_things['childTopics'] = [
-      t for t in related_things['childTopics'] if t['dcid'] in valid_topics_set
-  ]
+  if dc != "sdg":
+    related_things = prune_related_topics(related_things, state.uttr)
 
   state.uttr.counters.timeit('topic_expansion', start)
 
@@ -127,6 +112,28 @@ def _trim_dups(related_things: Dict):
       added.add(it['dcid'])
       k_list.append(it)
     related_things[k] = k_list
+
+
+def prune_related_topics(related_things, uttr):
+  # Check the data existence for related topics
+  all_topics = list(
+      set(([x['dcid'] for x in related_things['parentTopics']] +
+           [x['dcid'] for x in related_things['peerTopics']] +
+           [x['dcid'] for x in related_things['childTopics']])))
+
+  valid_topics, _ = utils.sv_existence_for_places([x.dcid for x in uttr.places],
+                                                  all_topics, uttr.counters)
+  valid_topics_set = set(valid_topics)
+  related_things['parentTopics'] = [
+      t for t in related_things['parentTopics'] if t['dcid'] in valid_topics_set
+  ]
+  related_things['peerTopics'] = [
+      t for t in related_things['peerTopics'] if t['dcid'] in valid_topics_set
+  ]
+  related_things['childTopics'] = [
+      t for t in related_things['childTopics'] if t['dcid'] in valid_topics_set
+  ]
+  return related_things
 
 
 def _node_to_topic_dict(n: Node, dc: str) -> Dict:
