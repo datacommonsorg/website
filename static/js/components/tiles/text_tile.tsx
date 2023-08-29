f5676d2eb78e92ea5376a1fd5dedaeea31739826
Julia Wu <wujulia.jw@gmail.com>
2023-08-29 16:58:31 -0700

    add new text component to sdg tracker

--- static/js/components/tiles/text_tile.tsx
+++ static/js/components/tiles/text_tile.tsx
@@ -18,70 +18,32 @@
  * Component for rendering a text tile.
  */
 
-import React, { useState } from "react";
+import React from "react";
 
 import { ASYNC_ELEMENT_HOLDER_CLASS } from "../../constants/css_constants";
 
 export interface TextTilePropType {
-  // number of characters of text body to show before "show more"
-  characterLimit?: number;
-  // Heading to display with the text
-  heading: string;
-  // Text body to display
-  text: string;
-  // Whether to show entire story regardless of character limit
-  showFullText?: boolean;
-}
-
-// Default number of characters of text body to show before "show more".
-const DEFAULT_STORY_CHARACTER_LIMIT = 250;
-
-/**
- * Formats text into paragraphs within <p> tags
- */
-function formatText(text: string): JSX.Element[] {
-  return text
-    .replaceAll(/\\n/g, "\n") // Catch "\n" as text instead of newline character
-    .split(/\r?\n/) // Split into paragraphs by newline character
-    .map((item, i) => <p key={i}>{item}</p>); // wrap each paragraph in a tag
+  // url to
+  link?: string;
 }
 
 export function TextTile(props: TextTilePropType): JSX.Element {
-  const [showMore, setShowMore] = useState(false);
-
-  if (!(props.heading && props.text)) {
-    return null;
-  }
-
-  const characterLimit = props.characterLimit || DEFAULT_STORY_CHARACTER_LIMIT;
-  const shouldShowFullText =
-    props.showFullText || props.text.length <= characterLimit;
-  const fullTextBody = formatText(props.text);
-  const shortTextBody = formatText(props.text.substring(0, characterLimit));
-
   return (
     <div
       className={`chart-container text-tile ${ASYNC_ELEMENT_HOLDER_CLASS}`}
       {...{ part: "container" }}
     >
       <div className="text-header" {...{ part: "heading" }}>
-        {props.heading}
+        <slot name="heading" />
       </div>
-      <div
-        className={`text-body${
-          showMore || shouldShowFullText ? "" : " fade-text"
-        }`}
-        {...{ part: "text" }}
-      >
-        {showMore || shouldShowFullText ? fullTextBody : shortTextBody}
+      <div className="text-body" {...{ part: "text" }}>
+        <slot name="text" />
       </div>
-      {!shouldShowFullText && (
-        <div
-          className="show-more-toggle"
-          onClick={() => setShowMore(!showMore)}
-          {...{ part: "show-more-toggle" }}
-        >
-          {showMore ? "Show less" : "Show more"}
+      {props.link && (
+        <div className="text-link" {...{ part: "link" }}>
+          <a href={props.link} target="_blank" rel="noreferrer">
+            See more
+          </a>
         </div>
       )}
     </div>
