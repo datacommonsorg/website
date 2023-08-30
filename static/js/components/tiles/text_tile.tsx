da85835f9eabd8330d8a73626376f81d0b4d4a22
Julia Wu <wujulia.jw@gmail.com>
2023-08-30 08:59:18 -0700

    fix unfinished comment

--- static/js/components/tiles/text_tile.tsx
+++ static/js/components/tiles/text_tile.tsx
@@ -23,7 +23,7 @@ import React from "react";
 import { ASYNC_ELEMENT_HOLDER_CLASS } from "../../constants/css_constants";
 
 export interface TextTilePropType {
-  // url to
+  // url to link to under "see more"
   link?: string;
 }
 
