801578523b9cc81bc9bc21d4af1d3d6ef95bb8a3
chejennifer <69875368+chejennifer@users.noreply.github.com>
2023-09-10 15:13:54 -0700

    [explore] update user message (#3490)
    update user message according to mock
    
    <img width="1753" alt="Screenshot 2023-09-10 at 12 33 51 PM"
    src="https://github.com/datacommonsorg/website/assets/69875368/690b308f-581f-4732-a6f8-cba714aed499">

--- static/js/apps/explore/user_message.tsx
+++ static/js/apps/explore/user_message.tsx
@@ -50,23 +50,26 @@ export function UserMessage(props: UserMessagePropType): JSX.Element {
     : [];
 
   return (
-    <div className="user-message">
-      <div className="user-message-text">
-        <span className="main-message">{props.userMessage.msg}</span>
-        {props.userMessage.showForm && (
-          <span className="sub-message">
-            <a href={DATA_FORM_URL}>Fill out this form</a> to add data to answer
-            this query.
-          </span>
-        )}
-        {!props.userMessage.showForm &&
-          props.userMessage.msg.includes("Low confidence") && (
+    <div className="user-message-container">
+      <div className="material-icons">info</div>
+      <div className="user-message">
+        <div className="user-message-text">
+          <span className="main-message">{props.userMessage.msg}</span>
+          {props.userMessage.showForm && (
             <span className="sub-message">
-              <a href={LOW_CONFIDENCE_FORM_URL}>Flag inappropriate results</a>
+              <a href={DATA_FORM_URL}>Fill out this form</a> to add data to
+              answer this query.
             </span>
           )}
+          {!props.userMessage.showForm &&
+            props.userMessage.msg.includes("Low confidence") && (
+              <span className="sub-message">
+                <a href={LOW_CONFIDENCE_FORM_URL}>Flag inappropriate results</a>
+              </span>
+            )}
+        </div>
+        {!_.isEmpty(topicList) && <ItemList items={topicList} />}
       </div>
-      {!_.isEmpty(topicList) && <ItemList items={topicList} />}
     </div>
   );
 }
