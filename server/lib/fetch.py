c365ca1a864664b8230ed376be6284ffd4bb416a
Jennifer Chen <chejennifer@google.com>
2023-09-11 11:10:29 -0700

    fix lint

--- server/lib/fetch.py
+++ server/lib/fetch.py
@@ -261,7 +261,6 @@ def series_facet(entities, variables, all_facets):
   return _compact_series(resp, all_facets)
 
 
-
 def point_within_facet(ancestor_entity, descendent_type, variables, date,
                        all_facets):
   """Fetches facet of child places of a certain place type contained in a parent
