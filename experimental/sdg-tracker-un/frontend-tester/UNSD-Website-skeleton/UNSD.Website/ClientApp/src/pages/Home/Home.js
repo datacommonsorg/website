dcb361fdc026a2bf8573d4008f7988c146d994f1
Dan Noble <dwnoble@google.com>
2023-09-12 16:01:21 -0400

    Added merged UN sdg site to experimental/sdg-tracker-un (#3536)
    New dev site:
    https://datcom-un-sdg.ue.r.appspot.com/UNSDWebsite/undatacommons/sdgs/

---
+++ experimental/sdg-tracker-un/frontend-tester/UNSD-Website-skeleton/UNSD.Website/ClientApp/src/pages/Home/Home.js
@@ -0,0 +1,7 @@
+﻿import React, { Component } from "react";
+import { HomeTemplate } from "../../templates/Home/HomeTemplate";
+export class Home extends Component {
+  render() {
+    return <HomeTemplate />;
+  }
+}
---
+++ experimental/sdg-tracker-un/frontend-tester/UNSD-Website-skeleton/UNSD.Website/ClientApp/src/themes/RTL_Theme.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../custom_ar.css";
+
+const RTLTheme = () => <React.Fragment></React.Fragment>;
+
+export default RTLTheme;
