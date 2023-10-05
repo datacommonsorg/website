ee68db2fd5b1031bcaefa8a52639b68af638ebbe
Bo Xu <shifucun@users.noreply.github.com>
2023-10-05 08:28:22 -0700

    Bundle import tool into RSI docker image; Add website endpoint for RSI actions

--- server/routes/import_wizard/html.py
+++ server/routes/import_wizard/html.py
@@ -14,9 +14,10 @@
 """Import Wizard routes"""
 
 import os
+import subprocess
 
-import flask
 from flask import Blueprint
+from flask import jsonify
 from flask import render_template
 
 bp = Blueprint('import_wizard', __name__, url_prefix='/import')
@@ -30,3 +31,57 @@ def main():
 @bp.route('/')
 def main_new():
   return render_template('/import_wizard2.html')
+
+
+# This is only
+@bp.route('/simple/convert', methods=['POST'])
+def convert():
+  RAW_DATA_PATH = os.environ.get("RAW_DATA_PATH")
+  SQL_DATA_PATH = os.environ.get("SQL_DATA_PATH")
+  command1 = [
+      "python",
+      "import/simple/stats/main.py",
+      "--input_path",
+      f"{RAW_DATA_PATH}/input.csv",  # TODO: use gcs and local folder path.
+      "--output_dir",
+      f"{SQL_DATA_PATH}",
+  ]
+  # TODO: save debug information in another folder
+  command2 = ["rm", f"{SQL_DATA_PATH}/debug_resolve.csv"]
+  output = []
+  for command in [command1, command2]:
+    try:
+      result = subprocess.run(command, capture_output=True, text=True)
+      if result.returncode == 0:
+        output.append({"status": "success", "output": result.stdout.strip()})
+      else:
+        return jsonify({
+            "status": "failure",
+            "error": result.stderr.strip()
+        }), 500
+    except Exception as e:
+      return jsonify({"status": "error", "message": str(e)}), 500
+  return jsonify(output), 200
+
+
+# This is a proxy to trigger Mixer import for csv + SQL backend.
+# This can only be used for bundled mixer and website in the same docker image.
+@bp.route('/simple/load2sql', methods=['POST'])
+def load2sql():
+  command = [
+      "curl",
+      "-X",
+      "POST",
+      "localhost:8081/import",
+  ]
+  try:
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode == 0:
+      return jsonify({
+          "status": "success",
+          "output": result.stdout.strip()
+      }), 200
+    else:
+      return jsonify({"status": "failure", "error": result.stderr.strip()}), 500
+  except Exception as e:
+    return jsonify({"status": "error", "message": str(e)}), 500
