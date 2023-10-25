fef25f3a6dea7f9daa89d313292a49ed442497ec
Carolyn Au <6052978+beets@users.noreply.github.com>
2023-10-25 11:21:31 -0700

    [SEO] Generate more place summaries (#3717)
    - Add plumbing to display place summaries
    - Generate summaries in a few steps:
      - Summarize rankings and associated time series for each ranking row
      - Resend each summary so Palm can generate a nice sounding paragraph
    - Fix off-by-one error in our rankings tables :-o
    - Only generate summaries for top and bottom 20 populous cities in the
    US, and top / bottom 5 states
    
    Still need to add better verification for the numbers chosen in the
    summary. I also caught some erroneous misinterpretation of the rankings,
    like Houston is the most populous city in the US..
    
    
    ![image](https://github.com/datacommonsorg/website/assets/6052978/349ca68d-9853-4bf7-85a8-f73121a60a83)

--- tools/summaries/README.md
+++ tools/summaries/README.md
@@ -8,4 +8,10 @@ To run:
 python3 main.py
 ```
 
+To debug:
+
+```shell
+python3 main.py --num_processes=1 --places_in_file=places-short.json --save_prompt=True --dc_base_url=http://localhost:8080
+```
+
 The PaLM API Key must be specified under an environment variable named `PALM_API_KEY`.
--- tools/summaries/dc.py
+++ tools/summaries/dc.py
@@ -15,28 +15,78 @@
 import json
 import logging
 
+from absl import flags
 import pandas as pd
 import requests
 
-_API_BASE_URL = "https://datacommons.org/api/place/ranking"
-_RANKINGS_HEADER = "Rankings"
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('dc_base_url', 'https://datacommons.org',
+                    'DC instance to query')
+
+_RANKING_URL = "{host}/api/place/ranking/{dcid}?all=1"
+_PLACE_DATA_URL = "{host}/api/landingpage/data/{dcid}?category=Overview&hl=en&seed=0"
 _LABEL_KEY = "label"
 
+_SERIES_TO_KEEP = {
+    "Demographics": {
+        "Median Age": "Median_Age_Person",
+        "Population": "Count_Person",
+    },
+    "Economics": {
+        "Median individual income": "Median_Income_Person",
+        # "Unemployment rate": "UnemploymentRate_Person",
+    },
+}
 
-def get_ranking_csv(dcid: str):
-  url = f"{_API_BASE_URL}/{dcid}"
+_PLACE_TYPE_PLURAL = {
+    "city": "cities",
+    "state": "states",
+}
+
+
+def get_ranking_data(dcid: str, place_type: str):
+  """Returns ranking data as a list, keyed by rank label"""
+  url = _RANKING_URL.format(host=FLAGS.dc_base_url, dcid=dcid)
   response = requests.get(url).json()
   logging.debug("Ranking response:\n%s", json.dumps(response, indent=True))
-  csv = []
+  data = {}
+  place_type_plural = _PLACE_TYPE_PLURAL[place_type]
   for variable, places in response.items():
     if variable == _LABEL_KEY:
       continue
 
-    row = {_RANKINGS_HEADER: variable}
-    csv.append(row)
+    data[variable] = []
     for place in places:
       rank = place["data"]
-      row[place[
-          "name"]] = f"{rank['rankFromTop']} of {rank['rankFromTop'] + rank['rankFromBottom']}"
+      data[variable].append(
+          f"Ranked {rank['rankFromTop']} of {rank['rankFromTop'] + rank['rankFromBottom'] - 1} {place_type_plural} in {place['name']}"
+      )
 
-  return pd.DataFrame(csv).to_csv(index=False)
+  return data
+
+
+def get_data_series(dcid: str, place_name: str):
+  """Returns series data as a CSV keyed by stat var"""
+  url = _PLACE_DATA_URL.format(host=FLAGS.dc_base_url, dcid=dcid)
+  response = requests.get(url).json()
+
+  prompt_tables = {}
+
+  for category, sv_titles in _SERIES_TO_KEEP.items():
+    category_data = response["pageChart"]["Overview"].get(category, {})
+    for chart_block in category_data:
+      block_title = chart_block["title"]
+      if block_title in sv_titles:
+        sv = sv_titles[block_title]
+        series = chart_block["trend"]["series"]
+
+        # Convert dict into a csv with sorted date keys
+        j = json.dumps(series, sort_keys=True)
+        j = j.replace("'", '"')  # Needed for pd.read_json
+        j = j.replace(sv, f"{block_title} in {place_name}")
+        df = pd.read_json(j)
+        prompt_tables[sv] = 'date' + df.to_csv()
+
+  logging.debug(prompt_tables)
+  return prompt_tables
