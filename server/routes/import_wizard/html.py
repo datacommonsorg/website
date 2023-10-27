abaf6308ab7965ec70bbe01d122397d38b838504
Bo Xu <shifucun@users.noreply.github.com>
2023-10-27 16:32:38 +0000

    Change old admin page (based on oauth and multi-tenent model) to RSI model with data load support (#3716)
    On server start, can set an environment variable "ADMIN_SECRET". When
    user perform actions in this admin page, they need to input the secret.
    This is a naive way for authentication before we have a full solution.
    
    
    ![image](https://github.com/datacommonsorg/website/assets/5951856/21b2489a-fa37-45fa-ae27-f448226d85d8)

--- server/routes/import_wizard/html.py
+++ server/routes/import_wizard/html.py
@@ -13,13 +13,8 @@
 # limitations under the License.
 """Import Wizard routes"""
 
-import os
-import subprocess
-
 from flask import Blueprint
-from flask import jsonify
 from flask import render_template
-from flask import request
 
 bp = Blueprint('import_wizard', __name__, url_prefix='/import')
 
@@ -32,55 +27,3 @@ def main():
 @bp.route('/')
 def main_new():
   return render_template('/import_wizard2.html')
-
-
-# This is only used in CSV + SQL backend, converting raw CSV data into resolved
-# CSV.
-@bp.route('/simple/load', methods=['POST'])
-def load():
-  sql_data_path = None
-  if request.is_json:
-    sql_data_path = request.json.get("sqlDataPath")
-  if not sql_data_path:
-    sql_data_path = os.environ.get("SQL_DATA_PATH")
-
-  # TODO: dynamically create the output dir.
-  output_dir = os.path.join(sql_data_path, 'data')
-
-  command1 = [
-      "python",
-      "-m",
-      "stats.main",
-      "--input_path",
-      f"{sql_data_path}",
-      "--output_dir",
-      f"{output_dir}",
-  ]
-  command2 = [
-      "curl",
-      "-X",
-      "POST",
-      "localhost:8081/import",
-      "-d",
-      f'{{"data_path": "{output_dir}"}}',
-  ]
-  output = []
-  for command, cwd in [(command1, "import/simple"), (command2, ".")]:
-    try:
-      result = subprocess.run(command,
-                              capture_output=True,
-                              text=True,
-                              check=True,
-                              cwd=cwd)
-      output.append({
-          "status": "success",
-          "stdout": result.stdout.strip().splitlines()
-      })
-    except subprocess.CalledProcessError as cpe:
-      return jsonify({
-          "status": "failure",
-          "error": cpe.stderr.strip().splitlines()
-      }), 500
-    except Exception as e:
-      return jsonify({"status": "error", "message": str(e)}), 500
-  return jsonify(output), 200
