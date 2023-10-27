8eec779be8c69e9bfabb72e3a4768bacc09a6b3a
Bo Xu <shifucun@users.noreply.github.com>
2023-10-27 11:23:12 -0700

    Fix sql data path manipulation for GCS bucket

--- server/routes/admin/html.py
+++ server/routes/admin/html.py
@@ -35,7 +35,6 @@ def load_data():
     sql_data_path = request.json.get("sqlDataPath")
   if not sql_data_path:
     sql_data_path = os.environ.get("SQL_DATA_PATH")
-  sql_data_path = os.path.abspath(sql_data_path)
 
   # TODO: dynamically create the output dir.
   output_dir = os.path.join(sql_data_path, 'data')
