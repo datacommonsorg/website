ce2e4abfa788e94aabf61030fb9dde15480d6a84
Julia Wu <juliawu@google.com>
2023-11-03 10:03:40 -0700

    Fix webdriver tests (#3759)
    The webdriver tests have started failing after the last data push,
    because it is expecting older data values. This PR updates the failing
    tests to check for new values.
    
    Note: Also updates integration test goldens.
    
    Screenshots of current autopush behavior of pages being tested:
    ![Screenshot 2023-11-03 at 9 36 29
    AM](https://github.com/datacommonsorg/website/assets/4034366/2b6a8250-8ba7-46bc-8ca3-4a2f6d8a4ca2)
    ![Screenshot 2023-11-03 at 9 34 19
    AM](https://github.com/datacommonsorg/website/assets/4034366/212f2838-b7db-4257-8aa2-ffa48aaa421c)

--- server/webdriver/tests/download_test.py
+++ server/webdriver/tests/download_test.py
@@ -34,7 +34,7 @@ TABLE_HEADERS = [
 TABLE_ROW_1 = [
     'geoId/06001', 'Alameda County', '2021', '38',
     'https://www.census.gov/programs-surveys/acs/data/data-via-ftp.html',
-    '2021', '1648556', 'https://www2.census.gov/programs-surveys/popest/tables'
+    '2022', '1628997', 'https://www2.census.gov/programs-surveys/popest/tables'
 ]
 MAX_NUM_FILE_CHECK_TRIES = 3
 
