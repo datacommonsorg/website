5a78d31e32b84e3ee407995013b8d8ca4fa64769
Benedict <ben@benedict-wallis.com>
2023-11-07 11:41:34 +0000

    Adds custom static assets including logo

--- server/app_env/one.py
+++ server/app_env/one.py
@@ -18,9 +18,9 @@ from server.app_env import local
 
 class Config(_base.Config):
   CUSTOM = True
-  NAME = "Custom Data Commons"
-  OVERRIDE_CSS_PATH = '/custom_dc/custom/overrides.css'
-  LOGO_PATH = "/custom_dc/custom/logo.png"
+  NAME = "Data Commons"
+  OVERRIDE_CSS_PATH = '/custom_dc/one/overrides.css'
+  LOGO_PATH = "/custom_dc/one/one-logo.svg"
   SHOW_DISASTER = False
 
 
