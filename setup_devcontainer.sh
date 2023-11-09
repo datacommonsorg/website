3fa2a8faf7b18954e69d19f866c4343b29ef3e4a
Benedict <ben@benedict-wallis.com>
2023-11-09 12:50:58 +0000

    Adds environment var to setup_devcontainer.sh

--- setup_devcontainer.sh
+++ setup_devcontainer.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
 
+# Default environment
+ENVIRONMENT="custom"
+
+# Check for -e argument and assign it to ENVIRONMENT
+while getopts "e:" opt; do
+  case $opt in
+    e) ENVIRONMENT="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
 # Run npm install and setup
 # The nohup command runs run_npm.sh in a way that it won't be killed when the terminal closes, and & puts it in the background.
 nohup ./run_npm.sh >/dev/null 2>&1 &
@@ -23,4 +36,4 @@ if [ -f ".envvars" ]; then
 fi
 
 # Run the server
-./run_server.sh -e one
+./run_server.sh -e "$ENVIRONMENT"
