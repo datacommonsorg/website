5cf0780b88695a621802145af787073a6ccf4874
Benedict <ben@benedict-wallis.com>
2023-11-09 12:44:32 +0000

    Adds a theme directory for compiling overrides and other static assets with webpack

---
+++ static/themes/webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  devtool: false, // No source maps
+  entry: {
+    'overrides': './one/scss/overrides.scss',
+  },
+  output: {
+    path: path.resolve(__dirname, 'one/css'),
+    filename: '[name].css.js', // This will still be produced initially
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: false, // Make sure source maps are disabled
+              importLoaders: 2, // Number of loaders applied before CSS loader
+            },
+          },
+          {
+            loader: 'postcss-loader', // Add this loader
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ['postcss-discard-comments', { removeAll: true }], // This plugin discards comments
+                ],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: false, // Make sure source maps are disabled
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            'default',
+            {
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new CleanWebpackPlugin({
+      // Automatically remove all unused webpack assets on rebuild, 
+      // this means that the .js files will be deleted after they are created.
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false, // This allows the plugin to remove the .js files
+      // Clean only the .js files in the specified directory
+      cleanAfterEveryBuildPatterns: ['*.js'],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        // Copy the overrides.css file to the custom_dc directory
+        { from: 'one/css/overrides.css', to: path.resolve(__dirname, '../custom_dc/one/overrides.css') },
+        // Copy the overrides.css file to the server/dist/custom_dc directory
+        { from: 'one/css/overrides.css', to: path.resolve(__dirname, '../../server/dist/custom_dc/one/overrides.css') },
+      ],
+    }),
+  ],
+};
