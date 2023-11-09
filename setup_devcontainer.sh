9e3503c82d2d94806f259d44dad8595585839105
Benedict <ben@benedict-wallis.com>
2023-11-09 12:45:10 +0000

    Update dev `seetup_container.sh` to run theme watch

--- setup_devcontainer.sh
+++ setup_devcontainer.sh
@@ -5,6 +5,17 @@ set -e
 # The nohup command runs run_npm.sh in a way that it won't be killed when the terminal closes, and & puts it in the background.
 nohup ./run_npm.sh >/dev/null 2>&1 &
 
+# Build themes...
+# Navigate to the themes directory
+cd static/themes
+
+# Install npm dependencies and run the watch script
+npm install
+npm run watch &
+
+# Navigate back to the original directory
+cd -
+
 # Set up environment variables (these should already be set in devcontainer.json, but just in case)
 # Load environment variables
 if [ -f ".envvars" ]; then
