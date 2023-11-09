fd37a93ae68bf2b8e4efe1228f2d226bcca3a5d4
Dan Noble <dwnoble@google.com>
2023-11-08 17:03:29 -0800

    linting

--- static/js/components/tiles/bar_tile.tsx
+++ static/js/components/tiles/bar_tile.tsx
@@ -47,11 +47,11 @@ import {
 import { getPlaceNames, getPlaceType } from "../../utils/place_utils";
 import { getDateRange } from "../../utils/string_utils";
 import {
-  ReplacementStrings,
   getDenomInfo,
   getNoDataErrorMsg,
   getStatFormat,
   getStatVarNames,
+  ReplacementStrings,
   showError,
 } from "../../utils/tile_utils";
 import { ChartTileContainer } from "./chart_tile";
