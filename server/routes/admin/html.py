08f6bca545da340da7d363bc7848f678649034f2
Keyur Shah <keyurs@google.com>
2023-11-14 16:00:16 -0800

    Address review comment.

--- server/routes/admin/html.py
+++ server/routes/admin/html.py
@@ -47,6 +47,7 @@ def load_data():
   output_dir = os.path.join(sql_data_path, 'data')
   nl_dir = os.path.join(output_dir, "nl")
   sentences_path = os.path.join(nl_dir, "sentences.csv")
+  # TODO: Support enabling / disabling NL via env / Dockerfile.
   # TODO: Enable NL for GCS paths once we add support for it.
   load_nl = not sql_data_path.lower().startswith("gs://")
 
