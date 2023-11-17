127041cc4dde8ec37995221cccd034632d7e38e9
Prashanth Radhakrishnan <shanth@google.com>
2023-11-17 09:36:51 -0800

    Fix

--- server/lib/nl/common/debug_utils.py
+++ server/lib/nl/common/debug_utils.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict, cast
+from typing import cast, Dict
 
 from server.lib.nl.detection.types import ClassificationType
 from server.lib.nl.detection.types import Detection
