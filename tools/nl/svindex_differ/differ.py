7faaca355bf69e2da992280ebc8d8f7b30605d53
Prashanth Radhakrishnan <shanth@google.com>
2024-01-10 17:47:35 -0800

    Use the right native and foreign SVs in the index

--- tools/nl/svindex_differ/differ.py
+++ tools/nl/svindex_differ/differ.py
@@ -51,6 +51,7 @@ flags.DEFINE_string('queryset', '', 'Full path to queryset CSV')
 flags.DEFINE_string('indextype', '',
                     'The base index type such as small or medium_ft')
 
+_GCS_PREFIX = 'https://storage.mtls.cloud.google.com'
 _TEMPLATE = 'tools/nl/svindex_differ/template.html'
 _REPORT = '/tmp/diff_report.html'
 _FILE_PATTERN_EMBEDDINGS = r'embeddings_.*_\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}\.csv'
@@ -248,7 +249,7 @@ def run_diff(base_file, test_file, base_model_path, test_model_path, query_file,
   # Since the files can be fairly large, use a 10min timeout to be safe.
   blob.upload_from_filename(output_file, timeout=600)
   print("Done uploading to gcs.")
-  print(f"\t Diff report Filename: {gcs_filename}")
+  print(f"\t Diff report Filename: {_GCS_PREFIX}/{_GCS_BUCKET}/{gcs_filename}")
 
 
 def main(_):
