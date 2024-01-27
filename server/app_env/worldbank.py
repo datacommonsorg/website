903a048eec4e897fe98c0136ca398b35729c5c42
Dan Noble <dwnoble@google.com>
2024-01-26 16:30:45 -0800

    removed unused import

--- server/app_env/worldbank.py
+++ server/app_env/worldbank.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
-
 from server.app_env import _base
 from server.app_env import local
 
