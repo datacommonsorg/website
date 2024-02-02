6518259535779b6fd873443567f68285b79eed0a
Prashanth Radhakrishnan <shanth@google.com>
2024-02-02 14:22:45 -0800

    Fix

--- nl_server/routes.py
+++ nl_server/routes.py
@@ -29,8 +29,14 @@ bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/healthz')
 def healthz():
-  # TODO: Add a better healthz check
-  return ''
+  nl_embeddings = current_app.config[config.NL_EMBEDDINGS_KEY].get(
+      config.DEFAULT_INDEX_TYPE)
+  result = nl_embeddings.detect_svs('life expectancy',
+                                    SV_SCORE_DEFAULT_THRESHOLD,
+                                    skip_multi_sv=True)
+  if result.get('SV'):
+    return 'OK', 200
+  return 'Service Unavailable', 500
 
 
 @bp.route('/api/search_sv/', methods=['GET'])
