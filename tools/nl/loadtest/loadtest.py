210a5473e222b8b9476bb01e1cc619253db74313
Jennifer Chen <chejennifer@google.com>
2024-02-16 14:19:56 -0800

    address pr comments

--- tools/nl/loadtest/loadtest.py
+++ tools/nl/loadtest/loadtest.py
@@ -12,12 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import asyncio
+import concurrent.futures
 import csv
 
 from absl import app
 from absl import flags
-import aiohttp
 import requests
 
 FLAGS = flags.FLAGS
@@ -55,119 +54,75 @@ def _get_query(row):
   return query
 
 
-# Runs regular load test where each query is requested one by one
-def load_test(query_file, output_file):
-  with open(output_file, 'w') as outf:
-    writer = csv.writer(outf)
-    writer.writerow(_RESULT_HEADER_ROW)
-    with open(query_file) as inf:
-      for row in csv.reader(inf):
-        query = _get_query(row)
-        if not query:
-          continue
-
-        # Make the API request
-        print(f'Query: {query}')
-        response = requests.get(_URL + query)
-
-        # Check the response status code
-        if response.status_code == 200:
-          # Parse the JSON response
-          data = response.json()
-          if not data.get('charts'):
-            writer.writerow(['EMPTY', query, "0", "", 0, 0, 0])
-            continue
-          chart_types = list(set([c['type'] for c in data['charts']]))
-          writer.writerow([
-              'SUCCESS', query,
-              str(len(data['charts'])), chart_types,
-              data['debug']['timing'].get('getNlResult', 0),
-              data['debug']['timing'].get('getTileResults', 0),
-              data['debug']['timing'].get('total', 0)
-          ])
-        else:
-          print("Request failed with status code:", response.status_code)
-          writer.writerow(
-              ['FAILURE', query, "0", "",
-               str(response.status_code)])
-  print('')
-  print(f'Output written to {output_file}')
-  print('')
-
-
-# Asynchronous function to run a query and add result of the run to results
-async def run_query(results, key, session, query):
-  try:
-    print(f'Query {key}: {query}')
-    response = await session.get(_URL + query)
-    # Check the response status code
-    if response.status == 200:
-      # parse the json response and add to the result
-      data = await response.json()
-      print(f'Query {key} completed successfully')
-      debug_timing = data.get('debug', {}).get('timing', {})
-      charts = data.get('charts', [])
-      chart_types = list(set([c['type'] for c in charts]))
-      results[key] = {
-          _RESULT_KEY_STATUS: 'EMPTY' if len(charts) == 0 else 'SUCCESS',
-          _RESULT_KEY_QUERY: query,
-          _RESULT_KEY_NUM_CHARTS: str(len(data['charts'])),
-          _RESULT_KEY_CHART_TYPES: chart_types,
-          _RESULT_KEY_NL_TIME: debug_timing.get('getNlResult', 0),
-          _RESULT_KEY_TILE_TIME: debug_timing.get('getTileResults', 0),
-          _RESULT_KEY_TOTAL_TIME: debug_timing.get('total', 0)
-      }
-    else:
-      print(f'Query {key} failed with status code:', response.status_code)
-      results[key] = {
-          _RESULT_KEY_STATUS: 'FAILURE',
-          _RESULT_KEY_QUERY: query,
-      }
-  except Exception as e:
-    print(f'Error running query {key}: {str(e)}')
-    results[key] = {
+# Gets the result for a single query.
+def run_query(query):
+  # Make the API request
+  print(f'Query: {query}')
+  response = requests.get(_URL + query)
+  # Check the response status code
+  if response.status_code == 200:
+    # parse the json response and add to the result
+    data = response.json()
+    debug_timing = data.get('debug', {}).get('timing', {})
+    charts = data.get('charts', [])
+    chart_types = list(set([c['type'] for c in charts]))
+    print(f'Query completed: {query}')
+    return {
+        _RESULT_KEY_STATUS: 'EMPTY' if len(charts) == 0 else 'SUCCESS',
+        _RESULT_KEY_QUERY: query,
+        _RESULT_KEY_NUM_CHARTS: str(len(data['charts'])),
+        _RESULT_KEY_CHART_TYPES: chart_types,
+        _RESULT_KEY_NL_TIME: debug_timing.get('getNlResult', 0),
+        _RESULT_KEY_TILE_TIME: debug_timing.get('getTileResults', 0),
+        _RESULT_KEY_TOTAL_TIME: debug_timing.get('total', 0)
+    }
+  else:
+    print(f'Query ({query}) failed with status code:', response.status_code)
+    return {
         _RESULT_KEY_STATUS: 'FAILURE',
         _RESULT_KEY_QUERY: query,
     }
 
 
-# Runs asynchronous load test where all the queries are requested in parallel
-async def run_async_load_test(query_file, output_file):
-  results = {}
-  result_keys = []
-  async with aiohttp.ClientSession() as session:
-    with open(query_file) as inf:
-      tasks = []
-      # Run each query in the query_file
-      for i, row in enumerate(csv.reader(inf)):
-        query = _get_query(row)
-        if not query:
-          continue
-        result_keys.append(i)
-        tasks.append(run_query(results, i, session, query))
-      # Wait for the results of all the query runs
-      await asyncio.gather(*tasks, return_exceptions=True)
-      # Write the results to the output_file
-      with open(output_file, 'w') as outf:
-        writer = csv.writer(outf)
-        writer.writerow(_RESULT_HEADER_ROW)
-        for i in result_keys:
-          i_result = results.get(i, {})
-          result_row = []
-          for row_key in _RESULT_HEADER_ROW:
-            result_row.append(i_result.get(row_key, ''))
-          writer.writerow(result_row)
-      print('')
-      print(f'Output written to {output_file}')
-      print('')
+# Runs regular load test where each query is requested one by one
+def load_test(query_file, output_file, run_async):
+  # Get the list of queries
+  query_list = []
+  with open(query_file) as inf:
+    query = _get_query
+    for row in csv.reader(inf):
+      query = _get_query(row)
+      if not query:
+        continue
+      query_list.append(query)
+
+  # Run each query from the list. If run_async is true, run all the queries
+  # asynchronously. Otherwise, run them one by one.
+  results = []
+  num_threads = len(query_list) if run_async else 1
+  with concurrent.futures.ThreadPoolExecutor(num_threads) as executor:
+    results = executor.map(run_query, query_list)
+
+  # Write the results to the output_file
+  with open(output_file, 'w') as outf:
+    writer = csv.writer(outf)
+    writer.writerow(_RESULT_HEADER_ROW)
+    for result in results:
+      result_row = []
+      for key in _RESULT_HEADER_ROW:
+        result_row.append(result.get(key, ''))
+      writer.writerow(result_row)
+  print('')
+  print(f'Output written to {output_file}')
+  print('')
 
 
 def main(_):
   assert FLAGS.queryset
   if FLAGS.run_async:
-    asyncio.run(run_async_load_test(FLAGS.queryset, _ASYNC_OUTPUT_FILE))
+    load_test(FLAGS.queryset, _ASYNC_OUTPUT_FILE, True)
   else:
-    load_test(FLAGS.queryset, _REGULAR_OUTPUT_FILE)
+    load_test(FLAGS.queryset, _REGULAR_OUTPUT_FILE, False)
 
 
 if __name__ == "__main__":
