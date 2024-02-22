a537274c23c5f328b6c590071707fe43b1e97fa9
Bo Xu <shifucun@users.noreply.github.com>
2024-02-22 14:33:36 -0800

    revert

--- nl_server/query_util.py
+++ nl_server/query_util.py
@@ -15,9 +15,11 @@
 
 from dataclasses import dataclass
 import itertools
+import logging
 import re
 from typing import List
 
+from shared.lib import constants
 from shared.lib import utils
 
 # TODO: decouple words removal from detected attributes. Today, the removal
