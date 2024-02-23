4d5f38173d8ec846f1c80fd1a782870d95221dc3
Keyur Shah <keyurs@google.com>
2024-02-23 10:30:20 -0800

    Run web driver in headless mode.

--- server/webdriver/tests/standalone/sdg_sanity.py
+++ server/webdriver/tests/standalone/sdg_sanity.py
@@ -163,7 +163,7 @@ class SdgWebsiteSanityTest:
 
   def __enter__(self):
     chrome_options = Options()
-    # chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     self.driver = webdriver.Chrome(options=chrome_options)
