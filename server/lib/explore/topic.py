cec93a1752a9389b76030cf86a86c9813cab4448
Prashanth R <shanth@google.com>
2024-03-04 21:13:02 -0800

    UNData: increase topic limit and fix edge-cases (#3996)
    1. Improves further on
    https://github.com/datacommonsorg/website/commit/a820bde8a37f9eb4fc3eeec69b239e2680b55161
    by opening more topics, but having an SV-based limit for Special DCs.
    
    2.  Sync mixer to fix the "number of cholera cases" query.
    
    3. For the edge-case, the following query was failing. Because
    "filter_with_single_var" fulfiller would not fallback to
    place-comparison, rather only to Basic (and basic won't fulfill
    multi-place query).
    
    ![image](https://github.com/datacommonsorg/website/assets/4375037/55819ab7-ed7f-4616-bf97-07d5d87b6c37)

--- server/lib/explore/topic.py
+++ server/lib/explore/topic.py
@@ -34,8 +34,13 @@ _MAX_TOPICS_TO_OPEN = 3
 
 # Pick a higher limit for SDG / Special DC
 _MAX_SUBTOPIC_SV_LIMIT_SDG = 500
+
+# We open many more topics for Special DC because
+# it combines topics from multiple sources (WHO, SDG,
+# ILO, etc).  But as a limit we have a total #var limit.
+_MAX_TOPICS_TO_OPEN_SPECIAL_DC = 10
 _MAX_SUBTOPIC_SV_LIMIT_SPECIAL_DC = 100
-_MAX_TOPICS_TO_OPEN_SPECIAL_DC = 5
+_MAX_SVS_TO_PROCESS_SPECIAL_DC = 500
 
 
 @dataclass
@@ -48,10 +53,13 @@ class TopicMembers:
 def compute_chart_vars(
     state: ftypes.PopulateState) -> OrderedDict[str, List[ftypes.ChartVars]]:
   num_topics_limit = _max_topics_to_open(state.uttr)
+  # Only Special DC has an SV limit.
+  num_svs_limit = _max_svs_to_process(state.uttr)
 
   dc = state.uttr.insight_ctx.get(Params.DC.value, DCNames.MAIN_DC.value)
   chart_vars_map = OrderedDict()
   num_topics_opened = 0
+  num_svs_processed = 0
   for sv in state.uttr.svs:
     cv = []
     if cutils.is_sv(sv):
@@ -67,7 +75,11 @@ def compute_chart_vars(
       if cv:
         num_topics_opened += 1
     if cv:
+      for e in cv:
+        num_svs_processed += len(e.svs)
       chart_vars_map[sv] = cv
+    if num_svs_limit and num_svs_processed >= num_svs_limit:
+      break
   return chart_vars_map
 
 
@@ -292,3 +304,10 @@ def _max_topics_to_open(uttr: ftypes.Utterance) -> int:
     # since there are fewer vars.
     max_topics = _MAX_TOPICS_TO_OPEN
   return max_topics
+
+
+def _max_svs_to_process(uttr: ftypes.Utterance) -> int:
+  if not is_sdg(uttr.insight_ctx) and is_special_dc(uttr.insight_ctx):
+    return _MAX_SVS_TO_PROCESS_SPECIAL_DC
+  # No special limit
+  return 0
