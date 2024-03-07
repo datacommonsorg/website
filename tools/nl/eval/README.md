dbf023530c3c91322aa31912a4a057656b5c47d6
Bo Xu <shifucun@users.noreply.github.com>
2024-03-07 13:57:03 -0800

    Eval tool for stat var description embedding match

--- tools/nl/eval/README.md
+++ tools/nl/eval/README.md
@@ -1,6 +1,6 @@
-# Run NL Eval
+# Data Commons Embeddings Eval
 
-## Produce eval golden data from Explore landing pages
+## Produce Eval Golden Data from Explore Landing Pages
 
 ```bash
 export NODEJS_API_KEY=<api_key_for_bard.datacommons.org>
@@ -9,3 +9,41 @@ python3 fetch_explore_queries.py
 
 This produces a csv file that can be imported to Google Sheet for further manual
 curation to produce golden data for eval purpose.
+
+## Prepare Model and Embedding Endpoints
+
+To evaluate a model and embeddings generated from the model, it needs to be
+deployed to Vertex AI. Concrete steps can be found in
+[model server](../../../model_server/README.md).
+
+Once a model is deployed, generate embeddings and index them in Vertex AI vector
+search. Concreate steps can be found in [build
+embeddings](../embeddings/README.md).
+
+Once model and embeddings endpoints are avaiable, record the endpoints id in
+`vertex_ai_endpoints.yaml` file.
+
+## Evalutation Unit
+
+One evaluation unit locates in a sub folder that contains a `golden.json` file
+that is a mapping between golden query and ranked stat vars to be matched from
+the vector store. Note there is logic to get to ranked stat vars from ranked
+embedding vectors. Right now it's simply based on the stat vars for a
+description embedding. This can be changed in the future. Multiple evalutation
+unit could exist for different eval sets (like UNSDG, WHO).
+
+## Run Eval
+
+Choose a model from `vertex_ai_endpoints.yaml` and eval unit folder and run:
+
+```bash
+./run.sh -m <model_name> -f <folder>
+```
+
+Example commands: `./run.sh -m dc-all-minilm-l6-v2 -f base`
+
+This produces `debug.json` and `report.csv` in the `result` folder of the eval
+unit folder.
+
+The `report.csv` contains query and the ndcg score per row. `debug.json`
+contains the matched embeddings description, stat var dcid and the cosine score.
---
+++ tools/nl/eval/main.py
@@ -0,0 +1,111 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import csv
+import json
+import os
+
+from absl import app
+from absl import flags
+from google.cloud import aiplatform
+from google.cloud import aiplatform_v1
+import ndcg
+import numpy as np
+import yaml
+
+PROJECT = 'datcom-website-dev'
+LOCATION = 'us-central1'
+VECTOR_SERACH_ENDPOINT = "302175072.us-central1-496370955550.vdb.vertexai.goog"
+INDEX_ENDPOINT = "projects/496370955550/locations/us-central1/indexEndpoints/8500794985312944128"
+NEIGHBOR_COUNT = 30
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string(
+    'model_name', '',
+    'Model name used for eval. Full list can be found in endpoints.yaml')
+
+flags.DEFINE_string(
+    'eval_folder', 'base',
+    'The folder for a eval unit. It should contain a golden.json file')
+
+
+def _load_models():
+  with open('vertex_ai_endpoints.yaml') as f:
+    return yaml.full_load(f)
+
+
+def _load_baseline():
+  with open(os.path.join(FLAGS.eval_folder, 'golden.json')) as f:
+    return json.load(f)
+
+
+def main(_):
+  aiplatform.init(project=PROJECT, location=LOCATION)
+  base_line = _load_baseline()
+  models = _load_models()
+  if FLAGS.model_name not in models:
+    print('Model not found')
+    return
+  model_info = models.get(FLAGS.model_name)
+  prediction_endpoint = aiplatform.Endpoint(
+      model_info['prediction_endpoint_id'])
+  vector_search_client = aiplatform_v1.MatchServiceClient(
+      client_options={"api_endpoint": VECTOR_SERACH_ENDPOINT},)
+
+  debug = {}
+  report = []
+  for query, base_line_matches in base_line.items():
+    print(query)
+    query_vector = prediction_endpoint.predict(instances=[query]).predictions[0]
+    query_norm = np.linalg.norm(query_vector)
+    vector_datapoint = aiplatform_v1.IndexDatapoint(feature_vector=query_vector)
+    vector_search_query = aiplatform_v1.FindNeighborsRequest.Query(
+        datapoint=vector_datapoint, neighbor_count=NEIGHBOR_COUNT)
+    vector_search_req = aiplatform_v1.FindNeighborsRequest(
+        index_endpoint=INDEX_ENDPOINT,
+        deployed_index_id=model_info['index_id'],
+        queries=[vector_search_query],
+        return_full_datapoint=True,
+    )
+    vector_search_resp = vector_search_client.find_neighbors(vector_search_req)
+    ranked_stat_vars = []
+    debug[query] = {'vector_search': []}
+    for n in vector_search_resp.nearest_neighbors[0].neighbors:
+      dp = n.datapoint
+      stat_var = dp.restricts[0].allow_list[0]
+      if stat_var not in ranked_stat_vars:
+        ranked_stat_vars.append(stat_var)
+      dp_norm = np.linalg.norm(dp.feature_vector)
+      debug[query]['vector_search'].append({
+          'sentence': dp.datapoint_id,
+          'stat_var': dp.restricts[0].allow_list[0],
+          'distance': n.distance / dp_norm / query_norm
+      })
+    if len(ranked_stat_vars) > len(base_line_matches):
+      ranked_stat_vars = ranked_stat_vars[:len(base_line_matches)]
+    debug[query]['ranked_stat_vars'] = ranked_stat_vars
+    ranking_score = ndcg.ndcg(ranked_stat_vars, base_line_matches)
+    report.append([query, ranking_score])
+
+  folder = os.path.join(FLAGS.eval_folder, 'result')
+  with open(os.path.join(folder, f'report_{FLAGS.model_name}.csv'), 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(report)
+  with open(os.path.join(folder, f'debug_{FLAGS.model_name}.json'), 'w') as f:
+    json.dump(debug, f)
+
+
+if __name__ == "__main__":
+  app.run(main)
