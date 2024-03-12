cd5997fc814e15c9f37a0af21f9a800899261fd5
Prashanth R <shanth@google.com>
2024-03-12 16:44:14 -0700

    Try to run NL integration tests against staging (#4016)

--- run_test.sh
+++ run_test.sh
@@ -166,7 +166,10 @@ function run_integration_test {
   source .env/bin/activate
   export ENABLE_MODEL=true
   export FLASK_ENV=integration_test
-  export GOOGLE_CLOUD_PROJECT=datcom-website-dev
+  export DC_API_KEY=
+  export LLM_API_KEY=
+  export ENV_PREFIX=Staging
+  export GOOGLE_CLOUD_PROJECT=datcom-website-staging
   export TEST_MODE=test
   python3 -m pytest -vv --reruns 2 server/integration_tests/$1
   deactivate
@@ -176,11 +179,16 @@ function update_integration_test_golden {
   source .env/bin/activate
   export ENABLE_MODEL=true
   export FLASK_ENV=integration_test
-  export GOOGLE_CLOUD_PROJECT=datcom-website-dev
+  export GOOGLE_CLOUD_PROJECT=datcom-website-staging
   export TEST_MODE=write
+  export DC_API_KEY=
+  export LLM_API_KEY=
+
+  export ENV_PREFIX=Autopush
   python3 -m pytest -vv server/integration_tests/topic_cache
-  python3 -m pytest -vv -n 5 --reruns 2 server/integration_tests/
   python3 -m pytest -vv server/tests/nodejs_e2e_test.py
+  export ENV_PREFIX=Staging
+  python3 -m pytest -vv -n 5 --reruns 2 server/integration_tests/
 }
 
 function run_all_tests {
