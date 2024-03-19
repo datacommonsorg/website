0b3962c7b930d8b0bcf2e163744f34b41b46555e
Julia Wu <juliawu@google.com>
2024-03-19 15:46:48 -0700

    [Biomedical DC] Add the main landing page (#4003)
    This PR adds the main landing page for biomedical DC.
    
    Changes Include:
    * Adding a route /bio that takes the user to the new landing page (but
    hidden behind 404 on prod)
    * CSS, TS, HTML for the new landing page
    * React components for the search box and clickable cards
    
    NOT included, but to be added in followup PRs:
    * A custom header, menu, and footer for biomedical pages
    * A "more examples" button for the sample questions section
    * Moving the homepage to its final destination in custom DC
    * Internationalization of the content
    
    Sample screenshots:
    <img
    src="https://github.com/datacommonsorg/website/assets/4034366/108f6250-eda2-44cc-9b80-a0f45d848c6e"
    />
    <img
    src="https://github.com/datacommonsorg/website/assets/4034366/31c08542-a924-45bd-97c9-a23631c5d227"
    width="25%" />

---
+++ static/js/biomedical/landing/data_sources_section.tsx
@@ -0,0 +1,74 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Data sources section of the Biomedical DC landing page.
+ */
+
+import React from "react";
+import { styled } from "styled-components";
+
+import { ContentContainer, TextBlock } from "./shared_styled_components";
+
+const UnderlinedLink = styled.a`
+  text-decoration-line: underline;
+`;
+
+const ExploreStructureLink = styled.a`
+  align-items: center;
+  display: flex;
+  gap: 8px;
+
+  &:hover {
+    text-decoration: none;
+  }
+`;
+
+export function DataSourcesSection(): JSX.Element {
+  return (
+    <ContentContainer className="container">
+      <h2>Data Sources</h2>
+      <TextBlock className="row">
+        <div className="col-lg-8">
+          <p>
+            The Biomedical Data Commons is a comprehensive repository that
+            integrates data from over{" "}
+            <UnderlinedLink href="https://docs.datacommons.org/datasets/Biomedical.html">
+              18 different trusted sources
+            </UnderlinedLink>{" "}
+            like NIH NCBI and EMBL-EBI. Including commonly used open-source
+            datasets such as ChEMBL, ClinVar, Gene, GTEx, MeSH, PharmGKB, and
+            UniProt. This collaborative effort unifies and harmonizes diverse
+            biomedical data, enabling innovative research that transcends the
+            limitations of isolated sources.
+          </p>
+        </div>
+        <div className="col-lg-4">
+          This collaborative effort unifies and harmonizes diverse biomedical
+          data, enabling innovative research that transcends the limitations of
+          isolated sources.
+        </div>
+      </TextBlock>
+      <ExploreStructureLink
+        href="/browser/bio"
+        className="explore-structure-link"
+      >
+        <span className="material-icons-outlined">account_tree</span>
+        Explore the structure
+      </ExploreStructureLink>
+    </ContentContainer>
+  );
+}
