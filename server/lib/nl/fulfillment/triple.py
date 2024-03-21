ec3160c3abaaa35154aa8c59427b207f75ce926a
chejennifer <69875368+chejennifer@users.noreply.github.com>
2024-03-21 09:43:04 -0700

    [biomed nl] allow in arcs (#4051)
    Add fulfillment handling for properties that exist in the 'in' direction
    
    Note: Updating title formatting is on the to-do list. Currently just
    displaying something to show that the flow is working.
    
    <img width="1770" alt="Screenshot 2024-03-19 at 4 34 08 PM"
    src="https://github.com/datacommonsorg/website/assets/69875368/57c23ae6-b313-4fe7-a3ef-4b44836963c0">

--- server/lib/nl/fulfillment/triple.py
+++ server/lib/nl/fulfillment/triple.py
@@ -23,6 +23,39 @@ from server.lib.nl.fulfillment.utils import add_chart_to_utterance
 # Handler for TRIPLE queries.
 #
 
+_OUT_ARROW = '->'
+_IN_ARROW = '<-'
+
+
+# Gets the chart vars for a property and direction if values exist for that
+# property and direction,
+def _get_chart_vars(uttr: nl_uttr.Utterance, prop: str, out: bool) -> ChartVars:
+  entity_dcids = [e.dcid for e in uttr.entities]
+  prop_values = raw_property_values(entity_dcids, prop, out)
+  prop_with_direction = f'{_OUT_ARROW}{prop}' if out else f'{_IN_ARROW}{prop}'
+  if not any(prop_values.values()):
+    uttr.counters.err('triple_property_failed_existence', prop_with_direction)
+    return None
+  chart_vars = ChartVars(props=[prop_with_direction])
+  return chart_vars
+
+
+# Handles properties with no direction specified
+def _populate_any_direction_prop(uttr: nl_uttr.Utterance, prop: str):
+  # TODO: prefer index to use properties with arrows
+  out_chart_vars = _get_chart_vars(uttr, prop, True)
+  in_chart_vars = _get_chart_vars(uttr, prop, False)
+  chart_added = False
+  state = PopulateState(uttr=uttr)
+  for cv in [out_chart_vars, in_chart_vars]:
+    if not cv:
+      continue
+    chart_added |= add_chart_to_utterance(ChartType.ANSWER,
+                                          state,
+                                          cv, [],
+                                          entities=uttr.entities)
+  return chart_added
+
 
 def populate(uttr: nl_uttr.Utterance) -> bool:
   if not uttr.entities:
@@ -32,20 +65,27 @@ def populate(uttr: nl_uttr.Utterance) -> bool:
     uttr.counters.err('triple_failed_no_properties', 1)
     return False
 
+  chart_added = False
+  # TODO: consider messaging if top matches fail existence
   for prop in uttr.properties:
     # TODO: handle properties that are links that use ->
     if '->' in prop or '<-' in prop:
       uttr.counters.err('triple_property_unsupported', prop)
       continue
-    entity_dcids = [e.dcid for e in uttr.entities]
-    prop_values = raw_property_values(entity_dcids, prop)
-    if not any(prop_values.values()):
-      uttr.counters.err('triple_property_failed_existence', prop)
-      continue
-    state = PopulateState(uttr=uttr)
-    chart_vars = ChartVars(props=[prop])
-    return add_chart_to_utterance(ChartType.ANSWER_WITH_ENTITY_OVERVIEW,
-                                  state,
-                                  chart_vars, [],
-                                  entities=uttr.entities)
-  return False
+    else:
+      if _populate_any_direction_prop(uttr, prop):
+        chart_added = True
+        # Currently only handling one prop, so if populate succeeds for any one
+        # prop, return.
+        # TODO: revisit how we want to handle multiple props
+        break
+  # If we are populating for a single entity and we've added an answer for this
+  # entity, also add an entity overview chart
+  # TODO: consider if we want to always add an entity overview chart when there
+  # are entities
+  if chart_added and len(uttr.entities) == 1:
+    add_chart_to_utterance(ChartType.ENTITY_OVERVIEW,
+                           PopulateState(uttr=uttr),
+                           ChartVars(), [],
+                           entities=uttr.entities)
+  return chart_added
