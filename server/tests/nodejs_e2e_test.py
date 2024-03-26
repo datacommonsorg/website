b64678154a4b44cb7e7068e01407b03330dfcd54
Dan Noble <dwnoble@google.com>
2024-03-26 16:33:28 -0700

    pr feedback

--- server/tests/nodejs_e2e_test.py
+++ server/tests/nodejs_e2e_test.py
@@ -11,9 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# Disabled nodejs test to avoid dependency on dev 
-"""
 import json
 import os
 import unittest
@@ -69,4 +66,3 @@ class IntegrationTest(unittest.TestCase):
     self.run_test('disaster', 'fires in california')
     self.run_test('rank_top_only',
                   'California counties with the highest female population')
-"""
