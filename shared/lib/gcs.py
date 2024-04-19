de6f08a6045fef3836e92b67d063aac4bd2989cd
Keyur Shah <keyurs@google.com>
2024-04-19 00:57:54 -0700

    Fix test failures due to method param name change from bucket to bucketname.

--- shared/lib/gcs.py
+++ shared/lib/gcs.py
@@ -55,17 +55,17 @@ def download_gcs_file(gcs_path: str, use_anonymous_client: bool = False) -> str:
 # Downloads the `filename` from GCS to TEMP_DIR
 # and returns its path.
 #
-def download_file(bucketname: str,
+def download_file(bucket: str,
                   filename: str,
                   use_anonymous_client: bool = False) -> str:
   if use_anonymous_client:
     storage_client = storage.Client.create_anonymous_client()
   else:
     storage_client = storage.Client()
-  bucket = storage_client.bucket(bucket_name=bucketname)
-  blob = bucket.get_blob(filename)
+  bucket_object = storage_client.bucket(bucket_name=bucket)
+  blob = bucket_object.get_blob(filename)
   # Download
-  local_file_path = _get_local_path(bucketname, filename)
+  local_file_path = _get_local_path(bucket, filename)
   # Create directory to file if it does not exist.
   parent_dir = Path(local_file_path).parent
   if not parent_dir.exists():
@@ -74,17 +74,17 @@ def download_file(bucketname: str,
   return local_file_path
 
 
-def get_or_download_file(bucketname: str,
+def get_or_download_file(bucket: str,
                          filename: str,
                          use_anonymous_client: bool = False) -> str:
   """Returns the local file path if the file already exists. 
   Otherwise it downloads the file from GCS and returns the path it was downloaded to.
   """
-  local_file_path = _get_local_path(bucketname, filename)
+  local_file_path = _get_local_path(bucket, filename)
   if os.path.exists(local_file_path):
     logging.info("Using already downloaded GCS file: %s", local_file_path)
     return local_file_path
-  return download_file(bucketname, filename, use_anonymous_client)
+  return download_file(bucket, filename, use_anonymous_client)
 
 
 def _get_local_path(bucketname: str, filename: str) -> str:
