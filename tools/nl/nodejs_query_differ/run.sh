6a8cdae43a875740ffe3c37054954934be9069b8
chejennifer <69875368+chejennifer@users.noreply.github.com>
2024-04-22 10:13:39 -0700

    [cron testing] Tool for updating nodejs query diff goldens (#4106)
    Various updates for nodejs query diffing
    
    - updates differ process so that nodejs query differ runs diffs against
    local goldens
    - updating goldens means pulling the results from the latest run and
    saving them as the new goldens
    - adds instructions for debugging diffs to the email

--- tools/nl/nodejs_query_differ/run.sh
+++ tools/nl/nodejs_query_differ/run.sh
@@ -13,39 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-function help {
-  echo "Usage: $0 -ebt"
-  echo "-e       Instance environment to get diffs for"
-  echo "-t       folder to test"
-  echo "-g       gcs folder to output differ results to"
-  echo "-f       file to output a email template to if there are diffs."
-  exit 1
-}
-
-while getopts ":e:t:g:f:" OPTION; do
-  case $OPTION in
-    e)
-      ENV=$OPTARG
-      ;;
-    t)
-      TEST=$OPTARG
-      ;;
-    g)
-      GCS_OUTPUT=$OPTARG
-      ;;
-    f)
-      FAILURE_EMAIL=$OPTARG
-      ;;
-    *)
-      help
-      ;;
-  esac
-done
-
 python3 -m venv .env
 source .env/bin/activate
 python3 -m pip install --upgrade pip setuptools
 python3 -m pip install -r requirements.txt
-
-# Diff production embeddings against test.
-python3 differ.py --env=$ENV --test_folder=$TEST --gcs_output_folder=$GCS_OUTPUT --failure_email_file=$FAILURE_EMAIL
+python3 differ.py "$@"
