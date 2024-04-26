98e8915d51394e40f158f3b7a1707ccbea6c6254
Julia Wu <wujulia.jw@gmail.com>
2024-04-26 11:04:04 -0700

    fix typo

--- server/__init__.py
+++ server/__init__.py
@@ -270,7 +270,7 @@ def create_app(nl_root=DEFAULT_NL_ROOT):
   if ingress_config_path:
     configure_endpoints_from_ingress(ingress_config_path)
 
-  if os.environ.get('FlASK_ENV') == 'biomedical':
+  if os.environ.get('FLASK_ENV') == 'biomedical':
     register_routes_biomedical_dc(app)
 
   register_routes_common(app)
