cba18d1341bc2d7b7193fba0001a8b8840e5c93f
Prashanth R <shanth@google.com>
2024-05-14 19:07:12 -0700

    NL: avoid NULL access in healthz (#4240)
    Custom DC crashes here. This likely won't fix it, but we might know
    more...

--- nl_server/loader.py
+++ nl_server/loader.py
@@ -103,6 +103,7 @@ def _load_yaml(flask_env: str) -> Dict[str, any]:
     embeddings_map['indexes'].update(custom_map.get('indexes', {}))
     embeddings_map['models'].update(custom_map.get('models', {}))
 
+  logging.info(f'Attempting to load NL YAML: {embeddings_map}')
   return embeddings_map
 
 
