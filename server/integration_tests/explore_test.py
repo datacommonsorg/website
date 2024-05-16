dd5bdc54054538b4ac30d1307f54a2e389371e93
Dan Noble <dwnoble@google.com>
2024-05-16 03:34:20 -0400

    Updated UN ILO topic generation script, added variable groupings (#4242)
    * Updated UN ILO topic generation script 
    * Added `dc/g/Custom_UN` variable grouping root to un staging
    environment
    * Fixed some typos in undata who variable groupings
    * Updated un staging ILO bt version
    * added undata_ilo search index
    * added undata_ilo embeddings and topic cache

--- server/integration_tests/explore_test.py
+++ server/integration_tests/explore_test.py
@@ -280,6 +280,12 @@ class ExploreTest(NLWebServerTestCase):
                        test='unittest',
                        idx='undata_ft')
 
+  def test_detection_basic_undata_ilo(self):
+    self.run_detection('detection_api_undata_ilo_idx',
+                       ['Employment in the world'],
+                       test='unittest',
+                       idx='undata_ilo_ft')
+
   def test_detection_basic_bio(self):
     self.run_detection('detection_api_bio_idx', ['Commute in California'],
                        test='unittest',
