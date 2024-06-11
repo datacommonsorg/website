5bd6c224e71b84ccef3389a60b9091f4b65c0ed0
Jennifer Chen <chejennifer@google.com>
2024-06-11 15:13:57 -0700

    fix test

--- static/nodejs_server/mock_functions.ts
+++ static/nodejs_server/mock_functions.ts
@@ -49,7 +49,7 @@ export function queryAxiosMock(): void {
 
   when(axios.post)
     .calledWith(
-      "/api/explore/detect-and-fulfill?q=family earnings in north dakota&mode=strict&client=bard&detector=heuristic&varThreshold=&idx=medium_ft",
+      "/api/explore/detect-and-fulfill?q=family earnings in north dakota&mode=strict&client=bard&detector=heuristic&varThreshold=&idx=base_uae_mem",
       {}
     )
     .mockResolvedValue(TIMELINE_NL_RESP);
@@ -156,7 +156,7 @@ export function queryAxiosMock(): void {
    */
   when(axios.post)
     .calledWith(
-      "/api/explore/detect-and-fulfill?q=top jobs in santa clara county&mode=strict&client=bard&detector=heuristic&varThreshold=&idx=medium_ft",
+      "/api/explore/detect-and-fulfill?q=top jobs in santa clara county&mode=strict&client=bard&detector=heuristic&varThreshold=&idx=base_uae_mem",
       {}
     )
     .mockResolvedValue(BAR_NL_RESP);
@@ -213,7 +213,7 @@ export function queryAxiosMock(): void {
    */
   when(axios.post)
     .calledWith(
-      "/api/explore/detect-and-fulfill?q=obesity vs. poverty in counties of california&mode=strict&client=bard&detector=heuristic&varThreshold=&idx=medium_ft",
+      "/api/explore/detect-and-fulfill?q=obesity vs. poverty in counties of california&mode=strict&client=bard&detector=heuristic&varThreshold=&idx=base_uae_mem",
       {}
     )
     .mockResolvedValue(SCATTER_NL_RESP);
