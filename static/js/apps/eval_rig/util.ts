c494da266ea6b111b62d84d27cfa638552bba30c
Bo Xu <shifucun@users.noreply.github.com>
2024-06-11 13:45:06 -0700

    Better display footnote links

--- static/js/apps/eval_rig/util.ts
+++ static/js/apps/eval_rig/util.ts
@@ -19,7 +19,7 @@ export const processText = (text: string): string => {
   let processedText = text.replace("Answer:", "");
   // If "FOOTNOTES" in all caps is in the text, convert it to lower case
   processedText = processedText.replace("FOOTNOTES", "Footnotes");
-  return processedText.replace(
+  processedText = processedText.replace(
     // Replace [__DC__#1(dc stat text||llm stat text)] to
     // dc stat text||llm stat text with html and css annotations.
     /\[\s*__DC__#(\d+)\((.*?)\)\]/g,
@@ -39,5 +39,12 @@ export const processText = (text: string): string => {
       innerHtml += `<span class="llm-stat">${llmStat || "&nbsp;&nbsp;"}</span>`;
       return `<span class="annotation annotation-${callId}">${innerHtml}</span>`;
     }
+  );
+  // Format the links
+  const httpPattern = /https:\/\/[^\s]+/g;
+  // Replace each link with the desired HTML format
+  return processedText.replace(
+    httpPattern,
+    (match) => `<a href="${match}" target="_blank">Explore Page</a><br> `
   );
 };
