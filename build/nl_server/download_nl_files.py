672b56eea670533c341bd63307221b8f76ee3735
Jennifer Chen <chejennifer@google.com>
2024-06-14 08:28:47 -0700

    update num workers and load test

---
+++ build/nl_server/download_nl_files.py
@@ -0,0 +1,48 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Function to download all the nl files in catalog.yaml from gcs"""
+
+from nl_server import config_reader
+from nl_server.config import ModelType
+from nl_server.config import StoreType
+from shared.lib import gcs
+
+
+from absl import app
+import yaml
+
+def main(_):
+  # Read the catalog yaml
+  catalog_dict = {}
+  with open('catalog.yaml') as f:
+    catalog_dict = yaml.full_load(f)
+  catalog = config_reader.read_catalog(catalog_dict)
+
+  # Download all the models of LOCAL type
+  for model_info in catalog.models.values():
+    if model_info.type != ModelType.LOCAL:
+      continue
+    gcs.maybe_download(model_info.gcs_folder, use_anonymous_client=True)
+
+  # Download all the indexes that are MEMORY or LANCEDB store type
+  for index_info in catalog.indexes.values():
+    if not index_info.store_type in [StoreType.MEMORY, StoreType.LANCEDB]:
+      continue
+    if gcs.is_gcs_path(index_info.embeddings_path):
+      gcs.maybe_download(index_info.embeddings_path, use_anonymous_client=True)
+
+
+if __name__ == '__main__':
+  app.run(main)
--- tools/nl/loadtest/load.py
+++ tools/nl/loadtest/load.py
@@ -48,12 +48,16 @@ def _run_query(query):
     response = session.get(url, timeout=None)
     if response.status_code == 200:
       debug_timing = response.json().get('debug', {}).get('timing', {})
-      return {
+      result = {
           _RESULT_KEY_CODE: str(response.status_code),
           _RESULT_KEY_NL_TIME: debug_timing.get('getNlResult'),
           _RESULT_KEY_TOTAL_TIME: debug_timing.get('total'),
-          _RESULT_KEY_TILE_TIME: debug_timing.get('getTileResults')
+          _RESULT_KEY_TILE_TIME: debug_timing.get('getTileResults'),
       }
+      result.update(response.json().get('debug',
+                                        {}).get('debug',
+                                                {}).get('counters',
+                                                        {}).get('TIMING', {}))
     return {
         _RESULT_KEY_CODE: str(response.status_code),
         _RESULT_KEY_ERROR_CONTENT: str(response.content)
@@ -92,6 +96,20 @@ def _get_result_output(run_results):
       _RESULT_KEY_TILE_TIME: _get_empty_time_result(),
       _RESULT_KEY_TOTAL_TIME: _get_empty_time_result(),
   }
+  nl_debug_result = {
+      'explore_more_existence_check': _get_empty_time_result(),
+      'explore_more_sv_extensions': _get_empty_time_result(),
+      'fulfillment': _get_empty_time_result(),
+      'get_all_child_places': _get_empty_time_result(),
+      'get_sv_details': _get_empty_time_result(),
+      'query_detection': _get_empty_time_result(),
+      'setup_for_explore': _get_empty_time_result(),
+      'sv_existence_for_places': _get_empty_time_result(),
+      'sv_existence_for_places_check_single_point': _get_empty_time_result(),
+      'topic_calls': _get_empty_time_result(),
+      'topic_expansion': _get_empty_time_result(),
+  }
+
   error_content = {}
   for r in run_results:
     code = r.get(_RESULT_KEY_CODE)
@@ -103,21 +121,21 @@ def _get_result_output(run_results):
       content = r.get(_RESULT_KEY_ERROR_CONTENT)
       content_occurence = error_content[code].get(content, 0) + 1
       error_content[code][content] = content_occurence
-    for k in [
-        _RESULT_KEY_NL_TIME, _RESULT_KEY_TILE_TIME, _RESULT_KEY_TOTAL_TIME
-    ]:
-      if not r.get(k):
-        continue
-      num_seen = times_seen.get(k, 0) + 1
-      times_seen[k] = num_seen
-      time = float(r.get(k))
-      result[k]['max'] = max(time, result[k]['max'])
-      if result[k]['min'] == 0:
-        result[k]['min'] = time
-      else:
-        result[k]['min'] = min(time, result[k]['min'])
-      result[k]['avg'] = result[k]['avg'] * (num_seen -
-                                             1) / num_seen + time / num_seen
+    for result_dict in [result, nl_debug_result]:
+      for k in result_dict.keys():
+        if not r.get(k):
+          continue
+        num_seen = times_seen.get(k, 0) + 1
+        times_seen[k] = num_seen
+        time = float(r.get(k))
+        result_dict[k]['max'] = max(time, result_dict[k]['max'])
+        if result_dict[k]['min'] == 0:
+          result_dict[k]['min'] = time
+        else:
+          result_dict[k]['min'] = min(time, result_dict[k]['min'])
+        result_dict[k]['avg'] = result_dict[k]['avg'] * (
+            num_seen - 1) / num_seen + time / num_seen
+  result['nl_debug_timing'] = nl_debug_result
   result['codes'] = response_codes
   result['errors'] = error_content
   return result
