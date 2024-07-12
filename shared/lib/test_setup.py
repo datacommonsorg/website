3e3a8a7d9e83087ae37d6538d724ffcec43ca389
Prashanth Radhakrishnan <shanth@google.com>
2024-07-12 00:00:20 -0700

    Tweak

--- shared/lib/test_setup.py
+++ shared/lib/test_setup.py
@@ -12,11 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
 import multiprocessing
 import os
-import platform
-import sys
 
 
 def set_up_multiprocessing_for_tests():
