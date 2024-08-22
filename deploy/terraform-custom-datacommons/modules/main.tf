b4b27ca66bc8ef385e3a0f4ebd4b3e442f0f2314
Dan Noble <dwnoble@google.com>
2024-08-22 10:01:15 -0700

    updated custom dc terraform services container to use the new image

--- deploy/terraform-custom-datacommons/modules/main.tf
+++ deploy/terraform-custom-datacommons/modules/main.tf
@@ -256,9 +256,9 @@ resource "google_cloud_run_v2_service" "dc_web_service" {
           port = 8080
         }
         initial_delay_seconds = 60
-        period_seconds        = 30
+        period_seconds        = 10
         timeout_seconds       = 5
-        failure_threshold     = 5
+        failure_threshold     = 15
       }
 
       liveness_probe {
