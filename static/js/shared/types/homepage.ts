6736d33b5129198e6b22d91f16b6434be3d4488d
Nick B <nicholas.blumberg@gmail.com>
2024-08-28 18:05:05 -0700

    Conversion of homepage.html to React. The homepage now passes template variables to main.ts, which initializes a React app. All homepage components are now implemented via this React app.
    Typing files that could potentially be shared later were put into typing files in the shared directory.

---
+++ static/js/shared/types/homepage.ts
@@ -0,0 +1,17 @@
+export interface Partner {
+  id: string;
+  title: string;
+  url: string;
+  "sprite-index": number;
+}
+
+export interface Topic {
+  id: string;
+  title: string;
+  description: string;
+  icon: string;
+  image: string;
+  url: string;
+  browseUrl: string;
+  "sprite-index": number;
+}
