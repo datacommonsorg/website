8758913dcb518715a5ecd2d00f458157e6efa923
Nick B <nicholas.blumberg@gmail.com>
2024-09-04 20:22:00 -0700

    Following up from the previous commit, the .ts file for v2 (the new version).

---
+++ static/js/apps/homepage/main_v2.ts
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Entrypoint file for homepage.
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { loadLocaleData } from "../../i18n/i18n";
+import { Topic } from "../../shared/types/homepage";
+import { extractRoutes } from "../base/utilities/utilities";
+import { App } from "./app";
+
+window.addEventListener("load", (): void => {
+  loadLocaleData("en", [import("../../i18n/compiled-lang/en/units.json")]).then(
+    () => {
+      renderPage();
+    }
+  );
+});
+
+function renderPage(): void {
+  const topics = JSON.parse(
+    document.getElementById("metadata-homepage").dataset.topics
+  ) as Topic[];
+  const partners = JSON.parse(
+    document.getElementById("metadata-homepage").dataset.partners
+  );
+
+  const routes = extractRoutes();
+
+  ReactDOM.render(
+    React.createElement(App, {
+      topics,
+      partners,
+      routes,
+    }),
+    document.getElementById("app-container")
+  );
+}
