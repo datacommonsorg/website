c40bd3116eb685b89adfdd3a2b2599ff49a699e1
Nick B <nicholas.blumberg@gmail.com>
2024-10-16 18:32:47 -0700

    Standardized the TS breakpoint object to defaults, and annotated with a more thorough note.

--- static/js/apps/base/utilities/utilities.ts
+++ static/js/apps/base/utilities/utilities.ts
@@ -140,10 +140,16 @@ export const extractLabels = (elementId = "metadata-labels"): Labels => {
   return labels;
 };
 
-//A breakpoints constant object for responsiveness, for access on the TypeScript side.
+/*
+  A breakpoints constant object for responsiveness, for access on the TypeScript side.
+  These are the default breakpoints for a page that does not have custom breakpoints applied
+  to it. If a page does not use default breakpoints and those breakpoints are required on
+  the TypeScript side, a separate breakpoint object should be created.
+*/
 export const BREAKPOINTS = {
-  sm: 599,
-  md: 746,
-  mdExpanded: 859,
-  lg: 1440,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+  xxl: 1400,
 };
