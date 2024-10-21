d347f7ee041f8cd854fb70423b291e071a960817
Gabriel Mechali <gabriel.mechali@gmail.com>
2024-10-21 17:53:00 -0400

    Adds logging for autocomplete responses. (#4678)
    Logs the response count for autocompletion. Staging is not showing any
    responses. Would like to better understand where the breakdown is
    occurring.

--- server/routes/shared_api/autocomplete/helpers.py
+++ server/routes/shared_api/autocomplete/helpers.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import json
+import logging
 import re
 from typing import Dict, List
 from urllib.parse import urlencode
@@ -122,6 +123,8 @@ def predict(queries: List[str], lang: str) -> List[ScoredPrediction]:
       all_responses.append(scored_prediction)
 
   all_responses.sort(key=get_score)
+  logging.info("[Place_Autocomplete] Received %d total place predictions.",
+               len(all_responses))
 
   responses = []
   place_ids = set()
