ef0324f89f1a52a4fc74f3d600afbc5852a45c5b
Nick B <nicholas.blumberg@gmail.com>
2024-10-29 21:31:34 -0700

    Minor formatting, prettification and linting in prep for merge.

--- static/js/components/content/link_chips.tsx
+++ static/js/components/content/link_chips.tsx
@@ -65,7 +65,16 @@ export const LinkChips = ({
                   window.location.href = linkChip.url;
                 }}
               >
-                <span className="icon"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/></svg></span>
+                <span className="icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                  >
+                    <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+                  </svg>
+                </span>
                 {linkChip.title}
               </a>
             </li>
