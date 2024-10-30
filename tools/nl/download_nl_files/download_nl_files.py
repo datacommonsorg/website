1c5ad506c8be14a498be52d48910cfcd55f70a2f
Jennifer Chen <chejennifer@google.com>
2024-10-30 15:29:04 -0700

    clean up

--- tools/nl/download_nl_files/download_nl_files.py
+++ tools/nl/download_nl_files/download_nl_files.py
@@ -14,21 +14,26 @@
 """Function to download all the nl files in catalog.yaml from gcs to a cache
 folder"""
 
-from nl_server import config_reader
-from nl_server.config import ModelType
-from nl_server.config import StoreType
-from nl_server.cache import DOCKER_DATA_FOLDER_PATH
-from shared.lib import gcs
-
 from absl import app
 from absl import flags
 import yaml
 
+from nl_server import config_reader
+from nl_server.cache import DOCKER_DATA_FOLDER_PATH
+from nl_server.config import ModelType
+from nl_server.config import StoreType
+from shared.lib import gcs
+
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('catalog_path', 'catalog.yaml',
-                    'path to the catalog.yaml file, which is the main config file for NL models and embeddings')
-flags.DEFINE_bool('is_docker_mode', False, 'If is_docker_mode is True, download files to docker specific folder path.')
+flags.DEFINE_string(
+    'catalog_path', 'catalog.yaml',
+    'path to the catalog.yaml file, which is the main config file for NL models and embeddings'
+)
+flags.DEFINE_bool(
+    'is_docker_mode', False,
+    'If is_docker_mode is True, download files to docker specific folder path.')
+
 
 def main(_):
   # Read the catalog yaml
@@ -37,7 +42,6 @@ def main(_):
     catalog_dict = yaml.full_load(f)
   catalog = config_reader.read_catalog(catalog_dict=catalog_dict,
                                        catalog_paths=[])
-  
 
   # Get the path to the cache to download the files to
   cache_root = "/tmp"
