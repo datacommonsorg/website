43fbd329130bf636a11463ef1a4dbf273fc26d10
Jennifer Chen <chejennifer@google.com>
2024-11-04 13:55:05 -0800

    add some comments about the response fields for nodejs endpoint

--- static/nodejs_server/types.ts
+++ static/nodejs_server/types.ts
@@ -60,18 +60,29 @@ export interface TileResult {
 
 // Properties to use for drawing a single chart
 export interface ChartProps {
+  // Dcid of the place shown in the chart. If it's a chart that shows children
+  // places such as map or ranking, this is the dcid of the parent place.
   place: string;
+  // Type of place shown in the chart if it's a chart that shows children places
   enclosedPlaceType: string;
+  // Details about the variables to show in the chart including their dcids,
+  // units, denominators, etc.
   statVarSpec: StatVarSpec[];
+  // Details about the chart including the title, type of chart, etc.
   tileConfig: TileConfig;
+  // Event type information to be used by this chart. The key is the event
+  // type id and the value is the spec for that event type.
   eventTypeSpec: Record<string, EventTypeSpec>;
 }
 
 // Debug info to return in /nodejs/query response
 export interface DebugInfo {
   timing: {
+    // The time it took to generate the chart configurations for a given query
     getNlResult: number;
+    // time it took to generate a results given the chart configurations
     getTileResults: number;
+    // total time it took to get from query to the results
     total: number;
   };
   // debug info from detect-and-fulfill endpoint
