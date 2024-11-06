ede170191d5b4ad0c3e1fdf970a9a6042762c1bc
Nick B <nicholas.blumberg@gmail.com>
2024-11-05 22:25:21 -0800

    Updated the brick wall component to accurately report the number of rows to the CSS (via a class).

--- static/js/components/content/brick_wall.tsx
+++ static/js/components/content/brick_wall.tsx
@@ -1,54 +1,65 @@
-/**
- * Copyright 2024 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import React, { ReactElement, useMemo } from "react";
 
-/**
- * A component to display a wall of arbitrary elements arranged like bricks.
- * An example of this is on the explorer page, where query links are arranged
- * in this way.
- */
-
 interface BrickWallProps {
-  //if given the title will be displayed as an introduction to the wall
+  // Optional title for the wall
   title?: string;
-  //an array of Reacts elements to be arranged on the wall. If empty, the wall will not display.
+  // Array of React elements to be arranged on the wall
   bricks: ReactElement[];
 }
 
+// the number of columns that the brick wall is divided into
 const NUM_COLUMNS = 2;
+// the pattern of each column, where [2, 1, 1] means two bricks in the first row, then one brick then one brick
+// the pattern then repeats
+const patterns = [
+  [2, 1, 1],
+  [1, 2, 1],
+];
 
 export const BrickWall = ({ title, bricks }: BrickWallProps): ReactElement => {
-  const brickColumns = useMemo(() => {
-    const columnCount = Math.ceil(bricks.length / NUM_COLUMNS);
-
-    return Array.from({ length: NUM_COLUMNS }, (_, column) =>
-      bricks.slice(column * columnCount, column * columnCount + columnCount)
+  // This function divides the bricks into columns (based on the NUM_COLUMNS above), and tracks the number of rows in each column.
+  // The number of rows isn't simple to calculate because it varies based on the pattern of the bricks.
+  const { columns, numRows } = useMemo(() => {
+    const columns: ReactElement[][] = Array.from(
+      { length: NUM_COLUMNS },
+      () => []
     );
+    const numRows: number[] = Array(NUM_COLUMNS).fill(0);
+    const patternIndices = Array(NUM_COLUMNS).fill(0);
+
+    let currentColumn = 0;
+
+    for (let i = 0; i < bricks.length; ) {
+      const columnPattern = patterns[currentColumn % patterns.length];
+      const currentPattern = columnPattern[patternIndices[currentColumn]];
+
+      for (let j = 0; j < currentPattern && i < bricks.length; j++, i++) {
+        columns[currentColumn].push(bricks[i]);
+      }
+
+      numRows[currentColumn]++;
+      patternIndices[currentColumn] =
+        (patternIndices[currentColumn] + 1) % columnPattern.length;
+
+      currentColumn = (currentColumn + 1) % NUM_COLUMNS;
+    }
+
+    return { columns, numRows };
   }, [bricks]);
 
   if (bricks.length === 0) {
     return null;
   }
+
   return (
     <div className="container">
       <div className="brick-wall-block">
-        <h3>{title}</h3>
-        {brickColumns.map((column, columnIndex) => (
-          <div className={`brick-wall-section row-count-${column.length - 1}`} key={columnIndex}>
+        {title && <h3>{title}</h3>}
+        {columns.map((column, columnIndex) => (
+          <div
+            className={`brick-wall-section row-count-${numRows[columnIndex]}`}
+            key={columnIndex}
+          >
             {column.map((brick, i) => (
               <div className="brick-wall-item" key={i}>
                 {brick}
