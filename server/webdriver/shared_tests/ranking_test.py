c6619486441f16ab2d35170e55e3c15ea4af8f00
Nick B <nicholas.blumberg@gmail.com>
2024-11-13 19:00:30 -0800

    Python linting

--- server/webdriver/shared_tests/ranking_test.py
+++ server/webdriver/shared_tests/ranking_test.py
@@ -61,7 +61,8 @@ class RankingTestMixin():
     )
 
     title_present = EC.text_to_be_present_in_element(
-        (By.CSS_SELECTOR, '#main-navbar-container .navbar-brand'), self.dc_title_string)
+        (By.CSS_SELECTOR, '#main-navbar-container .navbar-brand'),
+        self.dc_title_string)
     WebDriverWait(self.driver, self.TIMEOUT_SEC).until(title_present)
 
     subtitle_present = EC.text_to_be_present_in_element(
@@ -108,7 +109,8 @@ class RankingTestMixin():
     )
 
     title_present = EC.text_to_be_present_in_element(
-        (By.CSS_SELECTOR, '#main-navbar-container .navbar-brand'), self.dc_title_string)
+        (By.CSS_SELECTOR, '#main-navbar-container .navbar-brand'),
+        self.dc_title_string)
     WebDriverWait(self.driver, self.TIMEOUT_SEC).until(title_present)
 
     subtitle_present = EC.text_to_be_present_in_element(
