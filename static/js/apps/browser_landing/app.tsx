5282a675da0a04f957ef768c15b581cc582795ea
Nick B <nicholas.blumberg@gmail.com>
2024-12-09 20:20:22 -0800

    Updated source content of browser language page, as well as a fix to the layout to reflect the section refactoring.

--- static/js/apps/browser_landing/app.tsx
+++ static/js/apps/browser_landing/app.tsx
@@ -18,10 +18,13 @@
  * Main component for the browser landing (knowledge graph) page
  */
 
+import { ThemeProvider } from "@emotion/react";
 import React, { ReactElement } from "react";
 
 import { IntroText } from "../../components/content/intro_text";
+import { Section } from "../../components/elements/layout/section";
 import { KnowledgeGraphCategory } from "../../shared/types/knowledge_graph";
+import theme from "../../theme/theme";
 import { KnowledgeGraphBrowser } from "./components/knowledge_graph_browser";
 import knowledgeGraphData from "./knowledge_graph.json";
 
@@ -32,19 +35,21 @@ const knowledgeGraph: KnowledgeGraphCategory[] = knowledgeGraphData;
  */
 export function App(): ReactElement {
   return (
-    <>
-      <IntroText>
-        <header>
-          <h1 id="kg-title">Knowledge Graph</h1>
-          <p id="kg-desc">
-            The Data Commons Knowledge Graph is constructed by synthesizing a
-            single database from many different data sources. This knowledge
-            graph can be used both to explore what data is available and to
-            understand the graph structure.
-          </p>
-        </header>
-      </IntroText>
+    <ThemeProvider theme={theme}>
+      <Section>
+        <IntroText>
+          <header>
+            <h1 id="kg-title">Knowledge Graph</h1>
+            <p id="kg-desc">
+              The Data Commons Knowledge Graph is constructed by synthesizing a
+              single database from many different data sources. This knowledge
+              graph can be used both to explore what data is available and to
+              understand the graph structure.
+            </p>
+          </header>
+        </IntroText>
+      </Section>
       <KnowledgeGraphBrowser knowledgeGraph={knowledgeGraph} />
-    </>
+    </ThemeProvider>
   );
 }
