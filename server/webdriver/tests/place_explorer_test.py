5f65ee2893afcff1fa46b36a090412162d547e6e
Gabriel Mechali <gabriel.mechali@gmail.com>
2024-12-13 15:57:53 -0500

    Fix indentation

--- server/webdriver/tests/place_explorer_test.py
+++ server/webdriver/tests/place_explorer_test.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import time
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -24,10 +26,11 @@ from server.webdriver.shared_tests.place_explorer_test import \
 class TestPlaceExplorer(PlaceExplorerTestMixin, BaseDcWebdriverTest):
   """Class to test place explorer page. Tests come from PlaceExplorerTestMixin."""
 
-def test_dev_place_overview_california(self):
+  def test_dev_place_overview_california(self):
     """Ensure experimental dev place page content loads"""
     self.driver.get(self.url_ + '/place/geoId/06?force_dev_places=true')
 
+    time.sleep(1000)
     # For the dev place page, the related places callout is under the
     # .related-places-callout div.
     related_places_callout_el_present = EC.presence_of_element_located(
@@ -49,3 +52,4 @@ def test_dev_place_overview_california(self):
     explore_in_link_el = WebDriverWait(
         self.driver, self.TIMEOUT_SEC).until(explore_in_link_present)
     self.assertTrue('Explore in' in explore_in_link_el.text)
+    time.sleep(100000)
