3902955604e99dc168a4c6897c1e4f4dc1f061d4
Gabriel Mechali <gabriel.mechali@gmail.com>
2025-01-13 22:45:21 -0500

    Lint fixes

--- server/webdriver/tests/place_explorer_test.py
+++ server/webdriver/tests/place_explorer_test.py
@@ -22,9 +22,6 @@ from server.webdriver.base_dc_webdriver import BaseDcWebdriverTest
 from server.webdriver.shared_tests.place_explorer_test import \
     PlaceExplorerTestMixin
 
-from server.webdriver import shared
-from server.webdriver import base_utils
-
 
 class TestPlaceExplorer(PlaceExplorerTestMixin, BaseDcWebdriverTest):
   """Class to test place explorer page. Tests come from PlaceExplorerTestMixin."""
@@ -44,20 +41,30 @@ class TestPlaceExplorer(PlaceExplorerTestMixin, BaseDcWebdriverTest):
                          expected_topics=expected_topics)
 
     # Assert the subheader contains the parent places.
-    place_subheader_callout_el = base_utils.wait_elem(self.driver, By.CLASS_NAME, 'subheader', self.TIMEOUT_SEC)
-    self.assertEqual(place_subheader_callout_el.text, 'State in United States of America, North America')
-    
+    place_subheader_callout_el = base_utils.wait_elem(self.driver,
+                                                      By.CLASS_NAME,
+                                                      'subheader',
+                                                      self.TIMEOUT_SEC)
+    self.assertEqual(place_subheader_callout_el.text,
+                     'State in United States of America, North America')
+
     # For the dev place page, the related places callout is under the
     # .related-places-callout div.
-    related_places_callout_el = base_utils.wait_elem(self.driver, By.CLASS_NAME, 'related-places-callout', self.TIMEOUT_SEC)
+    related_places_callout_el = base_utils.wait_elem(self.driver, By.CLASS_NAME,
+                                                     'related-places-callout',
+                                                     self.TIMEOUT_SEC)
     self.assertEqual(related_places_callout_el.text, 'Places in California')
 
     # Assert the "Download" link is present in charts
-    download_link_el = base_utils.wait_elem(self.driver, By.CLASS_NAME, 'download-outlink', self.TIMEOUT_SEC)
+    download_link_el = base_utils.wait_elem(self.driver, By.CLASS_NAME,
+                                            'download-outlink',
+                                            self.TIMEOUT_SEC)
     self.assertTrue('Download' in download_link_el.text)
 
     # Assert the "Explore in ... Tool" link is present in charts
-    explore_in_link_el = base_utils.wait_elem(self.driver, By.CLASS_NAME, 'explore-in-outlink', self.TIMEOUT_SEC)
+    explore_in_link_el = base_utils.wait_elem(self.driver, By.CLASS_NAME,
+                                              'explore-in-outlink',
+                                              self.TIMEOUT_SEC)
     self.assertTrue('Explore in' in explore_in_link_el.text)
 
   def test_explorer_redirect_place_explorer_populates_search_bar(self):
