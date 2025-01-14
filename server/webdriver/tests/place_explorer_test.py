fb21a787c0483b4550f4e81746aebd00a2760e59
Gabriel Mechali <gabriel.mechali@gmail.com>
2025-01-14 11:09:04 -0500

    Lint fix and merge conflict fix

--- server/webdriver/tests/place_explorer_test.py
+++ server/webdriver/tests/place_explorer_test.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 from server.webdriver import base_utils
 from server.webdriver import shared
