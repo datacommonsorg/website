5e8fa9970584490cc25c989291bac19ec570a751
Gabriel Mechali <gabriel.mechali@gmail.com>
2025-01-13 23:44:57 -0500

    Clean up PR

--- server/tests/routes/api/mock_data.py
+++ server/tests/routes/api/mock_data.py
@@ -494,8 +494,6 @@ SAMPLE_PLACE_PAGE_CHART_CONFIG = [{
         "CHART_TITLE-Educational_attainment",
     "title":
         "Education attainment",
-    "description":
-        "Total number of criminal incidents",
     "denominator": [
         "Sample_Denominator_1", "Sample_Denominator_2", "Sample_Denominator_3",
         "Sample_Denominator_4", "Sample_Denominator_5"
