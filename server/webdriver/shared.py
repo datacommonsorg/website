52b34223de6636e6145477e9926a7bdea3b91ab5
Gabriel Mechali <gabriel.mechali@gmail.com>
2025-01-21 17:18:15 -0500

    Remove unused import

--- server/webdriver/shared.py
+++ server/webdriver/shared.py
@@ -21,7 +21,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from server.webdriver.base_utils import find_elem
 from server.webdriver.base_utils import find_elems
 
 LOADING_WAIT_TIME_SEC = 3
