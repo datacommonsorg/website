6d7b08f083010b301f3af4f96dc1039fe4959145
Gabriel Mechali <gmechali@google.com>
2025-03-10 15:08:00 -0400

    Add comment to make it clear this should only be used in tests

--- server/tests/utils.py
+++ server/tests/utils.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Helpers for tests"""
+"""Helpers for tests - DO NOT USE IN PROD"""
 
 from typing import List
 
