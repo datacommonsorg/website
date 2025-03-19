11a6272e2d3cce361414ef4ff99d4e9ea86cf205
Christie Ellks <calinc@google.com>
2025-03-19 15:37:51 -0700

    Disable biomed_nl in autopush

--- server/routes/experiments/biomed_nl/api.py
+++ server/routes/experiments/biomed_nl/api.py
@@ -27,6 +27,8 @@ from pydantic import BaseModel
 from server.routes.experiments.biomed_nl.traversal import PathFinder
 import server.routes.experiments.biomed_nl.utils as utils
 
+logging.INFO("[biomed_nl] api.py file imported")
+
 GEMINI_PRO = 'gemini-1.5-pro'
 GEMINI_PRO_TOKEN_LIMIT = 2097152
 
