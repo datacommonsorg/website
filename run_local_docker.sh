4cd9abb6a5aef485960236010dc6cfa54c628fa7
Kara Moscoe <kmoscoe@google.com>
2025-03-27 17:27:34 -0700

    rename "mode"

--- run_local_docker.sh
+++ run_local_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2024 Google LLC
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,28 +19,29 @@ help() {
 cat << EOF
 Usage:
 
-./run_local_docker.sh [--env_file|-e <env.list file path>] [--mode|m run ] 
-  [container|-c all|service] [--release|-r latest|stable] [--image|-i <custom image name:tag>] [--schema-update|s]
+./run_local_docker.sh [--env_file|-e <env.list file path>] [--actions|-a run|build_run|build|build_upload|upload] 
+  [container|-c all|service] [--release|-r latest|stable] [--image|-i <custom image name:tag>] [--package|-p <package name:tag>] [--schema-update|s]
 
-./run_local_docker.sh [--env_file|-e <env.list file path>] --mode|m build_run
-  [container|-c all|service] [--release|-r latest|stable] --image|-i <custom image name:tag> [--schema-update|s]
+If no options are set, the default is '--env_file $PWD/custom_dc/env.list --actions run --container all --release stable': all containers are run, using the Data Commons-provided 'stable' image.
 
-./run_local_docker.sh [--env_file|-e <env.list file path>] --mode|m build --image <custom image name:tag>
+Some of these options are mutually exclusive and some are required depending on the setting of the '--actions' option. Here is a high-level summary of valid combinations:
 
-./run_local_docker.sh [--env_file|-e <env.list file path>] --mode|m build_upload|upload 
-  --image <custom image name:tag> [--package|-p <package name:tag>]
+-a build_run [-c all|service] [-r latest|stable] -i <custom image name:tag>
 
-If no options are set, the default is '--env_file $PWD/custom_dc/env.list --mode run --container all --release stable'
+-a build -i <custom image name:tag>
+
+-a build_upload|upload -i <custom image name:tag> [-p <package name:tag>]
+
+-action run|build_run [-c all] [-r latest|stable] [-i <custom image name:tag>] -s
 
 Options:
 
 --env_file|-e <path to env.list file> 
   Optional: The path and file name of the environment file env.list. 
   Default: custom_dc/env.list
-  Use this option to maintain multiple alternate environment files with
-  different settings and directories (helpful for testing).
+  Use this option to maintain multiple alternate environment files with different settings and directories (helpful for testing).
       
---mode|m
+--actions|-a
   Optional: The different Docker commands to run. 
   Default: run: Runs containers, using a prebuilt release or a custom build.
   Other options are:
@@ -52,34 +53,34 @@ Options:
 
 --container|-c all|service
   Optional: The containers to run.
-  Default with 'run' and 'build_run' modes: all: Run all containers. Other options are:
+  Default with 'run' and 'build_run': all: Run all containers. Other options are:
   * service: Only run the service container. You can use this if you have 
-    not made any changes to your data.
-  Only valid with 'run' and 'build_run' modes. Ignored otherwise.
+    not made any changes to your data. Exclusive with '--schema-update'.
+  Only valid with 'run' and 'build_run'. Ignored otherwise.
 
---release|r stable|latest
-  Optional in 'run' and 'build_run' mode.
+--release|-r stable|latest
+  Optional with 'run' and 'build_run'.
   Default: stable: run the prebuilt 'stable' image provided by Data Commons team.
   Other options:
   * latest: Run the 'latest' release provided by Data Commons team. 
   If you specify this with an additional '--image' option, the option applies 
   only to the data container. Otherise, it applies to both containers. 
-  Only valid with 'run' and 'build_run' modes. Ignored otherwise.
+  Only valid with 'run' and 'build_run'. Ignored otherwise.
 
---image <custom image name:tag>
-  Optional in 'run' mode: the name and tag of the custom image to run in the service container.
-  Required in all other modes: the name and tag of the custom image to build/run/upload.
+--image|-i <custom image name:tag>
+  Optional with 'run': the name and tag of the custom image to run in the service container.
+  Required for all other actions: the name and tag of the custom image to build/run/upload.
   
---package <target package name:tag>
+--package|-p <target package name:tag>
   Optional: The target image to be created and uploaded to Google Cloud.
   Default: same as the name and tag provided in the '--image' option.
-  Only valid with 'build_upload' and "upload" modes. Ignored otherwise.
+  Only valid with 'build_upload' and 'upload'. Ignored otherwise.
      
 --schema_update|-s
   Optional. In the rare case that you get a 'SQL checked failed' error in
   your running service, you can set this to run the data container in 
   schema update mode, which skips embeddings generation and completes much faster.
-  Only valid with 'run' and 'build_run' modes and 'all' containers. Ignored otherwise.
+  Only valid with 'run' and 'build_run' actions and 'all' containers. Ignored otherwise.
 
 Examples:
 
@@ -94,15 +95,15 @@ Examples:
 ./run_local_docker.sh --image my-datacommons:dev
   Start all containers, using a custom-built image for the service container.
 
-./run_local_docker.sh --mode build_run --image my-datacommons:dev --container service
-  Build a custom image and only start the service. Use this if you haven't made any 
-  changes to your data but are developing your custom site.
-
-./run_local_docker.sh --mode build --image my-datacommons:dev
+./run_local_docker.sh --actions build --image my-datacommons:dev
   Build a custom image only, and don't start any containers. Use this if you are 
   building a custom image that you will upload and test later in Google Cloud.
 
-./run_local_docker.sh --mode build_upload --image my-datacommons:dev
+./run_local_docker.sh --actions build_run --image my-datacommons:dev --container service
+  Build a custom image and only start the service. Use this if you haven't made any 
+  changes to your data but are developing your custom site.
+
+./run_local_docker.sh --actions build_upload --image my-datacommons:dev
   Build a custom image, create a package with the same name and tag, and upload
   it to the Cloud Artifact Registry. Does not start any local containers.
 
@@ -128,10 +129,9 @@ build() {
   echo -e "${GREEN}Starting Docker build of '$IMAGE'. This will take several minutes..."
   docker build --tag $IMAGE \
   -f build/cdc_services/Dockerfile .
-  return 0
 }
 
-# Upload custom image to GCP
+# Package and push custom image to GCP
 upload() {
   check_app_credentials
   # Need Docker credentials for running docker tag to create an Artifact Registry package
@@ -239,7 +239,7 @@ check_gcloud_credentials() {
 
 # Initialize variables for optional settings
 ENV_FILE="$PWD/custom_dc/env.list"
-MODE="run"
+ACTIONS="run"
 CONTAINER="all"
 RELEASE="stable"
 SCHEMA_UPDATE=false
@@ -247,7 +247,7 @@ IMAGE=""
 PACKAGE=""
 
 # Parse command-line options
-OPTS=$(getopt -o e:m:c:r:i:sp:h --long env_file:,mode:,container:,release:,image:,schema_update,package:,help -n 'run_local_docker.sh' -- "$@")
+OPTS=$(getopt -o e:a:c:r:i:sp:h --long env_file:,actions:,container:,release:,image:,schema_update,package:,help -n 'run_local_docker.sh' -- "$@")
 
 if [ $? != 0 ]; then
   echo "Failed to parse options." >&2
@@ -269,12 +269,12 @@ while true; do
       fi
       shift 2
       ;;
-    -m | --mode)
+    -a | --actions)
       if [ "$2" == "run" ] || [ "$2" == "build" ] || [ "$2" == "build_run" ] || [ "$2" == "build_upload" ] || [ "$2" == "upload" ]; then
-        MODE="$2"
+        ACTIONS="$2"
         shift 2  
       else
-        echo -e "${RED}Error:${NC} That is not a valid mode. Valid options are:\nrun\nbuild\nbuild_run\nbuild_upload\nupload\n" >&2
+        echo -e "${RED}Error:${NC} That is not a valid action. Valid options are:\nrun\nbuild\nbuild_run\nbuild_upload\nupload\n" >&2
         exit 1
       fi
       ;;
@@ -283,7 +283,7 @@ while true; do
         CONTAINER="$2"
         shift 2
       else
-        echo -e "${RED}Error:${NC} That is not a valid container option. Valid options are:\nall\nservice'\n" >&2
+        echo -e "${RED}Error:${NC} That is not a valid container option. Valid options are:\nall\nservice\n" >&2
         exit 1
       fi
       ;;
@@ -298,7 +298,7 @@ while true; do
       ;;
     -i | --image)
       if [ "$2" == "latest" ] || [ "$2" == "stable" ]; then
-        echo -e "${RED}Error:${NC} That is not an allowed custom image name. Did you mean to use the '--release' option?\n" >&2
+        echo -e "${RED}Error:${NC} That is not a valid custom image name. Did you mean to use the '--release' option?\n" >&2
         exit 1
       else
        IMAGE="$2"
@@ -335,23 +335,23 @@ fi
 source "$ENV_FILE"
 
 # Handle various error conditions
-#############################################
+#######################################################
 
 # Missing variables needed to construct Docker commands
 #============================================================
-
-# Missing options in input
+# Missing variables from input
 #-------------------------------------------------------------
 # Missing required custom image for build and upload
 if [ "$MODE" != "run" ] && [ -z "$IMAGE" ]; then
-    echo -e "${RED}Error:${NC} Missing an image name and tag for build and/or upload.\nPlease use the -'-image' or '-i' option with the name and tag of the custom image you are building or have already built.\n" >&2
-    exit 1
+  echo -e "${RED}Error:${NC} Missing an image name and tag for build and/or upload.\nPlease use the -'-image' or '-i' option with the name and tag of the custom image you are building or have already built.\n" >&2
+  exit 1
 fi
-# Missing package for upload; assign image name
+# Missing package for upload; not an error, just info
 if [[ "$MODE" == *"upload"* ]] && [ -z $PACKAGE ]; then
   echo -e "${YELLOW}Info:${NC} No '--package' option specified."
   echo -e "The target image will use the same name and tag as the source image '$IMAGE'.\n"
   sleep 3
+  # Assign image name
   PACKAGE=$IMAGE
 fi
 
@@ -369,8 +369,8 @@ if  [[ ( "$MODE" == *"upload"* ) && ( -z "$GOOGLE_CLOUD_PROJECT" || -z "$GOOGLE_
   exit 1
 fi
 
-# Handle invalid option combinations and reset to valid (others are silently 
-# ignored and handled in the case statement)
+# Handle invalid option combinations and reset to valid. Most are silently 
+# ignored and handled in the case statement.
 #--------------------------------------------------------------------
 if [ "$SCHEMA_UPDATE" == true ] && [ "$CONTAINER" == "service" ]; then
   CONTAINER="all"
@@ -378,7 +378,7 @@ fi
 
 # Call Docker commands
 ######################################
-case "$MODE" in
+case "$ACTIONS" in
   "build")
     build
     ;;
