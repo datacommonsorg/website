697f74e41afa27b4baf37fb5620816890af95747
Nick B <nicholas.blumberg@gmail.com>
2025-03-30 10:31:12 -0700

    This commit adds three new components:
    - The PlaceDataOverviewPage. This is an organizational component that displays a particular place's data source information.
    
    - PlaceDataOverviewHeader displays the top level header and links for the overview page
    
    - StatVarPlaceTopics is a component that displays a series of links for the particular place in question, adding in the generic link after.

---
+++ static/js/apps/data/places/components/place_data_overview_header.tsx
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * The header for a place overview page.
+ */
+
+/** @jsxImportSource @emotion/react */
+
+import { css, useTheme } from "@emotion/react";
+import React, { ReactElement } from "react";
+
+import { Section } from "../../../../components/elements/layout/section";
+import { Place } from "../place_data";
+
+interface PlaceDataOverviewHeaderProps {
+  //the place data object; an object comprising the place, data sources and example stat vars
+  place: Place;
+}
+
+export const PlaceDataOverviewHeader = ({
+  place,
+}: PlaceDataOverviewHeaderProps): ReactElement => {
+  const theme = useTheme();
+
+  return (
+    <>
+      <Section
+        variant="small"
+        innerSx={css`
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        `}
+      >
+        <h3
+          css={css`
+            ${theme.typography.family.heading};
+            ${theme.typography.heading.sm};
+            margin-bottom: ${theme.spacing.xl}px;
+            margin-left: ${theme.spacing.md}px;
+          `}
+        >
+          <a href={`/place/${place.dcid}`}>{place.name}</a> · Data Overview
+        </h3>
+        <div>
+          {place.dcid} ·{" "}
+          <a href={`/browser/${place.dcid}`}>See Knowledge Graph</a>
+        </div>
+      </Section>
+    </>
+  );
+};
---
+++ static/js/apps/data/places/components/place_data_overview_page.tsx
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * The place-based data overview page.
+ */
+
+/** @jsxImportSource @emotion/react */
+
+import React, { ReactElement } from "react";
+
+import { Section } from "../../../../components/elements/layout/section";
+import { PlaceData } from "../place_data";
+import { PlaceDataOverviewHeader } from "./place_data_overview_header";
+import { StatVarPlaceTopics } from "./stat_var_place_topics";
+
+interface PlaceDataOverviewPageProps {
+  //the place data object; an object comprising the place, data sources and example stat vars
+  placeData: PlaceData;
+}
+
+export const PlaceDataOverviewPage = ({
+  placeData,
+}: PlaceDataOverviewPageProps): ReactElement => {
+  return (
+    <>
+      <PlaceDataOverviewHeader place={placeData.place} />
+      <Section>
+        {Object.entries(placeData.topics).map(([topicKey, topic]) => (
+          <StatVarPlaceTopics
+            key={topicKey}
+            place={placeData.place}
+            topic={topic.title}
+            queries={topic.examples.statvar}
+          />
+        ))}
+      </Section>
+    </>
+  );
+};
