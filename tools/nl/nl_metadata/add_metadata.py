53616b8408f52c945f907a15b776d24766e7fe39
Alyssa Guo <alyssaguo@google.com>
2025-05-21 20:44:28 +0000

    Fix lint errors

--- tools/nl/nl_metadata/add_metadata.py
+++ tools/nl/nl_metadata/add_metadata.py
@@ -1,71 +1,72 @@
 """Retrieve metadata and add to the existing embeddings."""
+from datacommons_client.client import DataCommonsClient
 import pandas as pd
 
-from datacommons_client.client import DataCommonsClient
 
 def main():
-    client = DataCommonsClient()
-    embeddings = pd.read_csv("tools/nl/embeddings/input/base/sheets_svs.csv")
-    
-    # Extract common metadata properties from all StatVars 
-    properties_to_extract = [
-        "measuredProperty",
-        "name",
-        "populationType",
-        "statType"
-    ]
-    new_embeddings = []
-    batch_size = 100
+  client = DataCommonsClient()
+  embeddings = pd.read_csv("tools/nl/embeddings/input/base/sheets_svs.csv")
 
-    for i in range(0, len(embeddings), batch_size):
-        curr_batch = embeddings.iloc[i:i + batch_size]
-        curr_dcids = curr_batch['dcid'].tolist()
+  # Extract common metadata properties from all StatVars
+  properties_to_extract = [
+      "measuredProperty", "name", "populationType", "statType"
+  ]
+  new_embeddings = []
+  batch_size = 100
 
-        response = client.node.fetch(node_dcids=curr_dcids, expression="->*")
-        response_data = response.to_dict().get("data", {})
+  for i in range(0, len(embeddings), batch_size):
+    curr_batch = embeddings.iloc[i:i + batch_size]
+    curr_dcids = curr_batch['dcid'].tolist()
 
-        for dcid, sentence in zip(*curr_batch.to_dict("list").values()):
-            new_row = {
-                "dcid": dcid, 
-                "sentence": sentence,
-                "constraintProperties": '' # Initialize constraintProperties here since it cannot be natively extracted
-            }
-            dcid_data = response_data[dcid]["arcs"]
+    response = client.node.fetch(node_dcids=curr_dcids, expression="->*")
+    response_data = response.to_dict().get("data", {})
 
-            for property in properties_to_extract:
-                new_row[property] = ''
-                # Property values are encoded as an array of nodes - take the first node as the value 
-                first_node = dcid_data[property]["nodes"][0]
+    for dcid, sentence in zip(*curr_batch.to_dict("list").values()):
+      new_row = {
+          "dcid": dcid,
+          "sentence": sentence,
+          "constraintProperties":
+              ''  # Initialize constraintProperties here since it cannot be natively extracted
+      }
+      dcid_data = response_data[dcid]["arcs"]
 
-                if property == "name": # For the "name" property, the node field is "value"
-                    new_row[property] = first_node.get("value")
-                else: # For other properties, the node field is "name"
-                    new_row[property] = first_node.get("name")
+      for property in properties_to_extract:
+        new_row[property] = ''
+        # Property values are encoded as an array of nodes - take the first node as the value
+        first_node = dcid_data[property]["nodes"][0]
 
-            if "constraintProperties" in dcid_data:
-                curr_constraint_properties = []
-                
-                for node in dcid_data["constraintProperties"]["nodes"]:
-                    # Use the "name" field if it exists, otherwise fall back on "dcid"
-                    # Some SVs have both "name" and "dcid", others only have "dcid"
-                    if "name" in node:
-                        constrained_prop = node["name"]
-                    else:
-                        constrained_prop = node["dcid"]
+        if property == "name":  # For the "name" property, the node field is "value"
+          new_row[property] = first_node.get("value")
+        else:  # For other properties, the node field is "name"
+          new_row[property] = first_node.get("name")
 
-                    first_node = dcid_data[constrained_prop]["nodes"][0]
-                    if "name" in first_node:
-                        curr_constraint_properties.append(f'{constrained_prop}={first_node.get("name")}')
-                    else:
-                        curr_constraint_properties.append(f'{constrained_prop}={first_node.get("dcid")}')
+      if "constraintProperties" in dcid_data:
+        curr_constraint_properties = []
 
-                new_row["constraintProperties"] = ";".join(curr_constraint_properties)
+        for node in dcid_data["constraintProperties"]["nodes"]:
+          # Use the "name" field if it exists, otherwise fall back on "dcid"
+          # Some constraintProperties nodes have both "name" and "dcid", others only have "dcid"
+          if "name" in node:
+            constrained_prop = node["name"]
+          else:
+            constrained_prop = node["dcid"]
 
-            new_embeddings.append(new_row)
+          first_node = dcid_data[constrained_prop]["nodes"][0]
+          if "name" in first_node:
+            curr_constraint_properties.append(
+                f'{constrained_prop}={first_node.get("name")}')
+          else:
+            curr_constraint_properties.append(
+                f'{constrained_prop}={first_node.get("dcid")}')
 
-    new_embeddings_df = pd.DataFrame(new_embeddings)
-    new_embeddings_df.to_csv("tools/nl/nl_metadata/alyssaguo_statvars.csv", index=False)
+        new_row["constraintProperties"] = ";".join(curr_constraint_properties)
+
+      new_embeddings.append(new_row)
+
+  new_embeddings_df = pd.DataFrame(new_embeddings)
+  new_embeddings_df.to_csv("tools/nl/nl_metadata/alyssaguo_statvars.csv",
+                           index=False)
 
 
 if __name__ == "__main__":
-    main()
+  main()
