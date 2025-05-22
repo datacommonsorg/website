1201d171f037e4bb9665afc50a4dea683050d44a
Gabriel Mechali <gmechali@google.com>
2025-05-22 10:40:42 -0400

    More work to getting the description in

--- static/js/apps/explore/highlight_result.tsx
+++ static/js/apps/explore/highlight_result.tsx
@@ -1,20 +1,17 @@
-import React, { Fragment, ReactNode } from "react";
+import React from "react";
 
 import { SubjectPageMainPane } from "../../components/subject_page/main_pane";
-import { StatMetadata } from "../../shared/stat_types";
 import { NamedNode, StatVarFacetMap } from "../../shared/types";
-import { stripProtocol } from "../../shared/util";
 import {
   buildCitationParts,
   CitationPart,
 } from "../../tools/shared/metadata/citations";
 import { StatVarMetadata } from "../../tools/shared/metadata/metadata";
 import { fetchMetadata } from "../../tools/shared/metadata/metadata_fetcher";
-import { TileMetadataModalContent } from "../../tools/shared/metadata/tile_metadata_modal_content";
 import { FacetMetadata } from "../../types/facet_metadata";
 import { SubjectPageMetadata } from "../../types/subject_page_types";
 import { getDataCommonsClient } from "../../utils/data_commons_client";
-import { getFacets } from "../../utils/data_fetch_utils";
+import { FacetResponse, getFacets } from "../../utils/data_fetch_utils";
 import { trimCategory } from "../../utils/subject_page_utils";
 
 /**
@@ -47,33 +44,31 @@ async function doMetadataFetch(props: HighlightResultProps): Promise<{
   }
 
   const dataCommonsClient = getDataCommonsClient(props.apiRoot);
-  const statMetadataRecord: { [key: string]: StatMetadata } = {
-    "1": {
-      provenanceUrl: props.highlightFacet?.importName || "",
-      measurementMethod: props.highlightFacet?.measurementMethod || "",
-      observationPeriod: props.highlightFacet?.observationPeriod || "",
-      scalingFactor: "",
-      unit: props.highlightFacet?.unit || "",
-    },
-  };
 
   const statVarArray = Array.from(statVarSet);
-  const facets = await getFacets(
+  const facets: FacetResponse = await getFacets(
     props.apiRoot,
     [props.highlightPageMetadata.place.dcid],
     statVarArray
   );
-  console.log("facets", facets);
 
-  const facetIds = new Set<string>();
-  facetIds.add("10983471");
-  const statVarFacetMap: StatVarFacetMap = {
-    Count_Person: facetIds,
-  };
+  console.log("facets", JSON.stringify(facets));
+  // console.log("HighlightFacet", JSON.stringify(props.highlightFacet));
+  const statVarFacetMap: StatVarFacetMap = {};
+  for (const statVar in facets) {
+    // if props.highlightFacet is not null, match the right facet then add it to statVarFacetMap
+    // for (const facet of facets[statVar]) {
+    const facet = facets[statVar];
+    console.log("Facet is ", facet);
+    for (const facetId in facet) {
+      if (facet[facetId].importName === props.highlightFacet?.importName) {
+        console.log(`Found matching facet for ${statVar}: ${facet.importName}`);
+        statVarFacetMap[statVar] = new Set([facetId]);
+        break;
+      }
+    }
+  }
 
-  // const svSet = new Set<string>();
-  // svSet.add(svthing);
-  console.log("statVarSet", statVarSet);
   return fetchMetadata(
     statVarSet,
     facets["Count_Person"],
@@ -96,79 +91,44 @@ export function HighlightResult(
 
   React.useEffect(() => {
     const fetchData = async (): Promise<void> => {
-      const { metadata, statVarList } = await doMetadataFetch(props);
-      setMetadataMap(metadata);
-      setStatVarList(statVarList);
-      console.log("metadataMap", metadata);
-      console.log("statVarList", statVarList);
+      doMetadataFetch(props).then(({ metadata, statVarList }) => {
+        setMetadataMap(metadata);
+        setStatVarList(statVarList);
+      });
     };
     fetchData();
+
+    setPageConfig(props.highlightPageMetadata.pageConfig);
   }, [props]);
-
-  const optionDan = true;
-  const optionAdriana = false;
-
-  const joinElements = (
-    items: ReactNode[],
-    separator: ReactNode = ", "
-  ): ReactNode[] =>
-    items.flatMap((item, idx) => (idx === 0 ? [item] : [separator, item]));
 
   const generateCitationSources = (citationParts: CitationPart[]): string[] => {
     return citationParts.map(({ label }) => label);
   };
 
-  // const citationSources = generateCitationSources();
-
   React.useEffect(() => {
-    setPageConfig(props.highlightPageMetadata.pageConfig);
-  }, []);
-
-  React.useEffect(() => {
-    if (pageConfig && optionDan) {
-      // const met =
-      //   metadataMap && metadataMap["Count_Person"]
-      //     ? metadataMap["Count_Person"][0]
-      //     : null;
-      // const citationParts = buildCitationParts(statVarList, metadataMap);
-
-      console.log(
-        "citationParts",
-        joinElements(
-          generateCitationSources(
-            buildCitationParts(statVarList, metadataMap, true)
-          )
-        )
-      );
-      // const desc = met?.sourceName + ", with minor processing by Data Commons";
-      pageConfig.categories[0].blocks[0].description = generateCitationSources(
-        buildCitationParts(statVarList, metadataMap, true)
-      ).join(", ");
-      // "United States Census Bureau, American Community Survey (ACS) (data.census.gov/cedsci/table?q=S2601A&tid=ACSST5Y2019.S2601A), with minor processing by Data Commons.";
-      setPageConfig(JSON.parse(JSON.stringify(pageConfig)));
+    if (
+      !pageConfig ||
+      pageConfig.categories.length === 0 ||
+      pageConfig.categories[0].blocks.length === 0
+    ) {
+      return;
     }
-  }, [metadataMap]);
+
+    const pageConfigCopy = structuredClone(
+      props.highlightPageMetadata.pageConfig
+    );
+
+    const highlightChartDescription = generateCitationSources(
+      buildCitationParts(statVarList, metadataMap, true)
+    ).join(", ");
+
+    pageConfigCopy.categories[0].blocks[0].description =
+      highlightChartDescription;
+    setPageConfig(pageConfigCopy);
+  }, [statVarList, metadataMap]);
 
   return (
     <div>
-      {optionAdriana && (
-        <div>
-          <br></br>
-          <TileMetadataModalContent
-            statVars={statVarList}
-            metadataMap={metadataMap}
-            apiRoot={props.apiRoot}
-          />
-        </div>
-      )}
-      <br></br>
-      <br></br>
-      {/* {optionDan &&
-        joinElements(
-          generateCitationSources(
-            buildCitationParts(statVarList, metadataMap, true)
-          )
-        )} */}
       <SubjectPageMainPane
         id={PAGE_ID}
         place={props.highlightPageMetadata.place}
