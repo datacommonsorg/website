9ff01c30219169137bb8bffd663d26a85e634780
Nick B <nicholas.blumberg@gmail.com>
2025-05-25 17:14:06 -0700

    In this push, we update the highlight metadata process to effect the following changes:
    - We wire a loading state through to where the metadata summary (the citation list) is displayed.
    - We now no longer override the description (in order to preserve any description that comes with the block itself), but assign it to a new parameter.
    - We display a loading state as a spinner until loading is complete (preventing a page bump as the component loads and renders).
    - Generally through the components touched by this push, we improve the typing.

---
+++ static/js/components/subject_page/metadata_summary.tsx
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @jsxImportSource @emotion/react */
+
+import { css, useTheme } from "@emotion/react";
+import React, { ReactElement } from "react";
+
+import { Loading } from "../elements/loading";
+
+export interface MetadataSummaryProps {
+  // Metadata loading state: true (loading), false (loaded), undefined (no metadata)
+  metadataLoadingState?: boolean;
+  // The metadata summary (based on the citation)
+  metadataSummary?: string;
+}
+
+/**
+ * Renders a small metadata/citation section under the block description.
+ */
+export function MetadataSummary({
+  metadataLoadingState,
+  metadataSummary,
+}: MetadataSummaryProps): ReactElement | null {
+  const theme = useTheme();
+
+  const shouldShow =
+    metadataLoadingState === true ||
+    (metadataLoadingState === false && !!metadataSummary);
+
+  if (!shouldShow) {
+    return null;
+  }
+
+  return (
+    <div
+      css={css`
+        ${theme.typography.text.sm};
+      `}
+    >
+      {metadataLoadingState === true && <Loading />}
+      {metadataLoadingState === false && metadataSummary}
+    </div>
+  );
+}
