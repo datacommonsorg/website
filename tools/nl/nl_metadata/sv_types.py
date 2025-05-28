54fe6811abbb36682c280938747030c69d7fe21e
Alyssa Guo <37948466+alyssasuiyiguo@users.noreply.github.com>
2025-05-28 10:25:24 -0400

    Create script to add metadata to SV embeddings (#5177)
    Defines a new python script that pulls the existing SV embeddings and
    adds the metadata (`measuredProperty`, `name`, `populationType`,
    `statType`, `constraintProperties`) to them. Results are outputted as a
    json "alyssa_statvars.json". Also fixes two small typos in
    `sheets_svs.csv` that were causing the wrong stat vars to be pulled.
    
    I documented the problems I noticed with `sheets_svs.csv` in the
    following
    [spreadsheet](https://docs.google.com/spreadsheets/d/1hvUFwsBM8GrCRsMAi7SUUEVBib4rz4ZRVl5QOFlqgvA/edit?usp=sharing)

---
+++ tools/nl/nl_metadata/sv_types.py
@@ -0,0 +1,32 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from dataclasses import dataclass
+from dataclasses import field
+
+
+@dataclass
+class StatVarMetadata:
+  """
+    A dataclass to structure and normalize metadata pulled for statistical variables from the Data Commons API.
+    Some static fields are common across all stat vars (i.e. dcid, sentence, measuredProperty, name, populationType, statType),
+    whereas constraintProperties are dynamic and can vary for each stat var.
+    """
+
+  dcid: str
+  sentence: str
+  measuredProperty: str | None = None
+  name: str | None = None
+  populationType: str | None = None
+  statType: str | None = None
+  constraintProperties: dict[str, str] = field(default_factory=dict)
