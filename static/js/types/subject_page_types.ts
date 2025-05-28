ea3f678f04f24b4328d9734222d93039c05b703c
Nick B <nicholas.blumberg@gmail.com>
2025-05-27 20:31:36 -0700

    Collapsed the imports from shared/types into a single import.

--- static/js/types/subject_page_types.ts
+++ static/js/types/subject_page_types.ts
@@ -13,8 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { NamedTypedNode } from "../shared/types";
-import { ChildPlacesByType } from "../shared/types";
+import { ChildPlacesByType, NamedTypedNode } from "../shared/types";
 import {
   BlockConfig,
   CategoryConfig,
