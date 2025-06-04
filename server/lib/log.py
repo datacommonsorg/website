d814b26be1a55bb8e695f22b92dcb8b1afad7d89
rohit kumar <rohitrkumar@google.com>
2025-06-04 10:25:48 +0000

    Add logging for Datacommons API request payloads

--- server/lib/log.py
+++ server/lib/log.py
@@ -20,9 +20,12 @@
 
 import logging
 import os
+import random
 import time
 import traceback
+import json
 
+from flask import current_app
 import requests
 
 # 3500 may seem v high,  but there are known paths (like ranking across
@@ -35,7 +38,7 @@ _MAX_DURATION_SECS = 2
 
 # Usage:
 #
-# call_logger = ExtremeCallLogger(request)
+# call_logger = ExtremeCallLogger(request, url=url_if_post) # Pass URL for POSTs
 #
 # ... make the call ....
 #
@@ -44,15 +47,33 @@ _MAX_DURATION_SECS = 2
 #
 class ExtremeCallLogger:
 
-  def __init__(self, request: dict = None):
+  def __init__(self, request: dict = None, url: str = None):
     self.request = request
+    self.url = url # Store URL for potential payload logging
     self.start = time.time()
+
+  def _try_log_payload(self, log_enabled: bool, log_percentage: int):
+    """
+    Logs request payload if enabled and conditions are met.    
+    Fails silently on serialization errors.
+    """
+    if log_enabled and self.url and isinstance(self.request, dict):
+        if random.random() * 100 < log_percentage:
+            try:
+                payload_str = json.dumps(self.request, sort_keys=True)
+                logging.info("POST Request Payload: URL=%s, Payload=%s", self.url, payload_str)
+            except TypeError:
+                pass  # Silently ignore serialization errors
 
   def finish(self, resp: requests.Response = None):
     if os.environ.get('FLASK_ENV') in ['production', 'custom']:
       return
 
-    # Error msgs
+    self._try_log_payload( 
+        log_enabled=current_app.config.get('LOG_DC_REQUEST_PAYLOAD'),
+        log_percentage=current_app.config.get('LOG_DC_REQUEST_PAYLOAD_PERCENTAGE', 0)
+    )
+   
     cases = []
 
     # Always log long calls.
@@ -60,10 +81,9 @@ class ExtremeCallLogger:
     if dur > _MAX_DURATION_SECS:
       cases.append(f'long-call: {dur}s')
 
-    # If request is set, then check for known schema things
     nvars = 0
     nents = 0
-    if self.request:
+    if self.request: 
       nents = len(self.request.get('nodes', []))
       if not nents:
         nents = len(self.request.get('entity', {}).get('dcids', []))
@@ -73,11 +93,11 @@ class ExtremeCallLogger:
       if nvars > _VAR_LIMIT:
         cases.append(f'big-req: {nvars} vars')
 
-    # If response is set, check byte limit
     if resp:
       if resp.status_code != 200:
+        content_snippet = str(resp.content[:200]) if resp.content else ""
         cases.append(
-            f'failed-call: {resp.reason} - {nents} entities, {nvars} vars - {resp.content}'
+            f'failed-call: {resp.reason} - {nents} entities, {nvars} vars - {content_snippet}'
         )
       else:
         if len(resp.text) > _BYTE_LIMIT:
