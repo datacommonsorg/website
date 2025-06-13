3a71ad6ee587a1d32635c31627b58d51b1e96289
Javier Vazquez <javiervazquez@google.com>
2025-06-13 19:06:08 +0000

    Address feedback such as fixing comments, relocating variables, and renaming functions

--- server/routes/explore/api.py
+++ server/routes/explore/api.py
@@ -14,6 +14,7 @@
 """Endpoints for Datacommons NL"""
 
 import copy
+import json
 import time
 from typing import Dict
 
@@ -21,6 +22,7 @@ import flask
 from flask import Blueprint
 from flask import current_app
 from flask import request
+from flask import Response
 
 from server.lib.nl.common import serialize
 import server.lib.nl.common.bad_words as bad_words
@@ -31,11 +33,11 @@ import server.lib.nl.common.utterance as nl_utterance
 import server.lib.nl.config_builder.base as config_builder
 import server.lib.nl.detection.detector as nl_detector
 from server.lib.nl.detection.utils import create_utterance
+from server.lib.nl.explore import related
 import server.lib.nl.explore.fulfiller_bridge as nl_fulfillment
 from server.lib.nl.explore.params import Clients
 from server.lib.nl.explore.params import DCNames
 from server.lib.nl.explore.params import Params
-import server.lib.nl.explore.related as llm
 from server.lib.util import get_nl_disaster_config
 from server.routes.explore import helpers
 import server.services.bigtable as bt
@@ -133,26 +135,26 @@ def detect_and_fulfill():
 
 
 #
-# The follow up question endpoint that generates the questions under "Keep Exploring"
+# The follow up question endpoint that generates questions
 # based off of the initial query and topics found in the related topics.
 #
-@bp.route('/follow-up', methods=['POST'])
+@bp.route('/follow-up-questions', methods=['POST'])
 def follow_up_questions():
-  debug_logs = {}
 
-  initial_query = request.args.get('q', '')
+  initial_query = request.get_json().get('q', '')
   related_topics = request.get_json().get('relatedTopics', [])
-  if not related_topics:
-    return {'follow_up_questions': [], 'debug': debug_logs}
+  if not related_topics or not initial_query:
+    return Response(json.dumps({'follow_up_questions': []}),
+                    200,
+                    mimetype="application/json")
 
-  #Calls Gemini to generate the follow up questions
-  generated_questions = llm.followup_with_geminipro(
+  generated_questions = related.generate_follow_up_questions(
       query=initial_query, related_topics=related_topics)
 
   if not generated_questions:
-    debug_logs[
-        'ERROR'] = 'Error while calling Gemini to generate the follow up questions.'
-    return {'follow_up_questions': [], 'debug': debug_logs}
+    return Response(json.dumps({'follow_up_questions': []}),
+                    500,
+                    mimetype="application/json")
 
   #Checks for adversarial questions
   safe_generated_questions = [
@@ -161,7 +163,9 @@ def follow_up_questions():
                            bad_words=current_app.config['NL_BAD_WORDS'])
   ]
 
-  return {'follow_up_questions': safe_generated_questions, 'debug': debug_logs}
+  return Response(json.dumps({'follow_up_questions': safe_generated_questions}),
+                  200,
+                  mimetype="application/json")
 
 
 #
