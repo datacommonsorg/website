5f24bf70a58cb8b912bf45aa9f676500dbe5ce70
Javier Vazquez <javiervazquez@google.com>
2025-07-08 21:50:26 +0000

    Added the Explanation Feature Flags

--- static/js/shared/feature_flags/util.ts
+++ static/js/shared/feature_flags/util.ts
@@ -19,6 +19,8 @@ export const AUTOCOMPLETE_FEATURE_FLAG = "autocomplete";
 export const METADATA_FEATURE_FLAG = "metadata_modal";
 export const FOLLOW_UP_QUESTIONS_EXPERIMENT = "follow_up_questions_experiment";
 export const FOLLOW_UP_QUESTIONS_GA = "follow_up_questions_ga";
+export const OVERALL_EXPLANATION_EXPERIMENT = "overall_explanation_experiment";
+export const OVERALL_EXPLANATION_GENERAL_AUDIENCE = "overall_explanation_general_audience";
 
 // Feature flag URL parameters
 export const ENABLE_FEATURE_URL_PARAM = "enable_feature";
