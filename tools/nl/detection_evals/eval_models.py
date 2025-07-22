8e38ebb37eceebebbb1cf76d0dd1e0181edd71f9
Christie Ellks <calinc@google.com>
2025-07-21 18:33:57 -0700

    add license

--- tools/nl/detection_evals/eval_models.py
+++ tools/nl/detection_evals/eval_models.py
@@ -1,4 +1,23 @@
-# @title Pydantic Models
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Pydantic Models for NL detection evals
+#
+# To use in colab, use the following two lines of code:
+# !wget https://raw.githubusercontent.com/clincoln8/datcom-website/refs/heads/nl-pyd/tools/nl/detection_evals/eval_models.py
+# from eval_models import *
+
 import json
 from enum import Enum, auto
 from typing import Any, Dict, List, Optional
