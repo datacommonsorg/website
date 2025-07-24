b348ca623d0ffaff92759e5a0c49a9418cd71bd4
Gabriel Mechali <gmechali@google.com>
2025-07-24 10:34:48 -0400

    Fix home hero

--- static/js/apps/homepage/components/home_hero.tsx
+++ static/js/apps/homepage/components/home_hero.tsx
@@ -55,7 +55,7 @@ export const HomeHero = ({
             ${theme.typography.heading.lg}
           `}
         >
-          the world&apos;s public data, making it simple to explore
+          Data Commons brings together the world&apos;s public data, making it simple to explore
         </h1>
       </HeroColumns.Left>
       <HeroColumns.Right>
