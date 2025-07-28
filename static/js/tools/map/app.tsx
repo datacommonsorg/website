7c963463b9b9f7108a4895a1be7c6a0103526fbd
Nick B <nicholas.blumberg@gmail.com>
2025-07-28 16:54:07 -0700

    Update to the old vis tool map chart so that we detect whether the chart is ready to show and selectively show auxilary information based on that.

--- static/js/tools/map/app.tsx
+++ static/js/tools/map/app.tsx
@@ -19,7 +19,7 @@
  */
 
 import { ThemeProvider } from "@emotion/react";
-import React, { ReactElement, useEffect, useState } from "react";
+import React, { ReactElement, useContext, useEffect, useState } from "react";
 import { Container, Row } from "reactstrap";
 
 import { ASYNC_ELEMENT_HOLDER_CLASS } from "../../constants/css_constants";
@@ -44,6 +44,7 @@ import {
   applyHashDisplay,
   applyHashPlaceInfo,
   applyHashStatVar,
+  ifShowChart,
   MAP_URL_PATH,
   updateHashDisplay,
   updateHashPlaceInfo,
@@ -56,6 +57,11 @@ function App(): ReactElement {
   const useStandardizedUi = isFeatureEnabled(
     STANDARDIZED_VIS_TOOL_FEATURE_FLAG
   );
+
+  const { placeInfo, statVar } = useContext(Context);
+
+  const showChart = ifShowChart(statVar.value, placeInfo.value);
+  const showInstructions = !showChart;
 
   return (
     <React.StrictMode>
@@ -79,10 +85,16 @@ function App(): ReactElement {
           <Row>
             <PlaceOptions toggleSvHierarchyModal={toggleSvModalCallback} />
           </Row>
-          <Row>{useStandardizedUi ? <VisToolInstructionsBox /> : <Info />}</Row>
-          <Row id="chart-row">
-            <ChartLoader />
-          </Row>
+          {showInstructions && (
+            <Row>
+              {useStandardizedUi ? <VisToolInstructionsBox /> : <Info />}
+            </Row>
+          )}
+          {showChart && (
+            <Row id="chart-row">
+              <ChartLoader />
+            </Row>
+          )}
         </Container>
       </div>
     </React.StrictMode>
