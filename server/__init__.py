4d0b88db963b83cbca36c13d11e85c42e0134d27
Gabriel Mechali <gmechali@google.com>
2025-08-07 15:31:53 -0400

    Add robots.txt to staging only.

--- server/__init__.py
+++ server/__init__.py
@@ -97,6 +97,12 @@ def _enable_datagemma() -> bool:
 
 
 def register_routes_base_dc(app):
+  @app.route('/robots.txt')
+  def robots_txt():
+    if app.config.get('ENV') == 'staging':
+      return 'User-agent: *\nDisallow: /', 200, {'Content-Type': 'text/plain'}
+    else:
+      return 'User-agent: *\nDisallow:', 200, {'Content-Type': 'text/plain'}
   # apply the blueprints for all apps
   from server.routes.dev import html as dev_html
   app.register_blueprint(dev_html.bp)
