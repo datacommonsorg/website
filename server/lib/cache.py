d42828c04da467d3de0e94ca35e95b2934f22cc3
Christie Ellks <calinc@google.com>
2025-08-26 14:40:23 -0700

    small nit

--- server/lib/cache.py
+++ server/lib/cache.py
@@ -1,13 +1,13 @@
 # Copyright 2024 Google LLC
 #
-# Licensed under the Apache License, Version 2.0 (the 'License');
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -16,7 +16,6 @@ import logging
 import os
 from pathlib import Path
 
-from flask import has_request_context
 from flask import request
 from flask_caching import Cache
 
@@ -24,7 +23,7 @@ import server.lib.config as lib_config
 import server.lib.redis as lib_redis
 
 # _redis_cache is a redis cache client when a redis config is available.
-# It will be used by the regular flask 'cache' and/or the flask 'model_cache'.
+# It will be used by the regular flask "cache" and/or the flask "model_cache".
 _redis_cache = None
 
 # cache is the default flask cache for endpoints.
@@ -46,14 +45,14 @@ if redis_config:
           'CACHE_TYPE': 'RedisCache',
           'CACHE_REDIS_HOST': redis_host,
           'CACHE_REDIS_PORT': redis_port,
-          'CACHE_REDIS_URL': 'redis://{}:{}'.format(redis_host, redis_port),
+          'CACHE_REDIS_URL': 'redis://{}:{}'.format(redis_host, redis_port)
       })
   model_cache = _redis_cache
 else:
   model_cache = Cache(
       config={
           'CACHE_TYPE': 'FileSystemCache',
-          'CACHE_DIR': os.path.join(Path(__file__).parents[2], '.cache'),
+          'CACHE_DIR': os.path.join(Path(__file__).parents[2], '.cache')
       })
 
 cfg = lib_config.get_config()
@@ -72,19 +71,16 @@ else:
 
 def should_skip_cache():
   """Check if cache should be skipped based on request header.
-
-    Returns:
-      True if X-Skip-Cache header is set to 'true', False otherwise.
-      Always returns False on any error to ensure caching remains functional.
-    """
-  if not has_request_context():
-    # Cannot skip cache if there is no request context (e.g., in a thread).
-    return False
+  
+  Returns:
+    True if X-Skip-Cache header is set to 'true', False otherwise.
+    Always returns False on any error to ensure caching remains functional.
+  """
   try:
     skip_cache_header = request.headers.get('X-Skip-Cache')
-    return (skip_cache_header is not None and
-            str(skip_cache_header).lower() == 'true')
+    return skip_cache_header is not None and str(
+        skip_cache_header).lower() == 'true'
   except Exception:
-    logging.warning('Error checking X-Skip-Cache header.', exc_info=True)
+    logging.warning("Error checking X-Skip-Cache header.", exc_info=True)
     # Any error should default to False to preserve normal caching behavior
     return False
