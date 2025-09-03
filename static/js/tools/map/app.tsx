50476a3e3e219d5ce85b4f0b452fb8210084912d
Julia Wu <juliawu@google.com>
2025-09-03 12:13:25 -0700

    fix merge conflicts

--- static/js/tools/map/app.tsx
+++ static/js/tools/map/app.tsx
@@ -151,14 +151,6 @@ function updateHash(context: ContextType): void {
   const args = new URLSearchParams(location.search);
   // leaflet flag is part of the search arguments instead of hash, so need to
   // update that separately
-<<<<<<< HEAD
-  // TODO: forward along all args and then append hash in the url.
-  const args = new URLSearchParams(location.search);
-  if (context.display.value.allowLeaflet) {
-    args.set(`${ALLOW_LEAFLET_URL_ARG}`, "1");
-  }
-  const newHash = encodeURIComponent(hash);
-=======
   if (context.display.value.allowLeaflet) {
     args.set(ALLOW_LEAFLET_URL_ARG, "1");
   } else {
@@ -166,7 +158,6 @@ function updateHash(context: ContextType): void {
     args.delete(ALLOW_LEAFLET_URL_ARG);
   }
   const newHash = hash ? `#${encodeURIComponent(hash)}` : "";
->>>>>>> 45c417f3b3e0d842f1e2a25fecbf7422b2f83fda
   const newArgs = args.toString() ? `?${args.toString()}` : "";
   const currentHash = location.hash.replace("#", "");
   const currentArgs = location.search;
@@ -174,10 +165,6 @@ function updateHash(context: ContextType): void {
     (newHash || newArgs) &&
     (newHash !== currentHash || newArgs !== currentArgs)
   ) {
-<<<<<<< HEAD
-    history.pushState({}, "", `${MAP_URL_PATH}${newArgs}#${newHash}`);
-=======
     history.pushState({}, "", `${MAP_URL_PATH}${newArgs}${newHash}`);
->>>>>>> 45c417f3b3e0d842f1e2a25fecbf7422b2f83fda
   }
 }
