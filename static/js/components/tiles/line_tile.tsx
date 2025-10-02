dea08e5b8f78f5b92b1590165beb398ed914c2b3
Lucy King <lucysking@google.com>
2025-10-02 15:28:44 -0400

    cleaning up line_tile

--- static/js/components/tiles/line_tile.tsx
+++ static/js/components/tiles/line_tile.tsx
@@ -532,12 +532,7 @@ function rawToChart(
       if (spec.denom) {
         let denomInfo = denomsByFacet[series.facet];
         // if the placeDcid is not available in the facet-specific denom, use best available
-        if (
-          denomsByFacet[series.facet]?.data?.[spec.denom]?.[placeDcid]?.series
-            .length > 0
-        ) {
-          denomInfo = denomsByFacet[series.facet];
-        } else {
+        if (denomInfo?.data?.[spec.denom]?.[placeDcid]?.series.length <= 0) {
           denomInfo = defaultDenomData;
         }
         const denomSeries = denomInfo?.data?.[spec.denom]?.[placeDcid];
