a0e2a0b430afa841e924dd31bb8e0833308c7969
Lucy King <lucysking@google.com>
2025-10-05 14:35:50 -0400

    reverting accidental changes

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Google LLC
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,5 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TIMEOUT = 3600 * 24 * 1  # 1 day
-UNKNOWN_SURFACE_HEADER_VALUE = "unknown"
+TIMEOUT = 3600 * 24 * 7
