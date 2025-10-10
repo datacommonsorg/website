513104083ca6a3b92d54ad2f05257806bd3c80c4
Nicholas Blumberg <41446765+nick-next@users.noreply.github.com>
2025-10-09 19:19:23 -0700

    API Dialog - Python and Syntax Highlighting (#5579)
    ## Description
    
    This PR implements the following features in the API dialog:
    * Syntax highlighting
    * Python endpoints
    
    ## Libraries
    
    To effect the highlighting itself, we used Prism (prism.js), as it is
    lightweight, extensible and very well used.
    
    ## Notes
    
    To allow for multiple languages, we introduced a "language spec", object
    that describe a given language and indicate the conversion function that
    will be used to convert an observation spec into the API call in that
    language.
    
    Some languages and protocols such as Python are well suited to single
    scripts that can contain multiple API calls. Others, such as cURL, need
    to have each call separated. Whether a particular language is "single
    script" or "multiple endpoint" is indicated by that languages spec. The
    language will have an appropriate generator function based on what type
    it is.
    
    Instructions are provided in the comments of the API dialog component
    that describe how a new language can be added.
    
    ### Scrolling
    
    We had discussed the idea of having the scrolling within the elements
    that contain the code (to keep the height of the dialog stable).
    However, this results in double-scrolling when the dialog is small (and
    is also inconsistent because of the fact that some languages will
    contain more than one code section.
    
    Instead, we allowed the dialog to handle the scrolling as per normal,
    and put in a fade transition to help smooth the effect.
    
    ## Screenshots
    
    <img width="1018" height="732" alt="Screenshot 2025-10-04 at 11 44
    48 AM"
    src="https://github.com/user-attachments/assets/5740a8be-a5e5-47c3-9e03-944815ed62e4"
    />
    
    <img width="989" height="536" alt="Screenshot 2025-10-02 at 8 17 13 PM"
    src="https://github.com/user-attachments/assets/8ce85905-2d5a-4feb-af01-3eea8c18c8c1"
    />
    
    ## Tests
    
    The wedriver test for the API dialog has been updated for changes made
    by this PR, as well as augmented to test language switching via the
    select box.
    
    ---------
    
    Co-authored-by: Pablo Noel <pablo@next-solutions.ca>

--- static/js/components/tiles/components/api_dialog.tsx
+++ static/js/components/tiles/components/api_dialog.tsx
@@ -17,8 +17,19 @@
 /**
  * A dialog component to display the API calls for a particular
  * set of observations (usually sourced from a chart tile). The dialog will
- * take a list of specs and a stat var name lookup, and render a curl
- * implementation of each API call.
+ * take a list of specs and a stat var name lookup, and render examples
+ * of the API call in the languages given by the language specs.
+ *
+ * To add a new language, perform the following steps:
+ * - Create the generator function in `observation_specs`. This might be
+ *   called: `observationSpecsToPythonScript`. This function takes either
+ *   a single or multiple observation specs and renders the language
+ *   specific endpoints or scripts.
+ * - Add the language to the LANGUAGE_SPEC array. See instructions in that
+ *   section for information on how to create that object.
+ * - If the language is not already set up for syntax highlighting in
+ *   code_block.tsx, go to that component and add syntax highlighting for
+ *   that language. Instructions are provided in that file.
  *
  * Note that this can be extended later to allow for other methods of
  * calling the API (a raw GET or Python and other languages).
@@ -29,9 +40,8 @@ import React, {
   ReactElement,
   ReactNode,
   RefObject,
-  useLayoutEffect,
   useMemo,
-  useRef,
+  useState,
 } from "react";
 import { FormattedMessage } from "react-intl";
 
@@ -39,18 +49,94 @@ import { intl } from "../../../i18n/i18n";
 import { chartComponentMessages } from "../../../i18n/i18n_chart_messages";
 import { messages } from "../../../i18n/i18n_messages";
 import {
+  buildObservationSpecManifest,
   isCustomDataCommons,
   ObservationSpec,
+  observationSpecsToDataCommonsClientScript,
+  observationSpecsToPythonScript,
   observationSpecToCurl,
 } from "../../../shared/observation_specs";
 import { Button } from "../../elements/button/button";
 import { CopyToClipboardButton } from "../../elements/button/copy_to_clipboard_button";
+import {
+  CodeBlock,
+  HighlightLanguage,
+  SpecialTerms,
+} from "../../elements/code/code_block";
 import {
   Dialog,
   DialogActions,
   DialogContent,
   DialogTitle,
 } from "../../elements/dialog/dialog";
+
+type SingleSpecGenerator = (spec: ObservationSpec, apiRoot?: string) => string;
+type MultiSpecGenerator = (
+  specs: ObservationSpec[],
+  statVarNameMap: Record<string, string>,
+  apiRoot?: string
+) => string;
+
+/**
+  This interface provides the language specifications for rendering example API calls to users in
+  the API dialog.
+
+  The `displayStyle` determines how a given language behaves when we have multiple endpoints.
+
+  `single`: All endpoints are rendered together in a single block of code. This is suitable
+      for languages like python or R, where we present a script that can contain multiple endpoints.
+  `multiple` Each endpoint is shown in its own section and can be copied independently. This is
+      suitable for cURL or raw GET requests, where each call stands on its own.
+
+  The generator is the function that takes the observation spec and transforms it into an example
+  for the target language.
+    `single` languages require a generator with a `MultiSpecGenerator` shape (combine multiple specs
+      into a single script)
+    `multiple` languages require a generator with a `SingleSpecGenerator` shape (one block per spec).
+ */
+type LanguageSpec =
+  | {
+      slug: string;
+      name: string;
+      displayStyle: "multiple";
+      generator: SingleSpecGenerator;
+      highlightLanguage: HighlightLanguage;
+    }
+  | {
+      slug: string;
+      name: string;
+      displayStyle: "single";
+      generator: MultiSpecGenerator;
+      highlightLanguage: HighlightLanguage;
+    };
+
+const LANGUAGE_SPEC: LanguageSpec[] = [
+  {
+    slug: "python-dc-client",
+    name: "Python (Data Commons Client Library)",
+    displayStyle: "single",
+    generator: observationSpecsToDataCommonsClientScript,
+    highlightLanguage: "python",
+  },
+  {
+    slug: "python",
+    name: "Python",
+    displayStyle: "single",
+    generator: observationSpecsToPythonScript,
+    highlightLanguage: "python",
+  },
+  {
+    slug: "curl",
+    name: "cURL",
+    displayStyle: "multiple",
+    generator: observationSpecToCurl,
+    highlightLanguage: "bash",
+  },
+];
+
+type LanguageSlug = typeof LANGUAGE_SPEC[number]["slug"];
+
+const DEFAULT_LANGUAGE_SLUG = "python-dc-client";
 
 interface ApiDialogProps {
   //whether the dialog is open
@@ -70,23 +156,22 @@ interface ApiDialogProps {
   containerRef?: RefObject<HTMLElement>;
 }
 
-interface ApiCallTextAreaProps {
+interface ApiCallCodeBlockProps {
   // the value (the text of an API endpoint call) that will be
-  // displayed in the text area.
+  // displayed in the code block area.
   value: string;
+  // the language of the code block for syntax highlighting
+  language: HighlightLanguage;
+  // An optional record of special terms to be highlighted in the code block
+  specialTerms?: SpecialTerms;
 }
 
-const ApiCallTextArea = ({ value }: ApiCallTextAreaProps): ReactElement => {
+function ApiCallCodeBlock({
+  value,
+  language,
+  specialTerms,
+}: ApiCallCodeBlockProps): ReactElement {
   const theme = useTheme();
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-
-  useLayoutEffect(() => {
-    const textarea = textareaRef.current;
-    if (textarea) {
-      textarea.style.height = "auto";
-      textarea.style.height = `${textarea.scrollHeight}px`;
-    }
-  }, [value]);
 
   return (
     <div
@@ -107,28 +192,122 @@ const ApiCallTextArea = ({ value }: ApiCallTextAreaProps): ReactElement => {
           padding: ${theme.spacing.xs}px;
           cursor: pointer;
         `}
-      ></CopyToClipboardButton>
-      <textarea
-        ref={textareaRef}
-        css={css`
-          width: 100%;
-          border: 1px solid ${theme.colors.border.primary.light};
-          color: ${theme.colors.text.tertiary.base};
-          ${theme.radius.tertiary};
-          ${theme.typography.family.code};
-          ${theme.typography.text.sm};
-          padding: ${theme.spacing.md}px;
-          overflow-y: hidden;
-          resize: none;
-          &:focus {
-            outline: none;
-          }
-        `}
-        readOnly
-        value={value}
       />
+      <CodeBlock language={language} code={value} specialTerms={specialTerms} />
     </div>
   );
+}
+
+interface ApiLanguageContentProps {
+  // the spec for the language for this particular content section
+  lang: LanguageSpec;
+  // whether this is the block for the currently selected language
+  isCurrent: boolean;
+  //the list of observation specs the content block will render
+  specs: ObservationSpec[];
+  //a lookup of stat var DCIDs to names
+  statVarNameMap: Record<string, string>;
+  // API root for data fetch; if provided, this will be used to create
+  // the API endpoint call with that API root.
+  apiRoot?: string;
+  // the number of numerator specs associated with the content block
+  numeratorSpecsCount: number;
+  // An optional record of special terms to be highlighted in the code block
+  specialTerms?: SpecialTerms;
+}
+
+const ApiLanguageContent = ({
+  lang,
+  isCurrent,
+  specs,
+  statVarNameMap,
+  numeratorSpecsCount,
+  apiRoot,
+  specialTerms,
+}: ApiLanguageContentProps): ReactElement => {
+  const theme = useTheme();
+
+  if (lang.displayStyle === "multiple") {
+    return (
+      <div
+        key={lang.slug}
+        css={css`
+          display: flex;
+          flex-direction: column;
+          gap: ${theme.spacing.lg}px;
+          transition: opacity 0.3s ease-in-out;
+          opacity: ${isCurrent ? 1 : 0};
+          height: ${isCurrent ? "100%" : "0"};
+          overflow: hidden;
+        `}
+      >
+        {specs.map((observationSpec, index) => {
+          const apiCall = (lang.generator as SingleSpecGenerator)(
+            observationSpec,
+            apiRoot
+          );
+          const showHeader =
+            observationSpec.role === "denominator" || numeratorSpecsCount > 1;
+          const statVarNames = getStatVarListString(
+            observationSpec.statVarDcids,
+            statVarNameMap
+          );
+          const title =
+            observationSpec.role === "denominator"
+              ? `${statVarNames} ${intl.formatMessage(
+                  chartComponentMessages.ApiDialogDenomHelperText
+                )}`
+              : statVarNames;
+
+          return (
+            <div
+              key={`${lang.slug}-${index}`}
+              css={css`
+                display: block;
+                width: 100%;
+                && > h3 {
+                  ${theme.typography.family.text}
+                  ${theme.typography.text.lg}
+                  display: block;
+                  margin: 0 0 ${theme.spacing.md}px 0;
+                }
+              `}
+            >
+              {showHeader && <h3>{title}</h3>}
+              <ApiCallCodeBlock
+                value={apiCall}
+                language={lang.highlightLanguage}
+                specialTerms={specialTerms}
+              />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    const apiCall = (lang.generator as MultiSpecGenerator)(
+      specs,
+      statVarNameMap,
+      apiRoot
+    );
+    return (
+      <div
+        key={lang.slug}
+        css={css`
+          transition: opacity 0.3s ease-in-out;
+          opacity: ${isCurrent ? 1 : 0};
+          height: ${isCurrent ? "100%" : "0"};
+          overflow: hidden;
+        `}
+      >
+        <ApiCallCodeBlock
+          value={apiCall}
+          language={lang.highlightLanguage}
+          specialTerms={specialTerms}
+        />
+      </div>
+    );
+  }
 };
 
 function getStatVarListString(
@@ -166,15 +345,38 @@ export function ApiDialog({
   containerRef,
 }: ApiDialogProps): ReactElement {
   const theme = useTheme();
-
-  const apiCalls = useMemo(() => {
-    return specs.map((s) => observationSpecToCurl(s, apiRoot));
-  }, [specs, apiRoot]);
-
-  const concatenatedEndpointCalls = useMemo(
-    () => apiCalls.join("\n\n\n"),
-    [apiCalls]
+  const [apiLanguage, setApiLanguage] = useState<LanguageSlug>(
+    DEFAULT_LANGUAGE_SLUG
   );
+
+  const currentLanguageSpec =
+    LANGUAGE_SPEC.find((lang) => lang.slug === apiLanguage) || LANGUAGE_SPEC[0];
+
+  const apiContent = useMemo(() => {
+    if (currentLanguageSpec.displayStyle === "multiple") {
+      return specs.map((s) => currentLanguageSpec.generator(s, apiRoot));
+    } else {
+      return currentLanguageSpec.generator(specs, statVarNameMap, apiRoot);
+    }
+  }, [specs, apiRoot, statVarNameMap, currentLanguageSpec]);
+
+  const specialTerms = useMemo((): SpecialTerms => {
+    const manifest = buildObservationSpecManifest(specs);
+    return {
+      highlight: [
+        ...manifest.entities,
+        ...manifest.entityExpressions,
+        ...manifest.statVars,
+      ],
+    };
+  }, [specs]);
+
+  const concatenatedEndpointCalls = useMemo(() => {
+    if (Array.isArray(apiContent)) {
+      return apiContent.join("\n\n\n");
+    }
+    return apiContent;
+  }, [apiContent]);
 
   const numeratorSpecsCount = useMemo(
     () => specs.filter((s) => s.role !== "denominator").length,
@@ -226,12 +428,15 @@ export function ApiDialog({
                 apiKeyPlaceholder: (
                   <code
                     css={css`
-                      color: ${theme.colors.text.code.base};
-                      font-size: 100%;
-                      ${theme.typography.family.code}}
+                      && {
+                        padding: 1px ${theme.spacing.xs}px;
+                        ${theme.typography.family.code}}
+                        font-size: 100%;
+                        color: ${theme.colors.text.code.base};
+                      }
                     `}
                   >
-                    ${`{API_KEY}`}
+                    API_KEY
                   </code>
                 ),
               }),
@@ -239,39 +444,102 @@ export function ApiDialog({
           />
         </p>
 
-        {specs.map((observationSpec, index) => {
-          const showHeader =
-            observationSpec.role === "denominator" || numeratorSpecsCount > 1;
-          const statVarNames = getStatVarListString(
-            observationSpec.statVarDcids,
-            statVarNameMap
-          );
-          const title =
-            observationSpec.role === "denominator"
-              ? `${statVarNames} ${intl.formatMessage(
-                  chartComponentMessages.ApiDialogDenomHelperText
-                )}`
-              : statVarNames;
-
-          return (
-            <div
-              key={index}
+        <div
+          css={css`
+            && {
+              position: relative;
+              display: flex;
+              align-items: center;
+            }
+          `}
+        >
+          <label
+            css={css`
+              && {
+                ${theme.typography.family.text}
+                ${theme.typography.text.sm}
+                position: relative;
+                font-weight: 600;
+                display: flex;
+                gap: 16px;
+                align-items: center;
+              }
+              &&::after {
+                content: "";
+                position: absolute;
+                // This is a base64 rendering of the Material "arrow_drop_down" icon
+                background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjRweCIgdmlld0JveD0iMCAtOTYwIDk2MCA5NjAiIHdpZHRoPSIyNHB4IiBmaWxsPSIjMDAwMDAwIj48cGF0aCBkPSJNNDgwLTM2MCAyODAtNTYwaDQwMEw0ODAtMzYwWiIvPjwvc3ZnPg==);
+                background-repeat: no-repeat;
+                background-position: center center;
+                width: 16px;
+                height: 16px;
+                right: 6px;
+                top: 7px;
+                pointer-events: none;
+                z-index: 1;
+              }
+            `}
+          >
+            <span
               css={css`
-                display: block;
-                width: 100%;
-                && > h3 {
+                ${theme.typography.family.text}
+                ${theme.typography.text.sm}
+                font-weight: 600;
+                margin: 0;
+              `}
+            >
+              Language:
+            </span>
+
+            <select
+              id="api-language-selector"
+              value={apiLanguage}
+              onChange={(e): void => setApiLanguage(e.target.value)}
+              css={css`
+                && {
+                  appearance: none;
+                  z-index: 1;
+                  cursor: pointer;
                   ${theme.typography.family.text}
-                  ${theme.typography.text.lg}
-                  display: block;
-                  margin: 0 0 ${theme.spacing.md}px 0;
+                  ${theme.typography.text.sm}
+                  ${theme.radius.quaternary}
+                  border: 1px solid ${theme.colors.border.primary.light};
+                  padding: ${theme.spacing.xs}px ${theme.spacing.lg}px
+                    ${theme.spacing.xs}px ${theme.spacing.sm}px;
+                  background: white;
+                  min-width: 15ch;
+                  max-width: 30ch;
                 }
               `}
             >
-              {showHeader && <h3>{title}</h3>}
-              <ApiCallTextArea value={apiCalls[index] || ""} />
-            </div>
-          );
-        })}
+              {LANGUAGE_SPEC.map((lang) => (
+                <option key={lang.slug} value={lang.slug}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
+        <div
+          css={css`
+            display: flex;
+            flex-direction: column;
+            position: relative;
+          `}
+        >
+          {LANGUAGE_SPEC.map((lang) => (
+            <ApiLanguageContent
+              key={lang.slug}
+              lang={lang}
+              isCurrent={lang.slug === apiLanguage}
+              specs={specs}
+              statVarNameMap={statVarNameMap}
+              numeratorSpecsCount={numeratorSpecsCount}
+              apiRoot={apiRoot}
+              specialTerms={specialTerms}
+            />
+          ))}
+        </div>
       </DialogContent>
       <DialogActions>
         <Button variant="text" onClick={onClose}>
@@ -280,9 +548,10 @@ export function ApiDialog({
         {!loading && specs.length > 0 && (
           <CopyToClipboardButton valueToCopy={concatenatedEndpointCalls}>
             {intl.formatMessage(
-              specs.length === 1
-                ? chartComponentMessages.ApiDialogCopy
-                : chartComponentMessages.ApiDialogCopyAll
+              currentLanguageSpec.displayStyle === "multiple" &&
+                specs.length > 1
+                ? chartComponentMessages.ApiDialogCopyAll
+                : chartComponentMessages.ApiDialogCopy
             )}
           </CopyToClipboardButton>
         )}
