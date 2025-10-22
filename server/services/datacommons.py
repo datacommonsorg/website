50ab730a283f4f968371d766e925e5b382fbd422
Lucy King <lucysking@google.com>
2025-10-22 11:31:52 -0400

    formatting

--- server/services/datacommons.py
+++ server/services/datacommons.py
@@ -46,7 +46,8 @@ def get(url: str, surface_header_value: str = None):
   # header used in usage metric logging
   # this is set even if get() is called for endpoints that we don't write usage
   # logs for to maintain consistency
-  surface = request.headers.get('x-surface') if has_request_context() else surface_header_value
+  surface = request.headers.get(
+      'x-surface') if has_request_context() else surface_header_value
   headers['x-surface'] = surface or UNKNOWN_SURFACE
   # Send the request and verify the request succeeded
   call_logger = log.ExtremeCallLogger()
@@ -94,7 +95,8 @@ def post_wrapper(url,
   # Header from the flask call making this request
   # Represents the DC surface (website, web components, etc.) where the call originates
   # Used in mixer's usage logs
-  surface = request.headers.get('x-surface') if has_request_context() else surface_header_value
+  surface = request.headers.get(
+      'x-surface') if has_request_context() else surface_header_value
   headers['x-surface'] = surface or UNKNOWN_SURFACE
   # Send the request and verify the request succeeded
   call_logger = log.ExtremeCallLogger(req, url=url)
