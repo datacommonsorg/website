bfb186c8dbe276fbe5b4e4cc7fe850a6036a45d1
Hannah Pho <hpho@google.com>
2025-11-10 12:58:45 -0500

    Bring back one-at-a-time

--- run_test.sh
+++ run_test.sh
@@ -317,7 +317,8 @@ function run_cdc_webdriver_test {
     rerun_options="--reruns 2"
   fi
 
-  python3 -m pytest -n auto $rerun_options server/webdriver/cdc_tests/ ${@}
+  python3 -m pytest $rerun_options -m "one_at_a_time" server/webdriver/cdc_tests/ ${@}
+  python3 -m pytest -n auto $rerun_options -m "not one_at_a_time" server/webdriver/cdc_tests/ ${@}
 
   stop_servers
   deactivate
