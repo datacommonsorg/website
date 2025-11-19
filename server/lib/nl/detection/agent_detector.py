dd2fe19fe25bc8bc22840cd4a87c82ebe941f185
Christie Ellks <calinc@google.com>
2025-11-19 15:40:14 -0800

    Add ADK runner for agentic detection

--- server/lib/nl/detection/agent_detector.py
+++ server/lib/nl/detection/agent_detector.py
@@ -1,3 +1,21 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from flask import current_app
+import asyncio
+
 from server.lib.nl.common import utterance
 from server.lib.nl.common.counters import Counters
 from server.lib.nl.detection.types import ActualDetectorType
@@ -8,12 +26,23 @@ from server.lib.nl.detection.types import PlaceDetection
 from server.lib.nl.detection.types import SVDetection
 from shared.lib.detected_variables import MultiVarCandidates
 from shared.lib.detected_variables import VarCandidates
+from server.lib.nl.detection.agent.runner import get_detection
+
+
 
 
 def detect(query: str, prev_utterance: utterance.Utterance,
            query_detection_debug_logs: dict, counters: Counters,
            dargs: DetectionArgs) -> Detection:
-  # Place holder for actual agentic detection logic.
+
+  runner = current_app.config.get('NL_DETECTION_AGENT_RUNNER')
+
+  if runner:
+    # TODO: consider if alternatives to asycio.run are more appropriate in a production setting
+    agent_detection = asyncio.run(get_detection(runner, query))
+    print(agent_detection)
+
+  # TODO: replace empty detection object with data parsed from agent_detection
   empty_place_detection = PlaceDetection(
       query_original=query,
       query_without_place_substr="",
