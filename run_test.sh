d38c1c3dd9a30bb46208f5af47c61c91d0e44e6c
shixiao-coder <shixiao@google.com>
2025-12-03 10:03:28 -0500

    refine the logic to setup_python

--- run_test.sh
+++ run_test.sh
@@ -21,9 +21,8 @@ function setup_python {
   source .venv/bin/activate
   echo "installing server/requirements.txt"
   pip3 install -r server/requirements.txt -q
-  pip3 install torch==$(grep '^torch==' nl_requirements.txt | cut -d'=' -f3) --extra-index-url https://download.pytorch.org/whl/cpu
   echo "installing nl_server/requirements.txt"
-  pip3 install -r nl_server/requirements.txt -q
+  pip3 install -r nl_server/requirements.txt -q --extra-index-url https://download.pytorch.org/whl/cpu
   deactivate
 }
 
