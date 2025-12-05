38aa2794b65a5ecf7ef207f7913f13c3a14f41f4
Julia Wu <juliawu@google.com>
2025-12-05 13:54:15 -0800

    Cleanup color formatting

--- run_servers.sh
+++ run_servers.sh
@@ -27,6 +27,7 @@ set -e
 
 # ANSI color codes
 RED='\033[0;31m'
+YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
 VERBOSE=false
--- run_test.sh
+++ run_test.sh
@@ -18,7 +18,6 @@ set -e
 
 # ANSI Color Codes
 RED='\033[0;31m'
-GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
 
