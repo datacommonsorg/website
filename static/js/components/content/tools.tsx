7943eec738f560d7601fd4ffd843b32ad274264a
Nick B <nicholas.blumberg@gmail.com>
2025-12-04 20:00:33 -0800

    Update to the tools component to make it more generic and extensible (in particular, implementing functionality that allows us to display a version used by the coming custom home page).

--- static/js/components/content/tools.tsx
+++ static/js/components/content/tools.tsx
@@ -15,111 +15,175 @@
  */
 
 /**
- * A component that renders the tools section of the home page.
+ * A component that renders a list of available tools.
  */
 
 /** @jsxImportSource @emotion/react */
 
 import { css, useTheme } from "@emotion/react";
-import React, { ReactElement } from "react";
+import React, { ReactElement, ReactNode } from "react";
 
-import { Download } from "../../../components/elements/icons/download";
-import { IntegrationInstructions } from "../../../components/elements/icons/integration_instructions";
-import { Public } from "../../../components/elements/icons/public";
-import { ScatterPlot } from "../../../components/elements/icons/scatter_plot";
-import { Timeline } from "../../../components/elements/icons/timeline";
-import { LinkIconBox } from "../../../components/elements/link_icon_box";
-import { resolveHref } from "../../base/utilities/utilities";
+import { resolveHref } from "../../apps/base/utilities/utilities";
+import { Download } from "../elements/icons/download";
+import { IntegrationInstructions } from "../elements/icons/integration_instructions";
+import { Public } from "../elements/icons/public";
+import { ScatterPlot } from "../elements/icons/scatter_plot";
+import { Timeline } from "../elements/icons/timeline";
+import { LinkIconBox } from "../elements/link_icon_box";
+
+export type Tool =
+  | "statVarExplorer"
+  | "map"
+  | "scatter"
+  | "timeline"
+  | "download"
+  | "api";
 
 interface ToolsProps {
-  //the routes dictionary - this is used to convert routes to resolved urls
+  // a list of tools to include; if left undefined, all tools are included.
+  tools?: Tool[];
+  // the routes dictionary - this is used to convert routes to resolved urls.
   routes: Record<string, string>;
+  // className for styling / Emotion's styled.
+  className?: string;
+  // whether to include descriptions for each item; defaults to false.
+  showDescriptions?: boolean;
+  // content displayed above the tools is passed in as the children.
+  children?: ReactNode;
 }
 
-export const Tools = ({ routes }: ToolsProps): ReactElement => {
+export const Tools = ({
+  tools,
+  routes,
+  className,
+  showDescriptions = false,
+  children,
+}: ToolsProps): ReactElement => {
   const theme = useTheme();
+
+  const toolMap: Record<
+    Tool,
+    {
+      icon: React.JSX.Element;
+      link: {
+        id: string;
+        title: string;
+        url: string;
+        description: string;
+      };
+    }
+  > = {
+    statVarExplorer: {
+      icon: <IntegrationInstructions height={32} />,
+      link: {
+        id: "stat-var-explorer",
+        title: "Statistical Variable Explorer",
+        url: resolveHref("{tools.stat_var}", routes),
+        description:
+          "Explorer statistical variable details including metadata and observations",
+      },
+    },
+    map: {
+      icon: <Public height={32} />,
+      link: {
+        id: "map",
+        title: "Map Explorer",
+        url: resolveHref("{tools.visualization}#visType=map", routes),
+        description:
+          "See how selected statistical variables vary across geographic regions",
+      },
+    },
+    scatter: {
+      icon: <ScatterPlot height={32} />,
+      link: {
+        id: "scatter",
+        title: "Scatter Plot Explorer",
+        url: resolveHref("{tools.visualization}#visType=scatter", routes),
+        description:
+          "Visualize the correlation between two statistical variables",
+      },
+    },
+    timeline: {
+      icon: <Timeline height={32} />,
+      link: {
+        id: "timeline",
+        title: "Timelines Explorer",
+        url: resolveHref("{tools.visualization}#visType=timeline", routes),
+        description: "See trends over time for selected statistical variables",
+      },
+    },
+    download: {
+      icon: <Download height={32} />,
+      link: {
+        id: "download",
+        title: "Data Download Tool",
+        url: resolveHref("{tools.download}", routes),
+        description: "Download data for selected statistical variables",
+      },
+    },
+    api: {
+      icon: <IntegrationInstructions height={32} />,
+      link: {
+        id: "api",
+        title: "API Access",
+        url: "https://docs.datacommons.org/api/",
+        description: "Access Data Commons data programmatically using our APIs",
+      },
+    },
+  };
+
+  const toolsToDisplay = tools === undefined ? Object.keys(toolMap) : tools;
+
+  const hasChildren = React.Children.count(children) > 0;
+
   return (
-    <>
-      <header
-        css={css`
+    <div
+      className={className}
+      css={css`
+        width: 100%;
+
+        & > header {
           width: 100%;
           max-width: ${theme.width.sm}px;
           margin-bottom: ${theme.spacing.xl}px;
           @media (max-width: ${theme.breakpoints.md}px) {
             max-width: 100%;
           }
-        `}
-      >
-        <h3
-          css={css`
+          & > h3 {
             ${theme.typography.family.heading};
             ${theme.typography.heading.md};
             margin-bottom: ${theme.spacing.xl}px;
-          `}
-        >
-          Data Commons tools
-        </h3>
-        <p
-          css={css`
+          }
+          & > p {
             ${theme.typography.family.text};
             ${theme.typography.text.lg};
-          `}
-        >
-          Data Commons offers data exploration tools and cloud-based APIs to
-          access and integrate cleaned datasets.
-        </p>
-      </header>
-      <div
-        css={css`
+          }
+        }
+
+        & > .tools {
           display: flex;
           align-items: stretch;
           flex-wrap: wrap;
           gap: ${theme.spacing.lg}px;
           margin: 0;
           padding: 0;
-        `}
-      >
-        <LinkIconBox
-          icon={<Public height={32} />}
-          link={{
-            id: "map",
-            title: "Map Explorer",
-            url: resolveHref("{tools.visualization}#visType=map", routes),
-          }}
-        />
-        <LinkIconBox
-          icon={<ScatterPlot height={32} />}
-          link={{
-            id: "scatter",
-            title: "Scatter Plot Explorer",
-            url: resolveHref("{tools.visualization}#visType=scatter", routes),
-          }}
-        />
-        <LinkIconBox
-          icon={<Timeline height={32} />}
-          link={{
-            id: "timeline",
-            title: "Timelines Explorer",
-            url: resolveHref("{tools.visualization}#visType=timeline", routes),
-          }}
-        />
-        <LinkIconBox
-          icon={<Download height={32} />}
-          link={{
-            id: "download",
-            title: "Data Download Tool",
-            url: resolveHref("{tools.download}", routes),
-          }}
-        />
-        <LinkIconBox
-          icon={<IntegrationInstructions height={32} />}
-          link={{
-            id: "api",
-            title: "API Access",
-            url: "https://docs.datacommons.org/api/",
-          }}
-        />
+        }
+      `}
+    >
+      {hasChildren && <header>{children}</header>}
+      <div className="tools">
+        {toolsToDisplay.map((toolId) => {
+          const tool = toolMap[toolId];
+          return (
+            <LinkIconBox
+              key={toolId}
+              icon={tool.icon}
+              link={tool.link}
+              description={showDescriptions ? tool.link.description : undefined}
+            />
+          );
+        })}
       </div>
-    </>
+    </div>
   );
 };
