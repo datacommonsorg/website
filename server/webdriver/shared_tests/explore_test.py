01ec8616ca67519be75c87365f13a39d62864e99
Gabriel Mechali <gmechali@google.com>
2025-12-08 10:25:28 -0500

    Lint

--- server/webdriver/shared_tests/explore_test.py
+++ server/webdriver/shared_tests/explore_test.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
 import time
+
+import pytest
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
