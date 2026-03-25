2d20445e6aa1079788455f7b2740c8b526d63ee7
Julia Wu <juliawu@google.com>
2026-03-25 14:27:21 -0700

    add subtitle i18n messages

--- static/js/i18n/i18n_ranking_messages.ts
+++ static/js/i18n/i18n_ranking_messages.ts
@@ -26,4 +26,32 @@ export const rankingMessages = defineMessages({
     description:
       "Title of the page, which shows ranking of all contained places of a type within a place, where {statVarName} will be replaced by the stat var name, {pluralPlaceType} will be replaced by the place type of the contained places (could be cities, countries, counties), and {placeName} is the containing place. Please maintain the {placeName} and {pluralPlaceType} as is in the final translation, and use a gender neutral structure that conveys the same meaning (e.g. dashes to separate).",
   },
+  // The ids contain "subtitle" because they were copied from the old ranking page
+  // We need to reuse the same ids to be able to reuse existing translations
+  // These are used in the page title in the current UI
+  allPlacesTitle: {
+    id: "ranking-subtitle_all",
+    defaultMessage: "All {pluralPlaceType} in {placeName}",
+    description:
+      "Subtitle of the page, which shows ranking of all contained places of a type within a place, where {pluralPlaceType} will be replaced by the place type of the contained places (could be cities, countries, counties), and {placeName} is the containing place. Please maintain the {placeName} and {pluralPlaceType} as is in the final translation, and use a gender neutral structure that conveys the same meaning (e.g. dashes to separate).",
+  },
+  allPlacesPerCapitaTitle: {
+    id: "ranking-subtitle_all_percapita",
+    defaultMessage: "All {pluralPlaceType} in {placeName}, per capita",
+    description:
+      "Subtitle of the page, which shows ranking of all contained places of a type within a place, computed on a per capita basis, where {pluralPlaceType} will be replaced by the place type of the contained places (could be cities, countries, counties), and {placeName} is the containing place. Please maintain the {placeName} and {pluralPlaceType} as is in the final translation, and use a gender neutral structure that conveys the same meaning (e.g. dashes to separate).",
+  },
+  topPlacesTitle: {
+    id: "ranking-subtitle_top",
+    defaultMessage: "Top {rankSize} {pluralPlaceType} in {placeName}",
+    description:
+      "Subtitle of the page, which shows ranking of the top / highest {rankSize} contained places of a type within a place, where {rankSize} will be replaced by a number, {pluralPlaceType} will be replaced by the place type of the contained places (could be cities, countries, counties), and {placeName} is the containing place. Please maintain the {rankSize}, {placeName} and {pluralPlaceType} as is in the final translation, and use a gender neutral structure that conveys the same meaning (e.g. dashes to separate).",
+  },
+  topPlacesPerCapita: {
+    id: "ranking-subtitle_top_percapita",
+    defaultMessage:
+      "Top {rankSize} {pluralPlaceType} in {placeName}, per capita",
+    description:
+      "Subtitle of the page, which shows ranking of the top / highest {rankSize} contained places of a type within a place, computed on a per capita basis, where {rankSize} will be replaced by a number, {pluralPlaceType} will be replaced by the place type of the contained places (could be cities, countries, counties), and {placeName} is the containing place. Please maintain the {rankSize}, {placeName} and {pluralPlaceType} as is in the final translation, and use a gender neutral structure that conveys the same meaning (e.g. dashes to separate).",
+  },
 });
