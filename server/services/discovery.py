6ef302f5d077e1ed606c2fb52cf97a741320d8f5
Rohit Kumar <rohitrkumar@google.com>
2026-03-31 17:36:49 +0530

    [v0/v1] Migrate website to  /v2/recognize/places  (#6139)
    - /v2/recognize/places - new endpoint is live in staging and inline for
    next prod rollout.
    - Tested locally, goldens are passing

--- server/services/discovery.py
+++ server/services/discovery.py
@@ -139,7 +139,6 @@ endpoints = Endpoints([
     '/v1/place/ranking',
     '/v1/place/related',
     '/v1/variable/search',
-    '/v1/recognize/places',
     '/v1/recognize/entities',
     # v2
     '/v2/observation',
@@ -148,6 +147,7 @@ endpoints = Endpoints([
     '/v2/event',
     '/v2/variable/filter',
     '/v2/bulk/info/variable',
+    '/v2/recognize/places',
 ])
 
 
