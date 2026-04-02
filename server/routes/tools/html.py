4f735e99490e3f0f35b8e05b66f9c460dd72098e
Nicholas Blumberg <41446765+nick-nlb@users.noreply.github.com>
2026-04-02 16:34:11 -0700

    [v0/v1 migration] Replace /v1/variable/search (#6140)
    ## Issue
    
    [b/493339134](https://b.corp.google.com/issues/493339134)
    
    ## Description
    
    This PR converts the usage of ` /v1/variable/search` to `v2` endpoints
    centered around `/v2/resolve`.
    
    ## Notes
    
    In order to keep search functionality for custom DCs while migrating
    away from the `v1`-centric search performed before, we have moved the
    logic of the search to use `v2/resolve`.
    
    The `search_statvar` endpoint now follows the following logic:
    
    * If Vertex AI is active (governed by a flag), we use Vertex AI (this
    functionality has not changed in this PR)
    * Else, if the NL server is running and we are a custom DC, we use the
    new `v2/resolve`-centric method to do the search
    * Else, we do not allow search (noting that this should not occur in
    practice, given the above paths).
    
    When we are in the `v2/resolve` branch, we follow the following logic:
    
    * Resolve Query to Candidate DCIDS: We call `/v2/resolve` to convert the
    natural language query into a list of candidate DCIDs.
    * Check Observations: If the request filters by entities, we call
    `/v2/observation` for all candidates. We discards any variables that
    don't have active observation data for those entities (we skip this if
    no entities given)
    * Filter by typeOf: We calls `/v2/node` with property: "->typeOf" to
    ensure candidates are actually StatisticalVariables and filters out any
    StatVarGroups or Topics.
    * Fetch Names: For the remaining valid DCIDs, we call dc.v2node with
    property: "->name" to retrieve their human-readable display names.
    * Format Response: Finally, we merges the DCIDs and display names into a
    standard result structure ({"statVars": [...], "matches": [],
    "statVarGroups": []}) to maintain compatibility with existing frontend
    usage.
    
    
    ## Testing
    
    [Stat Var Explorer](http://localhost:8080/tools/statvar)
    
    * When running the server normally: `./run_server.sh -m`, a search will
    use Vertex AI and should be identical to what it was before.
    * When running the server as a custom DC: `./run_server.sh -m -e
    custom`, the new search path is invoked. The search experience should
    generally be the same as in master, but the results may not (because we
    are using the embeddings-based based `v2/resolve` search that will not
    surface all variables.
    * When running the server as a custom DC without the NL server:
    `./run_server.sh -m -e custom`, not search box should appear in the
    front end.
    
    [Timeline Explorer](http://localhost:8080/tools/timeline)
    
    All below are for running as a custom DC `./run_server.sh -m -e custom`
    
    * When no place is selected, a search for population will bring up many
    stat vars (a set of results unfiltered by entity)
    * When a place is selected (such as India), the same search should
    produce noticeably fewer results (only results that are relevant for
    India).

--- server/routes/tools/html.py
+++ server/routes/tools/html.py
@@ -81,6 +81,16 @@ def _get_vis_tool_examples(tool_name):
   return _load_example_file(tool_name, default={}), [], False
 
 
+def _is_search_supported():
+  """Returns true if search is supported (either vai or enable model)."""
+  if not is_feature_enabled("use_v2_api"):
+    return True
+  return (is_feature_enabled("vai_for_statvar_search_feature_flag",
+                             request=request) or
+          (current_app.config.get("ENABLE_MODEL", False) and
+           current_app.config.get("CUSTOM", False)))
+
+
 @bp.route('/timeline')
 def timeline():
   vis_tool_examples_json = _load_example_file('timeline_vis_tool', default=[])
@@ -88,6 +98,7 @@ def timeline():
   return flask.render_template('tools/timeline.html',
                                vis_tool_examples_json=vis_tool_examples_json,
                                maps_api_key=current_app.config['MAPS_API_KEY'],
+                               is_search_supported=_is_search_supported(),
                                sample_questions=json.dumps(
                                    current_app.config.get(
                                        'HOMEPAGE_SAMPLE_QUESTIONS', [])))
@@ -105,6 +116,7 @@ def map():
 
   return flask.render_template('tools/map.html',
                                maps_api_key=current_app.config['MAPS_API_KEY'],
+                               is_search_supported=_is_search_supported(),
                                vis_tool_examples_json=vis_tool_examples_json,
                                sample_questions=json.dumps(
                                    current_app.config.get(
@@ -117,6 +129,7 @@ def scatter():
 
   return flask.render_template('tools/scatter.html',
                                vis_tool_examples_json=vis_tool_examples_json,
+                               is_search_supported=_is_search_supported(),
                                maps_api_key=current_app.config['MAPS_API_KEY'],
                                sample_questions=json.dumps(
                                    current_app.config.get(
@@ -126,6 +139,7 @@ def scatter():
 @bp.route('/statvar')
 def stat_var():
   return flask.render_template('tools/stat_var.html',
+                               is_search_supported=_is_search_supported(),
                                sample_questions=json.dumps(
                                    current_app.config.get(
                                        'HOMEPAGE_SAMPLE_QUESTIONS', [])))
