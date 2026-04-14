d0657d2d066ff1ec2df421810e298dbca5df0b02
Nicholas Blumberg <41446765+nick-nlb@users.noreply.github.com>
2026-04-13 19:18:30 -0700

    [v0/v1 migration] /bulk/info/variable-group (#6199)
    ## Issue
    
    [b/491885197] (https://b.corp.google.com/issues/491885197)
    
    ## Description
    
    This PR implements the migration of `v1/bulk/info/variable-group` to
    `v2`.
    
    The change is gated behind the `use_v2_api` flag.
    
    ## Notes
    
    The core of the migration is very simple: a flag-mediated gate that
    determines which endpoint is called.
    
    The complication comes (that makes up most of the diff) comes from the
    fact that the `v2` endpoint is no longer able to provide a `definition`
    along with each stat var. This definition is used (relatively rarely) in
    the natural language search, in order to find sibling stat vars to
    provide further exploration topics for the user.
    
    Because this is no longer available in the `v2` endpoint, the
    functionality had to be reconstructed via direct v2 calls.
    
    A discussion of the methodology, testing, fidelity and latency
    considerations of this can be found [at this
    link](https://docs.google.com/document/d/1LEZI_6-2wkwpmv0ELYIKg28wqJJpsyZSZohl7CJ3-HE/edit?resourcekey=0-_UcPAttv9jeT4qas_rIGhw&tab=t.0).
    (Message if access is required).
    
    This document describes how the `definition` functionality is used and
    analyses the latency and fidelity implications of moving that
    functionality to Flask and `v2`.
    
    
    ## Testing
    
    There are two aspects of the NL search that are affected by the
    "definitions". These are described in the document as Flow 1 and Flow2.
    
    Flow 1 is rarely invoked, but can be seen in the following query (which
    should produce the same results for "Related" charts populated at the
    bottom of the results section).
    
    *
    [V2](http://localhost:8080/explore#q=population+of+people+aged+85+and+over+in+Seattle&client=ui_query)
    *
    [V1](http://localhost:8080/explore?disable_feature=use_v2_api#q=population+of+people+aged+85+and+over+in+Seattle&client=ui_query)
    
    Flow 2 is much more common, and is the primary driver of latency
    discrepancies between `v1` and `v2`.
    
    This flow is invoked on a standard query such as:
    
    [Query]()
    
    ## Goldens
    
    This PR also includes explicit directives to the integration tests to
    use v1. The goldens would have to be regenerated for v2 at some point
    before the feature flag is dropped.

---
+++ server/tests/lib/nl/variable_test.py
@@ -0,0 +1,184 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for variable extension functions."""
+
+import unittest
+from unittest.mock import patch
+
+from server import create_app
+from server.lib.nl.common import variable
+
+
+class TestVariableExtension(unittest.TestCase):
+
+  def setUp(self):
+    self.app = create_app()
+    self.app_context = self.app.app_context()
+    self.app_context.push()
+
+  def tearDown(self):
+    self.app_context.pop()
+
+  @patch('server.lib.nl.common.variable.is_feature_enabled')
+  @patch('server.lib.fetch.property_values')
+  @patch('server.services.datacommons.get_variable_group_info')
+  def test_extend_svs(self, mock_get_variable_group_info, mock_property_values,
+                      mock_is_feature_enabled):
+    mock_is_feature_enabled.return_value = False
+    sv = "Count_Person_BelowPovertyLevelInThePast12Months_Female"
+
+    # Mock property_values to return the group for the SV
+    mock_property_values.return_value = {sv: ["dc/g/Person_PovertyStatus"]}
+
+    # Mock get_variable_group_info to return children with definitions
+    mock_get_variable_group_info.return_value = {
+        "data": [{
+            "node": "dc/g/Person_PovertyStatus",
+            "info": {
+                "childStatVars": [{
+                    "id":
+                        sv,
+                    "definition":
+                        "pt=Person,mp=povertyStatus,st=count,gender=Female"
+                }, {
+                    "id":
+                        "Count_Person_BelowPovertyLevelInThePast12Months_Male",
+                    "definition":
+                        "pt=Person,mp=povertyStatus,st=count,gender=Male"
+                }]
+            }
+        }]
+    }
+
+    res = variable.extend_svs([sv])
+
+    # Expected result: should include both the original and the sibling
+    # Main SV comes first, then others sorted.
+    expected = [sv, "Count_Person_BelowPovertyLevelInThePast12Months_Male"]
+    self.assertEqual(res[sv], expected)
+
+  @patch('server.lib.nl.common.variable.is_feature_enabled')
+  @patch('server.lib.fetch.property_values')
+  @patch('server.services.datacommons.get_variable_group_info')
+  @patch('server.services.datacommons.get_variable_definitions')
+  def test_extend_svs_v2(self, mock_get_variable_definitions,
+                         mock_get_variable_group_info, mock_property_values,
+                         mock_is_feature_enabled):
+    mock_is_feature_enabled.return_value = True
+
+    sv = "Count_Person_BelowPovertyLevelInThePast12Months_Female"
+
+    # Mock property_values to return the group for the SV
+    mock_property_values.return_value = {sv: ["dc/g/Person_PovertyStatus"]}
+
+    # Mock get_variable_group_info (without definition)
+    mock_get_variable_group_info.return_value = {
+        "data": [{
+            "node": "dc/g/Person_PovertyStatus",
+            "info": {
+                "childStatVars": [{
+                    "id": sv
+                }, {
+                    "id": "Count_Person_BelowPovertyLevelInThePast12Months_Male"
+                }]
+            }
+        }]
+    }
+
+    # Mock get_variable_definitions
+    mock_get_variable_definitions.return_value = {
+        sv:
+            "pt=Person,mp=povertyStatus,st=count,gender=Female",
+        "Count_Person_BelowPovertyLevelInThePast12Months_Male":
+            "pt=Person,mp=povertyStatus,st=count,gender=Male"
+    }
+
+    res = variable.extend_svs([sv])
+
+    # Expected result: should include both the original and the sibling
+    expected = [sv, "Count_Person_BelowPovertyLevelInThePast12Months_Male"]
+    self.assertEqual(res[sv], expected)
+
+  @patch('server.lib.nl.common.variable.parse_svg')
+  @patch('server.lib.nl.common.variable.is_feature_enabled')
+  @patch('server.lib.fetch.property_values')
+  @patch('server.services.datacommons.get_variable_group_info')
+  @patch('server.services.datacommons.get_variable_definitions')
+  def test_extend_svs_indirect_siblings_v2(self, mock_get_variable_definitions,
+                                           mock_get_variable_group_info,
+                                           mock_property_values,
+                                           mock_is_feature_enabled,
+                                           mock_parse_svg):
+    mock_is_feature_enabled.return_value = True
+
+    # To trigger the indirect sibling traversal, the SVG properties must match the SV properties.
+    # The SV definition has 1 extra property ('gender'). We mock parse_svg to also return 1 property.
+    mock_parse_svg.return_value.pvs = {'dummy_prop': 'dummy_value'}
+
+    sv = "Count_Person_BelowPovertyLevelInThePast12Months_Female"
+    sibling_sv = "Count_Person_BelowPovertyLevelInThePast12Months_Male"
+
+    # We use more specific SVGs to trigger len(svg_obj.pvs) == len(sv_obj.pvs)
+    svg = "dc/g/Person_PovertyStatus_Female"
+    parent_svg = "dc/g/Person_PovertyStatus"
+    sibling_svg = "dc/g/Person_PovertyStatus_Male"
+
+    # Property values side effect to handle the multi-stage traversal
+    def property_values_side_effect(nodes, prop, out=True):
+      if prop == "memberOf" and out:
+        # Initial direct group lookup
+        return {sv: [svg]}
+      elif prop == "specializationOf" and out:
+        # Batch 1: Fetch parents
+        return {svg: [parent_svg]}
+      elif prop == "specializationOf" and not out:
+        # Batch 2: Fetch siblings for all parents
+        return {parent_svg: [svg, sibling_svg]}
+      return {}
+
+    mock_property_values.side_effect = property_values_side_effect
+
+    # Group info side effect to handle direct and sibling group requests
+    def group_info_side_effect(nodes, _):
+      data = []
+      if svg in nodes:
+        data.append({"node": svg, "info": {"childStatVars": [{"id": sv}]}})
+      if sibling_svg in nodes:
+        data.append({
+            "node": sibling_svg,
+            "info": {
+                "childStatVars": [{
+                    "id": sibling_sv
+                }]
+            }
+        })
+      return {"data": data}
+
+    mock_get_variable_group_info.side_effect = group_info_side_effect
+
+    # Mock get_variable_definitions for all encountered child SVs
+    mock_get_variable_definitions.return_value = {
+        sv: "pt=Person,mp=povertyStatus,st=count,gender=Female",
+        sibling_sv: "pt=Person,mp=povertyStatus,st=count,gender=Male"
+    }
+
+    res = variable.extend_svs([sv])
+
+    # Expected result: should successfully traverse parent -> sibling SVG -> sibling SV
+    expected = [sv, sibling_sv]
+    self.assertEqual(res[sv], expected)
+
+
+if __name__ == '__main__':
+  unittest.main()
