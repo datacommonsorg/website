83fdf922ac070a09c79cc98df5e3e0c674e80041
Xiao Shi <shixiao@google.com>
2026-06-04 15:30:08 -0400

    add the mixer feature flags in Dockerfile and use the custom.yaml in DCP (#6327)
    This PR copy the feature flag folder in custom DC Dockerfile
    
    It also add logic to include feature_flags_path for DCP only case, it
    will read the feature flag when use spanner graph.
    
    We also add the logic to read an env variable
    RESOLVE_WITH_SPANNER_EMBEDDINGS to tune the yaml to do resolve through
    spanner embeddings

--- build/cdc_services/run.sh
+++ build/cdc_services/run.sh
@@ -108,6 +108,11 @@ except Exception as e:
     
     # 2. Enable V2 API for Mixer
     MIXER_ARGS+=("--spanner_graph_info=$SPANNER_CONFIG_YAML" "--use_spanner_graph=true")
+
+    # 3. Use mixer custom feature flags
+    if [[ $RESOLVE_WITH_SPANNER_EMBEDDINGS == "true" ]]; then
+        MIXER_ARGS+=('--feature_flags_path=deploy/featureflags/custom.yaml')
+    fi
 fi
 
 # Start mixer.
