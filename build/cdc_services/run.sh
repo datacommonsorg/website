ee6f70e05df11568bddbcb320cfda7e7435038f2
Xiao Shi <shixiao@google.com>
2026-06-04 15:36:49 -0400

    update comments based on https://github.com/datacommonsorg/website/pull/6327/changes#discussion_r3357862911 (#6338)

--- build/cdc_services/run.sh
+++ build/cdc_services/run.sh
@@ -110,6 +110,9 @@ except Exception as e:
     MIXER_ARGS+=("--spanner_graph_info=$SPANNER_CONFIG_YAML" "--use_spanner_graph=true")
 
     # 3. Use mixer custom feature flags
+
+    # Currently we only read custom feature flags when we enable resolving with spanner embeddings.
+    # We will eventually always resolve from spanner embeddings.
     if [[ $RESOLVE_WITH_SPANNER_EMBEDDINGS == "true" ]]; then
         MIXER_ARGS+=('--feature_flags_path=deploy/featureflags/custom.yaml')
     fi
