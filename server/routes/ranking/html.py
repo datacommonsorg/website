d564538032fb43fe945fcd97f5d6713fc133dbea
Julia Wu <juliawu@google.com>
2026-06-09 15:59:20 -0700

    Revive server py files

---
+++ server/routes/ranking/html.py
@@ -0,0 +1,64 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Place Ranking related handlers."""
+
+import json
+
+import flask
+from flask import current_app
+
+import server.lib.feature_flags as feature_flags
+import server.routes.shared_api.place as place_api
+
+bp = flask.Blueprint('ranking', __name__, url_prefix='/ranking')
+
+
+@bp.route('/<stat_var>/<place_type>', strict_slashes=False)
+@bp.route('/<stat_var>/<place_type>/<path:place_dcid>')
+def ranking(stat_var, place_type, place_dcid=''):
+  place_name = ''
+  if place_dcid:
+    place_names = place_api.get_i18n_name([place_dcid])
+    place_name = place_names[place_dcid]
+    if place_name == '':
+      place_name = place_dcid
+  else:
+    place_dcid = 'Earth'
+    # TODO(juliawu): Once the "the World" page titles in i18n_ranking_messages.ts are translated,
+    # remove this default place name and use the translated "world" page titles instead.
+    #
+    # Currently, when defaulting to "Earth" as the parent place, we don't localize the place name.
+    # This is because at the time of revamping the ranking pages, we were restricted to
+    # using already available translations, and we don't have translations in place for
+    # titles that use "the world" (with leading particle "the") for all locales. Localizing by using
+    # place_api.get_i18n_name("Earth") would not work because the resulting titles would be missing the
+    # leading "the" (e.g. "Top 100 in World" instead of "Top 100 in the World"). Furthermore, we would also
+    # get the wrong word depending on the language (e.g. Spanish requires "del Mundo" instead of "de el Mundo").
+    place_name = 'the World'
+  per_capita = flask.request.args.get('pc', False) != False
+
+  # Check if the new ranking page feature flag is enabled
+  use_new_ranking = feature_flags.is_feature_enabled(
+      feature_flags.NEW_RANKING_PAGE, app=current_app, request=flask.request)
+
+  return flask.render_template('ranking.html',
+                               place_name=place_name,
+                               place_dcid=place_dcid,
+                               place_type=place_type,
+                               per_capita=per_capita,
+                               stat_var=stat_var,
+                               use_new_ranking=use_new_ranking,
+                               sample_questions=json.dumps(
+                                   current_app.config.get(
+                                       'HOMEPAGE_SAMPLE_QUESTIONS', [])))
